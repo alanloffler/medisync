e1c3afec6f246e96611ca325c1fdb44f41ad5b73
alanloffler <alanmatiasloffler@gmail.com>
2024-06-12 17:53:54 -0300

    New service for findOne professional

--- src/professionals/services/professional-api.service.ts
+++ src/professionals/services/professional-api.service.ts
@@ -7,15 +7,17 @@ export class ProfessionalApiService {
   private static readonly API_URL = import.meta.env.VITE_API_URL;
 
   public static async findAll(search: string, sorting: SortingState, skip: number, limit: number) {
-    const query: string = `${this.API_URL}/professionals?search=${search}&skip=${skip}&limit=${limit}&sk=${sorting[0].id}&sv=${sorting[0].desc ? 'desc' : 'asc'}`;
+    const url: string = `${this.API_URL}/professionals?search=${search}&skip=${skip}&limit=${limit}&sk=${sorting[0].id}&sv=${sorting[0].desc ? 'desc' : 'asc'}`;
+    
     try {
-      const fetchData = await fetch(query, {
+      const query: Response = await fetch(url, {
         method: 'GET',
         headers: {
           'content-type': 'application/json;charset=UTF-8',
         },
       });
-      return await fetchData.json();
+
+      return await query.json();
     } catch (error) {
       return error;
     }
@@ -23,19 +25,37 @@ export class ProfessionalApiService {
 
   public static async create(data: IProfessionalForm) {
     const transformedData = ProfessionalUtils.lowercaseFormItems(data);
-    const query: string = `${this.API_URL}/professionals`;
-    
+    const url: string = `${this.API_URL}/professionals`;
+
     try {
-      const fetchData = await fetch(query, {
+      const query = await fetch(url, {
         method: 'POST',
         headers: {
           'content-type': 'application/json;charset=UTF-8',
         },
         body: JSON.stringify(transformedData),
       });
-      return await fetchData.json();
+
+      return await query.json();
     } catch (error) {
       return error;
+    }
+  }
+
+  public static async findOne(id: string) {
+    const url: string = `${this.API_URL}/professionals/${id}`;
+
+    try {
+      const query: Response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+        },
+      });
+
+      return await query.json();
+    } catch (e) {
+      return e;
     }
   }
 }
