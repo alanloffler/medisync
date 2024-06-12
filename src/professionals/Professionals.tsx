4ef79ec7eebaf3c8565890c8371bb266a1790503
alanloffler <alanmatiasloffler@gmail.com>
2024-06-12 16:26:17 -0300

    Refactored to use table with data include in table

--- src/professionals/Professionals.tsx
+++ src/professionals/Professionals.tsx
@@ -1,156 +1,59 @@
 // Icons: https://lucide.dev/icons/
-import { ArrowDownUp, CirclePlus, FilePen, FileText, List, ListRestart, PlusCircle, Search, Trash2 } from 'lucide-react';
+import { CirclePlus, List, ListFilter, ListRestart, PlusCircle, Search } from 'lucide-react';
 // Components: https://ui.shadcn.com/docs/components
 import { Button } from '@/core/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/core/components/ui/card';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/core/components/ui/dropdown-menu';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuPortal, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger, DropdownMenuTrigger } from '@/core/components/ui/dropdown-menu';
 import { Input } from '@/core/components/ui/input';
 // App components
 import { ProfessionalsDataTable } from './components/ProfessionalsDataTable';
 // App
-import { ChangeEvent, MouseEvent, useState } from 'react';
-import { ColumnDef } from '@tanstack/react-table';
+import { AreaService } from '@/core/services/area.service';
+import { ChangeEvent, useEffect, useState } from 'react';
+import { IArea } from '@/core/interfaces/area.interface';
 import { IBreadcrumb } from '../core/components/common/interfaces/breadcrumb.interface';
-import { IProfessional } from './interfaces/professional.interface';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { PROF_CONFIG } from './config/professionals.config';
 import { PageHeader } from '@/core/components/common/PageHeader';
 import { useCapitalize } from '@/core/hooks/useCapitalize';
 import { useDebounce } from '@/core/hooks/useDebounce';
-import { useHeaderMenuStore } from '@/layout/stores/header-menu.service';
+import { useNotificationsStore } from '@/core/stores/notifications.store';
 // Constants
 const DEBOUNCE_TIME: number = 500;
 // React component
 export default function Professionals() {
+  const [areas, setAreas] = useState<IArea[]>([]);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const [reload, setReload] = useState<number>(0);
   const [search, setSearch] = useState<string>('');
+  const addNotification = useNotificationsStore((state) => state.addNotification);
   const capitalize = useCapitalize();
   const debouncedSearch = useDebounce<string>(search, DEBOUNCE_TIME); // load data
-  // const itemsPerPage: number = 5; // Pagination (get this from config or db)
-  const navigate = useNavigate();
-  const setItemSelected = useHeaderMenuStore((state) => state.setHeaderMenuSelected);
+
   const breadcrumb: IBreadcrumb[] = [
     { id: 1, name: 'Inicio', path: '/' },
     { id: 2, name: 'Profesionales', path: '/professionals' },
   ];
 
-  function handleNavigation(event: MouseEvent<HTMLAnchorElement>, path: string) {
-    event.preventDefault();
-    setItemSelected(2);
-    navigate(path);
-  }
-
-  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+  function handleSearch(event: ChangeEvent<HTMLInputElement>): void {
     setSearch(event.target.value);
   }
-  // #region Table
-  const columns: ColumnDef<IProfessional>[] = [
-    // {
-    //   accessorKey: 'index',
-    //   size: 50,
-    //   header: () => {
-    //     return <div className='text-center'>#</div>;
-    //   },
-    //   cell: ({ row }) => {
-    //     return <div className='text-center text-sm'>{row.index + 1}</div>;
-    //   },
-    // },
-    {
-      accessorKey: 'lastName',
-      // size: 150,
-      // minSize: 150,
-      header: ({ column }) => {
-        return (
-          <div className='text-left'>
-            <button className='flex items-center gap-2 hover:text-accent-foreground' onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
-              {PROF_CONFIG.table.headers[0]}
-              <ArrowDownUp className='h-3 w-3' />
-            </button>
-          </div>
-        );
-      },
-      cell: ({ row }) => {
-        return <div className='text-left font-medium'>{`${capitalize(row.original.titleAbbreviation)} ${capitalize(row.original.lastName)}, ${capitalize(row.original.firstName)}`}</div>;
-      },
-    },
-    {
-      accessorKey: 'area',
-      size: 80,
-      header: ({ column }) => {
-        return (
-          <div className='text-left'>
-            <button className='flex items-center gap-2 hover:text-accent-foreground' onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
-              {PROF_CONFIG.table.headers[1]}
-              <ArrowDownUp className='h-3 w-3' />
-            </button>
-          </div>
-        );
-      },
-      cell: ({ row }) => {
-        return <div className='text-left'>{capitalize(row.original.area.name)}</div>;
-      },
-    },
-    {
-      accessorKey: 'specialization',
-      size: 80,
-      header: ({ column }) => {
-        return (
-          <div className='text-center'>
-            <button className='flex items-center gap-2 hover:text-accent-foreground' onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
-              {PROF_CONFIG.table.headers[2]}
-              <ArrowDownUp className='h-3 w-3' />
-            </button>
-          </div>
-        );
-      },
-      cell: ({ row }) => {
-        return <div className='text-left text-sm'>{capitalize(row.original.specialization.name)}</div>;
-      },
-    },
-    {
-      accessorKey: 'available',
-      size: 50,
-      header: ({ column }) => {
-        return (
-          <div className='text-center'>
-            <button className='flex items-center gap-2 hover:text-accent-foreground' onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
-              {PROF_CONFIG.table.headers[3]}
-              <ArrowDownUp className='h-3 w-3' />
-            </button>
-          </div>
-        );
-      },
-      cell: ({ row }) => {
-        return (
-          <div className='flex flex-row items-center justify-start space-x-2 text-xs'>
-            <div className={`flex ${row.original.available ? 'h-2 w-2 rounded-full bg-green-400' : 'h-2 w-2 rounded-full bg-red-400'}`}></div>
-            <div className={`flex ${row.original.available ? 'text-slate-800' : 'text-slate-400'}`}>{row.original.available ? 'Activo' : 'Inactivo'}</div>
-          </div>
-        );
-      },
-    },
-    {
-      accessorKey: 'actions',
-      size: 100,
-      header: () => {
-        return <div className='text-center'>{PROF_CONFIG.table.headers[4]}</div>;
-      },
-      cell: () => {
-        return (
-          <div className='flex flex-row items-center justify-center space-x-4'>
-            <Button variant={'ghost'} size={'miniIcon'} className=''>
-              <FileText className='h-4 w-4' />
-            </Button>
-            <Button variant={'ghost'} size={'miniIcon'}>
-              <FilePen className='h-4 w-4' />
-            </Button>
-            <Button variant={'ghost'} size={'miniIcon'}>
-              <Trash2 className='h-4 w-4' />
-            </Button>
-          </div>
-        );
-      },
-    },
-  ];
+
+  function handleReload(): void {
+    setSearch('');
+    setReload(Math.random());
+  }
+
+  useEffect(() => {
+    AreaService.findAll().then((response) => {
+      if (!response.statusCode) {
+        setAreas(response);
+      }
+      if (response.statusCode > 399) addNotification({ type: 'error', message: response.message });
+      if (response instanceof Error) addNotification({ type: 'error', message: 'Internal Server Error' });
+    });
+  }, [addNotification]);
+
   // #endregion
   return (
     <main className='flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8 lg:gap-8 lg:p-8'>
@@ -162,21 +65,23 @@ export default function Professionals() {
       <div className='grid gap-6 md:grid-cols-4 md:gap-8 lg:grid-cols-4 xl:grid-cols-4'>
         <Card className='col-span-1 border-none bg-slate-200 bg-transparent shadow-none md:col-span-4 lg:col-span-1 xl:col-span-1'>
           <CardContent className='p-0'>
-            <div className='flex flex-col gap-6'>
-              <Link to={`/professionals/create`}>
+            <div className='flex flex-col gap-6 md:w-full'>
+              <Link to={`/professionals/create`} className='w-fit'>
                 <Button variant={'default'} size={'sm'} className='gap-2'>
                   <PlusCircle className='h-4 w-4' strokeWidth={2} />
                   {PROF_CONFIG.buttons.addProfessional}
                 </Button>
               </Link>
-              <div className='relative flex'>
-                <Search className='absolute left-3 top-3 h-4 w-4 text-muted-foreground' />
-                <Input onChange={handleSearch} type='search' placeholder={PROF_CONFIG.search.placeholder} className='w-fit grow bg-background pl-9 shadow-sm' />
+              <div className='flex flex-col space-y-2'>
+                <div className='relative w-full'>
+                  <Search className='absolute left-3 top-3 h-4 w-4 text-muted-foreground' />
+                  <Input onChange={handleSearch} value={search} type='search' placeholder={PROF_CONFIG.search.placeholder} className='bg-background pl-9 shadow-sm' />
+                </div>
+                {errorMessage && <div className='flex flex-row items-center text-xs font-medium text-rose-400'>{errorMessage}</div>}
               </div>
             </div>
           </CardContent>
         </Card>
-
         <Card className='col-span-1 overflow-y-auto md:col-span-4 lg:col-span-3 xl:col-span-3'>
           <CardHeader>
             <div className='grid gap-2'>
@@ -188,22 +93,30 @@ export default function Professionals() {
                 <div className='flex items-center gap-2'>
                   <DropdownMenu>
                     <DropdownMenuTrigger asChild>
-                      <Button variant={'tableHeader'} size={'miniIcon'}>
-                        <ArrowDownUp className='h-4 w-4' strokeWidth={2} />
+                      <Button variant={'tableHeader'} size={'miniIcon'} className='flex items-center'>
+                        <ListFilter className='h-4 w-4' strokeWidth={2} />
                       </Button>
                     </DropdownMenuTrigger>
                     <DropdownMenuContent className='w-fit' align='center'>
-                      <DropdownMenuItem>
-                        <Link to='/professionals' onClick={(event) => handleNavigation(event, '/')}>
-                          Ascendente - prof
-                        </Link>
-                      </DropdownMenuItem>
-                      <DropdownMenuItem>
-                        <Link to='/'>Descendente</Link>
-                      </DropdownMenuItem>
+                      {areas.map((area) => (
+                        <DropdownMenuSub key={area._id}>
+                          <DropdownMenuSubTrigger>
+                            <span>{capitalize(area.name)}</span>
+                          </DropdownMenuSubTrigger>
+                          <DropdownMenuPortal>
+                            <DropdownMenuSubContent>
+                              {area.specializations.map((spec) => (
+                                <DropdownMenuItem key={spec._id} onClick={() => setSearch(spec.name)}>
+                                  <span>{capitalize(spec.name)}</span>
+                                </DropdownMenuItem>
+                              ))}
+                            </DropdownMenuSubContent>
+                          </DropdownMenuPortal>
+                        </DropdownMenuSub>
+                      ))}
                     </DropdownMenuContent>
                   </DropdownMenu>
-                  <Button variant={'tableHeader'} size={'miniIcon'}>
+                  <Button variant={'tableHeader'} size={'miniIcon'} onClick={handleReload}>
                     <ListRestart className='h-4 w-4' strokeWidth={2} />
                   </Button>
                   <Button variant={'tableHeaderPrimary'} size={'miniIcon'}>
@@ -214,7 +127,7 @@ export default function Professionals() {
             </div>
           </CardHeader>
           <CardContent>
-            <ProfessionalsDataTable columns={columns} search={debouncedSearch} />
+            <ProfessionalsDataTable search={debouncedSearch} reload={reload} setErrorMessage={setErrorMessage} />
           </CardContent>
         </Card>
       </div>
