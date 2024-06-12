b64345ac9528d6002401c8000f686bcf5c7e0540
alanloffler <alanmatiasloffler@gmail.com>
2024-06-12 17:53:01 -0300

    Route added for udapte professional

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ const Appointments = lazy(() => import('./appointments/Appointments'));
 const Dashboard = lazy(() => import('./dashboard/Dashboard'));
 const Professionals = lazy(() => import('./professionals/Professionals'));
 const CreateProfessional = lazy(() => import('./professionals/components/CreateProfessional'));
+const UpdateProfessional = lazy(() => import('./professionals/components/UpdateProfessional'));
 // React component
 export default function App() {
   const router = createBrowserRouter([
@@ -16,23 +17,47 @@ export default function App() {
       children: [
         {
           path: '/dashboard',
-          element: <Suspense fallback={<>Loading...</>}><Dashboard /></Suspense>,
+          element: (
+            <Suspense fallback={<>Loading...</>}>
+              <Dashboard />
+            </Suspense>
+          ),
         },
         {
           path: '/',
-          element: <Suspense fallback={<>Loading...</>}><Appointments /></Suspense>,
-        //   async lazy() { 
-        //     const { Appointments } = await import('./appointments/Appointments');
-        //     return { Component: Appointments };
-        //   }
+          element: (
+            <Suspense fallback={<>Loading...</>}>
+              <Appointments />
+            </Suspense>
+          ),
+          //   async lazy() {
+          //     const { Appointments } = await import('./appointments/Appointments');
+          //     return { Component: Appointments };
+          //   }
         },
         {
           path: '/professionals',
-          element: <Suspense fallback={<>Loading...</>}><Professionals /></Suspense>,
+          element: (
+            <Suspense fallback={<>Loading...</>}>
+              <Professionals />
+            </Suspense>
+          ),
         },
         {
           path: '/professionals/create',
-          element: <Suspense fallback={<>Loading...</>}><CreateProfessional /></Suspense>,
+          element: (
+            <Suspense fallback={<>Loading...</>}>
+              <CreateProfessional />
+            </Suspense>
+          ),
+        },
+        {
+          path: '/professionals/update/:id',
+          element: (
+            <Suspense fallback={<>Loading...</>}>
+              <UpdateProfessional />
+            </Suspense>
+          ),
         },
         {
           path: '*',
