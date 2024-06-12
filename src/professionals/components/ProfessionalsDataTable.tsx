b58768286ed5b335bf62d3d8697a45283168089d
alanloffler <alanmatiasloffler@gmail.com>
2024-06-12 16:27:24 -0300

    Added data to table from own scope

--- src/professionals/components/ProfessionalsDataTable.tsx
+++ src/professionals/components/ProfessionalsDataTable.tsx
@@ -1,118 +1,200 @@
 // Icons: https://lucide.dev/icons/
-import { ArrowLeftIcon, ArrowRightIcon, ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
+import { ArrowDownUp, ArrowLeftIcon, ArrowRightIcon, ChevronLeftIcon, ChevronRightIcon, FilePen, FileText, Trash2 } from 'lucide-react';
 // Components: https://ui.shadcn.com/docs/components
 import { Button } from '@/core/components/ui/button';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/core/components/ui/select';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/core/components/ui/table';
 // Tanstack Data Table: https://tanstack.com/table/latest
-import { ColumnDef, ColumnFiltersState, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, PaginationState, SortingState, useReactTable } from '@tanstack/react-table';
+import { ColumnDef, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, PaginationState, SortingState, useReactTable } from '@tanstack/react-table';
 // App
+import { IProfessional } from '../interfaces/professional.interface';
 import { PROF_CONFIG } from '../config/professionals.config';
 import { ProfessionalApiService } from '../services/professional-api.service';
-import { useEffect, useState } from 'react';
+import { useCapitalize } from '@/core/hooks/useCapitalize';
+import { useEffect, useRef, useState } from 'react';
 import { useNotificationsStore } from '@/core/stores/notifications.store';
-// Interface
-interface DataTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[];
+import { useTruncateText } from '@/core/hooks/useTruncateText';
+// Table interfaces
+interface DataTableProps {
   search: string;
+  reload: number;
+  setErrorMessage: React.Dispatch<React.SetStateAction<string>>;
 }
-// Configuration (put in config file)
-const DataTableConfig = {
-  itemsPerPage: [3, 5, 10, 20],
-  pagination: {
-    page: 'Página',
-    of: 'de',
-  },
-  rowsPerPage: 'Filas por página',
-};
+
+interface TableManager {
+  sorting: SortingState;
+  pagination: PaginationState;
+}
+// Default values for pagination and sorting
+const defaultSorting = [{ id: PROF_CONFIG.table.defaultSortingId, desc: PROF_CONFIG.table.defaultSortingType }];
+const defaultPagination = { pageIndex: 0, pageSize: PROF_CONFIG.table.defaultPageSize };
 // React component
-export function ProfessionalsDataTable<TData, TValue>({ columns, search }: DataTableProps<TData, TValue>) {
-  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
-  const [data2, setData2] = useState([]);
-  const [sorting, setSorting] = useState<SortingState>([]);
+export function ProfessionalsDataTable({ search, reload, setErrorMessage }: DataTableProps) {
+  const [columns, setColumns] = useState<ColumnDef<IProfessional>[]>([]);
+  const [data, setData] = useState([]);
+  const [pagination, setPagination] = useState<PaginationState>(defaultPagination);
+  const [sorting, setSorting] = useState<SortingState>(defaultSorting);
+  const [tableManager, setTableManager] = useState<TableManager>({ sorting, pagination });
   const [totalItems, setTotalItems] = useState<number>(0);
-  const [itemsPerPage, setItemsPerPage] = useState<number>(3);
   const addNotification = useNotificationsStore((state) => state.addNotification);
-  const [pagination, setPagination] = useState<PaginationState>({ pageIndex: 0, pageSize: itemsPerPage });
-
-  const [key, setKey] = useState(0);
-
+  const capitalize = useCapitalize();
+  const firstUpdate = useRef(true);
+  const truncate = useTruncateText();
+  // #region Table columns
+  const tableColumns: ColumnDef<IProfessional>[] = [
+    {
+      accessorKey: 'index',
+      size: 50,
+      header: () => <div className='text-center'>#</div>,
+      cell: ({ row }) => <div className='text-center text-sm'>{truncate(row.original._id, -4)}</div>,
+    },
+    {
+      accessorKey: 'lastName',
+      header: ({ column }) => (
+        <div className='text-left'>
+          <button className='flex items-center gap-2 hover:text-accent-foreground' onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+            {PROF_CONFIG.table.headers[0]}
+            <ArrowDownUp className='h-3 w-3' />
+          </button>
+        </div>
+      ),
+      cell: ({ row }) => <div className='text-left font-medium'>{`${capitalize(row.original.titleAbbreviation)} ${capitalize(row.original.lastName)}, ${capitalize(row.original.firstName)}`}</div>,
+    },
+    {
+      accessorKey: 'area',
+      size: 80,
+      header: ({ column }) => (
+        <div className='text-left'>
+          <button className='flex items-center gap-2 hover:text-accent-foreground' onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+            {PROF_CONFIG.table.headers[1]}
+            <ArrowDownUp className='h-3 w-3' />
+          </button>
+        </div>
+      ),
+      cell: ({ row }) => <div className='text-left'>{capitalize(row.original.area.name)}</div>,
+    },
+    {
+      accessorKey: 'specialization',
+      size: 80,
+      header: ({ column }) => (
+        <div className='text-center'>
+          <button className='flex items-center gap-2 hover:text-accent-foreground' onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+            {PROF_CONFIG.table.headers[2]}
+            <ArrowDownUp className='h-3 w-3' />
+          </button>
+        </div>
+      ),
+      cell: ({ row }) => <div className='text-left text-sm'>{capitalize(row.original.specialization.name)}</div>,
+    },
+    {
+      accessorKey: 'available',
+      size: 50,
+      header: ({ column }) => (
+        <div className='text-center'>
+          <button className='flex items-center gap-2 hover:text-accent-foreground' onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+            {PROF_CONFIG.table.headers[3]}
+            <ArrowDownUp className='h-3 w-3' />
+          </button>
+        </div>
+      ),
+      cell: ({ row }) => (
+        <div className='flex flex-row items-center justify-start space-x-2 text-xs'>
+          <div className={`flex ${row.original.available ? 'h-2 w-2 rounded-full bg-green-400' : 'h-2 w-2 rounded-full bg-red-400'}`}></div>
+          <div className={`flex ${row.original.available ? 'text-slate-800' : 'text-slate-400'}`}>{row.original.available ? 'Activo' : 'Inactivo'}</div>
+        </div>
+      ),
+    },
+    {
+      accessorKey: 'actions',
+      size: 100,
+      header: () => <div className='text-center'>{PROF_CONFIG.table.headers[4]}</div>,
+      cell: () => (
+        <div className='flex flex-row items-center justify-center space-x-4'>
+          <Button variant={'ghost'} size={'miniIcon'} className=''>
+            <FileText className='h-4 w-4' />
+          </Button>
+          <Button variant={'ghost'} size={'miniIcon'}>
+            <FilePen className='h-4 w-4' />
+          </Button>
+          <Button variant={'ghost'} size={'miniIcon'}>
+            <Trash2 className='h-4 w-4' />
+          </Button>
+        </div>
+      ),
+    },
+  ];
+  // #endregion
+  // #region Table constructor
   const table = useReactTable({
-    columns,
-    data: data2,
+    columns: columns,
+    data: data,
     getCoreRowModel: getCoreRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
     getSortedRowModel: getSortedRowModel(),
     manualPagination: true,
-    onColumnFiltersChange: setColumnFilters,
+    manualSorting: true,
     onPaginationChange: setPagination,
     onSortingChange: setSorting,
     rowCount: totalItems,
     state: {
       sorting,
-      columnFilters,
       pagination,
     },
   });
+  // #endregion
+  // #region Load data, pagination and sorting
+  useEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
+    setTableManager({
+      sorting: sorting,
+      pagination: pagination,
+    });
+  }, [sorting, pagination]);
 
   useEffect(() => {
-    const skipItems: number = pagination.pageIndex * pagination.pageSize;
-
-    ProfessionalApiService.findAll(search, skipItems, itemsPerPage).then((response) => {
-      console.log('pagination load data');
-      // if (!response.statusCode) {
-      setData2(response.data);
-      setTotalItems(response.count);
-      // }
-      if (response.statusCode > 399) addNotification({ type: 'error', message: response.message });
-      if (response instanceof Error) addNotification({ type: 'error', message: 'Internal Server Error' });
-    });
-    setKey(Math.random());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pagination]);
+    setSorting(defaultSorting);
+    setPagination(defaultPagination);
+  }, [reload]);
 
   useEffect(() => {
-    const skipItems: number = 0;
-    setPagination({ pageIndex: 0, pageSize: itemsPerPage });
-
-    ProfessionalApiService.findAll(search, skipItems, itemsPerPage).then((response) => {
-      console.log('search load data');
-      if (!response.statusCode) {
-        setData2(response.data);
-        setTotalItems(response.count);
-      }
-      if (response.statusCode > 399) addNotification({ type: 'error', message: response.message });
-      if (response instanceof Error) addNotification({ type: 'error', message: 'Internal Server Error' });
-    });
-    setKey(Math.random());
-
+    const fetchData = (search: string, sorting: SortingState, skipItems: number, itemsPerPage: number) => {
+      ProfessionalApiService.findAll(search, sorting, skipItems, itemsPerPage).then((response) => {
+        if (!response.statusCode) {
+          setData(response.data);
+          setColumns(tableColumns);
+          setTotalItems(response.count);
+          setErrorMessage('');
+        }
+        if (response.statusCode > 399) {
+          setErrorMessage(response.message);
+          addNotification({ type: 'error', message: response.message });
+        }
+        if (response instanceof Error) addNotification({ type: 'error', message: 'Internal Server Error' });
+      });
+    };
+    fetchData(search, tableManager.sorting, tableManager.pagination.pageIndex * tableManager.pagination.pageSize, tableManager.pagination.pageSize);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [search, itemsPerPage]);
-
-  // function handlePageSizeChange(value: string) {
-  //   console.log(Number(value));
-  //   setPagination({ pageIndex: 0, pageSize: Number(value) });
-  //   setItemsPerPage(parseInt(value));
-  // }
-
+  }, [search, tableManager]);
+  // #endregion
   return (
     <>
       <Table>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id} style={{ width: `${header.getSize()}px` }}>
-                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                  </TableHead>
-                );
-              })}
+              {headerGroup.headers.map((header) => (
+                <TableHead key={header.id} style={{ width: `${header.getSize()}px` }}>
+                  {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                </TableHead>
+              ))}
             </TableRow>
           ))}
         </TableHeader>
-        <TableBody key={key}>
+        <TableBody>
           {table.getRowModel().rows?.length ? (
             table.getRowModel().rows.map((row) => (
               <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
@@ -134,13 +216,13 @@ export function ProfessionalsDataTable<TData, TValue>({ columns, search }: DataT
       </Table>
       <div className='flex items-center justify-between space-x-6 pt-6 lg:space-x-8'>
         <div className='flex items-center space-x-2'>
-          <p className='text-xs font-normal text-slate-400'>{DataTableConfig.rowsPerPage}</p>
-          <Select value={`${table.getState().pagination.pageSize}`} onValueChange={(e) => setItemsPerPage(parseInt(e))}>
+          <p className='text-xs font-normal text-slate-400'>{PROF_CONFIG.table.rowsPerPage}</p>
+          <Select value={`${table.getState().pagination.pageSize}`} onValueChange={(e) => setPagination({ pageIndex: 0, pageSize: parseInt(e) })}>
             <SelectTrigger className='h-8 w-[65px] text-xs font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring/40 focus-visible:ring-offset-0'>
-              <SelectValue placeholder={itemsPerPage} />
+              <SelectValue placeholder={table.getState().pagination.pageSize} />
             </SelectTrigger>
             <SelectContent side='top' className='min-w-[4rem]'>
-              {DataTableConfig.itemsPerPage.map((pageSize) => (
+              {PROF_CONFIG.table.itemsPerPage.map((pageSize) => (
                 <SelectItem key={pageSize} value={`${pageSize}`} className='text-xs'>
                   {pageSize}
                 </SelectItem>
@@ -149,11 +231,11 @@ export function ProfessionalsDataTable<TData, TValue>({ columns, search }: DataT
           </Select>
         </div>
         <div className='flex w-[100px] items-center justify-center text-xs font-normal text-slate-400'>
-          {DataTableConfig.pagination.page} {pagination.pageIndex + 1} {DataTableConfig.pagination.of} {table.getPageCount()}
+          {PROF_CONFIG.table.pagination.page} {pagination.pageIndex + 1} {PROF_CONFIG.table.pagination.of} {table.getPageCount()}
         </div>
-        {itemsPerPage > 1 && (
+        {table.getPageCount() > 1 && (
           <div className='flex items-center space-x-2'>
-            <Button variant='ghost' className=' h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 lg:flex dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.setPageIndex(0)} disabled={!table.getCanPreviousPage()}>
+            <Button variant='ghost' className='h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 lg:flex dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.setPageIndex(0)} disabled={!table.getCanPreviousPage()}>
               <span className='sr-only'>Go to first page</span>
               <ArrowLeftIcon className='h-4 w-4' />
             </Button>
@@ -165,7 +247,7 @@ export function ProfessionalsDataTable<TData, TValue>({ columns, search }: DataT
               <span className='sr-only'>Go to next page</span>
               <ChevronRightIcon className='h-4 w-4' />
             </Button>
-            <Button variant='ghost' className=' h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 lg:flex dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.setPageIndex(table.getPageCount() - 1)} disabled={!table.getCanNextPage()}>
+            <Button variant='ghost' className='h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 lg:flex dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.setPageIndex(table.getPageCount() - 1)} disabled={!table.getCanNextPage()}>
               <span className='sr-only'>Go to last page</span>
               <ArrowRightIcon className='h-4 w-4' />
             </Button>
