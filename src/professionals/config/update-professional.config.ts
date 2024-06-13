032ba023a1ccbc999770c910fea3d74b15494b73
alanloffler <alanmatiasloffler@gmail.com>
2024-06-13 16:48:43 -0300

    Update config file created

---
+++ src/professionals/config/update-professional.config.ts
@@ -0,0 +1,33 @@
+export const PROF_UPDATE_CONFIG = {
+  title: 'Actualizar un profesional',
+  formTitle: 'Formulario de actualización',
+  formDescription: 'Modificá los datos para actualizar a un profesional',
+  buttons: {
+    cancel: 'Cancelar',
+    create: 'Actualizar profesional',
+  },
+  breadcrumb: [
+    { id: 1, name: 'Inicio', path: '/' },
+    { id: 2, name: 'Profesionales', path: '/professionals' },
+    { id: 3, name: 'Actualizar', path: '/update' },
+  ],
+  labels: {
+    area: 'Área',
+    available: 'Disponible',
+    email: 'Correo electrónico',
+    firstName: 'Nombre',
+    lastName: 'Apellido',
+    phone: 'Teléfono',
+    specialization: 'Especialidad',
+    titleAbbreviation: 'Título',
+  },
+  placeholders: {
+    area: 'Ingresá el área',
+    email: 'Ingresá el correo electrónico',
+    firstName: 'Ingresá el nombre',
+    lastName: 'Ingresá el apellido',
+    phone: 'Ingresá el teléfono',
+    specialization: 'Ingresá la especialidad',
+    titleAbbreviation: 'Ingresá el título',
+  },
+};
