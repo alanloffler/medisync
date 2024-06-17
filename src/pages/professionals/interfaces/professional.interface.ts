78e73144e9d4adec45900478447221ea0a4b1a89
alanloffler <alanmatiasloffler@gmail.com>
2024-06-17 18:45:25 -0300

    New configuration interface for professionals

--- src/pages/professionals/interfaces/professional.interface.ts
+++ src/pages/professionals/interfaces/professional.interface.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { IArea } from "@/core/interfaces/area.interface";
-import { ISpecialization } from "@/core/interfaces/specialization.interface";
+import { IArea } from '@/core/interfaces/area.interface';
+import { ISpecialization } from '@/core/interfaces/specialization.interface';
 
 export interface IProfessionalForm {
   area: string;
@@ -11,16 +11,25 @@ export interface IProfessionalForm {
   phone: number | string;
   specialization: string;
   titleAbbreviation: string;
+  configuration: IProfessionalConfiguration;
 }
 
 export interface IProfessional {
   _id: string;
   area: IArea;
   available: boolean;
+  configuration: IProfessionalConfiguration;
   email: string;
   firstName: string;
   lastName: string;
   phone: number;
   specialization: ISpecialization;
   titleAbbreviation: string;
+}
+
+interface IProfessionalConfiguration {
+  scheduleTimeInit: string;
+  scheduleTimeEnd: string;
+  timeSlotUnavailableInit: string;
+  timeSlotUnavailableEnd: string;
 }
