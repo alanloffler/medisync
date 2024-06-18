dacf7107e895cee01e1cb4647bf038b2e899591b
alanloffler <alanmatiasloffler@gmail.com>
2024-06-18 15:02:19 -0300

    Hooks for date manipulation

---
+++ src/core/hooks/useDateToString.ts
@@ -0,0 +1,38 @@
+import { useCallback } from 'react';
+import { useCapitalize } from './useCapitalize';
+
+export function useDateToString(): (date: Date) => string {
+  return useCallback((date: Date) => {
+    if (date === undefined) return 'Invalid date';
+
+    const year: number = date.getFullYear();
+    const month: number = date.getMonth() + 1;
+    const day: number = date.getDate();
+    let _m, _d: string;
+
+    month < 10 ? (_m = `0${month}`) : (_m = String(month));
+    day < 10 ? (_d = `0${day}`) : (_d = String(day));
+
+    return `${year}-${_m}-${_d}`;
+  }, []);
+}
+
+export function useLegibleDate(): (date: Date) => string {
+  const capitalize = useCapitalize();
+
+  return useCallback((date: Date) => {
+    if (date === undefined) return 'Invalid date';
+
+    const _day = date.getDate();
+    const _month = date.getMonth();
+    const _year = date.getFullYear();
+    const newDate = new Date(_year, _month, _day);
+
+    const weekDay = capitalize(newDate.toLocaleString('es', { weekday: 'long' }));
+    const day = newDate.toLocaleString('es', { day: 'numeric' });
+    const month = capitalize(date.toLocaleString('es', { month: 'long' })) || '';
+    const year = date.toLocaleString('es', { year: 'numeric' });
+
+    return `${weekDay}, ${day} de ${month} de ${year}`;
+  }, [capitalize]);
+}
