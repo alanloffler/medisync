595ec231d8edbd5434a16ca194e580c1d78af7d2
alanloffler <alanmatiasloffler@gmail.com>
2024-06-17 23:31:33 -0300

    Added findAllActive (used in combobox)

--- src/pages/professionals/services/professional-api.service.ts
+++ src/pages/professionals/services/professional-api.service.ts
@@ -8,7 +8,24 @@ export class ProfessionalApiService {
 
   public static async findAll(search: string, sorting: SortingState, skip: number, limit: number) {
     const url: string = `${this.API_URL}/professionals?search=${search}&skip=${skip}&limit=${limit}&sk=${sorting[0].id}&sv=${sorting[0].desc ? 'desc' : 'asc'}`;
-    console.log(url);
+
+    try {
+      const query: Response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+        },
+      });
+
+      return await query.json();
+    } catch (error) {
+      return error;
+    }
+  }
+
+  public static async findAllActive() {
+    const url: string = `${this.API_URL}/professionals/active`;
+
     try {
       const query: Response = await fetch(url, {
         method: 'GET',
