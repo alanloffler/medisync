919299abe829dd0d1203a8b70b9ba64b946ddbcf
alanloffler <alanmatiasloffler@gmail.com>
2024-06-18 23:15:52 -0300

    Appointment service created

---
+++ src/pages/appointments/services/appointment.service.ts
@@ -0,0 +1,58 @@
+import { IAppointmentForm } from "../test";
+
+
+export class AppointmentApiService {
+  private static readonly API_URL = import.meta.env.VITE_API_URL;
+
+  public static async findAllByProfessional(id: string, day: string) {
+    const url: string = `${this.API_URL}/appointments/byProfessional?id=${id}&day=${day}`;
+
+    try {
+      const query: Response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+        },
+      });
+
+      return await query.json();
+    } catch (error) {
+      return error;
+    }
+  }
+
+  public static async create(data: IAppointmentForm) {
+    const url: string = `${this.API_URL}/appointments`;
+
+    try {
+      const query = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+        },
+        body: JSON.stringify(data),
+      });
+
+      return await query.json();
+    } catch (error) {
+      return error;
+    }
+  }
+
+  public static async findOne(id: string) {
+    const url: string = `${this.API_URL}/appointments/${id}`;
+
+    try {
+      const query: Response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+        },
+      });
+
+      return await query.json();
+    } catch (e) {
+      return e;
+    }
+  }
+}
