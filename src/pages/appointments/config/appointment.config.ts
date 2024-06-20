b9b9e0550d3d725546e21738305366d880e015c8
alanloffler <alanmatiasloffler@gmail.com>
2024-06-20 18:04:17 -0300

    Appointment config file created

---
+++ src/pages/appointments/config/appointment.config.ts
@@ -0,0 +1,25 @@
+export const APPO_CONFIG = {
+  buttons: {
+    addAppointment: 'Reservar',
+    cancelAppointment: 'Cancelar',
+    viewAppointment: 'Ver',
+  },
+  calendar: {
+    language: 'es',
+  },
+  combobox: {
+    placeholder: 'Profesional...',
+    searchText: 'Buscar...',
+  },
+  steps: {
+    text1: 'Seleccionar profesional',
+    text2: 'Seleccionar fecha',
+    text3: 'Seleccionar turno',
+  },
+  table: {
+    title: 'Turnos diarios',
+  },
+  words: {
+    hours: 'hs',
+  },
+};
