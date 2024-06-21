212e9d528b1873a12a1abd98eec7fc028894c47c
alanloffler <alanmatiasloffler@gmail.com>
2024-06-21 13:11:00 -0300

    Added settings for dialog

--- src/pages/appointments/config/appointment.config.ts
+++ src/pages/appointments/config/appointment.config.ts
@@ -8,8 +8,31 @@ export const APPO_CONFIG = {
     language: 'es',
   },
   combobox: {
+    notFoundText: 'Profesional no encontrado',
     placeholder: 'Profesional...',
     searchText: 'Buscar...',
+  },
+  dialog: {
+    reserve: {
+      buttons: {
+        cancel: 'Cancelar',
+        save: 'Confirmar reserva',
+      },
+      description: 'Al confirmar, vas a realizar una reserva de turno',
+      title: 'Reserva de turno',
+    },
+    cancel: {
+      buttons: {
+        cancel: 'Cancelar',
+        save: 'Cancelar reserva',
+      },
+      contentText: 'Cancelación del turno para ',
+      description: 'Al confirmar, vas a cancelar la reserva de turno',
+      title: 'Cancelación de turno',
+    }
+  },
+  phrases: {
+    availableAppointments: 'turnos disponibles',
   },
   steps: {
     text1: 'Seleccionar profesional',
@@ -20,6 +43,6 @@ export const APPO_CONFIG = {
     title: 'Turnos diarios',
   },
   words: {
-    hours: 'hs',
+    hours: 'hs.',
   },
 };
