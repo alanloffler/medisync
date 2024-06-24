16fc01d1f7dfee1721c5da0da5d3e8396dc191a4
alanloffler <alanmatiasloffler@gmail.com>
2024-06-24 10:31:40 -0300

    Formatted

--- src/App.tsx
+++ src/App.tsx
@@ -6,83 +6,32 @@ import { lazy, Suspense } from 'react';
 const Appointments = lazy(() => import('./pages/appointments/Appointments'));
 const ViewAppointment = lazy(() => import('./pages/appointments/components/ViewAppointment'));
 const Dashboard = lazy(() => import('./pages/dashboard/Dashboard'));
+
 const Professionals = lazy(() => import('./pages/professionals/Professionals'));
 const CreateProfessional = lazy(() => import('./pages/professionals/components/CreateProfessional'));
 const UpdateProfessional = lazy(() => import('./pages/professionals/components/UpdateProfessional'));
+
+const Users = lazy(() => import('./pages/users/Users'));
+
 const WhatsApp = lazy(() => import('./pages/whatsapp/WhatsApp'));
 // React component
 export default function App() {
+  // prettier-ignore
   const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Layout />,
-      children: [
-        {
-          path: '/dashboard',
-          element: (
-            <Suspense fallback={<>Loading...</>}>
-              <Dashboard />
-            </Suspense>
-          ),
-        },
-        {
-          path: '/',
-          element: (
-            <Suspense fallback={<>Loading...</>}>
-              <Appointments />
-            </Suspense>
-          ),
-          //   async lazy() {
-          //     const { Appointments } = await import('./appointments/Appointments');
-          //     return { Component: Appointments };
-          //   }
-        },
-        {
-          path: '/appointments/:id',
-          element: (
-            <Suspense fallback={<>Loading...</>}>
-              <ViewAppointment />
-            </Suspense>
-          ),
-        },
-        {
-          path: '/professionals',
-          element: (
-            <Suspense fallback={<>Loading...</>}>
-              <Professionals />
-            </Suspense>
-          ),
-        },
-        {
-          path: '/professionals/create',
-          element: (
-            <Suspense fallback={<>Loading...</>}>
-              <CreateProfessional />
-            </Suspense>
-          ),
-        },
-        {
-          path: '/professionals/update/:id',
-          element: (
-            <Suspense fallback={<>Loading...</>}>
-              <UpdateProfessional />
-            </Suspense>
-          ),
-        },
-        {
-          path: '/whatsapp/:id',
-          element: (
-            <Suspense fallback={<>Loading...</>}>
-              <WhatsApp />
-            </Suspense>
-          ),
-        },
-        {
-          path: '*',
-          element: <>Not found 404</>,
-        },
+    { path: '/', element: <Layout />, children: 
+      [
+        { path: '/dashboard', element: (<Suspense fallback={<>Loading...</>}><Dashboard /></Suspense>) },
+        { path: '/', element: (<Suspense fallback={<>Loading...</>}><Appointments /></Suspense>) },
+        { path: '/appointments/:id', element: (<Suspense fallback={<>Loading...</>}><ViewAppointment /></Suspense>) },
+        { path: '/professionals', element: (<Suspense fallback={<>Loading...</>}><Professionals /></Suspense>) },
+        { path: '/professionals/create', element: (<Suspense fallback={<>Loading...</>}><CreateProfessional /></Suspense>) },
+        { path: '/professionals/update/:id', element: (<Suspense fallback={<>Loading...</>}><UpdateProfessional /></Suspense>) },
+        { path: '/users', element: (<Suspense fallback={<>Loading...</>}><Users /></Suspense>) },
+        { path: '/whatsapp/:id',  element: (<Suspense fallback={<>Loading...</>}><WhatsApp /></Suspense>) },
+        { path: '*', element: <>Not found 404</> },
       ],
     },
   ]);
+
   return <RouterProvider router={router} />;
 }
