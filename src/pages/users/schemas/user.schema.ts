9291a6ce17eea9d0360cf4868bb6c9b7b5eea88b
alanloffler <alanmatiasloffler@gmail.com>
2024-06-24 21:37:06 -0300

    New user zod schema

---
+++ src/pages/users/schemas/user.schema.ts
@@ -0,0 +1,54 @@
+import { z } from 'zod';
+import { USER_SCHEMA } from '@/config/schemas/user.schema';
+// prettier-ignore
+export const userSchema = z.object({
+  dni: z
+    .coerce
+    .number()
+    .min(
+      USER_SCHEMA.dni.min.value, 
+      { message: USER_SCHEMA.dni.min.message }
+    )
+    .max(
+      USER_SCHEMA.dni.max.value, 
+      { message: USER_SCHEMA.dni.max.message }
+    ),
+
+  lastName: z
+    .string()
+    .min(
+      USER_SCHEMA.lastName.min.value, 
+      { message: USER_SCHEMA.lastName.min.message }
+    )
+    .max(
+      USER_SCHEMA.lastName.max.value, 
+      { message: USER_SCHEMA.lastName.max.message }
+    ),
+
+  firstName: z
+    .string()
+    .min(
+      USER_SCHEMA.firstName.min.value, 
+      { message: USER_SCHEMA.firstName.min.message }
+    )
+    .max(
+      USER_SCHEMA.firstName.max.value, 
+      { message: USER_SCHEMA.firstName.max.message }
+    ),
+
+  phone: z
+    .coerce
+    .number()
+    .min(
+      USER_SCHEMA.phone.min.value, 
+      { message: USER_SCHEMA.phone.min.message }
+    )
+    .max(
+      USER_SCHEMA.phone.max.value, 
+      { message: USER_SCHEMA.phone.max.message }
+    ),
+
+  email: z
+    .string()
+    .email({ message: USER_SCHEMA.email.message }),
+});
