e40f7cce66a7946f0bfad600a131b6e09cb62b55
alanloffler <alanmatiasloffler@gmail.com>
2024-06-26 20:00:15 -0300

    Hook for delimiter DNIs and phone numbers

---
+++ src/core/hooks/useDelimiter.ts
@@ -0,0 +1,21 @@
+export function useDelimiter() {
+  const insertDelimiterEveryThreeDigits = (input: string | number, delimiter: string, each: number): string => {
+    if (typeof input === 'number') input = input.toString();
+
+    const cleanedInput = input.replace(new RegExp(`\\${delimiter}`, 'g'), '');
+
+    let result = '';
+    let count = 0;
+
+    for (let i = cleanedInput.length - 1; i >= 0; i--) {
+      result = cleanedInput[i] + result;
+      count++;
+
+      if (count % each === 0 && i > 0) result = delimiter + result;
+    }
+
+    return result;
+  };
+
+  return insertDelimiterEveryThreeDigits;
+}
