9aaba281d2f7e631a97f4a867992fb5431521b4e
alanloffler <alanmatiasloffler@gmail.com>
2024-06-26 20:01:29 -0300

    Added search by names and DNIs

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,33 +1,39 @@
 // Icons: https://lucide.dev/icons/
-import { CirclePlus, List, ListFilter, ListRestart, PlusCircle } from 'lucide-react';
+import { CirclePlus, List, ListFilter, ListRestart, PlusCircle, Search, X } from 'lucide-react';
 // Components: https://ui.shadcn.com/docs/components
 import { Button } from '@/core/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/core/components/ui/card';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@/core/components/ui/dropdown-menu';
-
+import { Input } from '@/core/components/ui/input';
+import { Label } from '@/core/components/ui/label';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/core/components/ui/tooltip';
 // App components
-import { PageHeader } from "@/core/components/common/PageHeader";
-import { UsersDataTable } from "./components/UsersDataTable";
+import { PageHeader } from '@/core/components/common/PageHeader';
+import { UsersDataTable } from './components/UsersDataTable';
 // App
-import { USER_CONFIG } from "@/config/user.config";
+import { ChangeEvent, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-// import { useCapitalize } from '@/core/hooks/useCapitalize';
-import { useState } from 'react';
+import { USER_CONFIG } from '@/config/user.config';
 import { useDebounce } from '@/core/hooks/useDebounce';
-// Constants
-const DEBOUNCE_TIME: number = 500;
 // React component
 export default function Users() {
-  const [reload, setReload] = useState<number>(0);
-  const [search, setSearch] = useState<string>('');
   const [errorMessage, setErrorMessage] = useState<string>('');
-
-  // const capitalize = useCapitalize();
-  const debouncedSearch = useDebounce<string>(search, DEBOUNCE_TIME); // load data
+  const [reload, setReload] = useState<number>(0);
+  const [searchByName, setSearchByName] = useState<string>('');
+  const [searchByDNI, setSearchByDNI] = useState<string>('');
+  const debouncedSearch = useDebounce<string>(searchByName, USER_CONFIG.search.debounceTime);
+  const debouncedSearch2 = useDebounce<string>(searchByDNI, USER_CONFIG.search.debounceTime);
   const navigate = useNavigate();
 
+  function handleSearchByName(event: ChangeEvent<HTMLInputElement>): void {
+    setSearchByName(event.target.value);
+  }
+
+  function handleSearchByDNI(event: ChangeEvent<HTMLInputElement>): void {
+    setSearchByDNI(event.target.value);
+  }
+
   function handleReload(): void {
-    setSearch('');
+    setSearchByName('');
     setReload(Math.random());
   }
 
@@ -37,27 +43,42 @@ export default function Users() {
       <div className='flex items-center justify-between'>
         <PageHeader title={USER_CONFIG.title} breadcrumb={USER_CONFIG.breadcrumb} />
       </div>
-      {/* Users table */}
+      {/* Page content */}
       <div className='grid gap-6 md:grid-cols-4 md:gap-8 lg:grid-cols-4 xl:grid-cols-4'>
+        {/* Left side content */}
         <Card className='col-span-1 border-none bg-slate-200 bg-transparent shadow-none md:col-span-4 lg:col-span-1 xl:col-span-1'>
           <CardContent className='p-0'>
             <div className='flex flex-col gap-6 md:w-full'>
               <Link to={`/users/create`} className='w-fit'>
                 <Button variant={'default'} size={'sm'} className='gap-2'>
                   <PlusCircle className='h-4 w-4' strokeWidth={2} />
-                  {USER_CONFIG.buttons.addUser}
+                  {USER_CONFIG.buttons.createUser}
                 </Button>
               </Link>
-              <div className='flex flex-col space-y-2'>
-                {/* <div className='relative w-full'>
+              {/* Search by firstname or lastname */}
+              <div className='flex flex-col space-y-4'>
+                <Label>{USER_CONFIG.search.label.name}</Label>
+                <div className='relative w-full items-center'>
                   <Search className='absolute left-3 top-3 h-4 w-4 text-muted-foreground' />
-                  <Input onChange={handleSearch} value={search} type='search' placeholder={PROF_CONFIG.search.placeholder} className='bg-background pl-9 shadow-sm' />
-                </div> */}
+                  <Input onClick={() => setSearchByDNI('')} onChange={handleSearchByName} value={searchByName} type='text' placeholder={USER_CONFIG.search.placeholder.name} className='bg-background pl-9 shadow-sm' />
+                  <button onClick={() => setSearchByName('')} className='absolute right-3 top-3 text-muted-foreground'><X className='h-4 w-4' /></button>
+                </div>
                 {errorMessage && <div className='flex flex-row items-center text-xs font-medium text-rose-400'>{errorMessage}</div>}
               </div>
+              {/* Search by DNI */}
+              <div className='flex flex-col space-y-4'>
+                <Label>{USER_CONFIG.search.label.dni}</Label>
+                <div className='relative w-full items-center'>
+                  <Search className='absolute left-3 top-3 h-4 w-4 text-muted-foreground' />
+                  <Input onClick={() => setSearchByName('')} onChange={handleSearchByDNI} value={searchByDNI} type='number' placeholder={USER_CONFIG.search.placeholder.dni} className='bg-background pl-9 shadow-sm' />
+                  <button onClick={() => setSearchByDNI('')} className='absolute right-3 top-3 text-muted-foreground'><X className='h-4 w-4' /></button>
+                </div>
+                {errorMessage && <div className='flex flex-row items-center text-xs font-medium text-rose-400'>{errorMessage}</div>}
               </div>
+            </div>
           </CardContent>
         </Card>
+        {/* Right side content */}
         <Card className='col-span-1 overflow-y-auto md:col-span-4 lg:col-span-3 xl:col-span-3'>
           <CardHeader>
             <div className='grid gap-2'>
@@ -67,14 +88,67 @@ export default function Users() {
                   {USER_CONFIG.table.title}
                 </div>
                 <div className='flex items-center gap-2'>
-                  <DropdownMenu>
+                  {/* Sort */}
+                  <TooltipProvider delayDuration={0.3}>
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <Button variant={'tableHeader'} size={'miniIcon'} className='flex items-center'>
+                          <ListFilter className='h-4 w-4' strokeWidth={2} />
+                        </Button>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p className='text-xs font-medium'>{USER_CONFIG.tooltip.sort}</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                  {/* Reload */}
+                  <TooltipProvider delayDuration={0.3}>
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <Button variant={'tableHeader'} size={'miniIcon'} onClick={handleReload}>
+                          <ListRestart className='h-4 w-4' strokeWidth={2} />
+                        </Button>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p className='text-xs font-medium'>{USER_CONFIG.tooltip.reload}</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                  {/* Create user */}
+                  <TooltipProvider delayDuration={0.3}>
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <Button variant={'tableHeaderPrimary'} size={'miniIcon'} onClick={() => navigate('/users/create')}>
+                          <CirclePlus className='h-4 w-4' strokeWidth={2} />
+                        </Button>
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p className='text-xs font-medium'>{USER_CONFIG.tooltip.createUser}</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </div>
+              </CardTitle>
+            </div>
+          </CardHeader>
+          {/* Table */}
+          <CardContent>
+            <UsersDataTable search={debouncedSearch || debouncedSearch2} reload={reload} setErrorMessage={setErrorMessage} />
+          </CardContent>
+        </Card>
+      </div>
+    </main>
+  );
+}
+{
+  /* <DropdownMenu>
                     <DropdownMenuTrigger asChild>
                       <Button variant={'tableHeader'} size={'miniIcon'} className='flex items-center'>
                         <ListFilter className='h-4 w-4' strokeWidth={2} />
                       </Button>
                     </DropdownMenuTrigger>
                     <DropdownMenuContent className='w-fit' align='center'>
-                      {/* {areas.map((area) => (
+                      {areas.map((area) => (
                         <DropdownMenuSub key={area._id}>
                           <DropdownMenuSubTrigger>
                             <span>{capitalize(area.name)}</span>
@@ -89,24 +163,7 @@ export default function Users() {
                             </DropdownMenuSubContent>
                           </DropdownMenuPortal>
                         </DropdownMenuSub>
-                      ))} */}
+                      ))} 
                     </DropdownMenuContent>
-                  </DropdownMenu>
-                  <Button variant={'tableHeader'} size={'miniIcon'} onClick={handleReload}>
-                    <ListRestart className='h-4 w-4' strokeWidth={2} />
-                  </Button>
-                  <Button variant={'tableHeaderPrimary'} size={'miniIcon'} onClick={() => navigate('/users/create')}>
-                    <CirclePlus className='h-4 w-4' strokeWidth={2} />
-                  </Button>
-                </div>
-              </CardTitle>
-            </div>
-          </CardHeader>
-          <CardContent>
-            <UsersDataTable search={debouncedSearch} reload={reload} setErrorMessage={setErrorMessage} />
-          </CardContent>
-        </Card>
-      </div>
-    </main>
-  );
+                  </DropdownMenu> */
 }
