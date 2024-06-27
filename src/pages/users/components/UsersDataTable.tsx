992b6f690346c7a61d39351f0620071bc32c5d06
alanloffler <alanmatiasloffler@gmail.com>
2024-06-26 21:30:30 -0300

    Tooltips on buttons

--- src/pages/users/components/UsersDataTable.tsx
+++ src/pages/users/components/UsersDataTable.tsx
@@ -4,6 +4,7 @@ import { ArrowDownUp, ArrowLeftIcon, ArrowRightIcon, ChevronLeftIcon, ChevronRig
 import { Button } from '@/core/components/ui/button';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/core/components/ui/select';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/core/components/ui/table';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/core/components/ui/tooltip';
 // Tanstack Data Table: https://tanstack.com/table/latest
 import { ColumnDef, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, PaginationState, SortingState, useReactTable } from '@tanstack/react-table';
 // App
@@ -97,24 +98,61 @@ export function UsersDataTable({ search, reload, setErrorMessage }: DataTablePro
       size: 100,
       header: () => <div className='text-center'>{USER_CONFIG.table.headers[3]}</div>,
       cell: ({ row }) => (
-        <div className='flex flex-row items-center justify-center space-x-4'>
-          
-          <Button variant={'ghost'} size={'miniIcon'}>
-            <FileText className='h-4 w-4' />
-          </Button>
-          <Button variant={'ghost'} size={'miniIcon'} onClick={() => navigate(`/professionals/update/${row.original._id}`)}>
-            <FilePen className='h-4 w-4' />
-          </Button>
-          <Button variant={'ghost'} size={'miniIcon'}>
-            <Trash2 className='h-4 w-4' />
-          </Button>
-          {/* <Button variant={'ghost'} size={'miniIcon'} className='fill-current hover:fill-green-500'> */}
-          {/* <a aria-disabled href={`https://web.whatsapp.com/send?phone=54${row.original.phone}`} target='_top'> */}
-          <Button disabled variant={'ghost'} size={'miniIcon'} className='fill-current hover:fill-green-500' onClick={() => navigate(`/whatsapp/${row.original._id}`)}>
-            <svg width='100' height='100' viewBox='0 0 464 488' className='h-4 w-4'>
-              <path d='M462 228q0 93-66 159t-160 66q-56 0-109-28L2 464l40-120q-32-54-32-116q0-93 66-158.5T236 4t160 65.5T462 228zM236 39q-79 0-134.5 55.5T46 228q0 62 36 111l-24 70l74-23q49 31 104 31q79 0 134.5-55.5T426 228T370.5 94.5T236 39zm114 241q-1-1-10-7q-3-1-19-8.5t-19-8.5q-9-3-13 2q-1 3-4.5 7.5t-7.5 9t-5 5.5q-4 6-12 1q-34-17-45-27q-7-7-13.5-15t-12-15t-5.5-8q-3-7 3-11q4-6 8-10l6-9q2-5-1-10q-4-13-17-41q-3-9-12-9h-11q-9 0-15 7q-19 19-19 45q0 24 22 57l2 3q2 3 4.5 6.5t7 9t9 10.5t10.5 11.5t13 12.5t14.5 11.5t16.5 10t18 8.5q16 6 27.5 10t18 5t9.5 1t7-1t5-1q9-1 21.5-9t15.5-17q8-21 3-26z' />
-            </svg>
-          </Button>
+        <div className='flex flex-row items-center justify-center space-x-2'>
+          {/* View User */}
+          <TooltipProvider delayDuration={0.3}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button variant={'ghost'} size={'miniIcon'} className='hover:bg-transparent hover:text-fuchsia-500'>
+                  <FileText className='h-4 w-4' />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p className='text-xs font-medium'>{USER_CONFIG.tooltip.actions.viewUser}</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          {/* Update User */}
+          <TooltipProvider delayDuration={0.3}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button variant={'ghost'} size={'miniIcon'} onClick={() => navigate(`/users/update/${row.original._id}`)} className='hover:bg-transparent hover:text-indigo-500'>
+                  <FilePen className='h-4 w-4' />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p className='text-xs font-medium'>{USER_CONFIG.tooltip.actions.updateUser}</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          {/* Delete User */}
+          <TooltipProvider delayDuration={0.3}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button variant={'ghost'} size={'miniIcon'} className='hover:bg-transparent hover:text-red-500'>
+                  <Trash2 className='h-4 w-4' />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p className='text-xs font-medium'>{USER_CONFIG.tooltip.actions.removeUser}</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          {/* Send WhatsApp message */}
+          <TooltipProvider delayDuration={0.3}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button disabled={!row.original.phone} variant={'ghost'} size={'miniIcon'} className='fill-current hover:bg-transparent hover:fill-green-500' onClick={() => navigate(`/whatsapp/${row.original._id}`)}>
+                  <svg width='100' height='100' viewBox='0 0 464 488' className='h-4 w-4'>
+                    <path d='M462 228q0 93-66 159t-160 66q-56 0-109-28L2 464l40-120q-32-54-32-116q0-93 66-158.5T236 4t160 65.5T462 228zM236 39q-79 0-134.5 55.5T46 228q0 62 36 111l-24 70l74-23q49 31 104 31q79 0 134.5-55.5T426 228T370.5 94.5T236 39zm114 241q-1-1-10-7q-3-1-19-8.5t-19-8.5q-9-3-13 2q-1 3-4.5 7.5t-7.5 9t-5 5.5q-4 6-12 1q-34-17-45-27q-7-7-13.5-15t-12-15t-5.5-8q-3-7 3-11q4-6 8-10l6-9q2-5-1-10q-4-13-17-41q-3-9-12-9h-11q-9 0-15 7q-19 19-19 45q0 24 22 57l2 3q2 3 4.5 6.5t7 9t9 10.5t10.5 11.5t13 12.5t14.5 11.5t16.5 10t18 8.5q16 6 27.5 10t18 5t9.5 1t7-1t5-1q9-1 21.5-9t15.5-17q8-21 3-26z' />
+                  </svg>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p className='text-xs font-medium'>{USER_CONFIG.tooltip.actions.sendWhatsAppMessage}</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </div>
       ),
     },
@@ -137,7 +175,6 @@ export function UsersDataTable({ search, reload, setErrorMessage }: DataTablePro
       pagination,
     },
   });
-
   // #endregion
   // #region Load data, pagination and sorting
   useEffect(() => {
@@ -159,9 +196,7 @@ export function UsersDataTable({ search, reload, setErrorMessage }: DataTablePro
   useEffect(() => {
     const fetchData = (search: string, sorting: SortingState, skipItems: number, itemsPerPage: number) => {
       if (!isNumericString(search)) {
-        console.log('is not numeric', search);
         UserApiService.findAll(search, sorting, skipItems, itemsPerPage).then((response) => {
-          console.log(response.data.data);
           if (response.statusCode === 200) {
             setData(response.data.data);
             setColumns(tableColumns);
@@ -175,9 +210,7 @@ export function UsersDataTable({ search, reload, setErrorMessage }: DataTablePro
           if (response instanceof Error) addNotification({ type: 'error', message: 'Internal Server Error' });
         });
       } else {
-        console.log('is numeric', search);
         UserApiService.findAllByDNI(search, sorting, skipItems, itemsPerPage).then((response) => {
-          console.log(response.data.data);
           if (response.statusCode === 200) {
             setData(response.data.data);
             setColumns(tableColumns);
@@ -251,22 +284,58 @@ export function UsersDataTable({ search, reload, setErrorMessage }: DataTablePro
         </div>
         {table.getPageCount() > 1 && (
           <div className='flex items-center space-x-2'>
-            <Button variant='ghost' className='h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 lg:flex dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.setPageIndex(0)} disabled={!table.getCanPreviousPage()}>
-              <span className='sr-only'>Go to first page</span>
-              <ArrowLeftIcon className='h-4 w-4' />
-            </Button>
-            <Button variant='ghost' className='h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
-              <span className='sr-only'>Go to previous page</span>
-              <ChevronLeftIcon className='h-4 w-4' />
-            </Button>
-            <Button variant='ghost' className='h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
-              <span className='sr-only'>Go to next page</span>
-              <ChevronRightIcon className='h-4 w-4' />
-            </Button>
-            <Button variant='ghost' className='h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 lg:flex dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.setPageIndex(table.getPageCount() - 1)} disabled={!table.getCanNextPage()}>
-              <span className='sr-only'>Go to last page</span>
-              <ArrowRightIcon className='h-4 w-4' />
-            </Button>
+            {/* First page */}
+            <TooltipProvider delayDuration={0.3}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button variant='ghost' className='h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 lg:flex dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.setPageIndex(0)} disabled={!table.getCanPreviousPage()}>
+                    <ArrowLeftIcon className='h-4 w-4' />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p className='text-xs font-medium'>{USER_CONFIG.tooltip.pagination.firstPage}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            {/* Previous page */}
+            <TooltipProvider delayDuration={0.3}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button variant='ghost' className='h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+                    <ChevronLeftIcon className='h-4 w-4' />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p className='text-xs font-medium'>{USER_CONFIG.tooltip.pagination.prevPage}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            {/* Next page */}
+            <TooltipProvider delayDuration={0.3}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button variant='ghost' className='h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+                    <ChevronRightIcon className='h-4 w-4' />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p className='text-xs font-medium'>{USER_CONFIG.tooltip.pagination.nextPage}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            {/* Last page */}
+            <TooltipProvider delayDuration={0.3}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button variant='ghost' className='h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 lg:flex dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.setPageIndex(table.getPageCount() - 1)} disabled={!table.getCanNextPage()}>
+                    <ArrowRightIcon className='h-4 w-4' />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p className='text-xs font-medium'>{USER_CONFIG.tooltip.pagination.lastPage}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           </div>
         )}
       </div>
