e8b6d32dc97f48ca7db08b15c542556715cbcd6f
alanloffler <alanmatiasloffler@gmail.com>
2024-06-29 18:32:42 -0300

    Added card and dynamic content from db

--- src/pages/users/components/ViewUser.tsx
+++ src/pages/users/components/ViewUser.tsx
@@ -1,10 +1,101 @@
-import { useParams } from "react-router-dom";
-
+// Icons: https://lucide.dev/icons/
+import { ArrowLeft, CreditCard, Mail, Menu, Smartphone } from 'lucide-react';
+// Components: https://ui.shadcn.com/docs/components
+import { Button } from '@/core/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/core/components/ui/card';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from '@/core/components/ui/dropdown-menu';
+// App components
+import { PageHeader } from '@/core/components/common/PageHeader';
+// App
+import { IUser } from '../interfaces/user.interface';
+import { USER_VIEW_CONFIG as UV_CONFIG } from '@/config/user.config';
+import { UserApiService } from '../services/user-api.service';
+import { useCapitalize } from '@/core/hooks/useCapitalize';
+import { useDelimiter } from '@/core/hooks/useDelimiter';
+import { useEffect, useState } from 'react';
+import { useLegibleDate } from '@/core/hooks/useDateToString';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useNotificationsStore } from '@/core/stores/notifications.store';
+// React component
 export default function ViewUser() {
-  const {id} = useParams();
+  const [showCard, setShowCard] = useState<boolean>(false);
+  const [user, setUser] = useState<IUser>({} as IUser);
+  const addNotification = useNotificationsStore((state) => state.addNotification);
+  const capitalize = useCapitalize();
+  const delimiter = useDelimiter();
+  const legibleDate = useLegibleDate();
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (id) {
+      UserApiService.findOne(id).then((response) => {
+        if (response.statusCode === 200) {
+          setUser(response.data);
+          setShowCard(true);
+        }
+        if (response.statusCode > 399) addNotification({ type: 'error', message: response.message });
+        if (response instanceof Error) addNotification({ type: 'error', message: 'Internal Server Error' });
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id]);
+
   return (
-    <div>
-      <h1>View User {id}</h1>
-    </div>
+    <main className='flex flex-1 flex-col gap-2 p-4 md:gap-2 md:p-6 lg:gap-2 lg:p-6'>
+      {/* Page Header */}
+      <div className='flex items-center justify-between'>
+        <PageHeader title={UV_CONFIG.title} breadcrumb={UV_CONFIG.breadcrumb} />
+        <Button variant={'outline'} size={'sm'} className='gap-2' onClick={() => navigate(-1)}>
+          <ArrowLeft className='h-4 w-4' />
+          {UV_CONFIG.buttons.back}
+        </Button>
+      </div>
+      {/* Page Content */}
+      <div className='mx-auto mt-4 flex w-1/2 flex-row'>
+        {showCard && (
+          <Card className='w-full'>
+            <CardHeader>
+              <CardTitle>
+                <div className='relative flex items-center justify-center'>
+                  <h1 className='text-center text-2xl font-bold'>
+                    {capitalize(user.lastName)}, {capitalize(user.firstName)}
+                  </h1>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button variant={'tableHeader'} size={'miniIcon'} className='absolute right-1 flex items-center'>
+                        <Menu className='h-4 w-4' strokeWidth={2} />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className='w-fit' align='end'>
+                      <DropdownMenuGroup>
+                        {/* TODO: add actions */}
+                        <DropdownMenuItem onClick={() => console.log('Send email')}>{UV_CONFIG.dropdownMenu[0].name}</DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => console.log('Send whatsapp')}>{UV_CONFIG.dropdownMenu[1].name}</DropdownMenuItem>
+                      </DropdownMenuGroup>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </div>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className='mt-3 space-y-3'>
+              <div className='flex items-center space-x-4'>
+                <CreditCard />
+                <span className='text-lg font-medium'>{delimiter(user.dni, '.', 3)}</span>
+              </div>
+              <div className='flex items-center space-x-4'>
+                <Smartphone />
+                <span className='text-lg font-medium'>{delimiter(user.phone, '-', 6)}</span>
+              </div>
+              <div className='flex items-center space-x-4'>
+                <Mail />
+                <span className='text-lg font-medium'>{user.email}</span>
+              </div>
+              <div className='flex justify-end pt-2 text-base leading-none text-slate-500'>{`${UV_CONFIG.phrase.userSince} ${legibleDate(new Date(user.createdAt), 'short')}`}</div>
+            </CardContent>
+          </Card>
+        )}
+      </div>
+    </main>
   );
 }
