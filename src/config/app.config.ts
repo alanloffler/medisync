4967cd6cc8fe7bd5e2d21736cbc62e1df63e8856
alanloffler <alanmatiasloffler@gmail.com>
2024-07-06 13:45:33 -0300

    Database loading settings

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,7 +1,15 @@
 export const APP_CONFIG = {
   loading: {
-    duration: .5,
+    duration: 0.5,
     size: 45,
     text: 'Cargando',
-  }
-}
+  },
+  loadingDB: {
+    defaultText: 'Cargando información de la base de datos',
+    findUsers: 'Cargando pacientes',
+    settings: {
+      duration: 0.4,
+      size: 18,
+    },
+  },
+};
