105ac7601554cb956ee04f0886bfbef4463ac5e5
alanloffler <alanmatiasloffler@gmail.com>
2024-07-06 15:05:08 -0300

    Updated InfoCard component for inline use. Removed description (only one line of text now)

--- src/core/components/common/InfoCard.tsx
+++ src/core/components/common/InfoCard.tsx
@@ -1,30 +1,26 @@
 // Components
-import { Card } from '@/core/components/ui/card';
-import { cn } from '@/lib/utils';
 import { CircleAlert, CircleCheck, CircleX } from 'lucide-react';
+import { cn } from '@/lib/utils';
 // Interface
 interface IInfoCard {
-  title: string;
-  description: string;
+  text: string;
   type: 'error' | 'success' | 'warning';
   className?: string;
 }
 // React component
-export function InfoCard({ title, description, type, className }: IInfoCard) {
-  const borderColor = type === 'error' ? 'border-red-400' : type === 'success' ? 'border-green-400' : 'border-yellow-400';
+export function InfoCard({ text, type, className }: IInfoCard) {
   const strokeColor = type === 'error' ? 'stroke-red-400' : type === 'success' ? 'stroke-green-400' : 'stroke-yellow-400';
 
   return (
-    <Card className={cn('flex flex-row items-center space-x-4 p-4', className)}>
-      <div className={cn('flex h-full flex-col justify-center border-r-2 pr-4', borderColor)}>
-        {type === 'error' && <CircleX className={cn('h-8 w-8', strokeColor)} strokeWidth={2} />}
-        {type === 'success' && <CircleCheck className={cn('h-8 w-8', strokeColor)} strokeWidth={2} />}
-        {type === 'warning' && <CircleAlert className={cn('h-8 w-8', strokeColor)} strokeWidth={2} />}
+    <div className={cn('flex flex-row items-center space-x-2 p-2 justify-center', className)}>
+      <div className='flex h-full flex-col justify-center'>
+        {type === 'error' && <CircleX className={cn('h-5 w-5', strokeColor)} strokeWidth={2} />}
+        {type === 'success' && <CircleCheck className={cn('h-5 w-5', strokeColor)} strokeWidth={2} />}
+        {type === 'warning' && <CircleAlert className={cn('h-5 w-5', strokeColor)} strokeWidth={2} />}
       </div>
       <div className='flex flex-col'>
-        <h1 className='text-lg font-bold'>{title}</h1>
-        <div>{description}</div>
+        <span className='text-sm font-medium text-slate-500'>{text}</span>
       </div>
-    </Card>
+    </div>
   );
 }
