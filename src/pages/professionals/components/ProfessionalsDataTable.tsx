3fd00c50f9ba17ac736e9606ef70ab675c1e8e98
alanloffler <alanmatiasloffler@gmail.com>
2024-07-06 21:13:06 -0300

    Added loading and info card for status messages. Removed empty result message of tanstack sample.

--- src/pages/professionals/components/ProfessionalsDataTable.tsx
+++ src/pages/professionals/components/ProfessionalsDataTable.tsx
@@ -7,9 +7,13 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@
 // Tanstack Data Table: https://tanstack.com/table/latest
 import { ColumnDef, flexRender, getCoreRowModel, getFilteredRowModel, getPaginationRowModel, getSortedRowModel, PaginationState, SortingState, useReactTable } from '@tanstack/react-table';
 // App
-import { IProfessional } from '../interfaces/professional.interface';
-import { PROF_CONFIG } from '../config/professionals.config';
-import { ProfessionalApiService } from '../services/professional-api.service';
+import { APP_CONFIG } from '@/config/app.config';
+import { IInfoCard } from '@/core/components/common/interfaces/infocard.interface';
+import { IProfessional } from '@/pages/professionals/interfaces/professional.interface';
+import { InfoCard } from '@/core/components/common/InfoCard';
+import { LoadingDB } from '@/core/components/common/LoadingDB';
+import { PROF_CONFIG } from '@/pages/professionals/config/professionals.config';
+import { ProfessionalApiService } from '@/pages/professionals/services/professional-api.service';
 import { useCapitalize } from '@/core/hooks/useCapitalize';
 import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -35,6 +39,8 @@ const defaultPagination = { pageIndex: 0, pageSize: PROF_CONFIG.table.defaultPag
 export function ProfessionalsDataTable({ search, reload, setErrorMessage }: DataTableProps) {
   const [columns, setColumns] = useState<ColumnDef<IProfessional>[]>([]);
   const [data, setData] = useState([]);
+  const [infoCard, setInfoCard] = useState<IInfoCard>({ text: '', type: 'error' });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [pagination, setPagination] = useState<PaginationState>(defaultPagination);
   const [sorting, setSorting] = useState<SortingState>(defaultSorting);
   const [tableManager, setTableManager] = useState<TableManager>({ sorting, pagination });
@@ -124,7 +130,7 @@ export function ProfessionalsDataTable({ search, reload, setErrorMessage }: Data
             <Trash2 className='h-4 w-4' />
           </Button>
           {/* <Button variant={'ghost'} size={'miniIcon'} className='fill-current hover:fill-green-500'> */}
-            {/* <a aria-disabled href={`https://web.whatsapp.com/send?phone=54${row.original.phone}`} target='_top'> */}
+          {/* <a aria-disabled href={`https://web.whatsapp.com/send?phone=54${row.original.phone}`} target='_top'> */}
           <Button disabled variant={'ghost'} size={'miniIcon'} className='fill-current hover:fill-green-500' onClick={() => navigate(`/whatsapp/${row.original._id}`)}>
             <svg width='100' height='100' viewBox='0 0 464 488' className='h-4 w-4'>
               <path d='M462 228q0 93-66 159t-160 66q-56 0-109-28L2 464l40-120q-32-54-32-116q0-93 66-158.5T236 4t160 65.5T462 228zM236 39q-79 0-134.5 55.5T46 228q0 62 36 111l-24 70l74-23q49 31 104 31q79 0 134.5-55.5T426 228T370.5 94.5T236 39zm114 241q-1-1-10-7q-3-1-19-8.5t-19-8.5q-9-3-13 2q-1 3-4.5 7.5t-7.5 9t-5 5.5q-4 6-12 1q-34-17-45-27q-7-7-13.5-15t-12-15t-5.5-8q-3-7 3-11q4-6 8-10l6-9q2-5-1-10q-4-13-17-41q-3-9-12-9h-11q-9 0-15 7q-19 19-19 45q0 24 22 57l2 3q2 3 4.5 6.5t7 9t9 10.5t10.5 11.5t13 12.5t14.5 11.5t16.5 10t18 8.5q16 6 27.5 10t18 5t9.5 1t7-1t5-1q9-1 21.5-9t15.5-17q8-21 3-26z' />
@@ -189,6 +195,8 @@ export function ProfessionalsDataTable({ search, reload, setErrorMessage }: Data
 
   useEffect(() => {
     const fetchData = (search: string, sorting: SortingState, skipItems: number, itemsPerPage: number) => {
+      setIsLoading(true);
+
       ProfessionalApiService.findAll(search, sorting, skipItems, itemsPerPage).then((response) => {
         if (!response.statusCode) {
           setData(response.data);
@@ -199,8 +207,13 @@ export function ProfessionalsDataTable({ search, reload, setErrorMessage }: Data
         if (response.statusCode > 399) {
           setErrorMessage(response.message);
           addNotification({ type: 'error', message: response.message });
+          setInfoCard({ text: response.message, type: 'warning' });
         }
-        if (response instanceof Error) addNotification({ type: 'error', message: 'Internal Server Error' });
+        if (response instanceof Error) {
+          addNotification({ type: 'error', message: 'Internal Server Error' });
+          setInfoCard({ text: APP_CONFIG.error.server, type: 'error' });
+        }
+        setIsLoading(false);
       });
     };
     fetchData(search, tableManager.sorting, tableManager.pagination.pageIndex * tableManager.pagination.pageSize, tableManager.pagination.pageSize);
@@ -209,7 +222,84 @@ export function ProfessionalsDataTable({ search, reload, setErrorMessage }: Data
   // #endregion
   return (
     <>
-      {/* <div>
+      {isLoading ? (
+        <LoadingDB text={APP_CONFIG.loadingDB.findProfesionals} className='mt-3' />
+      ) : table.getRowModel().rows?.length > 0 ? (
+        <>
+          <div className='flex items-center justify-end text-sm font-medium text-slate-400'>{`${totalItems} ${PROF_CONFIG.dbProfessionals}`}</div>
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => (
+                    <TableHead key={header.id} style={{ width: `${header.getSize()}px` }}>
+                      {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                    </TableHead>
+                  ))}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows.map((row) => (
+                <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell className='px-0 py-1' key={cell.id} style={{ width: `${cell.column.getSize()}px` }}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <div className='flex items-center justify-between space-x-6 pt-6 lg:space-x-8'>
+            <div className='flex items-center space-x-2'>
+              <p className='text-xs font-normal text-slate-400'>{PROF_CONFIG.table.rowsPerPage}</p>
+              <Select value={`${table.getState().pagination.pageSize}`} onValueChange={(e) => setPagination({ pageIndex: 0, pageSize: parseInt(e) })}>
+                <SelectTrigger className='h-8 w-[65px] text-xs font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring/40 focus-visible:ring-offset-0'>
+                  <SelectValue placeholder={table.getState().pagination.pageSize} />
+                </SelectTrigger>
+                <SelectContent side='top' className='min-w-[4rem]'>
+                  {PROF_CONFIG.table.itemsPerPage.map((pageSize) => (
+                    <SelectItem key={pageSize} value={`${pageSize}`} className='text-xs'>
+                      {pageSize}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+            <div className='flex w-[100px] items-center justify-center text-xs font-normal text-slate-400'>
+              {PROF_CONFIG.table.pagination.page} {pagination.pageIndex + 1} {PROF_CONFIG.table.pagination.of} {table.getPageCount()}
+            </div>
+            {table.getPageCount() > 1 && (
+              <div className='flex items-center space-x-2'>
+                <Button variant='ghost' className='h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 lg:flex dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.setPageIndex(0)} disabled={!table.getCanPreviousPage()}>
+                  <span className='sr-only'>Go to first page</span>
+                  <ArrowLeftIcon className='h-4 w-4' />
+                </Button>
+                <Button variant='ghost' className='h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+                  <span className='sr-only'>Go to previous page</span>
+                  <ChevronLeftIcon className='h-4 w-4' />
+                </Button>
+                <Button variant='ghost' className='h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+                  <span className='sr-only'>Go to next page</span>
+                  <ChevronRightIcon className='h-4 w-4' />
+                </Button>
+                <Button variant='ghost' className='h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 lg:flex dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.setPageIndex(table.getPageCount() - 1)} disabled={!table.getCanNextPage()}>
+                  <span className='sr-only'>Go to last page</span>
+                  <ArrowRightIcon className='h-4 w-4' />
+                </Button>
+              </div>
+            )}
+          </div>
+        </>
+      ) : (
+        <InfoCard text={infoCard.text} type={infoCard.type} className='mt-3' />
+      )}
+    </>
+  );
+}
+{
+  /* <div>
     <section>
       <h1>useMediaQuery</h1>
       Resize your browser windows to see changes.
@@ -232,80 +322,5 @@ export function ProfessionalsDataTable({ search, reload, setErrorMessage }: Data
         </figure>
       </article>
     </section>
-    </div> */}
-
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <TableHead key={header.id} style={{ width: `${header.getSize()}px` }}>
-                  {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                </TableHead>
-              ))}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell className='px-0 py-1' key={cell.id} style={{ width: `${cell.column.getSize()}px` }}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className='h-24 text-center'>
-                {PROF_CONFIG.table.noResults}
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-      <div className='flex items-center justify-between space-x-6 pt-6 lg:space-x-8'>
-        <div className='flex items-center space-x-2'>
-          <p className='text-xs font-normal text-slate-400'>{PROF_CONFIG.table.rowsPerPage}</p>
-          <Select value={`${table.getState().pagination.pageSize}`} onValueChange={(e) => setPagination({ pageIndex: 0, pageSize: parseInt(e) })}>
-            <SelectTrigger className='h-8 w-[65px] text-xs font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring/40 focus-visible:ring-offset-0'>
-              <SelectValue placeholder={table.getState().pagination.pageSize} />
-            </SelectTrigger>
-            <SelectContent side='top' className='min-w-[4rem]'>
-              {PROF_CONFIG.table.itemsPerPage.map((pageSize) => (
-                <SelectItem key={pageSize} value={`${pageSize}`} className='text-xs'>
-                  {pageSize}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-        </div>
-        <div className='flex w-[100px] items-center justify-center text-xs font-normal text-slate-400'>
-          {PROF_CONFIG.table.pagination.page} {pagination.pageIndex + 1} {PROF_CONFIG.table.pagination.of} {table.getPageCount()}
-        </div>
-        {table.getPageCount() > 1 && (
-          <div className='flex items-center space-x-2'>
-            <Button variant='ghost' className='h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 lg:flex dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.setPageIndex(0)} disabled={!table.getCanPreviousPage()}>
-              <span className='sr-only'>Go to first page</span>
-              <ArrowLeftIcon className='h-4 w-4' />
-            </Button>
-            <Button variant='ghost' className='h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
-              <span className='sr-only'>Go to previous page</span>
-              <ChevronLeftIcon className='h-4 w-4' />
-            </Button>
-            <Button variant='ghost' className='h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
-              <span className='sr-only'>Go to next page</span>
-              <ChevronRightIcon className='h-4 w-4' />
-            </Button>
-            <Button variant='ghost' className='h-8 w-8 bg-slate-200/50 p-0 hover:bg-slate-200 lg:flex dark:bg-neutral-950 dark:hover:bg-neutral-800' onClick={() => table.setPageIndex(table.getPageCount() - 1)} disabled={!table.getCanNextPage()}>
-              <span className='sr-only'>Go to last page</span>
-              <ArrowRightIcon className='h-4 w-4' />
-            </Button>
-          </div>
-        )}
-      </div>
-    </>
-  );
+    </div> */
 }
