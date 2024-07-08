7a0df2af2520cb7f9b2c1b3c63681b121bbf3abd
alanloffler <alanmatiasloffler@gmail.com>
2024-07-08 17:19:41 -0300

    New config file for whatsapp page

---
+++ src/config/whatsapp.config.ts
@@ -0,0 +1,14 @@
+export const WHATSAPP_CONFIG = {
+  breadcrumb: [
+    { id: 1, name: 'Inicio', path: '/' },
+    { id: 2, name: 'WhatsApp', path: '/whatsapp' },
+  ],
+  button: {
+    back: 'Volver',
+    sendMessage: 'Enviar mensaje',
+  },
+  label: {
+    phone: 'Teléfono',
+  },
+  title: 'Enviar mensaje de WhatsApp',
+};
