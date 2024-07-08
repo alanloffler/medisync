09b551403a4eab0d91d14756e1e9495ec15619d1
alanloffler <alanmatiasloffler@gmail.com>
2024-07-08 10:37:45 -0300

    New interface for options as object. LoaderDB as placeholder when is loading data from database

--- src/pages/professionals/components/ProfessionalsCombobox.tsx
+++ src/pages/professionals/components/ProfessionalsCombobox.tsx
@@ -4,36 +4,45 @@ import { Check, ChevronsUpDown } from 'lucide-react';
 import { Button } from '@/core/components/ui/button';
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/core/components/ui/command';
 import { Popover, PopoverContent, PopoverTrigger } from '@/core/components/ui/popover';
+// App components
+import { LoadingDB } from '@/core/components/common/LoadingDB';
 // App
-import { IProfessional } from '../interfaces/professional.interface';
-import { ProfessionalApiService } from '../services/professional-api.service';
+import { IProfessional } from '@/pages/professionals/interfaces/professional.interface';
+import { ProfessionalApiService } from '@/pages/professionals/services/professional-api.service';
 import { cn } from '@/lib/utils';
 import { useCapitalize } from '@/core/hooks/useCapitalize';
 import { useEffect, useState } from 'react';
-// React component
-export function ProfessionalsCombobox(
-  { 
-    onSelectProfessional,
-    notFoundText, 
-    placeholder, 
-    searchText 
-  }: { 
-    onSelectProfessional: (professional: IProfessional) => void; 
+// Interface
+interface IProfessionalsCombobox {
+  onSelectProfessional: (professional: IProfessional) => void;
+  options: {
+    loadingText: string;
     notFoundText: string;
-    placeholder: string; 
-    searchText: string 
-  }
-) {
-  const [professionals, setProfessionals] = useState<IProfessional[]>([]);
+    placeholder: string;
+    searchText: string;
+  };
+}
+// React component
+export function ProfessionalsCombobox({ onSelectProfessional, options }: IProfessionalsCombobox) {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [openCombobox, setOpenCombobox] = useState<boolean>(false);
+  const [professionals, setProfessionals] = useState<IProfessional[]>([] as IProfessional[]);
   const [value, setValue] = useState<string>('');
   const capitalize = useCapitalize();
+  const { loadingText, notFoundText, placeholder, searchText } = options;
 
   useEffect(() => {
-    ProfessionalApiService.findAllActive().then((response) => {
+    setIsLoading(true);
+    // prettier-ignore
+    ProfessionalApiService
+    .findAllActive()
+    .then((response) => {
+      // TODO: handle errors!
+      // TODO: change response from database IResponse
       if (!response.statusCode) {
         setProfessionals(response);
       }
+      setIsLoading(true);
     });
   }, []);
 
@@ -41,7 +50,7 @@ export function ProfessionalsCombobox(
     <Popover open={openCombobox} onOpenChange={setOpenCombobox}>
       <PopoverTrigger asChild>
         <Button role='combobox' aria-expanded={openCombobox} className='w-full justify-between bg-white text-foreground shadow-sm hover:bg-white'>
-          {value ? capitalize(value) : placeholder}
+          {isLoading ? <LoadingDB text={loadingText} /> : value ? capitalize(value) : placeholder}
           <ChevronsUpDown className='ml-2 h-4 w-4 shrink-0 opacity-50' />
         </Button>
       </PopoverTrigger>
@@ -61,7 +70,7 @@ export function ProfessionalsCombobox(
                     onSelectProfessional(professional);
                   }}
                 >
-                  <Check className={cn('mr-2 h-4 w-4', value === `${professional.titleAbbreviation} ${[professional.lastName, professional.firstName].join(', ')}` ? 'opacity-100' : 'opacity-0')} />
+                  <Check className={cn('mr-2 h-4 w-4', value === `${professional.titleAbbreviation} ${[professional.lastName, professional.firstName].join(',')}` ? 'opacity-100' : 'opacity-0')} />
                   {`${capitalize(professional.titleAbbreviation)} ${capitalize(professional.lastName)}, ${capitalize(professional.firstName)}`}
                 </CommandItem>
               ))}
