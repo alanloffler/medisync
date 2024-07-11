307642adfff2a42aad0b99f3983715858de976a3
alanloffler <alanmatiasloffler@gmail.com>
2024-07-11 09:49:29 -0300

    Added form buttons actions

--- src/pages/whatsapp/WhatsApp.tsx
+++ src/pages/whatsapp/WhatsApp.tsx
@@ -1,42 +1,45 @@
-import { WHATSAPP_CONFIG } from '@/config/whatsapp.config';
-import { PageHeader } from '@/core/components/common/PageHeader';
+// Icons: https://lucide.dev/icons
+import { ArrowLeft, FilePen } from 'lucide-react';
+// Components: https://ui.shadcn.com/docs/components
 import { Button } from '@/core/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/core/components/ui/card';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/core/components/ui/form';
 import { Input } from '@/core/components/ui/input';
 import { Textarea } from '@/core/components/ui/textarea';
-import { IResponse } from '@/core/interfaces/response.interface';
+// App components
+import { PageHeader } from '@/core/components/common/PageHeader';
+// App
 import { IProfessional } from '@/pages/professionals/interfaces/professional.interface';
+import { IResponse } from '@/core/interfaces/response.interface';
 import { UserApiService } from '@/pages/users/services/user-api.service';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { ArrowLeft, FilePen } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import { WHATSAPP_CONFIG } from '@/config/whatsapp.config';
+import { useCapitalize } from '@/core/hooks/useCapitalize';
+import { MouseEvent, useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate, useParams } from 'react-router-dom';
 import { z } from 'zod';
-
+import { zodResolver } from '@hookform/resolvers/zod';
+// React component
 export default function WhatsApp() {
   const [professional, setProfessional] = useState<IProfessional>({} as IProfessional);
+  const capitalize = useCapitalize();
   const navigate = useNavigate();
-  const { id } = useParams();
-
+  const params = useParams();
+  const { id } = params;
+  
   useEffect(() => {
+
+    console.log(params);
+
     if (id) {
-      UserApiService
-      .findOne(id)
-      .then((response: IResponse) => {
+      UserApiService.findOne(id).then((response: IResponse) => {
         setProfessional(response.data);
         console.log(response);
         whatsappForm.setValue('phone', response.data.phone);
       });
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
-
-  function sendMessage(e: any) {
-    e.preventDefault();
-    console.log(e.target[0].value, e.target[1].value);
-  }
   // #region Form
   const whatsappSchema = z.object({
     phone: z.coerce.number(),
@@ -49,6 +52,17 @@ export default function WhatsApp() {
     },
     resolver: zodResolver(whatsappSchema),
   });
+  // #endregion
+  // #region Buttons actions
+  function sendMessage(e: z.infer<typeof whatsappSchema>) {
+    console.log(e);
+    //console.log(e.target[0].value, e.target[1].value);
+  }
+
+  function handleCancel(event: MouseEvent<HTMLButtonElement>): void {
+    event.preventDefault();
+    navigate(-1);
+  }
   // #endregion
 
   return (
@@ -73,8 +87,12 @@ export default function WhatsApp() {
             </CardTitle>
           </CardHeader>
           <CardContent>
+            <div className='mt-1 text-base'>
+              {WHATSAPP_CONFIG.subtitle}
+              <span className='font-bold'>{` ${capitalize(professional.firstName)} ${capitalize(professional.lastName)}`}</span>.
+            </div>
             <Form {...whatsappForm}>
-              <form onSubmit={(e) => sendMessage(e)} className='mt-4 flex flex-col gap-4'>
+              <form onSubmit={whatsappForm.handleSubmit(sendMessage)} className='mt-4 flex flex-col gap-4'>
                 <FormField
                   control={whatsappForm.control}
                   name='phone'
@@ -88,11 +106,27 @@ export default function WhatsApp() {
                     </FormItem>
                   )}
                 />
-
-                <Textarea />
-                <Button type='submit' variant={'default'}>
-                  {WHATSAPP_CONFIG.button.sendMessage}
-                </Button>
+                <FormField
+                  control={whatsappForm.control}
+                  name='message'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>{WHATSAPP_CONFIG.label.message}</FormLabel>
+                      <FormControl>
+                        <Textarea {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className='grid grid-cols-1 space-y-2 pt-2 md:flex md:justify-end md:gap-6 md:space-y-0'>
+                  <Button type='submit' variant={'default'} className='order-1 md:order-2 lg:order-2'>
+                    {WHATSAPP_CONFIG.button.sendMessage}
+                  </Button>
+                  <Button variant={'ghost'} onClick={handleCancel} className='order-2 md:order-1 lg:order-1'>
+                    {WHATSAPP_CONFIG.button.cancel}
+                  </Button>
+                </div>
               </form>
             </Form>
           </CardContent>
