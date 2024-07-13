c0e8daa78e6158e08ace3c56c5eb2d59d83ca063
alanloffler <alanmatiasloffler@gmail.com>
2024-07-13 19:39:09 -0300

    Receive working days from database

--- src/pages/professionals/components/BusinessDays.tsx
+++ src/pages/professionals/components/BusinessDays.tsx
@@ -2,15 +2,16 @@
 import { Checkbox } from '@/core/components/ui/checkbox';
 import { Label } from '@/core/components/ui/label';
 // React component
-export function BusinessDays({label}: {label: string}) {
-  const DAYS = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado'];
+export function BusinessDays({label, workingDays}: {label: string; workingDays: number[]}) {
+  const DAYS = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+  // console.log('workingDays inside component', workingDays);
   return (
     <div className='flex flex-col w-full space-y-3'>
       <Label className=''>{label}</Label>
-      <div className='flex flex-row w-full items-center justify-start gap-2 bg-green-100'>
+      <div className='flex flex-row w-full items-center justify-start gap-2'>
         {DAYS.map((day) => (
           <div key={day} className='flex flex-col items-center'>
-            <Checkbox />
+            <Checkbox checked={workingDays.includes(DAYS.indexOf(day))} />
             <Label>{day}</Label>
           </div>
         ))}
