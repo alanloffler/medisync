e871b241890ccfb2a2616c1812a49a7d7e916052
alanloffler <alanmatiasloffler@gmail.com>
2024-07-13 21:16:04 -0300

    New updated values wip

--- src/pages/professionals/components/BusinessDays.tsx
+++ src/pages/professionals/components/BusinessDays.tsx
@@ -1,45 +1,54 @@
-// Components: https://ui.shadcn.com/docs/components
 import { Checkbox } from '@/core/components/ui/checkbox';
 import { Label } from '@/core/components/ui/label';
-// React component
-export function BusinessDays({label, workingDays}: {label: string; workingDays: number[]}) {
+
+// Definir el tipo para los días de trabajo
+interface WorkingDay {
+  day: number;
+  value: boolean;
+}
+
+interface BusinessDaysProps {
+  label: string;
+  bdValues: WorkingDay[];
+  setBdValues: (values: WorkingDay[]) => void;
+  // onValuesChange: (values: WorkingDay[]) => void; // Nueva prop para exponer valores al componente padre
+}
+
+export function BusinessDays({ label, bdValues, setBdValues }: BusinessDaysProps) {
   const DAYS = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
-  // console.log('workingDays inside component', workingDays);
+
+  function handleCheckedChange(dayIndex: number, checked: boolean) {
+    console.log('dayIndex', dayIndex, 'checked', checked);// working
+
+    // const updatedValues = bdValues.map((value) => 
+    //   value.day === dayIndex ? { ...value, value: checked } : value
+    // );
+
+    const updatedValues = bdValues.find((value) => value.day === dayIndex);
+    if (updatedValues) {
+      updatedValues.value = checked;
+      setBdValues([...bdValues]); // Actualizar los valores en el estado con la copia updatedValues);
+    }
+
+    console.log('updatesValues', updatedValues);
+    // console.log(bdValues);
+  }
+
   return (
-    <div className='flex flex-col w-full space-y-3'>
+    <div className='flex w-full flex-col space-y-3'>
       <Label className=''>{label}</Label>
-      <div className='flex flex-row w-full items-center justify-start gap-2'>
-        {DAYS.map((day) => (
-          <div key={day} className='flex flex-col items-center'>
-            <Checkbox checked={workingDays.includes(DAYS.indexOf(day))} />
-            <Label>{day}</Label>
+      <div className='flex w-full flex-row items-center justify-start gap-2'>
+        {bdValues.map((day, index) => (
+          <div key={index} className='flex flex-col items-center'>
+            <Checkbox
+              
+              defaultChecked={bdValues.find((value) => value.day === index)?.value || false}
+              onCheckedChange={(checked) => handleCheckedChange(index, checked as boolean)}
+            />
+            <Label>{DAYS[index]}</Label>
           </div>
         ))}
       </div>
-      {/* <div>
-        <Checkbox />
-        <Label>Lunes</Label>
-      </div>
-      <div>
-        <Checkbox />
-        <Label>Martes</Label>
-      </div>
-      <div>
-        <Checkbox />
-        <Label>Miércoles</Label>
-      </div>
-      <div>
-        <Checkbox />
-        <Label>Jueves</Label>
-      </div>
-      <div>
-        <Checkbox />
-        <Label>Viernes</Label>
-      </div>
-      <div>
-        <Checkbox />
-        <Label>Sábado</Label>
-      </div> */}
     </div>
   );
 }
