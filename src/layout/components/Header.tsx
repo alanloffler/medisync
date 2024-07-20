19e64eda1a52b061df25097eedabc48ff8f8742e
alanloffler <alanmatiasloffler@gmail.com>
2024-07-20 16:45:10 -0300

    Settings link

--- src/layout/components/Header.tsx
+++ src/layout/components/Header.tsx
@@ -1,14 +1,19 @@
+// Icons: https://lucide.dev/icons/
+import { Settings } from 'lucide-react';
+// App components
+import { HeaderMenu } from '@/layout/components/HeaderMenu';
+import { User } from '@/layout/components/User';
 // App
-import { HeaderMenu } from './HeaderMenu';
-// import { HeaderSearch } from './HeaderSearch';
-import { User } from './User';
+import { Link } from 'react-router-dom';
 // React component
 export function Header() {
   return (
     <header className='sticky top-0 z-50 flex h-16 items-center gap-4 shadow-sm bg-background px-4 md:px-6'>
       <HeaderMenu />
       <div className='flex w-full items-center justify-end gap-4 md:ml-auto md:gap-2 lg:gap-4'>
-        {/* <HeaderSearch /> */}
+        <Link to='/settings'>
+          <Settings className='h-5 w-5' strokeWidth={2} />
+        </Link>
         <User />
       </div>
     </header>
