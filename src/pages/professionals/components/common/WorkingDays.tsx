db269477caed07fb317f65ab2f9d39bcfb4f13db
alanloffler <alanmatiasloffler@gmail.com>
2024-07-29 12:19:04 -0300

    Modified for undefined or empty data

--- src/pages/professionals/components/common/WorkingDays.tsx
+++ src/pages/professionals/components/common/WorkingDays.tsx
@@ -3,16 +3,32 @@ import { Checkbox } from '@/core/components/ui/checkbox';
 import { Label } from '@/core/components/ui/label';
 // App
 import { APP_CONFIG } from '@/config/app.config';
-import { IWorkingDaysProps } from '@/pages/professionals/interfaces/working-days.interface';
+import { IWorkingDay, IWorkingDaysProps } from '@/pages/professionals/interfaces/working-days.interface';
+import { useEffect, useState } from 'react';
 // React component
 export function WorkingDays({ label, data, handleWorkingDaysValues }: IWorkingDaysProps) {
   const DAYS = APP_CONFIG.daysofWeek.long;
+  const [daysData, setDaysData] = useState<IWorkingDay[]>([]);
+
+  useEffect(() => {
+    // TODO: get this value from database
+    const defaultWorkingDaysValues: IWorkingDay[] = [
+      { day: 0, value: false },
+      { day: 1, value: false },
+      { day: 2, value: false },
+      { day: 3, value: false },
+      { day: 4, value: false },
+      { day: 5, value: false },
+    ];
+    !data || data === undefined || data.length === 0 ? setDaysData(defaultWorkingDaysValues) : setDaysData(data);
+  }, [data]);
 
   function handleCheckedChange(dayIndex: number, checked: boolean) {
-    const updatedValues = data.find((value) => value.day === dayIndex);
+    if (!daysData) return;
+    const updatedValues = daysData.find((value) => value.day === dayIndex);
     if (updatedValues) {
       updatedValues.value = checked;
-      handleWorkingDaysValues([...data]);
+      handleWorkingDaysValues([...daysData]);
     }
   }
 
@@ -20,15 +36,17 @@ export function WorkingDays({ label, data, handleWorkingDaysValues }: IWorkingDa
     <div className='flex w-full flex-col space-y-3'>
       <Label>{label}</Label>
       <div className='flex flex-row justify-start space-x-3'>
-        {data.map((_, index) => (
-          <div key={index} className='flex flex-col items-center'>
-            <Checkbox 
-              defaultChecked={data.find((value) => value.day === index)?.value || false} 
-              onCheckedChange={(checked) => handleCheckedChange(index, checked as boolean)} 
-            />
-            <span className='text-xs font-medium'>{DAYS[index]}</span>
-          </div>
-        ))}
+        {daysData &&
+          daysData.map((_, index) => (
+            <div key={index} className='flex flex-col items-center'>
+              {/* prettier-ignore */}
+              <Checkbox 
+                defaultChecked={daysData.find((value) => value.day === index)?.value || false} 
+                onCheckedChange={(checked) => handleCheckedChange(index, checked as boolean)} 
+              />
+              <span className='text-xs font-medium'>{DAYS[index]}</span>
+            </div>
+          ))}
       </div>
     </div>
   );
