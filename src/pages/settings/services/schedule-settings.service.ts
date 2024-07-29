b2c06424dc5480d7d3b3a34f4071478370dfbe52
alanloffler <alanmatiasloffler@gmail.com>
2024-07-29 20:00:18 -0300

    Name updated

---
+++ src/pages/settings/services/schedule-settings.service.ts
@@ -0,0 +1,41 @@
+import { IWorkingDay } from '@/pages/professionals/interfaces/working-days.interface';
+
+// TODO: get this data from database
+
+export interface IProfTitle {
+  id: string;
+  abbreviation: string;
+  title: string;
+}
+
+export class ScheduleService {
+  // private static readonly API_URL: string = import.meta.env.VITE_REACT_BACKEND_API;
+
+  public static async findAllProfessionalTitles(): Promise<IProfTitle[]> {
+    return [
+      { id: '1', abbreviation: 'Bioq.', title: 'Bioquímica' },
+      { id: '2', abbreviation: 'Bioq.', title: 'Bioquímico' },
+      { id: '3', abbreviation: 'Dr.', title: 'Doctor' },
+      { id: '4', abbreviation: 'Dra.', title: 'Doctora' },
+      { id: '5', abbreviation: 'Lic.', title: 'Licenciada' },
+      { id: '6', abbreviation: 'Lic.', title: 'Licenciado' },
+      { id: '7', abbreviation: 'Téc.', title: 'Técnica' },
+      { id: '8', abbreviation: 'Téc.', title: 'Técnico' },
+    ];
+  }
+
+  public static async findAllSlotDurations(): Promise<number[]> {
+    return [15, 30, 45, 60];
+  }
+
+  public static async findAllWorkingDays(): Promise<IWorkingDay[]> {
+    return [
+      { day: 0, value: false },
+      { day: 1, value: false },
+      { day: 2, value: false },
+      { day: 3, value: false },
+      { day: 4, value: false },
+      { day: 5, value: false },
+    ];
+  }
+}
