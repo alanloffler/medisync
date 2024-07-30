ad623be128e65c71012a435f6b1750e32ea57e95
alanloffler <alanmatiasloffler@gmail.com>
2024-07-30 20:31:18 -0300

    New service for professional titles

---
+++ src/core/services/title.service.ts
@@ -0,0 +1,18 @@
+export class TitleService {
+  private static readonly API_URL = import.meta.env.VITE_API_URL;
+
+  public static async findAll() {
+    try {
+      const url: string = `${this.API_URL}/titles`;
+      const query: Response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+        },
+      });
+      return await query.json();
+    } catch (error) {
+      return error;
+    }
+  }
+}
