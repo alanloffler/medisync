3669279874ae64853094c54d163075ff4a9ae2cd
alanloffler <alanmatiasloffler@gmail.com>
2024-08-22 18:26:01 -0300

    Unavailable time slot validation

--- src/pages/professionals/schemas/professional.schema.ts
+++ src/pages/professionals/schemas/professional.schema.ts
@@ -6,9 +6,10 @@ const workingDaySchema = z.object({
   value: z.boolean(),
 });
 
+let slotDuration: string;
 let timeInit: string;
 let timeEnd: string;
-let slotDuration: string;
+let timeSlotUnavailableInit: string | undefined;
 
 export const professionalSchema = z.object({
   available: z.boolean({ message: PROF_SCHEMA.availableMessage }),
@@ -71,66 +72,73 @@ export const professionalSchema = z.object({
           });
         }
       }),
-    timeSlotUnavailableInit: z
-      .string()
-      .optional()
+    // Unavailable time slot
+    unavailableTimeSlot: z
+      .object({
+        timeSlotUnavailableInit: z
+          .string()
+          .optional()
+          .superRefine((data, ctx) => {
+            if (data) {
+              data === '--:--' ? (timeSlotUnavailableInit = undefined) : (timeSlotUnavailableInit = data);
+              const [hour, minutes] = data.split(':');
+              // Hour format validation (00-23)
+              if (parseInt(hour) < 0 || parseInt(hour) > 23 || hasHyphen(hour)) {
+                ctx.addIssue({
+                  code: z.ZodIssueCode.custom,
+                  message: PROF_SCHEMA.configuration.common.hourRange,
+                });
+              }
+              // Minutes format validation (00-59)
+              if (parseInt(minutes) < 0 || parseInt(minutes) > 59 || hasHyphen(minutes)) {
+                ctx.addIssue({
+                  code: z.ZodIssueCode.custom,
+                  message: PROF_SCHEMA.configuration.common.minutesRange,
+                });
+              }
+              // Slot time greater than schedule time init plus one slot duration
+              if (timeToMinutes(data) < timeToMinutes(timeInit, slotDuration)) {
+                ctx.addIssue({
+                  code: z.ZodIssueCode.custom,
+                  message: PROF_SCHEMA.configuration.unavailableTimeSlot.timeInit.greaterThanTimeInit,
+                });
+              }
+              // Slot time less than schedule time end minus one slot duration
+              if (timeToMinutes(data) > timeToMinutesLess(timeEnd, slotDuration)) {
+                ctx.addIssue({
+                  code: z.ZodIssueCode.custom,
+                  message: PROF_SCHEMA.configuration.unavailableTimeSlot.timeInit.lessThanTimeEnd,
+                });
+              }
+            }
+          }),
+        timeSlotUnavailableEnd: z
+          .string()
+          .optional()
+          .superRefine((data, ctx) => {
+            if (data) {
+              data === '--:--' ? (timeSlotUnavailableInit = undefined) : (timeSlotUnavailableInit = data);
+              // Required if unavailable time init has a valid value
+              if (timeSlotUnavailableInit !== undefined && !data) {
+                ctx.addIssue({
+                  code: z.ZodIssueCode.custom,
+                  message: PROF_SCHEMA.configuration.unavailableTimeSlot.timeEnd.required,
+                });
+              }
+            }
+          }),
+      })
       .superRefine((data, ctx) => {
-        if (data) {
-          const [hour, minutes] = data.split(':');
-          // Hour format validation (00-23)
-          if (parseInt(hour) < 0 || parseInt(hour) > 23 || hasHyphen(hour)) {
-            ctx.addIssue({
-              code: z.ZodIssueCode.custom,
-              message: PROF_SCHEMA.configuration.common.hourRange,
-            });
-          }
-          // Minutes format validation (00-59)
-          if (parseInt(minutes) < 0 || parseInt(minutes) > 59 || hasHyphen(minutes)) {
-            ctx.addIssue({
-              code: z.ZodIssueCode.custom,
-              message: PROF_SCHEMA.configuration.common.minutesRange,
-            });
-          }
-          // Slot time greater than schedule time init plus one slot duration
-          if (timeToMinutes(data) < timeToMinutes(timeInit, slotDuration)) {
-            ctx.addIssue({
-              code: z.ZodIssueCode.custom,
-              // message: PROF_SCHEMA.unavailableTimeInit.formatError,
-              message: PROF_SCHEMA.configuration.unavailableTimeInit.greaterThanTimeInit,
-            });
-          }
-          // Slot time less than schedule time end minus one slot duration
-          if (timeToMinutes(data) > timeToMinutesLess(timeEnd, slotDuration)) {
-            ctx.addIssue({
-              code: z.ZodIssueCode.custom,
-              message: PROF_SCHEMA.configuration.unavailableTimeInit.lessThanTimeEnd,
-            });
-          }
-        }
-      }),
-    timeSlotUnavailableEnd: z
-      .string()
-      .min(1, { message: PROF_SCHEMA.timeSlotUnavailableEndMessage })
-      .superRefine((data, ctx) => {
-        const [hour, minutes] = data.split(':');
-
-        // console.log('--- timeEnd', timeToMinutes(timeEnd, slotDuration));
-        // console.log('timeEnd less 1 slot', timeToMinutesLess(timeEnd, slotDuration));
-        // console.log('unavailableEnd ---', timeToMinutes(data, slotDuration));
-
-        if (parseInt(hour) < 0 || parseInt(hour) > 23 || hasHyphen(hour)) {
+        // If unavailable time end has a value, then unavailable time init is required
+        if (timeSlotUnavailableInit === undefined && data) {
           ctx.addIssue({
+            path: ['timeSlotUnavailableInit'],
             code: z.ZodIssueCode.custom,
-            message: PROF_SCHEMA.configuration.common.hourRange,
+            message: 'Debes ingresar una hora de inicio de descanso!',
           });
         }
-        if (parseInt(minutes) < 0 || parseInt(minutes) > 59 || hasHyphen(minutes)) {
-          ctx.addIssue({
-            code: z.ZodIssueCode.custom,
-            message: PROF_SCHEMA.configuration.common.minutesRange,
-          });
-        }
       }),
+
     workingDays: z.array(workingDaySchema).refine(
       (days) => {
         return days.some((day) => day.value === true);
