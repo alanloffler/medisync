1394da0e4e60b9816737164c76875286f70e8909
alanloffler <alanmatiasloffler@gmail.com>
2024-08-27 21:11:05 -0300

    Breadcrumb and card title texts

--- src/pages/appointments/config/appointment.config.ts
+++ src/pages/appointments/config/appointment.config.ts
@@ -64,11 +64,20 @@ export const APPO_CONFIG = {
 };
 
 export const VIEW_APPOINTMENT_CONFIG = {
+  breadcrumb: [
+    { id: 1, name: 'Inicio', path: '/' },
+    { id: 2, name: 'Turnos', path: '/' },
+    { id: 3, name: 'Detalle', path: '' }, 
+  ],
+  button: {
+    back: 'Volver',
+  },
+  cardTitle: 'Turno',
   email: {
     body: 'Este es el mensaje de tu turno',
     subject: 'Tu turno para',
   },
-  title: 'Turno',
+  title: 'Detalle de turno',
   words: {
     hoursAbbreviation: 'hs.',
   }
