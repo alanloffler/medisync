dd78e20eb1d658b3d1b940d9007b7b97f4bbbbfe
alanloffler <alanmatiasloffler@gmail.com>
2024-09-20 10:08:28 -0300

    Border and optional className (primary color by default)

--- src/core/components/common/Steps.tsx
+++ src/core/components/common/Steps.tsx
@@ -1,9 +1,16 @@
 import { cn } from '@/lib/utils';
 
-export function Steps({ text, step, className }: { text: string; step: string; className: string }) {
+export function Steps({ text, step, className }: { text: string; step: string; className?: string }) {
   return (
     <div className='flex flex-row items-center space-x-4'>
-      <span className={cn('flex h-8 w-8 flex-col items-center justify-center rounded-full bg-indigo-500 text-xl font-semibold text-indigo-200 shadow-sm', className)}>{step}</span>
+      <span
+        className={cn(
+          'flex h-8 w-8 flex-col items-center justify-center rounded-full bg-primary/20 text-xl font-semibold text-primary shadow-sm border border-primary/10',
+          className,
+        )}
+      >
+        {step}
+      </span>
       <h1 className='font-medium'>{text}</h1>
     </div>
   );
