52ff908d7073b5c6d0d7f1bd651d7e1876d40383
alanloffler <alanmatiasloffler@gmail.com>
2024-09-20 17:25:43 -0300

    Combobox styles and dni delimiter

--- src/pages/users/components/UsersCombo.tsx
+++ src/pages/users/components/UsersCombo.tsx
@@ -8,13 +8,23 @@ import { IUser } from '@/pages/users/interfaces/user.interface';
 import { UserApiService } from '@/pages/users/services/user-api.service';
 import { useCapitalize } from '@/core/hooks/useCapitalize';
 import { useDebounce } from '@/core/hooks/useDebounce';
+import { useDelimiter } from '@/core/hooks/useDelimiter';
 // React component
-export function UsersCombo({ searchBy, searchResult, placeholder }: { searchBy: 'name' | 'dni'; searchResult: (user: IUser) => void, placeholder: string }) {
+export function UsersCombo({
+  searchBy,
+  searchResult,
+  placeholder,
+}: {
+  searchBy: 'name' | 'dni';
+  searchResult: (user: IUser) => void;
+  placeholder: string;
+}) {
   const [search, setSearch] = useState<string>('');
   const [users, setUsers] = useState<IUser[]>([] as IUser[]);
   const DEBOUNCE_TIME = 500;
   const capitalize = useCapitalize();
   const debouncedSearch = useDebounce<string>(search, DEBOUNCE_TIME);
+  const delimiter: (input: string | number, delimiter: string, each: number) => string = useDelimiter();
   const [openCombobox, setOpenCombobox] = useState<boolean>(false);
   const [showNoResults, setShowNoResults] = useState<boolean>(false);
 
@@ -68,19 +78,29 @@ export function UsersCombo({ searchBy, searchResult, placeholder }: { searchBy: 
 
   return (
     <div className='relative w-full'>
-      <div className='flex flex-row items-center space-x-3 w-full'>
+      <div className='flex w-full flex-row items-center space-x-3'>
         <Input type={searchBy === 'name' ? 'text' : 'number'} value={search} onChange={handleSearch} placeholder={placeholder} className='h-9' />
-        {openCombobox && <button onClick={handleCloseCombobox} className='p-2 bg-slate-200 rounded-full'>
-          <X className='h-4 w-4' strokeWidth={2} />
-        </button>}
+        {openCombobox && (
+          <button onClick={handleCloseCombobox} className='rounded-full bg-slate-200 p-2'>
+            <X className='h-4 w-4' strokeWidth={2} />
+          </button>
+        )}
       </div>
       {openCombobox && (
-        <ul className='absolute z-50 w-72 rounded-md bg-popover p-3 text-popover-foreground shadow-md'>
-          {users.length > 0 && users.map((user) => (
-            <li key={user._id} className='relative'>
-              <button type='button' onClick={() => handleSelectedUser(user)} className='w-full text-left font-normal text-sm hover:bg-slate-100 px-1.5 py-0.5 rounded-sm hover:transition-all'>{`${capitalize(user.lastName)}, ${capitalize(user.firstName)} (${user.dni})`}</button>
-            </li>
-          ))}
+        <ul className='absolute z-50 w-72 mt-1 border overflow-y-auto h-40 rounded-md bg-popover p-3 text-popover-foreground shadow-md'>
+          {users.length > 0 &&
+            users.map((user) => (
+              <li key={user._id} className='relative'>
+                <button
+                  type='button'
+                  onClick={() => handleSelectedUser(user)}
+                  className='w-full rounded-sm px-1.5 py-0.5 text-left text-sm font-normal hover:bg-slate-100 hover:transition-all space-x-2'
+                >
+                  <span>{`${capitalize(user.lastName)}, ${capitalize(user.firstName)}`}</span>
+                  <span className='italic text-slate-500'>{`- DNI ${delimiter(user.dni, '.', 3)}`}</span>
+                </button>
+              </li>
+            ))}
           {showNoResults && <>No results</>}
         </ul>
       )}
