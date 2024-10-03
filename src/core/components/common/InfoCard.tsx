b3caa2e45160f18ed04690c91d78e70fed0840d7
alanloffler <alanmatiasloffler@gmail.com>
2024-10-03 19:21:53 -0300

    Code order

--- src/core/components/common/InfoCard.tsx
+++ src/core/components/common/InfoCard.tsx
@@ -8,7 +8,7 @@ export function InfoCard({ text, type, className }: IInfoCard) {
   const strokeColor = type === 'error' ? 'stroke-red-400' : type === 'success' ? 'stroke-green-400' : 'stroke-yellow-400';
 
   return (
-    <div className={cn('flex flex-row items-center space-x-2 p-2 justify-center', className)}>
+    <div className={cn('flex flex-row items-center justify-center space-x-2 p-2', className)}>
       <div className='flex h-full flex-col justify-center'>
         {type === 'error' && <CircleX className={cn('h-5 w-5', strokeColor)} strokeWidth={2} />}
         {type === 'success' && <CircleCheck className={cn('h-5 w-5', strokeColor)} strokeWidth={2} />}
