326a8137ddd916ad57bed7defe591e9b781e1590
alanloffler <alanmatiasloffler@gmail.com>
2024-10-03 19:24:12 -0300

    Code order and comments

--- src/core/components/common/Steps.tsx
+++ src/core/components/common/Steps.tsx
@@ -1,11 +1,12 @@
+// App
 import { cn } from '@/lib/utils';
-
+// React component
 export function Steps({ text, step, className }: { text: string; step: string; className?: string }) {
   return (
     <div className='flex flex-row items-center space-x-4'>
       <span
         className={cn(
-          'flex h-8 w-8 flex-col items-center justify-center rounded-full bg-primary/20 text-xl font-semibold text-primary shadow-sm border border-primary/10',
+          'flex h-8 w-8 flex-col items-center justify-center rounded-full border border-primary/10 bg-primary/20 text-xl font-semibold text-primary shadow-sm',
           className,
         )}
       >
