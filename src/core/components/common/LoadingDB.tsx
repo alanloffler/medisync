bbf5b1f35512f239514ff58602f66ffea7424463
alanloffler <alanmatiasloffler@gmail.com>
2024-10-04 18:01:56 -0300

    Button variant to use when an action is rolling

--- src/core/components/common/LoadingDB.tsx
+++ src/core/components/common/LoadingDB.tsx
@@ -6,6 +6,7 @@ import { cva } from 'class-variance-authority';
 const loadingDBVariants = cva('flex items-center justify-center gap-2 text-sm font-medium text-slate-500', {
   variants: {
     variant: {
+      button: 'mx-auto bg-transparent text-white [&>svg]:fill-white',
       card: 'mx-auto bg-white rounded-lg shadow-sm',
       default: 'mx-auto bg-transparent',
       primary: 'mx-auto bg-primary/25 rounded-lg text-slate-900',
@@ -29,11 +30,11 @@ export function LoadingDB({
 }: {
   text?: string;
   className?: string;
-  variant?: 'card' | 'default' | 'primary';
+  variant?: 'button' | 'card' | 'default' | 'primary';
   size?: 'default' | 'xs';
 }) {
   return (
-    <div className={cn(loadingDBVariants({ variant, size, className }))}>
+    <div className={cn('group', loadingDBVariants({ variant, size, className }))}>
       <svg width={APP_CONFIG.loadingDB.settings.size} height={APP_CONFIG.loadingDB.settings.size} viewBox='0 0 24 24' className='fill-primary'>
         <path d='M12,4a8,8,0,0,1,7.89,6.7A1.53,1.53,0,0,0,21.38,12h0a1.5,1.5,0,0,0,1.48-1.75,11,11,0,0,0-21.72,0A1.5,1.5,0,0,0,2.62,12h0a1.53,1.53,0,0,0,1.49-1.3A8,8,0,0,1,12,4Z'>
           <animateTransform
