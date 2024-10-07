e6d131c6814773e4ace75e0c4fd861ee26d4ccb9
alanloffler <alanmatiasloffler@gmail.com>
2024-10-07 09:50:47 -0300

    Description is not lowercase formatted

--- src/pages/professionals/services/professional.utils.ts
+++ src/pages/professionals/services/professional.utils.ts
@@ -1,14 +1,17 @@
-import { IProfessionalForm } from '../interfaces/professional.interface';
+import { IProfessionalForm } from '@/pages/professionals/interfaces/professional.interface';
 
 export class ProfessionalUtils {
   public static lowercaseFormItems(items: IProfessionalForm): IProfessionalForm {
     const mutableItems = { ...items };
+    const { description } = mutableItems;
 
     for (const [key, value] of Object.entries(mutableItems)) {
       if (typeof value === 'string') {
         mutableItems[key as keyof IProfessionalForm] = value.toLowerCase() as never;
       }
     }
+
+    mutableItems.description = description;
     return mutableItems;
   }
 }
