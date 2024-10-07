1f8968e0c72565939354c1b53390ac76fdb738f0
alanloffler <alanmatiasloffler@gmail.com>
2024-10-07 10:18:46 -0300

    Import type for interface and typed constant

--- src/pages/professionals/utils/week-working-days.util.ts
+++ src/pages/professionals/utils/week-working-days.util.ts
@@ -1,6 +1,6 @@
-import { IWorkingDay } from '@/pages/professionals/interfaces/working-days.interface';
+import type { IWorkingDay } from '@/pages/professionals/interfaces/working-days.interface';
 
-export function generateWeekOfWorkingDays() {
+export function generateWeekOfWorkingDays(): IWorkingDay[] {
   const daysOfWeek: IWorkingDay[] = Array.from({ length: 7 }, (_, index) => ({ day: index, value: false }));
   
   return daysOfWeek;
