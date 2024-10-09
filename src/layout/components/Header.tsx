cc9acc433daa808c9b3cffe041cc11e04737d4da
alanloffler <alanmatiasloffler@gmail.com>
2024-10-09 08:52:07 -0300

    Settings button animation with framer-motion/mini

--- src/layout/components/Header.tsx
+++ src/layout/components/Header.tsx
@@ -6,20 +6,26 @@ import { HeaderMenu } from '@/layout/components/HeaderMenu';
 import { User } from '@/layout/components/User';
 // External imports
 import { Link } from 'react-router-dom';
+import { useAnimate } from 'framer-motion/mini';
 // Imports
-import { HEADER_CONFIG } from '@/config/layout/header.config';
 import type { ILinks } from '@/layout/interfaces/links.interface';
+import { HEADER_CONFIG } from '@/config/layout/header.config';
 // React component
 export function Header() {
   const links: ILinks[] = HEADER_CONFIG.actionsButton;
+  const [settingScope, settingAnimation] = useAnimate();
 
   return (
     <header className='sticky top-0 z-50 flex h-16 items-center gap-4 bg-background px-4 shadow-sm md:px-6'>
       <HeaderMenu />
       <div className='flex w-full items-center justify-end gap-4 md:ml-auto md:gap-2 lg:gap-4'>
         <ActionsButton links={links} />
-        <Link to='/settings'>
-          <Settings className='h-5 w-5' strokeWidth={2} />
+        <Link
+          to='/settings'
+          onMouseOver={() => settingAnimation(settingScope.current, { rotate: '90deg' }, { duration: 0.25, ease: 'easeIn' })}
+          onMouseOut={() => settingAnimation(settingScope.current, { rotate: '-90deg' }, { duration: 0.25, ease: 'easeIn' })}
+        >
+          <Settings ref={settingScope} className='h-5 w-5' strokeWidth={2} />
         </Link>
         <User />
       </div>
