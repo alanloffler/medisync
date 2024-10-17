32db3ecb8fdebb132fa627b382acebec1f96aebf
alanloffler <alanmatiasloffler@gmail.com>
2024-10-17 18:36:27 -0300

    Typed private constant

--- src/pages/appointments/services/appointment.service.ts
+++ src/pages/appointments/services/appointment.service.ts
@@ -1,7 +1,7 @@
-import { IAppointmentForm } from '@/pages/appointments/interfaces/appointment.interface';
+import type { IAppointmentForm } from '@/pages/appointments/interfaces/appointment.interface';
 
 export class AppointmentApiService {
-  private static readonly API_URL = import.meta.env.VITE_API_URL;
+  private static readonly API_URL: string = import.meta.env.VITE_API_URL;
 
   public static async findAllByProfessional(id: string, day: string) {
     const url: string = `${this.API_URL}/appointments/byProfessional?id=${id}&day=${day}`;
