86d277a94d62916df94fe300f5055dd46c3acc87
alanloffler <alanmatiasloffler@gmail.com>
2024-10-17 18:38:12 -0300

    Removed unused method

--- src/pages/settings/services/schedule-settings.service.ts
+++ src/pages/settings/services/schedule-settings.service.ts
@@ -9,19 +9,6 @@ export interface IProfTitle {
 export class ScheduleService {
   // private static readonly API_URL: string = import.meta.env.VITE_REACT_BACKEND_API;
 
-  public static async findAllProfessionalTitles(): Promise<IProfTitle[]> {
-    return [
-      { id: '1', abbreviation: 'Bioq.', title: 'Bioquímica' },
-      { id: '2', abbreviation: 'Bioq.', title: 'Bioquímico' },
-      { id: '3', abbreviation: 'Dr.', title: 'Doctor' },
-      { id: '4', abbreviation: 'Dra.', title: 'Doctora' },
-      { id: '5', abbreviation: 'Lic.', title: 'Licenciada' },
-      { id: '6', abbreviation: 'Lic.', title: 'Licenciado' },
-      { id: '7', abbreviation: 'Téc.', title: 'Técnica' },
-      { id: '8', abbreviation: 'Téc.', title: 'Técnico' },
-    ];
-  }
-
   public static async findAllSlotDurations(): Promise<number[]> {
     return [15, 30, 45, 60];
   }
