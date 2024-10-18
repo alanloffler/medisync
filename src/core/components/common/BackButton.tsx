23fae6af010473a66e8ffec645f0ebc5bb65a603
alanloffler <alanmatiasloffler@gmail.com>
2024-10-18 17:16:46 -0300

    BackButton component for history navigation on pages

---
+++ src/core/components/common/BackButton.tsx
@@ -0,0 +1,35 @@
+// Icons: https://lucide.dev/icons/
+import { ArrowLeft } from 'lucide-react';
+// External components: https://ui.shadcn.com/docs/components
+import { Button } from '@/core/components/ui/button';
+// External imports
+import { spring } from 'framer-motion';
+import { useAnimate } from 'framer-motion/mini';
+import { useNavigate } from 'react-router-dom';
+// React component
+export function BackButton({ label }: { label: string }) {
+  const [backScope, backAnimation] = useAnimate();
+  const navigate = useNavigate();
+
+  function handleMouseOver(): void {
+    backAnimation(backScope.current, { translate: '-3px' }, { duration: 0.7, ease: 'linear', type: spring, bounce: 0.7 });
+  }
+
+  function handleMouseOut(): void {
+    backAnimation(backScope.current, { translate: '0px' }, { duration: 0.7, ease: 'linear', type: spring, bounce: 0.7 });
+  }
+
+  return (
+    <Button
+      variant='outline'
+      size='sm'
+      className='flex flex-row items-center space-x-2 text-xs font-medium uppercase'
+      onClick={() => navigate(-1)}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+    >
+      <ArrowLeft ref={backScope} size={16} strokeWidth={2} />
+      <span>{label}</span>
+    </Button>
+  );
+}
