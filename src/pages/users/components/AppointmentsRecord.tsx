76eca07e37fa3f8da5642ae0cae58e0aeb392efd
alanloffler <alanmatiasloffler@gmail.com>
2024-10-22 20:50:11 -0300

    ProfessionalsSelect implemented with filtered data of unique professionals in all appointments

--- src/pages/users/components/AppointmentsRecord.tsx
+++ src/pages/users/components/AppointmentsRecord.tsx
@@ -1,18 +1,19 @@
 // Icons: https://lucide.dev/icons/
 import { FileText, Trash2 } from 'lucide-react';
-// External imports
-import { useEffect, useState } from 'react';
 // External components: https://ui.shadcn.com/docs/components
 import { Button } from '@/core/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/core/components/ui/card';
 // Components
 import { InfoCard } from '@/core/components/common/InfoCard';
 import { LoadingDB } from '@/core/components/common/LoadingDB';
+import { ProfessionalsSelect } from '@/pages/professionals/components/common/ProfessionalsSelect';
 // External imports
 import { format } from '@formkit/tempo';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // Imports
 import type { IAppointmentView } from '@/pages/appointments/interfaces/appointment.interface';
+import type { IProfessional } from '@/pages/professionals/interfaces/professional.interface';
 import type { IResponse } from '@/core/interfaces/response.interface';
 import { APP_CONFIG } from '@/config/app.config';
 import { AppointmentApiService } from '@/pages/appointments/services/appointment.service';
@@ -24,6 +25,7 @@ export function AppointmentsRecord({ userId, loaderText }: { userId: string; loa
   const [errorMessage, setErrorMessage] = useState<string>('');
   const [isError, setIsError] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [professionals, setProfessionals] = useState<IProfessional[]>([]);
   const capitalize = useCapitalize();
   const navigate = useNavigate();
 
@@ -34,7 +36,17 @@ export function AppointmentsRecord({ userId, loaderText }: { userId: string; loa
       AppointmentApiService.findAllByUser(userId)
         .then((response: IResponse) => {
           if (response.statusCode === 200) {
-            response.data.length > 0 ? setAppointments(response.data) : setErrorMessage(response.message);
+            if (response.data.length > 0) {
+              setAppointments(response.data);
+
+              const professionalsFiltered: IProfessional[] = response.data
+                .map((appointment: IAppointmentView) => appointment.professional)
+                .filter(
+                  (professional: IProfessional, index: number, self: IProfessional[]) => index === self.findIndex((p) => p._id === professional._id),
+                )
+                .sort((a: IProfessional, b: IProfessional) => a.lastName.localeCompare(b.lastName));
+              setProfessionals(professionalsFiltered);
+            } else setErrorMessage(response.message);
           }
           if (response.statusCode > 399) {
             setIsError(true);
@@ -62,8 +74,10 @@ export function AppointmentsRecord({ userId, loaderText }: { userId: string; loa
         <LoadingDB text={loaderText || 'Loading data'} className='pb-4 pt-2' />
       ) : (
         <CardContent className='px-3 pb-3'>
-          <section className='mb-3 bg-primary/10 p-2 text-slate-500'>Here filters and search</section>
-          <section className='flex font-medium text-sm border-b-2 pb-1'>
+          <section className='mb-3 space-x-3 bg-primary/10 p-2 text-slate-500'>
+          <ProfessionalsSelect professionals={professionals} />
+          </section>
+          <section className='flex border-b-2 pb-1 text-sm font-medium'>
             <div className='flex w-3/4 items-center'>
               <div className='w-1/4 text-center'>{USER_VIEW_CONFIG.appointmentRecords.tableHeaders[0]}</div>
               <div className='w-3/4 text-center'>{USER_VIEW_CONFIG.appointmentRecords.tableHeaders[1]}</div>
