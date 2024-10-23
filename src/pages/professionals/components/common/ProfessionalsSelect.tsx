d43b9b12d83d62b0fc6a4fb92045f9dffc019ae7
alanloffler <alanmatiasloffler@gmail.com>
2024-10-22 21:07:35 -0300

    Styles and component properties

--- src/pages/professionals/components/common/ProfessionalsSelect.tsx
+++ src/pages/professionals/components/common/ProfessionalsSelect.tsx
@@ -3,20 +3,25 @@ import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectVa
 // Imports
 import type { IProfessional } from '@/pages/professionals/interfaces/professional.interface';
 import { PROFESSIONALS_SELECT_CONFIG as PS_CONFIG } from '@/config/professionals.config';
+import { cn } from '@/lib/utils';
 import { useCapitalize } from '@/core/hooks/useCapitalize';
 // React component
-export function ProfessionalsSelect({ professionals }: { professionals: IProfessional[] }) {
+export function ProfessionalsSelect({
+  className,
+  professionals,
+  onValueChange,
+}: {
+  className?: string;
+  professionals: IProfessional[];
+  onValueChange?: (e: string) => void;
+}) {
   const capitalize = useCapitalize();
-
-  function handleSelectProfessional(professional: string): void {
-    console.log('Select professional', professional);
-  }
 
   return (
     <main className='flex flex-col items-start space-y-1'>
-      <span className='text-sm font-medium text-slate-500'>{PS_CONFIG.label}</span>
-      <Select onValueChange={(e) => handleSelectProfessional(e)}>
-        <SelectTrigger className='h-8 w-[200px] border bg-white shadow-sm'>
+      <span className='text-[13px] font-medium text-slate-500'>{PS_CONFIG.label}</span>
+      <Select onValueChange={onValueChange}>
+        <SelectTrigger className={cn('h-8 w-full space-x-2 border bg-white text-[13px] shadow-sm', className)}>
           <SelectValue placeholder={PS_CONFIG.placeholder} />
         </SelectTrigger>
         <SelectContent>
