bcde7ef157fd12642acba45653f85f11ccc4a643
alanloffler <alanmatiasloffler@gmail.com>
2024-10-25 10:48:47 -0300

    Unique years api call. Search button and clear selected year button

--- src/pages/users/components/ApposDateSelect.tsx
+++ src/pages/users/components/ApposDateSelect.tsx
@@ -1,79 +1,94 @@
 // Icons: https://lucide.dev/icons/
-import { CalendarIcon } from 'lucide-react';
+import { CalendarIcon, X } from 'lucide-react';
 // External components: https://ui.shadcn.com/docs/components
 import { Button } from '@/core/components/ui/button';
 import { Popover, PopoverContent, PopoverTrigger } from '@/core/components/ui/popover';
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/core/components/ui/select';
 // External imports
-import { format, parse } from '@formkit/tempo';
 import { spring } from 'framer-motion';
 import { useAnimate } from 'framer-motion/mini';
 import { useEffect, useState } from 'react';
 // Imports
-import type { IAppointmentView } from '@/pages/appointments/interfaces/appointment.interface';
-import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/core/components/ui/select';
+import type { IResponse } from '@/core/interfaces/response.interface';
+import { AppointmentApiService } from '@/pages/appointments/services/appointment.service';
 // React component
-export function ApposDateSelect({ appointments, onValueChange }: { appointments: IAppointmentView[]; onValueChange: (e: string) => void }) {
-  const [year, setYear] = useState<string>('');
+export function ApposDateSelect({ userId, onValueChange }: { userId: string; onValueChange: (value: string) => void }) {
   const [calendarScope, calendarAnimation] = useAnimate();
+  const [openPopover, setOpenPopover] = useState<boolean>(false);
+  const [selectedYear, setSelectedYear] = useState<string | undefined>(undefined);
+  const [years, setYears] = useState<string[]>([]);
 
   useEffect(() => {
-    if (appointments.length > 0) {
-      console.log(appointments.length);
-    }
-  }, [appointments.length]);
-
-  const uniqueYears: string[] = appointments
-    .map((appointment: IAppointmentView) => parse(appointment.day).getFullYear().toString())
-    .filter((value, index, array) => {
-      return array.indexOf(value) === index;
+    AppointmentApiService.findAppointmentsYearsByUser(userId).then((response: IResponse) => {
+      console.log(response);
+      setYears(response.data);
     });
+  }, [userId]);
 
-    function clearYear(): void {
-      setYear('');
+  function handleYearChange(year: string | undefined): void {
+    if (year !== undefined) {
+      console.log(year);
+      onValueChange(year);
+      setOpenPopover(false);
+    } else {
+      setSelectedYear(undefined);
       onValueChange('');
+      setOpenPopover(false);
     }
-    // TODO: show year on date picker, see the method to clear year and set placeholder
-    // Also, must style the inputs
-    // FIXME: create a api call which will return the unique years from a db query (now has a bug)
+  }
 
   return (
     <main className='flex flex-row items-center space-x-2'>
       <span className='text-[13px] font-medium text-slate-500'>Fecha</span>
-      <Popover>
+      <Popover open={openPopover} onOpenChange={(e) => setOpenPopover(e)}>
         <PopoverTrigger asChild>
           <Button
-            className='h-8 w-[180px] justify-start space-x-3 border bg-white px-3 text-left text-[13px] font-normal text-foreground shadow-sm hover:bg-white'
+            className='h-8 w-fit justify-start space-x-3 border bg-white px-3 text-left text-[13px] font-normal text-foreground shadow-sm hover:bg-white'
             variant='ghost'
             onMouseOver={() => calendarAnimation(calendarScope.current, { scale: 1.1 }, { duration: 0.7, ease: 'linear', type: spring, bounce: 0.7 })}
             onMouseOut={() => calendarAnimation(calendarScope.current, { scale: 1 }, { duration: 0.7, ease: 'linear', type: spring, bounce: 0.7 })}
           >
             <CalendarIcon ref={calendarScope} size={16} strokeWidth={2} />
-            {year !== '' ? <span>{format(year, 'dd/MM/yyyy')}</span> : <span>Seleccionar</span>}
+            {selectedYear !== undefined ? <span>{selectedYear}</span> : <span>Seleccionar</span>}
           </Button>
         </PopoverTrigger>
         <PopoverContent className='w-auto p-0'>
           <section className='flex w-[180px] flex-col space-y-2 p-3'>
-            <section className='flex flex-row items-center space-x-2'>
-              <span className='text-[13px] font-medium text-slate-500'>Año</span>
-              <Select onValueChange={onValueChange}>
-                <SelectTrigger className={'h-8 w-full space-x-2 border bg-white text-[13px] shadow-sm'}>
-                  <SelectValue />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    {uniqueYears.map((year) => (
-                      <SelectItem key={crypto.randomUUID()} value={year}>
-                        {year}
-                      </SelectItem>
-                    ))}
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-              <Button onClick={clearYear}>X</Button>
+            <section className='flex flex-col space-y-3'>
+              <div className='flex flex-row place-content-start items-center space-x-2'>
+                <span className='text-[13px] font-medium text-slate-500'>Año</span>
+                <Select value={selectedYear} onValueChange={(e) => setSelectedYear(e)}>
+                  <SelectTrigger className={'h-6 w-fit space-x-2 border bg-white text-xs shadow-sm'}>
+                    <SelectValue placeholder='Año' />
+                  </SelectTrigger>
+                  <SelectContent className='w-fit min-w-10'>
+                    <SelectGroup>
+                      {years.map((year) => (
+                        <SelectItem key={crypto.randomUUID()} value={year} className='w-fit py-1 text-xs [&>span>span>svg]:h-3 [&>span>span>svg]:w-3'>
+                          {year}
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+              <Button variant='default' size='xs' onClick={() => handleYearChange(selectedYear)}>
+                Buscar
+              </Button>
             </section>
           </section>
         </PopoverContent>
       </Popover>
+      {selectedYear !== undefined && (
+        <Button
+          className='h-5 w-5 rounded-full bg-black p-0 text-xs font-medium text-white hover:bg-black/70'
+          size='miniIcon'
+          variant='default'
+          onClick={() => handleYearChange(undefined)}
+        >
+          <X size={14} strokeWidth={1.5} />
+        </Button>
+      )}
     </main>
   );
 }
