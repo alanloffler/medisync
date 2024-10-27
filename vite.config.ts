18e779b7815e426a93b1d03ac71cae8fde5d4c2f
alanloffler <alanmatiasloffler@gmail.com>
2024-10-26 22:23:01 -0300

    Paths for routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from "path"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@appointments': path.resolve(__dirname, './src/pages/appointments'),
+      '@core': path.resolve(__dirname, './src/core'),
+      '@professionals': path.resolve(__dirname, './src/pages/professionals'),
+      '@users': path.resolve(__dirname, './src/pages/users'),
     },
-  })
+  },
+});
