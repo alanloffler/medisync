69d85e088c8adad68352b1608a82293ab2f70faf
alanloffler <alanmatiasloffler@gmail.com>
2024-10-28 20:58:57 -0300

    resetFilters method and code order

--- src/pages/appointments/hooks/useApposFilters.ts
+++ src/pages/appointments/hooks/useApposFilters.ts
@@ -1,25 +1,22 @@
 // External imports
 import { useCallback } from 'react';
 import { useSearchParams } from 'react-router-dom';
-// TODO: put in an interface file
-export interface IApposFilters {
-  professional?: string;
-  year?: string;
-  month?: string;
-}
+// Imports
+import type { IApposFilters } from '@appointments/interfaces/appos-filters.interface';
 // React hook
 export function useApposFilters() {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const professional: string | undefined = searchParams.get('p') as IApposFilters['professional'];
-  const year: string | undefined = searchParams.get('y') as IApposFilters['year'];
-  const month: string | undefined = searchParams.get('m') as IApposFilters['month'];
+  const professional: IApposFilters['professional'] = searchParams.get('p') as IApposFilters['professional'];
+  const year = searchParams.get('y') as IApposFilters['year'];
+  const month = searchParams.get('m') as IApposFilters['month'];
 
   const setFilters = useCallback((filters: IApposFilters) => {
     setSearchParams((params) => {
       if (filters.professional !== undefined) params.set('p', filters.professional);
       if (filters.year !== undefined) params.set('y', filters.year);
       if (filters.month !== undefined) params.set('m', filters.month);
+
       return params;
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -30,5 +27,6 @@ export function useApposFilters() {
     year,
     month,
     setFilters,
+    resetFilters: () => setSearchParams({}),
   };
 }
