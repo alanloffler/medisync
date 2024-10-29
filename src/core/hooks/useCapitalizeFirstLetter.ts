df10e7c86ac5ee19c36e171ad31f37442e212c96
alanloffler <alanmatiasloffler@gmail.com>
2024-10-29 17:59:24 -0300

    Param only is string

--- src/core/hooks/useCapitalizeFirstLetter.ts
+++ src/core/hooks/useCapitalizeFirstLetter.ts
@@ -1,7 +1,7 @@
 import { useCallback } from 'react';
 
 function useCapitalizeFirstLetter() {
-	return useCallback((sentence: string | undefined) => {
+	return useCallback((sentence: string) => {
 		if (sentence) {
       const words = sentence.split(' ');
 			return words[0].charAt(0).toUpperCase() + words[0].slice(1) + ' ' + words.slice(1).join(' ');
