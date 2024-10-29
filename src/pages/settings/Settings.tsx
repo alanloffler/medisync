21c5cde4475f2740477f280d550931b2046106c2
alanloffler <alanmatiasloffler@gmail.com>
2024-10-29 20:50:51 -0300

    Help setting section added

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -1,3 +1,31 @@
+// External components: https://ui.shadcn.com/docs/components
+import { Checkbox } from '@core/components/ui/checkbox';
+// Imports
+import { SETTINGS_CONFIG } from '@config/settings.config';
+// React component
 export default function Settings() {
-  return <div>Settings</div>;
+  function handleHelpChecked(checked: boolean) {
+    console.log('Help checked: ', checked);
+    console.log('save on store');
+  }
+
+  return (
+    <main className='flex w-full flex-col space-y-6 p-6'>
+      <h1 className='text-2xl font-bold tracking-tight'>{SETTINGS_CONFIG.page.title}</h1>
+      <section>
+        <section className='flex flex-col space-y-3'>
+          <h2 className='text-xl font-semibold tracking-tight'>{SETTINGS_CONFIG.section.application.title}</h2>
+          <div className='space-y-2 pl-3'>
+            <h5>{SETTINGS_CONFIG.section.application.subsection.help.title}</h5>
+            <div className='flex items-center space-x-2'>
+              <Checkbox defaultChecked={true} onCheckedChange={handleHelpChecked} id='terms' className='h-5 w-5' />
+              <label htmlFor='terms' className='text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'>
+                {SETTINGS_CONFIG.section.application.subsection.help.label}
+              </label>
+            </div>
+          </div>
+        </section>
+      </section>
+    </main>
+  );
 }
