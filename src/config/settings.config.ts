5c10295bacab212c2764cda88a30c67a20c42623
alanloffler <alanmatiasloffler@gmail.com>
2024-10-29 20:49:53 -0300

    Settings config file created

---
+++ src/config/settings.config.ts
@@ -0,0 +1,16 @@
+export const SETTINGS_CONFIG = {
+  page: {
+    title: 'Configuraciones',
+  },
+  section: {
+    application: {
+      title: 'Generales de la aplicación',
+      subsection: {
+        help: {
+          title: 'Configuración de la ayuda',
+          label: 'Activar descripción emergente (tooltips)',
+        },
+      },
+    },
+  },
+};
