80f10498de4673526e9f00b3e96440b14ececf78
alanloffler <alanmatiasloffler@gmail.com>
2024-10-29 19:35:47 -0300

    Actions buttons. Cell and header width

--- src/pages/appointments/components/ApposList.tsx
+++ src/pages/appointments/components/ApposList.tsx
@@ -1,8 +1,11 @@
+// Icons: https://lucide.dev/icons/
+import { FileText, MessageCircle, Trash2 } from 'lucide-react';
 // External components: https://ui.shadcn.com/docs/components
+import { Button } from '@/core/components/ui/button';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/core/components/ui/table';
 // External imports
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
 import { format } from '@formkit/tempo';
+import { type Cell, type ColumnDef, flexRender, getCoreRowModel, type Row, useReactTable } from '@tanstack/react-table';
 import { useNavigate } from 'react-router-dom';
 // Imports
 import type { IAppointmentView } from '@appointments/interfaces/appointment.interface';
@@ -15,35 +18,65 @@ export function ApposList({ appointments }: { appointments: IAppointmentView[] }
   const columns: ColumnDef<IAppointmentView>[] = [
     {
       accessorKey: 'day',
-      size: 20,
+      size: 80,
       cell: ({ row }) => <div className='text-center'>{format(row.original.day, 'DD/MM/YYYY')}</div>,
       header: () => <div className='text-center'>Fecha</div>,
     },
     {
       accessorKey: 'lastName',
-      cell: ({ row }) => <div className=''>{capitalize(`${row.original.professional.title.abbreviation} ${row.original.professional.lastName}, ${row.original.professional.firstName}`)}</div>,
+      cell: ({ row }) => (
+        <div>
+          {capitalize(
+            `${row.original.professional.title.abbreviation} ${row.original.professional.lastName}, ${row.original.professional.firstName}`,
+          )}
+        </div>
+      ),
       header: () => <div className='text-left'>Profesional</div>,
     },
     {
       accessorKey: 'actions',
       size: 100,
-      cell: () => <div className='bg-yellow-300'>
-        Botones acá
-      </div>,
-      header: () => <div className='text-right'>Acciones</div>,
-    }
+      cell: ({ row }) => (
+        <div className='text-center space-x-2 bg-red-400'>
+          <Button
+            onClick={() => navigate(`/appointments/${row.original._id}`)}
+            variant='tableHeader'
+            size='miniIcon'
+            className='border border-slate-300 bg-white transition-transform hover:scale-110 hover:border-sky-500 hover:bg-white hover:text-sky-500 hover:animate-in'
+          >
+            <FileText size={16} strokeWidth={1.5} />
+          </Button>
+          <Button
+            // onClick={}
+            variant='tableHeader'
+            size='miniIcon'
+            className='border border-slate-300 bg-white transition-transform hover:scale-110 hover:border-emerald-500 hover:bg-white hover:text-emerald-500 hover:animate-in'
+          >
+            <MessageCircle size={16} strokeWidth={1.5} />
+          </Button>
+          <Button
+            // onClick={}
+            variant='tableHeader'
+            size='miniIcon'
+            className='border border-slate-300 bg-white transition-transform hover:scale-110 hover:border-red-500 hover:bg-white hover:text-red-500 hover:animate-in'
+          >
+            <Trash2 size={16} strokeWidth={1.5} />
+          </Button>
+        </div>
+      ),
+      header: () => <div className='text-center bg-red-400'>Acciones</div>,
+    },
   ];
 
   const table = useReactTable<IAppointmentView>({
     data: appointments,
     columns,
     getCoreRowModel: getCoreRowModel(),
-    defaultColumn: {
-      size: 200, //starting column size
-      minSize: 50, //enforced during column resizing
-      maxSize: 500, //enforced during column resizing
-    },
   });
+
+  function handleRowClick(row: Row<IAppointmentView>, cell: Cell<IAppointmentView, unknown>): void {
+    if (cell.column.getIndex() < row.getAllCells().length - 1) navigate(`/appointments/${row.original._id}`);
+  }
 
   return (
     <Table>
@@ -51,16 +84,23 @@ export function ApposList({ appointments }: { appointments: IAppointmentView[] }
         {table.getHeaderGroups().map((headerGroup) => (
           <TableRow key={headerGroup.id}>
             {headerGroup.headers.map((header) => (
-              <TableHead key={header.id} style={{ width: `${header.getSize()}px` }}>{header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}</TableHead>
+              <TableHead key={header.id} style={{ width: `${header.getSize()}px` }}>
+                {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+              </TableHead>
             ))}
           </TableRow>
         ))}
       </TableHeader>
       <TableBody className='text-sm'>
         {table.getRowModel().rows.map((row) => (
-          <TableRow key={row.id}>
+          <TableRow key={row.id} className='hover:bg-slate-100/50'>
             {row.getVisibleCells().map((cell) => (
-              <TableCell onClick={() => navigate(`/appointments/${row.original._id}`)} className='py-1 px-0' key={cell.id} style={{ width: `${cell.column.getSize()}px` }}>
+              <TableCell
+                onClick={() => handleRowClick(row, cell)}
+                className={`px-0 py-1 ${cell.column.id !== 'actions' && 'hover:cursor-pointer'}`}
+                key={cell.id}
+                style={{ width: `${cell.column.getSize()}px` }}
+              >
                 {flexRender(cell.column.columnDef.cell, cell.getContext())}
               </TableCell>
             ))}
