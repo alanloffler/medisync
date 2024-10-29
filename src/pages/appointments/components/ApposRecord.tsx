dc86fbacce14c7cdbd8952df58fce5cfa6b3c02b
alanloffler <alanmatiasloffler@gmail.com>
2024-10-29 10:22:27 -0300

    Loading and error handling. Styled as a Card component

--- src/pages/appointments/components/ApposRecord.tsx
+++ src/pages/appointments/components/ApposRecord.tsx
@@ -1,35 +1,61 @@
+// External components
+import { Card, CardContent, CardHeader, CardTitle } from '@/core/components/ui/card';
 // Components
 import { ApposFilters } from '@appointments/components/ApposFilters';
 import { ApposList } from '@appointments/components/ApposList';
+import { InfoCard } from '@/core/components/common/InfoCard';
+import { LoadingDB } from '@/core/components/common/LoadingDB';
 // External imports
 import { useEffect, useState } from 'react';
 // Imports
 import type { IAppointmentView } from '@appointments/interfaces/appointment.interface';
 import type { IResponse } from '@core/interfaces/response.interface';
+import { APP_CONFIG } from '@/config/app.config';
 import { AppointmentApiService } from '@appointments/services/appointment.service';
+import { USER_VIEW_CONFIG } from '@/config/user.config';
 import { useApposFilters } from '@appointments/hooks/useApposFilters';
+import { useNotificationsStore } from '@/core/stores/notifications.store';
 // React component
 export function ApposRecord({ userId }: { userId: string }) {
   const [appointments, setAppointments] = useState<IAppointmentView[]>([]);
+  const [error, setError] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const { professional, year } = useApposFilters();
+  const addNotification = useNotificationsStore((state) => state.addNotification);
 
   useEffect(() => {
-    // TODO: manage error for all api calls
-    AppointmentApiService.findApposRecordWithFilters(userId, professional, year).then((response: IResponse) => {
-      if (response.statusCode === 200) {
-        if (response.data.length > 0) {
-          setAppointments(response.data);
+    setIsLoading(true);
+
+    AppointmentApiService.findApposRecordWithFilters(userId, professional, year)
+      .then((response: IResponse) => {
+        if (response.statusCode === 200) {
+          if (response.data.length > 0) setAppointments(response.data);
         }
-      }
-    });
+        if (response.statusCode > 399) {
+          setError(true);
+          addNotification({ type: 'error', message: response.message });
+        }
+        if (response instanceof Error) {
+          setError(true);
+          addNotification({ type: 'error', message: APP_CONFIG.error.server });
+        }
+      })
+      .finally(() => setIsLoading(false));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [professional, year, userId]);
 
   return (
     <main>
-      <section className='flex flex-col gap-3 w-full'>
-        <ApposFilters userId={userId} />
-        {appointments && <ApposList appointments={appointments} />}
-      </section>
+      <Card className='flex w-full flex-col gap-3 border'>
+        <CardHeader>
+          <CardTitle>{USER_VIEW_CONFIG.apposRecord.title}</CardTitle>
+        </CardHeader>
+        <CardContent className='flex flex-col gap-3'>
+          <ApposFilters userId={userId} />
+          {isLoading && <LoadingDB variant='default' text={USER_VIEW_CONFIG.apposRecord.apposList.loadingText} />}
+          {!error ? <ApposList appointments={appointments} /> : <InfoCard type='error' text={USER_VIEW_CONFIG.apposRecord.apposList.errorText} className='pt-5' />}
+        </CardContent>
+      </Card>
     </main>
   );
 }
