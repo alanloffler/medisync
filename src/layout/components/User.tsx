9ed02ebe3e6891e73139f2bc6fee29fc7f2e20fe
alanloffler <alanmatiasloffler@gmail.com>
2024-10-30 16:07:50 -0300

    Alias path fixed, code and comments order

--- src/layout/components/User.tsx
+++ src/layout/components/User.tsx
@@ -1,15 +1,19 @@
 // Icons: https://lucide.dev/icons/
 import { CircleUser } from 'lucide-react';
-// Components: https://ui.shadcn.com/docs/components
-import { Button } from '@/core/components/ui/button';
-import { DropdownMenu, DropdownMenuTrigger } from '@/core/components/ui/dropdown-menu';
-import { DropdownMenuContent } from '@/core/components/ui/dropdown-menu';
-import { DropdownMenuItem } from '@/core/components/ui/dropdown-menu';
-import { DropdownMenuLabel } from '@/core/components/ui/dropdown-menu';
-import { DropdownMenuSeparator } from '@/core/components/ui/dropdown-menu';
-// App
-import { HEADER_CONFIG } from '../../config/layout/header.config';
+// External components: https://ui.shadcn.com/docs/components
+import { Button } from '@core/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@core/components/ui/dropdown-menu';
+// External imports
 import { Link } from 'react-router-dom';
+// Imports
+import { HEADER_CONFIG } from '@config/layout/header.config';
 // React component
 export function User() {
   return (
@@ -17,7 +21,6 @@ export function User() {
       <DropdownMenuTrigger asChild>
         <Button variant='secondary' size='icon' className='rounded-full'>
           <CircleUser className='h-5 w-5' />
-          <span className='sr-only'>Toggle user menu</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align='end'>
