276c01cabbf844133d80125a70dac53f9fbc1277
alanloffler <alanmatiasloffler@gmail.com>
2024-10-30 15:38:40 -0300

    Alias path fixed

--- src/core/components/common/BreadCrumb.tsx
+++ src/core/components/common/BreadCrumb.tsx
@@ -1,9 +1,9 @@
 // Icons: https://lucide.dev/icons/
 import { ChevronRight } from 'lucide-react';
 // Components: https://ui.shadcn.com/docs/components
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage } from '@/core/components/ui/breadcrumb';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage } from '@core/components/ui/breadcrumb';
 // App
-import type { IBreadcrumb } from '@/core/components/common/interfaces/breadcrumb.interface';
+import type { IBreadcrumb } from '@core/components/common/interfaces/breadcrumb.interface';
 import { Link } from 'react-router-dom';
 // React component
 export function BreadCrumb({ paths }: { paths: IBreadcrumb[] }) {
