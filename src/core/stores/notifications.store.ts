b6aef67e544b8636f745570da3aec9299837ccb8
alanloffler <alanmatiasloffler@gmail.com>
2024-10-30 15:56:33 -0300

    Type order and remove comments

--- src/core/stores/notifications.store.ts
+++ src/core/stores/notifications.store.ts
@@ -1,15 +1,10 @@
-// Zustand https://zustand-demo.pmnd.rs/
-// Store used for global state of Notifications.
-// The Notifications are stored in a session storage mode, and can be accessed from any component at any level.
-// This store is used in the footer component, at the left side.
-
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
 
 export interface INotification {
   date?: string;
   message: string;
-  type: 'success' | 'warning' | 'error';
+  type: 'error' | 'success' | 'warning';
 }
 
 type States = {
