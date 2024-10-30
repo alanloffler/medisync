923932b6c8c3aa2c9cbb62fafd391523d11e49c1
alanloffler <alanmatiasloffler@gmail.com>
2024-10-30 09:02:51 -0300

    Props interface

--- src/core/components/common/TooltipWrapper.tsx
+++ src/core/components/common/TooltipWrapper.tsx
@@ -1,7 +1,14 @@
 // External components: https://ui.shadcn.com/docs/components
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@core/components/ui/tooltip';
+// External imports
+import { ReactNode } from 'react';
+// Interface
+interface ITooltipWrapper {
+  children: ReactNode;
+  content: string;
+}
 // React component
-export function TooltipWrapper({ children, content }: { children: React.ReactNode; content: string }) {
+export function TooltipWrapper({ children, content }: ITooltipWrapper) {
   return (
     <TooltipProvider delayDuration={0.3}>
       <Tooltip>
