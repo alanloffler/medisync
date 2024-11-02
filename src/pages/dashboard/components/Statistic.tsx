28a84cd098b2fb371b6c73c7886e49a3cf607eab
alanloffler <alanmatiasloffler@gmail.com>
2024-11-02 14:21:20 -0300

    Children prop for icons

--- src/pages/dashboard/components/Statistic.tsx
+++ src/pages/dashboard/components/Statistic.tsx
@@ -1,16 +1,14 @@
-// Icons: https://lucide.dev/icons/
-import { DollarSign } from 'lucide-react';
 // External components: https://ui.shadcn.com/docs/components
 import { Card, CardContent, CardHeader, CardTitle } from '@core/components/ui/card';
 // Imports
 import type { IStatistic } from '@dashboard/interfaces/statistic.interface';
 // React component
-export function Statistic({ content, title, value }: IStatistic) {
+export function Statistic({ children, content, title, value }: IStatistic) {
   return (
     <Card>
       <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
         <CardTitle className='text-sm font-medium'>{title}</CardTitle>
-        <DollarSign className='h-4 w-4 text-muted-foreground' />
+        {children}
       </CardHeader>
       <CardContent>
         <div className='text-2xl font-bold'>{value}</div>
