7933c14efd31b8a8777104c22a7a5f9902347b14
alanloffler <alanmatiasloffler@gmail.com>
2024-11-03 11:08:37 -0300

    isLoading prop (by react query)

--- src/pages/dashboard/interfaces/statistic.interface.ts
+++ src/pages/dashboard/interfaces/statistic.interface.ts
@@ -3,6 +3,7 @@ import type { ReactNode } from 'react';
 export interface IStatistic {
   children: ReactNode;
   content: string;
+  isLoading?: boolean;
   title: string;
   value1?: string;
   value2?: string;
