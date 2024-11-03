0409b41c14a00fc74363852a8dee6001b2f002d8
alanloffler <alanmatiasloffler@gmail.com>
2024-11-03 11:08:08 -0300

    React query for Statistic components

--- src/pages/dashboard/components/StatisticGroup.tsx
+++ src/pages/dashboard/components/StatisticGroup.tsx
@@ -1,37 +1,47 @@
 // Icons: https://lucide.dev/icons/
 import { Activity, CalendarCheck, CreditCard, Users } from 'lucide-react';
-// External imports
-import { useEffect, useState } from 'react';
 // Components
 import { Statistic } from '@dashboard/components/Statistic';
+// External imports
+import { useQuery } from '@tanstack/react-query';
 // Imports
-import type { IResponse } from '@core/interfaces/response.interface';
 import { DashboardApiService } from '@dashboard/services/dashboard-api.service';
 import { useDelimiter } from '@core/hooks/useDelimiter';
 // React component
 export function StatisticGroup() {
-  const [apposStats, setApposStats] = useState<IResponse>({} as IResponse);
-  const [userStats, setUserStats] = useState<IResponse>({} as IResponse);
   const delimiter = useDelimiter();
 
-  useEffect(() => {
-    // TODO: handle errors
-    DashboardApiService.countAppointments().then((response: IResponse) => {
-      if (response.statusCode === 200) setApposStats(response);
-    });
+  const { data: apposData, isLoading: apposDataIsLoading } = useQuery({
+    queryKey: ['appos'],
+    queryFn: () => DashboardApiService.countAppointments(),
+    staleTime: 0
+  });
 
-    DashboardApiService.countAllUsers().then((response: IResponse) => {
-      if (response.statusCode === 200) setUserStats(response);
-      if (response.statusCode > 399) setUserStats({ statusCode: response.statusCode, message: response.message, data: undefined });
-    });
-  }, []);
+  const { data: usersData, isLoading: usersDataIsLoading } = useQuery({
+    queryKey: ['users'],
+    queryFn: async () => {
+      return await DashboardApiService.countAllUsers();
+    },
+  });
 
   return (
     <main className='grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4'>
-      <Statistic content='el último mes' title='Turnos' value1={apposStats.data?.value1} value2={apposStats.data?.value2}>
+      <Statistic
+        content='el último mes'
+        title='Turnos'
+        value1={apposData?.data?.value1}
+        value2={apposData?.data?.value2}
+        isLoading={apposDataIsLoading}
+      >
         <CalendarCheck size={24} strokeWidth={2} className='text-fuchsia-400' />
       </Statistic>
-      <Statistic content={`${userStats.data?.value2} nuevos este mes`} title='Pacientes' value1={delimiter(userStats.data?.value1, '.', 3)} value2=''>
+      <Statistic
+        content='nuevos este mes'
+        title='Pacientes'
+        value1={delimiter(usersData?.data?.value1, '.', 3)}
+        value2={usersData?.data?.value2}
+        isLoading={usersDataIsLoading}
+      >
         <Users size={24} strokeWidth={2} className='text-sky-400' />
       </Statistic>
       <Statistic content='from last month' title='Sales' value1='+12,234' value2='+19%'>
