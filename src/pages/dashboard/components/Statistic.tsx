055b53b202c780d3bcdf7dd07ce8f2fad0bfe8c5
alanloffler <alanmatiasloffler@gmail.com>
2024-11-04 19:59:41 -0300

    Error handling

--- src/pages/dashboard/components/Statistic.tsx
+++ src/pages/dashboard/components/Statistic.tsx
@@ -3,7 +3,8 @@ import { Card, CardContent, CardHeader, CardTitle } from '@core/components/ui/ca
 // Imports
 import type { IStatistic } from '@dashboard/interfaces/statistic.interface';
 // React component
-export function Statistic({ children, content, isLoading, title, value1, value2 }: IStatistic) {
+export function Statistic({ children, content, isError, isLoading, title, value1, value2 }: IStatistic) {
+  console.log(isError)
   return (
     <Card>
       <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
@@ -12,14 +13,14 @@ export function Statistic({ children, content, isLoading, title, value1, value2 
         </CardTitle>
         {children}
       </CardHeader>
-      <CardContent>
+      <CardContent>{String(isError)}
+        {isError && <div className='pt-4 text-sm font-medium text-rose-500'>Error cargando estadística</div>}
         {isLoading ? (
           <div className='text-2xl font-bold text-dark-default'>Cargando...</div>
         ) : (
           <>
             <div className='text-2xl font-bold text-dark-default'>{value1}</div>
             <p className='text-xs text-dark-default'>{`${value2} ${content}`}</p>
-            {/* <div className='pt-4 text-sm font-medium text-rose-500'>Error cargando estadística</div> */}
           </>
         )}
       </CardContent>
