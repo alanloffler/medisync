34830a9433317e87b601920705d580f758a7a5ae
alanloffler <alanmatiasloffler@gmail.com>
2024-11-04 20:32:18 -0300

    Specializations shortcuts

--- src/pages/dashboard/components/shortcuts/CategoriesShortcuts.tsx
+++ src/pages/dashboard/components/shortcuts/CategoriesShortcuts.tsx
@@ -7,11 +7,13 @@ import { useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 // Imports
 import type { IArea } from '@core/interfaces/area.interface';
+import type { ISpecialization } from '@core/interfaces/specialization.interface';
 import { AreaService } from '@core/services/area.service';
 import { DASHBOARD_CONFIG } from '@config/dashboard.config';
 // React component
 export function CategoriesShortcuts() {
   const [areaSelected, setAreaSelected] = useState<string>('');
+  const [specializations, setSpecializations] = useState<ISpecialization[]>([]);
 
   const { data: categories } = useQuery<IArea[]>({
     queryKey: ['categories'],
@@ -21,9 +23,17 @@ export function CategoriesShortcuts() {
     },
   });
 
+  function extractSpecializations(categories: IArea[] | undefined) {
+    if (!categories) return [];
+    return categories.flatMap((category) => category.specializations);
+  }
+
   useEffect(() => {
     console.log(areaSelected);
-  }, [areaSelected]);
+    const _specializations = extractSpecializations(categories);
+    setSpecializations(_specializations);
+    console.log(_specializations);
+  }, [areaSelected, categories]);
 
   return (
     <Card className='space-y-4 p-4 xl:col-span-2'>
@@ -36,6 +46,18 @@ export function CategoriesShortcuts() {
             itemSize={90}
             key={crypto.randomUUID()}
             label={category.name}
+            setAreaSelected={setAreaSelected}
+          />
+        ))}
+      </section>
+      <section>
+        {specializations.map((specialization) => (
+          <IconShortcut
+            icon={specialization.icon}
+            iconSize={24}
+            itemSize={90}
+            key={crypto.randomUUID()}
+            label={specialization.name}
             setAreaSelected={setAreaSelected}
           />
         ))}
