945d018a15f892da6493d76a72fb3ea0073bf7db
alanloffler <alanmatiasloffler@gmail.com>
2024-11-05 10:57:01 -0300

    Column style for left section on container

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -15,9 +15,12 @@ export default function Dashboard() {
       {/* Section: Statistic Group */}
       <StatisticGroup />
       {/* Section: Container */}
-      <section className='grid grid-cols-1 gap-4 md:grid-cols-4 md:gap-8 lg:grid-cols-4 xl:grid-cols-4'>
-        <CategoriesShortcuts className='col-span-1 md:col-span-2 lg:col-span-2' />
-        <Card className='col-span-1 md:col-span-2 lg:col-span-2'>
+      <section className='grid grid-cols-1 gap-4 md:grid-cols-5 md:gap-8 lg:grid-cols-5 lg:gap-8 xl:grid-cols-5 xl:gap-8'>
+        <section className='col-span-1 h-fit space-y-4 md:col-span-2 md:space-y-8 lg:col-span-2 lg:space-y-8 xl:col-span-2 xl:space-y-8'>
+          <CategoriesShortcuts />
+          <Card>Here something else</Card>
+        </section>
+        <Card className='col-span-1 md:col-span-3 lg:col-span-3 xl:col-span-3'>
           <CardHeader>
             <CardTitle>Recent Sales</CardTitle>
           </CardHeader>
