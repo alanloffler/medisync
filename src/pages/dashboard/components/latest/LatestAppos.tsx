96367bcb23f1b3cf88aa0c4169c6b8eaa2738841
alanloffler <alanmatiasloffler@gmail.com>
2024-11-05 20:27:44 -0300

    LatestAppos get last 5 appointments on Dashboard

--- src/pages/dashboard/components/latest/LatestAppos.tsx
+++ src/pages/dashboard/components/latest/LatestAppos.tsx
@@ -1,71 +1,61 @@
+// Icons: https://lucide.dev/icons/
+import { CalendarClock, CalendarPlus } from 'lucide-react';
 // External components: https://ui.shadcn.com/docs/components
-import { Avatar, AvatarFallback, AvatarImage } from '@core/components/ui/avatar';
 import { Card, CardContent, CardHeader, CardTitle } from '@core/components/ui/card';
+// External imports
+import { format } from '@formkit/tempo';
+import { useQuery } from '@tanstack/react-query';
 // Imports
+import type { IAppointmentView } from '@appointments/interfaces/appointment.interface';
+import type { IResponse } from '@core/interfaces/response.interface';
 import { DASHBOARD_CONFIG } from '@config/dashboard.config';
+import { DashboardApiService } from '@dashboard/services/dashboard-api.service';
+import { USER_CREATE_CONFIG } from '@config/user.config';
+import { useCapitalize } from '@core/hooks/useCapitalize';
+import { useDelimiter } from '@core/hooks/useDelimiter';
 // React component
 export function LatestAppos() {
+  const capitalize = useCapitalize();
+  const delimiter = useDelimiter();
+
+  const { data: latestAppos } = useQuery<IResponse>({
+    queryKey: ['latestAppos'],
+    queryFn: async () => {
+      return await DashboardApiService.latestAppointments(5);
+    },
+  });
+
   return (
     <Card>
       <CardHeader>
         <CardTitle>{DASHBOARD_CONFIG.latestAppos.title}</CardTitle>
       </CardHeader>
-      <CardContent className='grid gap-8'>
-        <div className='flex items-center gap-4'>
-          <Avatar className='hidden h-9 w-9 sm:flex'>
-            <AvatarImage src='/avatars/01.png' alt='Avatar' />
-            <AvatarFallback>OM</AvatarFallback>
-          </Avatar>
-          <div className='grid gap-1'>
-            <p className='text-sm font-medium leading-none'>Olivia Martin</p>
-            <p className='text-sm text-muted-foreground'>olivia.martin@email.com</p>
-          </div>
-          <div className='ml-auto font-medium'>+$1,999.00</div>
-        </div>
-        <div className='flex items-center gap-4'>
-          <Avatar className='hidden h-9 w-9 sm:flex'>
-            <AvatarImage src='/avatars/02.png' alt='Avatar' />
-            <AvatarFallback>JL</AvatarFallback>
-          </Avatar>
-          <div className='grid gap-1'>
-            <p className='text-sm font-medium leading-none'>Jackson Lee</p>
-            <p className='text-sm text-muted-foreground'>jackson.lee@email.com</p>
-          </div>
-          <div className='ml-auto font-medium'>+$39.00</div>
-        </div>
-        <div className='flex items-center gap-4'>
-          <Avatar className='hidden h-9 w-9 sm:flex'>
-            <AvatarImage src='/avatars/03.png' alt='Avatar' />
-            <AvatarFallback>IN</AvatarFallback>
-          </Avatar>
-          <div className='grid gap-1'>
-            <p className='text-sm font-medium leading-none'>Isabella Nguyen</p>
-            <p className='text-sm text-muted-foreground'>isabella.nguyen@email.com</p>
-          </div>
-          <div className='ml-auto font-medium'>+$299.00</div>
-        </div>
-        <div className='flex items-center gap-4'>
-          <Avatar className='hidden h-9 w-9 sm:flex'>
-            <AvatarImage src='/avatars/04.png' alt='Avatar' />
-            <AvatarFallback>WK</AvatarFallback>
-          </Avatar>
-          <div className='grid gap-1'>
-            <p className='text-sm font-medium leading-none'>William Kim</p>
-            <p className='text-sm text-muted-foreground'>will@email.com</p>
-          </div>
-          <div className='ml-auto font-medium'>+$99.00</div>
-        </div>
-        <div className='flex items-center gap-4'>
-          <Avatar className='hidden h-9 w-9 sm:flex'>
-            <AvatarImage src='/avatars/05.png' alt='Avatar' />
-            <AvatarFallback>SD</AvatarFallback>
-          </Avatar>
-          <div className='grid gap-1'>
-            <p className='text-sm font-medium leading-none'>Sofia Davis</p>
-            <p className='text-sm text-muted-foreground'>sofia.davis@email.com</p>
-          </div>
-          <div className='ml-auto font-medium'>+$39.00</div>
-        </div>
+      <CardContent className='grid gap-2'>
+        {latestAppos &&
+          latestAppos.data.map((appo: IAppointmentView) => (
+            <section className='flex flex-row items-center justify-between gap-4 rounded-md border border-slate-200 p-3 text-sm'>
+              <div className='flex w-1/2 flex-row items-center space-x-3'>
+                <div className='flex h-11 w-11 flex-col items-center rounded-sm bg-slate-600 pt-1.5 text-slate-200'>
+                  <CalendarPlus size={16} strokeWidth={2} />
+                  <p className='text-[11px] font-light'>{format(appo.createdAt, 'DD/MM')}</p>
+                </div>
+                <div className='flex flex-col'>
+                  <p className='font-bold text-dark-default'>{`${capitalize(appo.user.firstName)} ${capitalize(appo.user.lastName)}`}</p>
+                  <p className='text-xs font-light text-muted-foreground'>{`${USER_CREATE_CONFIG.labels.dni} ${delimiter(appo.user.dni, '.', 3)}`}</p>
+                </div>
+              </div>
+              <div className='mr-2 flex w-1/2 flex-row items-center justify-end space-x-3 text-xs'>
+                <div className='flex flex-col text-right'>
+                  <p className='font-bold text-dark-default'>{`${capitalize(appo.professional.title.abbreviation)} ${capitalize(appo.professional.firstName)} ${capitalize(appo.professional.lastName)}`}</p>
+                  <p className='text-xs font-light text-muted-foreground'>{capitalize(appo.professional.specialization.name)}</p>
+                </div>
+                <div className='flex flex-row items-center space-x-2 rounded-sm bg-slate-200 p-1 pr-2 text-slate-600'>
+                  <CalendarClock size={16} strokeWidth={1.5} />
+                  <p>{`${format(appo.day, 'DD/MM')} - ${appo.hour}`}</p>
+                </div>
+              </div>
+            </section>
+          ))}
       </CardContent>
     </Card>
   );
