5ead8c7d1fe9c5d381f554495a21c97faee13b47
alanloffler <alanmatiasloffler@gmail.com>
2024-11-07 17:04:12 -0300

    Fetch method for queries. Response and error handling for Tanstack React Query

--- src/core/services/specialization.service.ts
+++ src/core/services/specialization.service.ts
@@ -1,18 +1,35 @@
+import type { IResponse } from '@core/interfaces/response.interface';
+import { APP_CONFIG } from '@config/app.config';
+
 export class SpecializationService {
   private static readonly API_URL: string = import.meta.env.VITE_API_URL;
 
   public static async findAll() {
+    const url: string = `${this.API_URL}/specializations`;
+    const method: string = 'GET';
+
+    return await this.fetch(url, method);
+  }
+
+  private static async fetch(url: string, method: string) {
     try {
-      const fetchData = await fetch(`${this.API_URL}/specializations`, {
-        method: 'GET',
+      const query: Response = await fetch(url, {
+        method: method,
         headers: {
           'content-type': 'application/json;charset=UTF-8',
         },
       });
-      
-      return await fetchData.json();
+
+      const response: IResponse = await query.json();
+
+      if (!query.ok) throw new Error(response.message);
+
+      return response;
     } catch (error) {
-      return error;
+      if (error instanceof TypeError) {
+        throw new Error(APP_CONFIG.error.server);
+      }
+      throw error;
     }
   }
 }
