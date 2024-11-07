e3d4658606c424bd68cd9460e6f448d761219510
alanloffler <alanmatiasloffler@gmail.com>
2024-11-07 18:01:15 -0300

    Icon with separate values for width and height. Styles and preventDefault on drag start (img tag)

--- src/pages/dashboard/components/shortcuts/IconShortcut.tsx
+++ src/pages/dashboard/components/shortcuts/IconShortcut.tsx
@@ -5,11 +5,12 @@ interface IIconShortcut {
   setAreaSelected: React.Dispatch<React.SetStateAction<string>>;
   icon: string;
   iconSize: number;
-  itemSize: number;
+  itemHeight: number;
+  itemWidth: number;
   label: string;
 }
 // React component
-export function IconShortcut({ setAreaSelected, icon, iconSize, itemSize, label }: IIconShortcut) {
+export function IconShortcut({ setAreaSelected, icon, iconSize, itemHeight, itemWidth, label }: IIconShortcut) {
   const capitalize = useCapitalize();
 
   function getSVG(icon: string): string {
@@ -19,11 +20,11 @@ export function IconShortcut({ setAreaSelected, icon, iconSize, itemSize, label 
   return (
     <button
       className='flex select-none flex-col items-center justify-center space-y-2 rounded-md border p-2 shadow-sm transition-transform hover:border-slate-300 [&>img]:hover:scale-125 [&>img]:hover:animate-in'
-      style={{ width: itemSize, height: itemSize, minWidth: itemSize, minHeight: itemSize }}
+      style={{ width: itemWidth, height: itemHeight, minWidth: itemWidth, minHeight: itemHeight }}
       onClick={() => setAreaSelected(label)}
     >
-      <img src={getSVG(icon)} width={iconSize} height={iconSize} />
-      <span className='text-xs font-medium'>{capitalize(label)}</span>
+      <img src={getSVG(icon)} width={iconSize} height={iconSize} onDragStart={(e) => e.preventDefault()} />
+      <span className='text-xs font-bold text-primary'>{capitalize(label)}</span>
     </button>
   );
 }
