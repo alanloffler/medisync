c3da05cb69f590080c41428dcaad68c4d1636696
alanloffler <alanmatiasloffler@gmail.com>
2024-11-08 00:00:13 -0300

    Added random number until set statistic value

--- src/pages/dashboard/components/Statistic.tsx
+++ src/pages/dashboard/components/Statistic.tsx
@@ -1,9 +1,58 @@
 // External components: https://ui.shadcn.com/docs/components
 import { Card, CardContent, CardHeader, CardTitle } from '@core/components/ui/card';
+// External imports
+import { useEffect, useState } from 'react';
 // Imports
 import type { IStatistic } from '@dashboard/interfaces/statistic.interface';
 // React component
 export function Statistic({ children, content, isError, isLoading, title, value1, value2 }: IStatistic) {
+  const targetNumber = value1?.toString();
+  const [displayedDigits, setDisplayedDigits] = useState<number[]>(new Array(targetNumber?.length).fill(0));
+
+  useEffect(() => {
+    function randomNumber(): number {
+      return Math.floor(Math.random() * 10);
+    }
+
+    if (targetNumber === undefined) return;
+
+    const counters = new Array(targetNumber.length).fill(0);
+    const timeouts: ReturnType<typeof setTimeout>[] = [];
+
+    function updateDigit(index: number) {
+      if (counters[index] < 30) {
+        setDisplayedDigits((prev) => {
+          const updatedDigits = [...prev];
+          updatedDigits[index] = randomNumber();
+          return updatedDigits;
+        });
+
+        counters[index]++;
+
+        if (counters[index] <= 20) {
+          timeouts[index] = setTimeout(() => updateDigit(index), 50);
+        } else {
+          const newDelay = 50 + (counters[index] - 20) * 30;
+          timeouts[index] = setTimeout(() => updateDigit(index), newDelay);
+        }
+      } else {
+        setDisplayedDigits((prev) => {
+          const updatedDigits = [...prev];
+          updatedDigits[index] = parseInt(targetNumber![index]);
+          return updatedDigits;
+        });
+
+        clearTimeout(timeouts[index]);
+      }
+    }
+
+    for (let i = 0; i < targetNumber.length; i++) {
+      updateDigit(i);
+    }
+
+    return () => timeouts.forEach((timeout) => clearTimeout(timeout));
+  }, [targetNumber]);
+
   return (
     <Card>
       <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
@@ -12,13 +61,17 @@ export function Statistic({ children, content, isError, isLoading, title, value1
         </CardTitle>
         {children}
       </CardHeader>
-      <CardContent>{String(isError)}
+      <CardContent>
         {isError && <div className='pt-4 text-sm font-medium text-rose-500'>Error cargando estadística</div>}
         {isLoading ? (
           <div className='text-2xl font-bold text-dark-default'>Cargando...</div>
         ) : (
           <>
-            <div className='text-2xl font-bold text-dark-default'>{value1}</div>
+            <div className='flex flex-row items-center text-2xl font-bold text-dark-default'>
+              {displayedDigits.map((digit, index) => (
+                <span key={index}>{digit}</span>
+              ))}
+            </div>
             <p className='text-xs text-dark-default'>{`${value2} ${content}`}</p>
           </>
         )}
