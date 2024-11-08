6ae49b4bee62ca6088c412be17b2d3d6ff499376
alanloffler <alanmatiasloffler@gmail.com>
2024-11-07 21:05:22 -0300

    Card padding

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -18,7 +18,7 @@ export default function Dashboard() {
       <section className='grid grid-cols-1 gap-4 md:grid-cols-5 md:gap-8 lg:grid-cols-5 lg:gap-8 xl:grid-cols-5 xl:gap-8'>
         <section className='col-span-1 h-fit space-y-4 md:col-span-5 md:space-y-8 lg:col-span-2 lg:space-y-8 xl:col-span-2 xl:space-y-8'>
           <CategoriesShortcuts />
-          <Card>Here something else</Card>
+          <Card className='p-6'>Here something else</Card>
         </section>
         <section className='col-span-1 md:col-span-5 lg:col-span-3 xl:col-span-3'>
           <LatestAppos />
