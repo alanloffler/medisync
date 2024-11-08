fac4cbcf42de96f5902f364264efceb2f01f933b
alanloffler <alanmatiasloffler@gmail.com>
2024-11-07 21:00:33 -0300

    Framer motion animation for IconShortcut icon and span

--- src/pages/dashboard/components/shortcuts/IconShortcut.tsx
+++ src/pages/dashboard/components/shortcuts/IconShortcut.tsx
@@ -1,3 +1,5 @@
+// External imports
+import { motion } from 'framer-motion';
 // Imports
 import { useCapitalize } from '@core/hooks/useCapitalize';
 // Interface
@@ -13,18 +15,59 @@ interface IIconShortcut {
 export function IconShortcut({ setAreaSelected, icon, iconSize, itemHeight, itemWidth, label }: IIconShortcut) {
   const capitalize = useCapitalize();
 
+  const animation = {
+    item: {
+      initial: { scale: 1 },
+      animate: {
+        scale: 1.2,
+        transition: {
+          type: 'spring',
+          stiffness: 800,
+          damping: 20,
+          duration: 0.2,
+          delay: 0,
+        },
+      },
+    },
+    label: {
+      initial: { y: 0 },
+      animate: {
+        y: 3,
+        transition: {
+          type: 'spring',
+          stiffness: 800,
+          damping: 20,
+          duration: 0.2,
+          delay: 0,
+        },
+      },
+    },
+  };
+
   function getSVG(icon: string): string {
     return new URL(`../../../../assets/icons/${icon}.svg`, import.meta.url).href;
   }
 
   return (
-    <button
-      className='flex select-none flex-col items-center justify-center space-y-2 rounded-md border p-2 shadow-sm transition-transform hover:border-slate-300 [&>img]:hover:scale-125 [&>img]:hover:animate-in'
+    <motion.button
+      initial='initial'
+      animate='initial'
+      whileHover='animate'
+      className='flex select-none flex-col items-center justify-center space-y-2 rounded-md border p-2 shadow-sm'
       style={{ width: itemWidth, height: itemHeight, minWidth: itemWidth, minHeight: itemHeight }}
       onClick={() => setAreaSelected(label)}
     >
-      <img src={getSVG(icon)} width={iconSize} height={iconSize} onDragStart={(e) => e.preventDefault()} />
-      <span className='text-xs font-bold text-primary'>{capitalize(label)}</span>
-    </button>
+      <motion.img
+        src={getSVG(icon)}
+        width={iconSize}
+        height={iconSize}
+        onDragStart={(e) => e.preventDefault()}
+        draggable={false}
+        variants={animation.item}
+      />
+      <motion.span variants={animation.label} className='text-xs font-bold text-primary'>
+        {capitalize(label)}
+      </motion.span>
+    </motion.button>
   );
 }
