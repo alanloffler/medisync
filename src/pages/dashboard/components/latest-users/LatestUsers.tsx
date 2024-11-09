137c93dc4557bffde96d8fd2227c789d14798d5a
alanloffler <alanmatiasloffler@gmail.com>
2024-11-09 10:12:01 -0300

    Styles, animation and button action

--- src/pages/dashboard/components/latest-users/LatestUsers.tsx
+++ src/pages/dashboard/components/latest-users/LatestUsers.tsx
@@ -1,9 +1,11 @@
 // Icons: https://lucide.dev
-import { CalendarPlus } from 'lucide-react';
+import { CircleChevronRight, CalendarPlus } from 'lucide-react';
 // External components: https://ui.shadcn.com/docs/components
 import { Card, CardContent } from '@core/components/ui/card';
 // External imports
 import { format } from '@formkit/tempo';
+import { motion } from 'framer-motion';
+import { useNavigate } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 // Imports
 import type { IResponse } from '@core/interfaces/response.interface';
@@ -14,6 +16,7 @@ import { useCapitalize } from '@core/hooks/useCapitalize';
 // React component
 export function LatestUsers() {
   const capitalize = useCapitalize();
+  const navigate = useNavigate();
 
   const { data: latestUsers } = useQuery<IResponse>({
     queryKey: ['dashboard', 'latestUsers'],
@@ -22,19 +25,49 @@ export function LatestUsers() {
     },
   });
 
+  const animation = {
+    arrow: {
+      initial: { opacity: 0 },
+      animate: { opacity: 1 },
+    },
+    date: {
+      initial: { x: 0 },
+      animate: { x: -5 },
+    },
+    user: {
+      initial: { x: 0 },
+      animate: { x: 15 },
+    },
+  };
+
   return (
     <main className='space-y-2'>
       <h2 className='text-xl font-medium text-dark-default'>{DASHBOARD_CONFIG.latestUsers.title}</h2>
       <Card>
-        <CardContent className='grid space-y-2 pt-6'>
+        <CardContent className='flex flex-col space-y-1 pl-0 pt-6'>
           {latestUsers?.data.map((user: IUser) => (
-            <button className='flex flex-row items-center justify-between space-x-2 text-sm'>
-              <span className='w-full px-1 py-0.5 text-left font-medium text-dark-default'>{`${capitalize(user.firstName)} ${capitalize(user.lastName)}`}</span>
-              <div className='flex flex-row items-center space-x-2 rounded-sm bg-orange-200 p-1 pr-2 text-orange-700'>
+            <motion.button
+              className='flex flex-row items-center justify-between space-x-2 p-1 text-sm'
+              key={crypto.randomUUID()}
+              onClick={() => navigate(`/users/${user._id}`)}
+              animate='initial'
+              initial='initial'
+              whileHover='animate'
+            >
+              <motion.div variants={animation.user} className='flex items-center space-x-2 py-0.5 text-left font-medium text-dark-default'>
+                <motion.span variants={animation.arrow}>
+                  <CircleChevronRight size={16} strokeWidth={1.5} />
+                </motion.span>
+                <span>{`${capitalize(user.firstName)} ${capitalize(user.lastName)}`}</span>
+              </motion.div>
+              <motion.div
+                variants={animation.date}
+                className='flex w-fit flex-row items-center space-x-2 rounded-sm bg-orange-200 p-1 pr-2 text-orange-700'
+              >
                 <CalendarPlus size={16} strokeWidth={1.5} />
-                <p className='text-xs'>{format(user.createdAt, 'DD/MM/YY')}</p>
-              </div>
-            </button>
+                <p className='text-xs'>{format(user.createdAt, 'DD MMM')}</p>
+              </motion.div>
+            </motion.button>
           ))}
         </CardContent>
       </Card>
