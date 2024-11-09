a4b6dc72e4f3199fdb9817d1b031aa8561b5d8d6
alanloffler <alanmatiasloffler@gmail.com>
2024-11-09 19:02:36 -0300

    Handle error and loading states

--- src/pages/dashboard/components/latest-users/LatestUsers.tsx
+++ src/pages/dashboard/components/latest-users/LatestUsers.tsx
@@ -2,6 +2,9 @@
 import { CircleChevronRight, CalendarPlus } from 'lucide-react';
 // External components: https://ui.shadcn.com/docs/components
 import { Card, CardContent } from '@core/components/ui/card';
+// Components
+import { InfoCard } from '@core/components/common/InfoCard';
+import { LoadingDB } from '@core/components/common/LoadingDB';
 // External imports
 import { format } from '@formkit/tempo';
 import { motion } from 'framer-motion';
@@ -18,11 +21,16 @@ export function LatestUsers() {
   const capitalize = useCapitalize();
   const navigate = useNavigate();
 
-  const { data: latestUsers } = useQuery<IResponse>({
+  const {
+    data: latestUsers,
+    error,
+    isLoading,
+  } = useQuery<IResponse>({
     queryKey: ['dashboard', 'latestUsers'],
     queryFn: async () => {
       return await DashboardApiService.latestUsers(5);
     },
+    retry: 1,
   });
 
   const animation = {
@@ -44,31 +52,35 @@ export function LatestUsers() {
     <main className='space-y-2'>
       <h2 className='text-xl font-medium text-dark-default'>{DASHBOARD_CONFIG.latestUsers.title}</h2>
       <Card>
-        <CardContent className='flex flex-col space-y-1 pl-0 pt-6'>
-          {latestUsers?.data.map((user: IUser) => (
-            <motion.button
-              className='flex flex-row items-center justify-between space-x-2 p-1 text-sm'
-              key={crypto.randomUUID()}
-              onClick={() => navigate(`/users/${user._id}`)}
-              animate='initial'
-              initial='initial'
-              whileHover='animate'
-            >
-              <motion.div variants={animation.user} className='flex items-center space-x-2 py-0.5 text-left font-medium text-dark-default'>
-                <motion.span variants={animation.arrow}>
-                  <CircleChevronRight size={16} strokeWidth={1.5} />
-                </motion.span>
-                <span>{`${capitalize(user.firstName)} ${capitalize(user.lastName)}`}</span>
-              </motion.div>
-              <motion.div
-                variants={animation.date}
-                className='flex w-fit flex-row items-center space-x-2 rounded-sm bg-orange-200 p-1 pr-2 text-orange-700'
+        <CardContent className='flex flex-col space-y-1 pt-6'>
+          {error && <InfoCard text={error.message} type='error' />}
+          {isLoading && <LoadingDB text={DASHBOARD_CONFIG.latestUsers.loadingText} variant='default' />}
+          {!error &&
+            !isLoading &&
+            latestUsers?.data.map((user: IUser) => (
+              <motion.button
+                className='-ml-6 flex flex-row items-center justify-between space-x-2 p-1 text-sm'
+                key={crypto.randomUUID()}
+                onClick={() => navigate(`/users/${user._id}`)}
+                animate='initial'
+                initial='initial'
+                whileHover='animate'
               >
-                <CalendarPlus size={16} strokeWidth={1.5} />
-                <p className='text-xs'>{`${format(user.createdAt, 'DD')} ${capitalize(format(user.createdAt, 'MMM'))}`}</p>
-              </motion.div>
-            </motion.button>
-          ))}
+                <motion.div variants={animation.user} className='flex items-center space-x-2 py-0.5 text-left font-medium text-dark-default'>
+                  <motion.span variants={animation.arrow}>
+                    <CircleChevronRight size={16} strokeWidth={1.5} />
+                  </motion.span>
+                  <span>{`${capitalize(user.firstName)} ${capitalize(user.lastName)}`}</span>
+                </motion.div>
+                <motion.div
+                  variants={animation.date}
+                  className='flex w-fit flex-row items-center space-x-2 rounded-sm bg-orange-200 p-1 pr-2 text-orange-700'
+                >
+                  <CalendarPlus size={16} strokeWidth={1.5} />
+                  <p className='text-xs'>{`${format(user.createdAt, 'DD')} ${capitalize(format(user.createdAt, 'MMM'))}`}</p>
+                </motion.div>
+              </motion.button>
+            ))}
         </CardContent>
       </Card>
     </main>
