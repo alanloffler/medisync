b21528333b725c43b190000709bff2403fc06a8a
alanloffler <alanmatiasloffler@gmail.com>
2024-11-08 22:00:44 -0300

    Main structure component

--- src/pages/dashboard/components/latest-users/LatestUsers.tsx
+++ src/pages/dashboard/components/latest-users/LatestUsers.tsx
@@ -1,3 +1,15 @@
+// External components: https://ui.shadcn.com/docs/components
+import { DASHBOARD_CONFIG } from '@config/dashboard.config';
+// Imports
+import { Card, CardContent } from '@core/components/ui/card';
+// React component
 export function LatestUsers() {
-  return <>Latest users component</>;
+  return (
+    <main className='space-y-2'>
+      <h2 className='text-xl font-medium text-dark-default'>{DASHBOARD_CONFIG.latestUsers.title}</h2>
+      <Card>
+        <CardContent className='grid gap-2 pt-6'>Latest users component</CardContent>
+      </Card>
+    </main>
+  );
 }
