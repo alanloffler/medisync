1fb7c97bc05caffc93c39080605af028efee9606
alanloffler <alanmatiasloffler@gmail.com>
2024-11-10 21:18:57 -0300

    Common components config file

---
+++ src/config/common.config.ts
@@ -0,0 +1,12 @@
+export const REMOVE_DIALOG_CONFIG = {
+  default: {
+    title: 'Diálogo de eliminación',
+    description: 'Acción irreversible',
+  },
+  appointment: {
+    cancelButton: 'Cancelar',
+    description: 'Esta acción es irreversible. Vas a eliminar un turno.',
+    removeButton: 'Eliminar',
+    title: 'Eliminación de turno',
+  }
+};
