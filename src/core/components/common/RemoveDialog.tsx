47d845cabf8fe9c41c07d0642d8ed5c0036d78cd
alanloffler <alanmatiasloffler@gmail.com>
2024-11-11 11:14:17 -0300

    WIP remove dialog states and actions

--- src/core/components/common/RemoveDialog.tsx
+++ src/core/components/common/RemoveDialog.tsx
@@ -1,11 +1,17 @@
-import type { IResponse } from '@core/interfaces/response.interface';
+// External components: https://ui.shadcn.com/docs/components
+import { Button } from '@core/components/ui/button';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@core/components/ui/dialog';
-import { ReactNode, useEffect, useState } from 'react';
-import { TooltipWrapper } from './TooltipWrapper';
-import { Button } from '../ui/button';
-import { REMOVE_DIALOG_CONFIG } from '@config/common.config';
+// Components
+import { InfoCard } from '@core/components/common/InfoCard';
+import { LoadingDB } from '@core/components/common/LoadingDB';
+import { TooltipWrapper } from '@core/components/common/TooltipWrapper';
+// External imports
+import { ReactNode, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
-
+// Imports
+import type { IResponse } from '@core/interfaces/response.interface';
+import { REMOVE_DIALOG_CONFIG } from '@config/common.config';
+// Interfaces
 interface IRemoveDialog {
   action: () => Promise<IResponse | Error>;
   help: boolean;
@@ -21,46 +27,31 @@ interface IDialogTexts {
   removeButton?: string;
   cancelButton?: string;
 }
-
+// React component
 export function RemoveDialog({ action, help, dialogContent, dialogTexts, tooltip, triggerButton }: IRemoveDialog) {
   const [openDialog, setOpenDialog] = useState(false);
-  const [queryResponse, setQueryResponse] = useState<string>('');
 
-  useEffect(() => {
-    setQueryResponse('');
-  }, [openDialog]);
-
-  const { error } = useQuery({
+  const { error, isFetching, refetch } = useQuery({
     queryKey: ['remove-dialog'],
     queryFn: async () => {
       await action();
     },
     enabled: false,
+    // retry: 1,
   });
 
-  // async function handleAction(): Promise<void> {
-  //   const response = await action();
-
-  //   if (response instanceof Error) {
-  //     setQueryResponse('Internal server error');
-  //   }
-  //   if (!(response instanceof Error) && response.statusCode === 200) {
-  //     setOpenDialog(false);
-  //     setQueryResponse(response.message);
-  //   }
-  //   if (!(response instanceof Error) && response.statusCode > 399) {
-  //     setQueryResponse(response.message);
-  //   }
-  //   if (response instanceof Error) {
-  //     setQueryResponse(response.message);
-  //   }
-  // }
+  async function handleAction(): Promise<void> {
+    refetch();
+  }
 
   return (
     <>
       <TooltipWrapper tooltip={tooltip} help={help}>
         <Button
-          onClick={() => setOpenDialog(true)}
+          onClick={() => {
+            setOpenDialog(true);
+            console.log(dialogContent);
+          }}
           variant='tableHeader'
           size='miniIcon'
           className='border border-slate-300 bg-white transition-transform hover:scale-110 hover:border-sky-500 hover:bg-white hover:text-sky-500 hover:animate-in'
@@ -73,19 +64,19 @@ export function RemoveDialog({ action, help, dialogContent, dialogTexts, tooltip
           <DialogHeader>
             <DialogTitle className='text-xl'>{dialogTexts.title || REMOVE_DIALOG_CONFIG.default.title}</DialogTitle>
             <DialogDescription>{dialogTexts.description || REMOVE_DIALOG_CONFIG.default.description}</DialogDescription>
-            <section className='flex flex-col pt-2'>
-              {dialogContent}
-              <span>{queryResponse}</span>
-              {/* <span className=''>{`${response?.message}: ${response?.data.value1}`}</span> */}
-              <footer className='mt-5 flex justify-end space-x-4'>
-                <Button onClick={() => setOpenDialog(false)} variant='ghost'>
-                  {dialogTexts.cancelButton}
-                </Button>
-                <Button variant='remove'>
-                  {dialogTexts.removeButton}
-                </Button>
-              </footer>
+            <section className='flex flex-col space-y-2 pt-2'>
+              <section>{dialogContent}</section>
+              {error && <InfoCard text='Error al eliminar el turno' type='error' className='pt-6' />}
+              {isFetching && <LoadingDB size='xs' variant='default' text='Eliminando...' className='pt-6' />}
             </section>
+            <footer className='flex justify-end space-x-4 pt-6'>
+              <Button onClick={() => setOpenDialog(false)} variant='ghost'>
+                {dialogTexts.cancelButton}
+              </Button>
+              <Button onClick={handleAction} variant='remove'>
+                {dialogTexts.removeButton}
+              </Button>
+            </footer>
           </DialogHeader>
         </DialogContent>
       </Dialog>
