38c74f193f317c8018bb43cfe244cca21675257a
alanloffler <alanmatiasloffler@gmail.com>
2024-11-10 21:25:39 -0300

    WIP: Normal fetch changed with Tanstack React Query

--- src/core/components/common/RemoveDialog.tsx
+++ src/core/components/common/RemoveDialog.tsx
@@ -1,40 +1,90 @@
 import type { IResponse } from '@core/interfaces/response.interface';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@core/components/ui/dialog';
-import { ReactNode, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
+import { TooltipWrapper } from './TooltipWrapper';
+import { Button } from '../ui/button';
+import { REMOVE_DIALOG_CONFIG } from '@config/common.config';
+import { useQuery } from '@tanstack/react-query';
 
 interface IRemoveDialog {
-  action: () => Promise<IResponse>;
-  button: ReactNode;
-  removeLabel: string;
+  action: () => Promise<IResponse | Error>;
+  help: boolean;
+  dialogContent: ReactNode;
+  dialogTexts: IDialogTexts;
+  tooltip: string;
+  triggerButton: ReactNode;
 }
 
-export function RemoveDialog({ action, button, removeLabel }: IRemoveDialog) {
-  const [openDialog, setOpenDialog] = useState(false);
-  const [response, setResponse] = useState<IResponse | null>(null);
+interface IDialogTexts {
+  title?: string;
+  description?: string;
+  removeButton?: string;
+  cancelButton?: string;
+}
 
-  async function handleAction() {
-    const response = await action();
-    if (response.statusCode === 200) {
-      // setOpenDialog(false);
-    }
-    console.log(response);
-    setResponse(response);
-  }
+export function RemoveDialog({ action, help, dialogContent, dialogTexts, tooltip, triggerButton }: IRemoveDialog) {
+  const [openDialog, setOpenDialog] = useState(false);
+  const [queryResponse, setQueryResponse] = useState<string>('');
+
+  useEffect(() => {
+    setQueryResponse('');
+  }, [openDialog]);
+
+  const { error } = useQuery({
+    queryKey: ['remove-dialog'],
+    queryFn: async () => {
+      await action();
+    },
+  });
+
+  // async function handleAction(): Promise<void> {
+  //   const response = await action();
+
+  //   if (response instanceof Error) {
+  //     setQueryResponse('Internal server error');
+  //   }
+  //   if (!(response instanceof Error) && response.statusCode === 200) {
+  //     setOpenDialog(false);
+  //     setQueryResponse(response.message);
+  //   }
+  //   if (!(response instanceof Error) && response.statusCode > 399) {
+  //     setQueryResponse(response.message);
+  //   }
+  //   if (response instanceof Error) {
+  //     setQueryResponse(response.message);
+  //   }
+  // }
 
   return (
     <>
-      <button onClick={() => setOpenDialog(true)} className='bg-slate-600 text-white w-fit p-2 rounded-full'>{button}</button>
+      <TooltipWrapper tooltip={tooltip} help={help}>
+        <Button
+          onClick={() => setOpenDialog(true)}
+          variant='tableHeader'
+          size='miniIcon'
+          className='border border-slate-300 bg-white transition-transform hover:scale-110 hover:border-sky-500 hover:bg-white hover:text-sky-500 hover:animate-in'
+        >
+          {triggerButton}
+        </Button>
+      </TooltipWrapper>
       <Dialog open={openDialog} onOpenChange={setOpenDialog}>
         <DialogContent>
           <DialogHeader>
-            <DialogTitle className='text-xl'>Remove dialog</DialogTitle>
-            <DialogDescription className='sr-only'></DialogDescription>
-            <div className='flex flex-col pt-2'>
-              <span className=''>{`${response?.message}: ${response?.data.value1}`}</span>
-              <div className='mt-5 flex justify-end space-x-4'>
-                <button onClick={handleAction}>{removeLabel}</button>
-              </div>
-            </div>
+            <DialogTitle className='text-xl'>{dialogTexts.title || REMOVE_DIALOG_CONFIG.default.title}</DialogTitle>
+            <DialogDescription>{dialogTexts.description || REMOVE_DIALOG_CONFIG.default.description}</DialogDescription>
+            <section className='flex flex-col pt-2'>
+              {dialogContent}
+              <span>{queryResponse}</span>
+              {/* <span className=''>{`${response?.message}: ${response?.data.value1}`}</span> */}
+              <footer className='mt-5 flex justify-end space-x-4'>
+                <Button onClick={() => setOpenDialog(false)} variant='ghost'>
+                  {dialogTexts.cancelButton}
+                </Button>
+                <Button onClick={handleAction} variant='remove'>
+                  {dialogTexts.removeButton}
+                </Button>
+              </footer>
+            </section>
           </DialogHeader>
         </DialogContent>
       </Dialog>
