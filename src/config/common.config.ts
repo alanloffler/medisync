1b62c7798314778e2ca1e23776c2d6872857baeb
alanloffler <alanmatiasloffler@gmail.com>
2024-11-11 20:47:16 -0300

    Default and appointment deleting message

--- src/config/common.config.ts
+++ src/config/common.config.ts
@@ -1,11 +1,13 @@
 export const REMOVE_DIALOG_CONFIG = {
   default: {
-    title: 'Diálogo de eliminación',
+    deleting: 'Eliminando',
     description: 'Acción irreversible',
+    title: 'Diálogo de eliminación',
   },
   appointment: {
     cancelButton: 'Cancelar',
     content: 'Vas a eliminar el turno del paciente',
+    deleting: 'Eliminando turno',
     description: 'Esta acción es irreversible. Vas a eliminar un turno.',
     removeButton: 'Eliminar',
     title: 'Eliminación de turno',
