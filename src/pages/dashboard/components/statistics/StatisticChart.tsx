d95890a83693a6054c33aab622837e0f8a27a34c
alanloffler <alanmatiasloffler@gmail.com>
2024-11-12 19:23:40 -0300

    Statistic chart component created

---
+++ src/pages/dashboard/components/statistics/StatisticChart.tsx
@@ -0,0 +1,6 @@
+// External components: https://ui.shadcn.com/docs/components
+import { Card } from '@core/components/ui/card';
+// React component
+export function StatisticChart() {
+  return <Card>this is a chart</Card>;
+}
