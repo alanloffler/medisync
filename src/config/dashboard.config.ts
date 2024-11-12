c11a2ca8a462939a2b2542d1d5cbd4229837282d
alanloffler <alanmatiasloffler@gmail.com>
2024-11-12 15:41:19 -0300

    Breadcrumb removed

--- src/config/dashboard.config.ts
+++ src/config/dashboard.config.ts
@@ -1,8 +1,4 @@
 export const DASHBOARD_CONFIG = {
-  breadcrumb: [
-    { id: 1, name: 'Inicio', path: '/' },
-    { id: 2, name: 'Dashboard', path: '/dashboard' },
-  ],
   categoriesShortcuts: {
     loadingText: 'Cargando especialidades',
     title: 'Categorías',
