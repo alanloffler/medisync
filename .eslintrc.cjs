03f2b72ab9daa2ffe33bfdb7cc7c6e876c5bdff7
alanloffler <alanmatiasloffler@gmail.com>
2024-11-20 17:10:34 -0300

    ..

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,5 @@
+
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
@@ -6,8 +8,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     '@typescript-eslint/no-explicit-any': 'off',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-  extends: ['plugin:@tanstack/query/recommended'],
+  "extends": ["plugin:@tanstack/query/recommended"]
 };
