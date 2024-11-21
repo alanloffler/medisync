89d9009eb8c8cf2fc4c44845977848cd9670b634
alanloffler <alanmatiasloffler@gmail.com>
2024-11-20 21:53:47 -0300

    input hover color

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,8 +17,10 @@ module.exports = {
       },
       colors: {
         border: 'hsl(var(--border))',
-        input: '#f1f5f9', // slate-100
-        'input-hover': 'rgb(226 232 240 / 0.7)',
+        input: {
+          DEFAULT: '#f1f5f9', // slate-100
+          hover: 'rgb(226 232 240 / 0.7)',
+        },
         ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
