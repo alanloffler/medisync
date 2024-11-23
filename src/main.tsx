9f27b9de0e1ddd6914b81eec6d77a904d15e3cff
alanloffler <alanmatiasloffler@gmail.com>
2024-11-22 21:37:01 -0300

    QueryClient app implementation

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import './index.css';
 import App from './App.tsx';
 // import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@lib/react-query.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
+  </QueryClientProvider>,
   // </React.StrictMode>,
 );
