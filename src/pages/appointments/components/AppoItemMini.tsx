f8546fc4b458953d1665e68bb119cbe3bf27eaea
alanloffler <alanmatiasloffler@gmail.com>
2024-11-23 20:14:34 -0300

    Padding on x axis fixed to be equal to table

--- src/pages/appointments/components/AppoItemMini.tsx
+++ src/pages/appointments/components/AppoItemMini.tsx
@@ -8,7 +8,7 @@ export function AppoItemMini({ data }: { data: IAppointment }) {
   const capitalize = useCapitalize();
 
   return (
-    <button className='w-full rounded-sm flex items-center space-x-4 border border-transparent hover:border hover:border-slate-200 py-1 pr-2 pl-1'>
+    <button className='w-full rounded-sm flex items-center space-x-4 border border-transparent hover:border hover:border-slate-200 py-1 px-1'>
       <div className='flex w-[135px] items-center space-x-3'>
         <p className='rounded-sm bg-slate-200 px-2 py-1 text-xsm text-slate-600'>{format(data.day, 'DD/MM/YY')}</p>
         <p className='rounded-sm bg-emerald-100 px-2 py-1 text-xsm text-emerald-600'>{data.hour}</p>
