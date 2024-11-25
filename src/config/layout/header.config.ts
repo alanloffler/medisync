99f906d4512c35e13ae8f7cce3a0a48e875e52d6
alanloffler <alanmatiasloffler@gmail.com>
2024-11-24 21:55:11 -0300

    Updated header objects with key data for i18n (header menu and quick actions button)

--- src/config/layout/header.config.ts
+++ src/config/layout/header.config.ts
@@ -1,16 +1,17 @@
 export const HEADER_CONFIG = {
   appName: 'MediSync',
   headerMenu: [
-    { id: 1, title: 'Tablero', path: '/dashboard' },
-    { id: 2, title: 'Turnos', path: '/appointments' },
-    { id: 3, title: 'Profesionales', path: '/professionals' },
-    { id: 4, title: 'Pacientes', path: '/users' },
+    { id: 1, key: 'headerMenu.dashboard', path: '/dashboard' },
+    { id: 2, key: 'headerMenu.appointments', path: '/appointments' },
+    { id: 3, key: 'headerMenu.professionals', path: '/professionals' },
+    { id: 4, key: 'headerMenu.users', path: '/users' },
   ],
   actionsButton: [
-    { id: 1, menuId: 2, title: 'Turno', path: '/reserve', default: true },
-    { id: 2, menuId: 4, title: 'Paciente', path: '/users/create' },
-    { id: 3, menuId: 3, title: 'Profesional', path: '/professionals/create' },
+    { id: 1, key: 'headerMenu.appointments', menuId: 2, path: '/reserve', default: true },
+    { id: 2, key: 'headerMenu.users', menuId: 4, path: '/users/create' },
+    { id: 3, key: 'headerMenu.professionals', menuId: 3, path: '/professionals/create' },
   ],
+  // Until here is used on header menu
   user: {
     title: 'Mi cuenta',
     menuItems: [
