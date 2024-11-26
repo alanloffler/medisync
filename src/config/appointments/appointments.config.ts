8c1669618027b89c400d674def1c52b0ce131ab0
alanloffler <alanmatiasloffler@gmail.com>
2024-11-26 17:40:36 -0300

    Order of content

--- src/config/appointments/appointments.config.ts
+++ src/config/appointments/appointments.config.ts
@@ -1,12 +1,10 @@
 export const APPO_CONFIG = {
-  // In use
-  pagination: {
-    defaultItemsPerPage: 20,
-    itemsPerPage: [10, 20, 50],
-  },
-  // In use
   breadcrumb: [
     { id: 1, name: 'breadcrumb.home', path: '/' },
     { id: 2, name: 'breadcrumb.appointments', path: '' },
   ],
+  pagination: {
+    defaultItemsPerPage: 20,
+    itemsPerPage: [10, 20, 50],
+  },
 };
