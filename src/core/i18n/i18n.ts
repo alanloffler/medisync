02392e1ffe4755a341d0728764b8aea381403fea
alanloffler <alanmatiasloffler@gmail.com>
2024-11-26 20:24:46 -0300

    i18next-http-backend implemented

--- src/core/i18n/i18n.ts
+++ src/core/i18n/i18n.ts
@@ -2,21 +2,23 @@
 import en from './lang/en.json';
 import es from './lang/es.json';
 import i18n from 'i18next';
+import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
 i18n
-.use(initReactI18next)
-.init({
-  // lng: APP_CONFIG.i18n.locale,
-  lng: 'es',
-  fallbackLng: 'en',
-  interpolation: {
-    escapeValue: false,
-  },
-  resources: {
-    en: { translation: en },
-    es: { translation: es },
-  },
-});
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    // lng: APP_CONFIG.i18n.locale,
+    lng: 'es',
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      en: { translation: en },
+      es: { translation: es },
+    },
+  });
 
 export default i18n;
