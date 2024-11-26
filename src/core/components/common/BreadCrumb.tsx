64ba5e046c896fe98909546db10ec6a51d4db299
alanloffler <alanmatiasloffler@gmail.com>
2024-11-26 17:23:07 -0300

    Full translated

--- src/core/components/common/BreadCrumb.tsx
+++ src/core/components/common/BreadCrumb.tsx
@@ -1,12 +1,16 @@
 // Icons: https://lucide.dev/icons/
 import { ChevronRight } from 'lucide-react';
-// Components: https://ui.shadcn.com/docs/components
+// External components: https://ui.shadcn.com/docs/components
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage } from '@core/components/ui/breadcrumb';
-// App
-import type { IBreadcrumb } from '@core/components/common/interfaces/breadcrumb.interface';
+// External imports
 import { Link } from 'react-router-dom';
+// Imports
+import type { IBreadcrumb } from '@core/components/common/interfaces/breadcrumb.interface';
+import { useTranslation } from 'react-i18next';
 // React component
 export function BreadCrumb({ paths }: { paths: IBreadcrumb[] }) {
+  const { t } = useTranslation();
+
   return (
     <Breadcrumb>
       <BreadcrumbList>
@@ -14,13 +18,13 @@ export function BreadCrumb({ paths }: { paths: IBreadcrumb[] }) {
           index !== paths.length - 1 ? (
             <BreadcrumbItem key={item.id} className='text-xsm'>
               <BreadcrumbLink asChild>
-                <Link to={item.path}>{item.name}</Link>
+                <Link to={item.path}>{t(item.name)}</Link>
               </BreadcrumbLink>
               <ChevronRight className='h-3.5 w-3.5' />
             </BreadcrumbItem>
           ) : (
             <BreadcrumbItem key={item.id} className='text-xsm'>
-              <BreadcrumbPage>{item.name}</BreadcrumbPage>
+              <BreadcrumbPage>{t(item.name)}</BreadcrumbPage>
             </BreadcrumbItem>
           ),
         )}
