7a9a80fdfa8baf905e1006eec3e9a0da543465cc
alanloffler <alanmatiasloffler@gmail.com>
2024-11-26 17:23:18 -0300

    Full translated

--- src/core/components/common/PageHeader.tsx
+++ src/core/components/common/PageHeader.tsx
@@ -1,13 +1,17 @@
 // Components
 import { BreadCrumb } from '@core/components/common/BreadCrumb';
+// External imports
+import { useTranslation } from 'react-i18next';
 // Imports
 import type { IBreadcrumb } from '@core/components/common/interfaces/breadcrumb.interface';
 // React component
 export function PageHeader({ title, breadcrumb }: { title: string; breadcrumb: IBreadcrumb[] }) {
+  const { t } = useTranslation();
+
   return (
     <div className='w-fit space-y-3'>
       <BreadCrumb paths={breadcrumb} />
-      <h1 className='text-lg font-semibold leading-none tracking-normal'>{title}</h1>
+      <h1 className='text-lg font-semibold leading-none tracking-normal'>{t(title)}</h1>
     </div>
   );
 }
