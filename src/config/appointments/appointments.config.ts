8c62f7a7eace0b0cab8d094240472615efcb1e4a
alanloffler <alanmatiasloffler@gmail.com>
2024-11-26 17:38:52 -0300

    Content for reserve appointments extracted to reserve-appointment config file

--- src/config/appointments/appointments.config.ts
+++ src/config/appointments/appointments.config.ts
@@ -1,80 +1,12 @@
 export const APPO_CONFIG = {
   // In use
   pagination: {
-      defaultItemsPerPage: 20,
-      itemsPerPage: [10, 20, 50],
+    defaultItemsPerPage: 20,
+    itemsPerPage: [10, 20, 50],
   },
   // In use
   breadcrumb: [
     { id: 1, name: 'breadcrumb.home', path: '/' },
     { id: 2, name: 'breadcrumb.appointments', path: '' },
   ],
-  // In use
-  calendar: {
-    yearsRange: 1,
-  },
-
-  // This is for reserve appointment
-  combobox: {
-    loadingText: 'Cargando profesionales',
-    notFoundText: 'Profesional no encontrado',
-    placeholder: 'Profesionales',
-    searchText: 'Buscar...',
-  },
-  dialog: {
-    reserve: {
-
-      summary: {
-        line1: 'Reserva de turno para',
-        line2: 'El día',
-        line3: 'A las',
-      },
-    },
-
-    userCombobox: {
-      dniLabel: '- DNI',
-      noResults: 'Búsqueda sin resultados',
-    },
-  },
-  phrases: {
-    alreadyReservedPlural: 'turnos reservados',
-    alreadyReservedSingular: 'turno reservado',
-    availableAppointmentPlural: 'turnos disponibles',
-    availableAppointmentSingular: 'turno disponible',
-    availableDays: 'Días de atención:',
-    notAvailable: 'No disponible',
-  },
-
-  words: {
-    from: 'de',
-    hours: 'hs.',
-    hoursSeparator: '-',
-    schedule: 'Horarios:',
-    shiftPrefix: 'T',
-    to: 'a',
-    unavailable: '-',
-  },
-};
-
-export const VIEW_APPOINTMENT_CONFIG = {
-  breadcrumb: [
-    { id: 1, name: 'Inicio', path: '/' },
-    { id: 2, name: 'Turnos', path: '/appointments' },
-    { id: 3, name: 'Detalle', path: '' },
-  ],
-  button: {
-    back: 'Volver',
-  },
-  cardTitle: 'Turno',
-  email: {
-    body: 'Este es el mensaje de tu turno',
-    subject: 'Tu turno para',
-  },
-  loading: {
-    appointmentDetails: 'Cargando detalles del turno',
-  },
-  title: 'Detalle de turno',
-  words: {
-    hoursAbbreviation: 'hs.',
-  },
 };
