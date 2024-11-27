1a5326076e8954e8cf9d4ede929de08b81b1d72e
alanloffler <alanmatiasloffler@gmail.com>
2024-11-26 23:31:22 -0300

    Content on own config file for view appointment

--- src/config/appointments/reserve-appointments.config.ts
+++ src/config/appointments/reserve-appointments.config.ts
@@ -3,26 +3,3 @@ export const RESERVE_APPOINTMENT_CONFIG = {
     yearsRange: 1,
   },
 };
-
-export const VIEW_APPOINTMENT_CONFIG = {
-  breadcrumb: [
-    { id: 1, name: 'Inicio', path: '/' },
-    { id: 2, name: 'Turnos', path: '/appointments' },
-    { id: 3, name: 'Detalle', path: '' },
-  ],
-  button: {
-    back: 'Volver',
-  },
-  cardTitle: 'Turno',
-  email: {
-    body: 'Este es el mensaje de tu turno',
-    subject: 'Tu turno para',
-  },
-  loading: {
-    appointmentDetails: 'Cargando detalles del turno',
-  },
-  title: 'Detalle de turno',
-  words: {
-    hoursAbbreviation: 'hs.',
-  },
-};
---
+++ src/config/appointments/view-appointment.config.ts
@@ -0,0 +1,22 @@
+export const VIEW_APPOINTMENT_CONFIG = {
+  breadcrumb: [
+    { id: 1, name: 'Inicio', path: '/' },
+    { id: 2, name: 'Turnos', path: '/appointments' },
+    { id: 3, name: 'Detalle', path: '' },
+  ],
+  button: {
+    back: 'Volver',
+  },
+  cardTitle: 'Turno',
+  email: {
+    body: 'Este es el mensaje de tu turno',
+    subject: 'Tu turno para',
+  },
+  loading: {
+    appointmentDetails: 'Cargando detalles del turno',
+  },
+  title: 'Detalle de turno',
+  words: {
+    hoursAbbreviation: 'hs.',
+  },
+};
