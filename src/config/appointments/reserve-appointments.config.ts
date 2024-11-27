18bbd928e633152c72c35c83a732dc4e388a38f6
alanloffler <alanmatiasloffler@gmail.com>
2024-11-26 23:26:49 -0300

    Removed unused content. Replaced by translation

--- src/config/appointments/reserve-appointments.config.ts
+++ src/config/appointments/reserve-appointments.config.ts
@@ -1,45 +1,6 @@
 export const RESERVE_APPOINTMENT_CONFIG = {
-  // In use
   calendar: {
     yearsRange: 1,
-  },
-  combobox: {
-    loadingText: 'Cargando profesionales',
-    notFoundText: 'Profesional no encontrado',
-    placeholder: 'Profesionales',
-    searchText: 'Buscar...',
-  },
-  dialog: {
-    reserve: {
-      summary: {
-        line1: 'Reserva de turno para',
-        line2: 'El día',
-        line3: 'A las',
-      },
-    },
-
-    userCombobox: {
-      dniLabel: '- DNI',
-      noResults: 'Búsqueda sin resultados',
-    },
-  },
-  phrases: {
-    alreadyReservedPlural: 'turnos reservados',
-    alreadyReservedSingular: 'turno reservado',
-    availableAppointmentPlural: 'turnos disponibles',
-    availableAppointmentSingular: 'turno disponible',
-    availableDays: 'Días de atención:',
-    notAvailable: 'No disponible',
-  },
-
-  words: {
-    from: 'de',
-    hours: 'hs.',
-    hoursSeparator: '-',
-    schedule: 'Horarios:',
-    shiftPrefix: 'T',
-    to: 'a',
-    unavailable: '-',
   },
 };
 
