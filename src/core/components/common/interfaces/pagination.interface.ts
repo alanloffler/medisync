ab88248942830c55fa094dd4b50f1259e5d56871
alanloffler <alanmatiasloffler@gmail.com>
2024-11-28 19:00:29 -0300

    Interface for tanstack table pagination

--- src/core/components/common/interfaces/pagination.interface.ts
+++ src/core/components/common/interfaces/pagination.interface.ts
@@ -3,6 +3,7 @@ import type { IUser } from '@users/interfaces/user.interface';
 import type { PaginationState, Table as ITable } from '@tanstack/react-table';
 
 export interface IPagination {
+  className?: string;
   help?: boolean;
   pagination: PaginationState;
   setPagination: Dispatch<SetStateAction<PaginationState>>;
