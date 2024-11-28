ed44e8f412d65a562326ccf65decece94b0dcec4
alanloffler <alanmatiasloffler@gmail.com>
2024-11-28 18:20:12 -0300

    Removed unused content. Replaced by translation

--- src/config/users/users.config.ts
+++ src/config/users/users.config.ts
@@ -1,67 +1,16 @@
-// TODO: when each section is in his own file, this file should be moved to the config folder
 export const USER_CONFIG = {
   breadcrumb: [
     { id: 1, name: 'breadcrumb.home', path: '/' },
     { id: 2, name: 'breadcrumb.users', path: '/users' },
   ],
-  // In use
   search: {
     debounceTime: 500,
   },
-
-  // buttons: {
-    // cancel: 'Cancelar',
-    // createUser: 'Agregar paciente',
-    // remove: 'Eliminar paciente',
-    // removing: 'Eliminando',
-  // },
-  dbUsersPlural: 'pacientes en la base de datos',
-  dbUsersSingular: 'paciente en la base de datos',
-  dialog: {
-    // button: {
-    //   close: 'Cerrar',
-    // },
-    remove: {
-      content: {
-        dni: 'DNI',
-        title: 'Vas a eliminar de la base de datos al paciente:',
-      },
-      // subtitle: '¿Estas seguro de querer eliminar el paciente? Esta acción es irreversible.',
-      // title: 'Eliminar paciente',
-    },
-  },
-
   table: {
-    databaseCount: {
-      difference: 'que el mes pasado',
-      totalPlural: 'pacientes en total',
-      totalSingular: 'paciente en total',
-    },
     defaultPageSize: 10,
     defaultSortingId: 'lastName',
     defaultSortingType: false, // desc: true | false
-    headers: ['Apellido y Nombre', 'DNI', 'Teléfono', 'Acciones'],
+    header: ['table.header.id', 'table.header.user', 'table.header.identityCard', 'table.header.phone', 'table.header.actions'],
     itemsPerPage: [5, 10, 20],
-    noResults: 'No hay resultados',
-    pagination: {
-      page: 'Página',
-      of: 'de',
-    },
-    rowsPerPage: 'Pacientes por página',
-    tooltip: {
-      button: {
-        delete: 'Eliminar',
-        edit: 'Editar',
-        sendWhatsApp: 'Enviar WhatsApp',
-        view: 'Ver detalles',
-      },
-      pagination: {
-        firstPage: 'Primera página',
-        itemsPerPage: 'Filas por página',
-        lastPage: 'Última página',
-        nextPage: 'Página siguiente',
-        prevPage: 'Página anterior',
-      },
-    },
   },
 };
