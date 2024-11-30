890f6fb446911400761f5e9df9fe6dd42464a8b0
alanloffler <alanmatiasloffler@gmail.com>
2024-11-29 22:17:39 -0300

    TData type parameter for use in multiple data types

--- src/core/components/common/interfaces/pagination.interface.ts
+++ src/core/components/common/interfaces/pagination.interface.ts
@@ -1,11 +1,10 @@
 import type { Dispatch, SetStateAction } from 'react';
-import type { IUser } from '@users/interfaces/user.interface';
-import type { PaginationState, Table as ITable } from '@tanstack/react-table';
+import type { PaginationState, Table } from '@tanstack/react-table';
 
-export interface IPagination {
+export interface IPagination<TData> {
   className?: string;
   help?: boolean;
   pagination: PaginationState;
   setPagination: Dispatch<SetStateAction<PaginationState>>;
-  table: ITable<IUser>;
+  table: Table<TData>;
 }
