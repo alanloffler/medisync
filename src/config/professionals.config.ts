52966a771093c6d080c70f483a15abc1ac927aa7
alanloffler <alanmatiasloffler@gmail.com>
2024-12-01 19:53:42 -0300

    Relocated content for professional view

--- src/config/professionals.config.ts
+++ src/config/professionals.config.ts
@@ -73,57 +73,6 @@ export const PROF_UPDATE_CONFIG = {
   },
   title: 'Actualizar profesional',
 };
-
-export const PROF_VIEW_CONFIG = {
-  breadcrumb: [
-    { id: 1, name: 'Inicio', path: '/' },
-    { id: 2, name: 'Profesionales', path: '/professionals' },
-    { id: 3, name: 'Detalles', path: '/professionals/:id' },
-  ],
-  button: {
-    back: 'Volver',
-    cancel: 'Cancelar',
-    deleteProfessional: 'Eliminar profesional',
-    goToProfessionals: 'Ir a profesionales',
-  },
-  dialog: {
-    title: 'Eliminar profesional',
-    description: 'Acción irreversible. Al eliminar al profesional no vas a poder restaurar sus datos.',
-  },
-  dropdownMenu: [
-    { id: 1, name: 'Enviar e-mail' },
-    { id: 2, name: 'Enviar WhatsApp' },
-    { id: 3, name: 'Editar profesional' },
-  ],
-  email: {
-    subject: 'MediSync - Turnos médicos',
-    body: ['Hola', ','],
-  },
-  phrases: {
-    scheduleTitle: 'Días y horario de atención',
-    contactTitle: 'Contacto',
-  },
-  // WIP: this traanslated and maybe in root file because is used in another components
-  select: [
-    { id: 0, label: 'Activo', value: true },
-    { id: 1, label: 'Inactivo', value: false },
-  ],
-  title: 'Detalles del profesional',
-  tooltip: {
-    delete: 'Eliminar',
-    edit: 'Editar',
-    sendEmail: 'Enviar e-mail',
-    sendWhatsApp: 'Enviar WhatsApp',
-    share: 'Compartir',
-  },
-  words: {
-    and: 'y',
-    error: 'Error',
-    hoursSeparator: 'a',
-    slotsSeparator: '-',
-  },
-};
-
 export const PROFESSIONALS_SELECT_CONFIG = {
   label: 'Profesional',
   placeholder: 'Seleccionar',
