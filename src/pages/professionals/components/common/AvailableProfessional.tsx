5e41f99344d3beeaa574bd6dc73e063b63d18e33
alanloffler <alanmatiasloffler@gmail.com>
2024-12-01 20:15:17 -0300

    Help store implemented via tooltip wrapper

--- src/pages/professionals/components/common/AvailableProfessional.tsx
+++ src/pages/professionals/components/common/AvailableProfessional.tsx
@@ -1,5 +1,7 @@
 // External components: https://ui.shadcn.com/docs/components
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@core/components/ui/select';
+// Components
+import { TooltipWrapper } from '@core/components/common/TooltipWrapper';
 // External imports
 import { useMutation } from '@tanstack/react-query';
 import { useState } from 'react';
@@ -14,10 +16,11 @@ import { useNotificationsStore } from '@core/stores/notifications.store';
 interface IAvailableProfessional {
   className?: string;
   data: Partial<IProfessional>;
+  help: boolean;
   items: { id: number; label: string; value: boolean }[];
 }
 // React component
-export function AvailableProfessional({ className, data, items }: IAvailableProfessional) {
+export function AvailableProfessional({ className, data, help, items }: IAvailableProfessional) {
   const { _id, available } = data;
   const [prevValue, setPrevValue] = useState<string>(String(available));
   const [showError, setShowError] = useState<boolean>(false);
@@ -51,9 +54,11 @@ export function AvailableProfessional({ className, data, items }: IAvailableProf
   return (
     <main className='flex flex-row items-center space-x-1'>
       <Select value={value} onValueChange={handleValueChange}>
-        <SelectTrigger className={cn('h-8 w-fit space-x-2 bg-transparent px-2 py-1 text-xs hover:bg-input', className)}>
-          <SelectValue placeholder='Select a fruit' />
-        </SelectTrigger>
+        <TooltipWrapper tooltip={t('tooltip.availability')} help={help}>
+          <SelectTrigger className={cn('h-8 w-fit space-x-2 bg-transparent px-2 py-1 text-xs hover:bg-input', className)}>
+            <SelectValue placeholder='Select a fruit' />
+          </SelectTrigger>
+        </TooltipWrapper>
         <SelectContent onCloseAutoFocus={(e) => e.preventDefault()}>
           <SelectGroup>
             <SelectItem value={items ? items[0].value.toString() : 'true'} className='px-2 py-1 text-xs focus:bg-input [&_svg]:hidden'>
