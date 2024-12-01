f47555f1cd345e3eecf9f3792cb219df33ab5ac6
alanloffler <alanmatiasloffler@gmail.com>
2024-12-01 19:25:19 -0300

    Professional create config

---
+++ src/config/professionals/professional-create.config.ts
@@ -0,0 +1,11 @@
+export const PROFESSIONAL_CREATE_CONFIG = {
+  breadcrumb: [
+    { id: 1, name: 'breadcrumb.home', path: '/' },
+    { id: 2, name: 'breadcrumb.professionals', path: '/professionals' },
+    { id: 3, name: 'breadcrumb.create', path: '/professionals/create' },
+  ],
+  dropdownMenu: [
+    { id: 1, name: 'button.addArea', path: '/' },
+    { id: 2, name: 'button.addSpecialty', path: '/' },
+  ],
+};
