57f40ec93e59bd9b4b8340c68ea4fb1cba994a02
alanloffler <alanmatiasloffler@gmail.com>
2024-12-01 10:24:07 -0300

    Items per page property

--- src/core/components/common/interfaces/pagination.interface.ts
+++ src/core/components/common/interfaces/pagination.interface.ts
@@ -4,6 +4,7 @@ import type { PaginationState, Table } from '@tanstack/react-table';
 export interface IPagination<TData> {
   className?: string;
   help?: boolean;
+  itemsPerPage?: number[];
   pagination: PaginationState;
   setPagination: Dispatch<SetStateAction<PaginationState>>;
   table: Table<TData>;
