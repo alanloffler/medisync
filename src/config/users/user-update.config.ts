dbfb5c194a3b631493b674f583f8204d3bece664
alanloffler <alanmatiasloffler@gmail.com>
2024-12-02 11:20:51 -0300

    Fix: updated breadcrumb path

--- src/config/users/user-update.config.ts
+++ src/config/users/user-update.config.ts
@@ -2,6 +2,6 @@ export const USER_UPDATE_CONFIG = {
   breadcrumb: [
     { id: 1, name: 'breadcrumb.home', path: '/' },
     { id: 2, name: 'breadcrumb.users', path: '/users' },
-    { id: 3, name: 'breadcrumb.update', path: '/users/update/:id' },
+    { id: 3, name: 'breadcrumb.userUpdate', path: '/users/update/:id' },
   ],
 };
