e7c14b54a1996169515ce24245c75a8a8861d223
alanloffler <alanmatiasloffler@gmail.com>
2024-12-03 14:59:36 -0300

    Wip: openai service for translations

---
+++ src/lib/openai.service.ts
@@ -0,0 +1,28 @@
+import { OpenAI } from 'openai';
+
+const openai = new OpenAI({
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
+
+export async function translateOpenAi(content: string, lng: string) {
+  const completion = await openai.chat.completions.create({
+    model: 'gpt-4o',
+    temperature: 0.5,
+    messages: [{ role: 'user', content: `Act as a translator. Translate this text to ${lng}: ${content}` }],
+  });
+  console.log(completion);
+  return completion;
+}
+
+export class OpenAIService {
+  // private static readonly OPENAI_API_KEY: string = import.meta.env.VITE_OPENAI_API_KEY;
+
+  public static async translate(content: string, language: string) {
+    console.log('OPENAI Translation');
+    console.log('--------------------');
+    console.log('Content:', content);
+    console.log('Translate to: ', language);
+    console.log('Default language: ');
+  }
+}
