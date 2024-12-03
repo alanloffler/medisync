86e9a6cf32d414a0ef40fecfc637f5d8944b601d
alanloffler <alanmatiasloffler@gmail.com>
2024-12-03 15:10:31 -0300

    Fetch response as TRQ response

--- src/core/services/area.service.ts
+++ src/core/services/area.service.ts
@@ -1,18 +1,34 @@
+import i18next from 'i18next';
+import type { IResponse } from '@core/interfaces/response.interface';
+import { EMethods } from '@core/enums/methods.enum';
+
 export class AreaService {
   private static readonly API_URL: string = import.meta.env.VITE_API_URL;
 
   public static async findAll() {
+    const url: string = `${this.API_URL}/areas`;
+    return await this.fetch(url, EMethods.GET);
+  }
+
+  private static async fetch(url: string, method: EMethods, body?: any): Promise<IResponse<any>> {
     try {
-      const fetchData = await fetch(`${this.API_URL}/areas/active`, {
-        method: 'GET',
+      const query: Response = await fetch(url, {
+        method: method,
         headers: {
           'content-type': 'application/json;charset=UTF-8',
         },
+        body: JSON.stringify(body),
       });
-      
-      return await fetchData.json();
+
+      const response: IResponse<any> = await query.json();
+      if (!query.ok) throw new Error(response.message);
+
+      return response;
     } catch (error) {
-      return error;
+      if (error instanceof TypeError) {
+        throw new Error(i18next.t('error.internalServer'));
+      }
+      throw error;
     }
   }
 }
