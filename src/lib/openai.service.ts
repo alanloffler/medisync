64a6d64a34071067048e71219efd10ccd866c718
alanloffler <alanmatiasloffler@gmail.com>
2024-12-03 22:27:19 -0300

    Working translation service with OpenAI API

--- src/lib/openai.service.ts
+++ src/lib/openai.service.ts
@@ -4,24 +4,23 @@ const OPENAI_API_KEY: string = import.meta.env.VITE_OPENAI_API_KEY;
 
 const openai = new OpenAI({
   apiKey: OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
 });
 
-export async function translateOpenAi(content: string, lng: string) {
-  const completion = await openai.chat.completions.create({
-    model: 'gpt-4o',
-    temperature: 0.5,
-    messages: [{ role: 'user', content: `Act as a translator. Translate this text to ${lng}: ${content}` }],
-  });
-  // console.log(completion);
-  return completion;
-}
-
 export class OpenAIService {
+  // TODO: catch errors
   public static async translate(content: string, language: string) {
-    console.log('OPENAI Translation');
-    console.log('--------------------');
-    console.log('Content:', content);
-    console.log('Translate to: ', language);
-    console.log('Default language: ');
+    const translation = await openai.chat.completions.create({
+      model: 'gpt-4o',
+      temperature: 0.5,
+      messages: [
+        {
+          role: 'system',
+          content: `Act as a translator. Translate the word ${content} to ${language}. Do not include explanations.`,
+        },
+      ],
+    });
+    
+    return translation.choices[0].message.content;
   }
 }
