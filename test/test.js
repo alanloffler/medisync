4a7a66321573dc79f9b6844170271956540d65c9
alanloffler <alanmatiasloffler@gmail.com>
2024-12-04 19:26:22 -0300

    Update: test slice

--- test/test.js
+++ test/test.js
@@ -1,10 +1,16 @@
-const selectedLanguage = 'es';
+// const selectedLanguage = 'es';
 
-const area = {
-  _id: '6661f82135345c8c83e2b916',
-  name: 'medicina',
-  plural: 'medicinas',
-  description: 'Área de médicina en general',
-  active: 1,
-};
-console.log(area, selectedLanguage);
+// const area = {
+//   _id: '6661f82135345c8c83e2b916',
+//   name: 'medicina',
+//   plural: 'medicinas',
+//   description: 'Área de médicina en general',
+//   active: 1,
+// };
+// console.log(area, selectedLanguage);
+
+const date = '12/12/24';
+
+const shorten = date.slice(0, -3);
+
+console.log(shorten);
