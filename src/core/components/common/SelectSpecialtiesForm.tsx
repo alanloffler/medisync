fc20a1a4d5e1cf693b196c0ffe2ad406cf099993
alanloffler <alanmatiasloffler@gmail.com>
2024-12-03 22:27:49 -0300

    Working implementation of translation in real time. Disabled now

--- src/core/components/common/SelectSpecialtiesForm.tsx
+++ src/core/components/common/SelectSpecialtiesForm.tsx
@@ -4,13 +4,16 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@
 // Components
 import { LoadingDB } from '@core/components/common/LoadingDB';
 // External imports
+import { useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
 // Imports
 import type { Control } from 'react-hook-form';
 import type { IArea } from '@core/interfaces/area.interface';
 import type { IResponse } from '@core/interfaces/response.interface';
+// import { APP_CONFIG } from '@config/app.config';
 import { AreaService } from '@core/services/area.service';
+// import { OpenAIService } from '@lib/openai.service';
 import { useCapitalize } from '@core/hooks/useCapitalize';
 // Interface
 interface IFormSelect {
@@ -19,18 +22,45 @@ interface IFormSelect {
 }
 // React component
 export function SelectSpecialtiesForm({ formControl, callback }: IFormSelect) {
+  const [translation, setTranslation] = useState<IArea[]>([] as IArea[]);
   const capitalize = useCapitalize();
-  const { t } = useTranslation();
+  const { i18n, t } = useTranslation();
 
   const {
     data: areas,
-    isLoading,
     isError,
+    isLoading,
   } = useQuery<IResponse<IArea[]>>({
     queryKey: ['areas', ['input-form-select']],
     queryFn: async () => await AreaService.findAll(),
-    retry: 1,
+    refetchOnWindowFocus: false,
+    retry: 0,
+    staleTime: 0,
   });
+
+  // async function execTranslation(element: string, language: string) {
+  //   return await OpenAIService.translate(element, language);
+  // }
+
+  useEffect(() => {
+    // console.log('Loading areas is success');
+    // if (areas) {
+    //   const selectedLanguage = localStorage.getItem('i18nextLng') ?? i18n.language;
+    //   if (selectedLanguage !== APP_CONFIG.i18n.locale) {
+    //     const translated = areas.data.map(async (area) => {
+    //       const translation = await execTranslation(area.name, selectedLanguage);
+    //       return { ...area, name: translation?.replace(/"/g, '') || area.name };
+    //     });
+    //     Promise.all(translated).then((data) => {
+    //       setTranslation(data);
+    //       console.log('All translations ended successfully');
+    //     });
+    //   } else {
+    //     setTranslation(areas.data);
+    //   }
+    // }
+    if (areas) setTranslation(areas.data)
+  }, [areas, i18n.language]);
 
   return (
     <FormField
@@ -41,7 +71,7 @@ export function SelectSpecialtiesForm({ formControl, callback }: IFormSelect) {
           <FormLabel>{t('table.header.area')}</FormLabel>
           <Select
             defaultValue={field.value}
-            disabled={areas && areas.data.length < 1}
+            disabled={translation && translation.length < 1}
             onValueChange={(event) => {
               field.onChange(event);
               callback(event);
@@ -61,9 +91,9 @@ export function SelectSpecialtiesForm({ formControl, callback }: IFormSelect) {
             </FormControl>
             <FormMessage />
             <SelectContent>
-              {areas &&
-                areas.data.length > 0 &&
-                areas.data.map((el: IArea) => (
+              {translation &&
+                translation.length > 0 &&
+                translation.map((el: IArea) => (
                   <SelectItem key={el._id} value={el._id} className='text-sm'>
                     {capitalize(el.name)}
                   </SelectItem>
