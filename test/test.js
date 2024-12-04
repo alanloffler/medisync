c04aefab24f972bca9219e4f97cd3a7842b05e77
alanloffler <alanmatiasloffler@gmail.com>
2024-12-04 17:04:32 -0300

    Test

---
+++ test/test.js
@@ -0,0 +1,10 @@
+const selectedLanguage = 'es';
+
+const area = {
+  _id: '6661f82135345c8c83e2b916',
+  name: 'medicina',
+  plural: 'medicinas',
+  description: 'Área de médicina en general',
+  active: 1,
+};
+console.log(area, selectedLanguage);
