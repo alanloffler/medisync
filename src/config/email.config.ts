a9d30c52e644717eeffa25e137e4cab7b2f3e5aa
alanloffler <alanmatiasloffler@gmail.com>
2024-12-07 20:36:00 -0300

    Create: email config file

---
+++ src/config/email.config.ts
@@ -0,0 +1,7 @@
+export const EMAIL_CONFIG = {
+  account: 'alanmatiasloffler@gmail.com',
+  breadcrumb: [
+    { id: 1, name: 'breadcrumb.home', path: '/' },
+    { id: 2, name: 'breadcrumb.email', path: '/email/:id' },
+  ],
+};
