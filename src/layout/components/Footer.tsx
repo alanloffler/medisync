33513a78bc51c6e23426fd90dc2b1722c201a6c6
alanloffler <alanmatiasloffler@gmail.com>
2024-12-06 22:09:10 -0300

    Add: help store on footer buttons (tooltips)

--- src/layout/components/Footer.tsx
+++ src/layout/components/Footer.tsx
@@ -2,30 +2,36 @@
 import { Settings } from 'lucide-react';
 // Components
 import { Notifications } from '@layout/components/Notifications';
+import { TooltipWrapper } from '@core/components/common/TooltipWrapper';
 import { User } from '@layout/components/User';
 // External imports
 import { Link } from 'react-router-dom';
 import { spring, useAnimate } from 'motion/react';
 import { useTranslation } from 'react-i18next';
+// Imports
+import { useHelpStore } from '@settings/stores/help.store';
 // React component
 export function Footer() {
   const [settingScope, settingAnimation] = useAnimate();
   const { t } = useTranslation();
+  const { help } = useHelpStore();
 
   return (
     <footer className='fixed bottom-0 z-50 flex h-12 w-full items-center justify-between gap-4 border-t bg-white p-4 px-4 md:p-6 md:px-4'>
       <section className='flex w-full items-center justify-between'>
         <section className='flex items-center space-x-3'>
-          <Link
-            ref={settingScope}
-            to='/settings'
-            onMouseOver={() => settingAnimation(settingScope.current, { scale: 1.1 }, { duration: 0.7, ease: 'linear', type: spring, bounce: 0.7 })}
-            onMouseOut={() => settingAnimation(settingScope.current, { scale: 1 }, { duration: 0.7, ease: 'linear', type: spring, bounce: 0.7 })}
-          >
-            <Settings size={20} strokeWidth={2} />
-          </Link>
-          <User />
-          <Notifications />
+          <User help={help} />
+          <TooltipWrapper tooltip={t('tooltip.settings')} help={help}>
+            <Link
+              ref={settingScope}
+              to='/settings'
+              onMouseOver={() => settingAnimation(settingScope.current, { scale: 1.1 }, { duration: 0.7, ease: 'linear', type: spring, bounce: 0.7 })}
+              onMouseOut={() => settingAnimation(settingScope.current, { scale: 1 }, { duration: 0.7, ease: 'linear', type: spring, bounce: 0.7 })}
+            >
+              <Settings size={20} strokeWidth={2} />
+            </Link>
+          </TooltipWrapper>
+          <Notifications help={help} />
         </section>
         <section className='flex items-center gap-2'>
           <div className='hidden justify-end text-sm font-medium text-primary md:flex lg:flex'>{`© ${new Date().getFullYear()} ${t('appName')}`}</div>
