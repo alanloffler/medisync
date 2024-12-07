7da60e6bd7280162e8d3b557dc2cd49418a96837
alanloffler <alanmatiasloffler@gmail.com>
2024-12-06 21:43:19 -0300

    Add: settings button. User to footer right side

--- src/layout/components/Footer.tsx
+++ src/layout/components/Footer.tsx
@@ -1,18 +1,33 @@
+// Icons: https://lucide.dev/icons/
+import { Settings } from 'lucide-react';
 // Components
 import { Notifications } from '@layout/components/Notifications';
 import { User } from '@layout/components/User';
+// External imports
+import { Link } from 'react-router-dom';
+import { spring, useAnimate } from 'motion/react';
 // Imports
 import { FOOTER_CONFIG } from '@config/layout/footer.config';
 // React component
 export function Footer() {
+  const [settingScope, settingAnimation] = useAnimate();
+
   return (
     <footer className='fixed bottom-0 z-50 flex h-12 w-full items-center justify-between gap-4 border-t bg-white p-4 px-4 md:p-6 md:px-4'>
       <section className='flex w-full items-center justify-between'>
-        <section>
+        <section className='flex items-center space-x-3'>
+          <Link
+            ref={settingScope}
+            to='/settings'
+            onMouseOver={() => settingAnimation(settingScope.current, { scale: 1.1 }, { duration: 0.7, ease: 'linear', type: spring, bounce: 0.7 })}
+            onMouseOut={() => settingAnimation(settingScope.current, { scale: 1 }, { duration: 0.7, ease: 'linear', type: spring, bounce: 0.7 })}
+          >
+            <Settings size={20} strokeWidth={2} />
+          </Link>
+          <User />
           <Notifications />
         </section>
         <section className='flex items-center gap-2'>
-          <User />
           <div className='hidden justify-end text-sm font-medium text-primary md:flex lg:flex'>{FOOTER_CONFIG.title}</div>
         </section>
       </section>
