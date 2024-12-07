245fbb02d252962456061f14df72fdfe76edf8ea
alanloffler <alanmatiasloffler@gmail.com>
2024-12-06 21:42:34 -0300

    Add: motion animation on mouse over and out. Icon props

--- src/layout/components/User.tsx
+++ src/layout/components/User.tsx
@@ -1,7 +1,6 @@
 // Icons: https://lucide.dev/icons/
 import { CircleUser } from 'lucide-react';
 // External components: https://ui.shadcn.com/docs/components
-import { Button } from '@core/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -12,19 +11,22 @@ import {
 } from '@core/components/ui/dropdown-menu';
 // External imports
 import { Link } from 'react-router-dom';
+import { spring, useAnimate } from 'motion/react';
 import { useTranslation } from 'react-i18next';
 // Imports
 import { HEADER_CONFIG } from '@config/layout/header.config';
 // React component
 export function User() {
+  const [userScope, userAnimation] = useAnimate();
   const { t } = useTranslation();
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant='secondary' size='icon' className='rounded-full'>
-          <CircleUser className='h-5 w-5' />
-        </Button>
+      <DropdownMenuTrigger
+        onMouseOver={() => userAnimation(userScope.current, { scale: 1.1 }, { duration: 0.7, ease: 'linear', type: spring, bounce: 0.7 })}
+        onMouseOut={() => userAnimation(userScope.current, { scale: 1 }, { duration: 0.7, ease: 'linear', type: spring, bounce: 0.7 })}
+      >
+        <CircleUser ref={userScope} size={20} strokeWidth={2} />
       </DropdownMenuTrigger>
       <DropdownMenuContent align='end'>
         <DropdownMenuLabel>{t('user.title')}</DropdownMenuLabel>
