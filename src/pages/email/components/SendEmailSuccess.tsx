0a1da0a315a4d38bbe63525fd6244184e3921b9c
alanloffler <alanmatiasloffler@gmail.com>
2024-12-08 17:35:41 -0300

    Create: success message sending email. Countdown with page redirection

---
+++ src/pages/email/components/SendEmailSuccess.tsx
@@ -0,0 +1,42 @@
+// Icons: https://lucide.dev/icons/
+import { MailCheck } from 'lucide-react';
+// External imports
+import NumberFlow from '@number-flow/react';
+import { Trans } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+// Imports
+import { APP_CONFIG } from '@config/app.config';
+// React component
+export function SendEmailSuccess() {
+  const [countdown, setCountdown] = useState<number>(APP_CONFIG.redirectTime / 1000);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let seconds: number = 5;
+
+    const interval = setInterval(() => {
+      setCountdown(seconds);
+
+      if (seconds === 0) {
+        clearInterval(interval);
+        navigate(-1);
+      }
+
+      seconds--;
+    }, 1000);
+  }, [navigate]);
+
+  return (
+    <section className='flex flex-row items-center gap-2 text-sm'>
+      <MailCheck size={20} strokeWidth={2} className='stroke-emerald-400' />
+      <div>
+        <Trans
+          i18nKey='email.sentConfirmation'
+          values={{ seconds: countdown }}
+          components={{ NumberFlow: <NumberFlow value={countdown} trend={0} className='font-semibold' /> }}
+        />
+      </div>
+    </section>
+  );
+}
