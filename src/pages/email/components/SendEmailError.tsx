e39660e13824540fa5e2e387071b4e79e40ee707
alanloffler <alanmatiasloffler@gmail.com>
2024-12-08 19:00:04 -0300

    Add: button navigation. Replaced button translation

--- src/pages/email/components/SendEmailError.tsx
+++ src/pages/email/components/SendEmailError.tsx
@@ -3,9 +3,11 @@ import { ArrowLeft, MailX, RefreshCw } from 'lucide-react';
 // External components: https://ui.shadcn.com/docs/components
 import { Button } from '@core/components/ui/button';
 // External imports
+import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 // React component
 export function SendEmailError() {
+  const navigate = useNavigate();
   const { t } = useTranslation();
 
   return (
@@ -15,11 +17,11 @@ export function SendEmailError() {
         <span>{t('email.sentError')}</span>
       </section>
       <footer className='flex justify-center space-x-6'>
-        <Button variant='secondary' size='sm' className='flex items-center gap-3'>
+        <Button variant='secondary' size='sm' className='flex items-center gap-3' onClick={() => navigate(-1)}>
           <ArrowLeft size={16} strokeWidth={2} />
-          {t('button.goToUsers')}
+          {t('button.back')}
         </Button>
-        <Button variant='default' size='sm' className='flex items-center gap-3'>
+        <Button variant='default' size='sm' className='flex items-center gap-3' onClick={() => navigate(0)}>
           <RefreshCw size={16} strokeWidth={2} />
           {t('button.tryAgain')}
         </Button>
