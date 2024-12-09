9a9282fde3981d7924b14ea0f998298d5bac8254
alanloffler <alanmatiasloffler@gmail.com>
2024-12-09 15:41:03 -0300

    Update: item action buttons replaced with TableButton component

--- src/pages/professionals/components/ProfessionalsDataTable.tsx
+++ src/pages/professionals/components/ProfessionalsDataTable.tsx
@@ -24,6 +24,7 @@ import { DBCountProfessionals } from '@professionals/components/common/DBCountPr
 import { InfoCard } from '@core/components/common/InfoCard';
 import { LoadingDB } from '@core/components/common/LoadingDB';
 import { Pagination } from '@core/components/common/Pagination';
+import { TableButton } from '@core/components/common/TableButton';
 import { TooltipWrapper } from '@core/components/common/TooltipWrapper';
 // External imports
 import { Trans, useTranslation } from 'react-i18next';
@@ -182,53 +183,39 @@ export function ProfessionalsDataTable({ search, reload, setReload, setErrorMess
       size: 100,
       header: () => <div className='text-center'>{t(PROF_CONFIG.table.header[5])}</div>,
       cell: ({ row }) => (
-        <div className='mx-auto flex w-fit flex-row items-center justify-center space-x-1'>
-          <TooltipWrapper tooltip={t('tooltip.details')} help={help}>
-            <Button
-              variant='ghost'
-              size='miniIcon'
-              onClick={() => navigate(`/professionals/${row.original._id}`)}
-              className='transition-transform duration-100 ease-in-out animate-in hover:scale-110 hover:bg-transparent hover:text-sky-400'
-            >
-              <FileText size={16} strokeWidth={1.5} />
-            </Button>
-          </TooltipWrapper>
-          <TooltipWrapper tooltip={t('tooltip.edit')} help={help}>
-            <Button
-              variant={'ghost'}
-              size={'miniIcon'}
-              onClick={() => navigate(`/professionals/update/${row.original._id}`)}
-              className='transition-transform duration-100 ease-in-out animate-in hover:scale-110 hover:bg-transparent hover:text-orange-400'
-            >
-              <PencilLine size={16} strokeWidth={1.5} />
-            </Button>
-          </TooltipWrapper>
-          <TooltipWrapper tooltip={t('tooltip.delete')} help={help}>
-            <Button
-              variant='ghost'
-              size='miniIcon'
-              onClick={() => handleRemoveDialog(row.original)}
-              className='transition-transform duration-100 ease-in-out animate-in hover:scale-110 hover:bg-transparent hover:text-rose-400'
-            >
-              <Trash2 size={16} strokeWidth={1.5} />
-            </Button>
-          </TooltipWrapper>
-          <TooltipWrapper tooltip={t('tooltip.sendMessage')} help={help}>
-            <Button
-              disabled={!row.original.phone}
-              variant='ghost'
-              size='miniIcon'
-              onClick={() => navigate(`/whatsapp/professional/${row.original._id}`)}
-              className='transition-transform duration-100 ease-in-out animate-in hover:scale-110 hover:bg-transparent hover:fill-green-500'
-            >
-              <svg width={16} height={16} viewBox='0 0 32 32'>
-                <path
-                  d='M25.873,6.069c-2.619-2.623-6.103-4.067-9.814-4.069C8.411,2,2.186,8.224,2.184,15.874c-.001,2.446,.638,4.833,1.852,6.936l-1.969,7.19,7.355-1.929c2.026,1.106,4.308,1.688,6.63,1.689h.006c7.647,0,13.872-6.224,13.874-13.874,.001-3.708-1.44-7.193-4.06-9.815h0Zm-9.814,21.347h-.005c-2.069,0-4.099-.557-5.87-1.607l-.421-.25-4.365,1.145,1.165-4.256-.274-.436c-1.154-1.836-1.764-3.958-1.763-6.137,.003-6.358,5.176-11.531,11.537-11.531,3.08,.001,5.975,1.202,8.153,3.382,2.177,2.179,3.376,5.077,3.374,8.158-.003,6.359-5.176,11.532-11.532,11.532h0Zm6.325-8.636c-.347-.174-2.051-1.012-2.369-1.128-.318-.116-.549-.174-.78,.174-.231,.347-.895,1.128-1.098,1.359-.202,.232-.405,.26-.751,.086-.347-.174-1.464-.54-2.788-1.72-1.03-.919-1.726-2.054-1.929-2.402-.202-.347-.021-.535,.152-.707,.156-.156,.347-.405,.52-.607,.174-.202,.231-.347,.347-.578,.116-.232,.058-.434-.029-.607-.087-.174-.78-1.88-1.069-2.574-.281-.676-.567-.584-.78-.595-.202-.01-.433-.012-.665-.012s-.607,.086-.925,.434c-.318,.347-1.213,1.186-1.213,2.892s1.242,3.355,1.416,3.587c.174,.232,2.445,3.733,5.922,5.235,.827,.357,1.473,.571,1.977,.73,.83,.264,1.586,.227,2.183,.138,.666-.1,2.051-.839,2.34-1.649,.289-.81,.289-1.504,.202-1.649s-.318-.232-.665-.405h0Z'
-                  fill='#currentColor'
-                ></path>
-              </svg>
-            </Button>
-          </TooltipWrapper>
+        <div className='mx-auto flex w-fit flex-row items-center justify-center space-x-2'>
+          <TableButton
+            callback={() => navigate(`/professionals/${row.original._id}`)}
+            className='hover:text-sky-500'
+            help={help}
+            tooltip={t('tooltip.details')}
+          >
+            <FileText size={16} strokeWidth={1.5} />
+          </TableButton>
+          <TableButton
+            callback={() => navigate(`/professionals/update/${row.original._id}`)}
+            className='hover:text-fuchsia-500'
+            help={help}
+            tooltip={t('tooltip.delete')}
+          >
+            <PencilLine size={16} strokeWidth={1.5} />
+          </TableButton>
+          <TableButton callback={() => handleRemoveDialog(row.original)} className='hover:text-rose-500' help={help} tooltip={t('tooltip.edit')}>
+            <Trash2 size={16} strokeWidth={1.5} />
+          </TableButton>
+          <TableButton
+            callback={() => navigate(`/whatsapp/professional/${row.original._id}`)}
+            className='hover:fill-green-500'
+            help={help}
+            tooltip={t('tooltip.sendMessage')}
+          >
+            <svg width={16} height={16} viewBox='0 0 32 32'>
+              <path
+                d='M25.873,6.069c-2.619-2.623-6.103-4.067-9.814-4.069C8.411,2,2.186,8.224,2.184,15.874c-.001,2.446,.638,4.833,1.852,6.936l-1.969,7.19,7.355-1.929c2.026,1.106,4.308,1.688,6.63,1.689h.006c7.647,0,13.872-6.224,13.874-13.874,.001-3.708-1.44-7.193-4.06-9.815h0Zm-9.814,21.347h-.005c-2.069,0-4.099-.557-5.87-1.607l-.421-.25-4.365,1.145,1.165-4.256-.274-.436c-1.154-1.836-1.764-3.958-1.763-6.137,.003-6.358,5.176-11.531,11.537-11.531,3.08,.001,5.975,1.202,8.153,3.382,2.177,2.179,3.376,5.077,3.374,8.158-.003,6.359-5.176,11.532-11.532,11.532h0Zm6.325-8.636c-.347-.174-2.051-1.012-2.369-1.128-.318-.116-.549-.174-.78,.174-.231,.347-.895,1.128-1.098,1.359-.202,.232-.405,.26-.751,.086-.347-.174-1.464-.54-2.788-1.72-1.03-.919-1.726-2.054-1.929-2.402-.202-.347-.021-.535,.152-.707,.156-.156,.347-.405,.52-.607,.174-.202,.231-.347,.347-.578,.116-.232,.058-.434-.029-.607-.087-.174-.78-1.88-1.069-2.574-.281-.676-.567-.584-.78-.595-.202-.01-.433-.012-.665-.012s-.607,.086-.925,.434c-.318,.347-1.213,1.186-1.213,2.892s1.242,3.355,1.416,3.587c.174,.232,2.445,3.733,5.922,5.235,.827,.357,1.473,.571,1.977,.73,.83,.264,1.586,.227,2.183,.138,.666-.1,2.051-.839,2.34-1.649,.289-.81,.289-1.504,.202-1.649s-.318-.232-.665-.405h0Z'
+                fill='#currentColor'
+              ></path>
+            </svg>
+          </TableButton>
         </div>
       ),
     },
