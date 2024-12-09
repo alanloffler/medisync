d8444a0321f4758ef59dfc501c56f01489e2a782
alanloffler <alanmatiasloffler@gmail.com>
2024-12-09 15:24:08 -0300

    Create: custom button for item action on table

---
+++ src/core/components/common/TableButton.tsx
@@ -0,0 +1,43 @@
+// Components
+import { TooltipWrapper } from '@core/components/common/TooltipWrapper';
+// External imports
+import type { ReactNode } from 'react';
+import { useAnimate } from 'motion/react';
+// Imports
+import { cn } from '@lib/utils';
+import { motion } from '@core/services/motion.service';
+// Interface
+interface IProps {
+  callback: () => void;
+  children: ReactNode;
+  className?: string;
+  help: boolean;
+  tooltip?: string;
+}
+// React component
+export function TableButton({ callback, children, className, help, tooltip }: IProps) {
+  const [scope, animation] = useAnimate();
+
+  function animateOver(): void {
+    const { keyframes, options } = motion.scale(1.1).type('bounce').animate();
+    animation(scope.current, keyframes, options);
+  }
+
+  function animateOut(): void {
+    const { keyframes, options } = motion.scale(1).type('bounce').animate();
+    animation(scope.current, keyframes, options);
+  }
+
+  return (
+    <TooltipWrapper help={help} tooltip={tooltip}>
+      <button
+        className={cn('flex h-6 w-6 items-center justify-center bg-transparent', className)}
+        onClick={callback}
+        onMouseOver={animateOver}
+        onMouseOut={animateOut}
+      >
+        <span ref={scope}>{children}</span>
+      </button>
+    </TooltipWrapper>
+  );
+}
