cf38d7bb710ed8508c2c2be499601012a665ba90
alanloffler <alanmatiasloffler@gmail.com>
2024-12-10 14:29:30 -0300

    Add: disabled state (animation and styles)

--- src/core/components/common/TableButton.tsx
+++ src/core/components/common/TableButton.tsx
@@ -11,27 +11,36 @@ interface IProps {
   callback: () => void;
   children: ReactNode;
   className?: string;
+  disabled?: boolean;
   help?: boolean;
   tooltip?: string;
 }
 // React component
-export function TableButton({ callback, children, className, help, tooltip }: IProps) {
+export function TableButton({ callback, children, className, disabled, help, tooltip }: IProps) {
   const [scope, animation] = useAnimate();
 
   function animateOver(): void {
-    const { keyframes, options } = motion.scale(1.1).type('bounce').animate();
-    animation(scope.current, keyframes, options);
+    if (disabled === false || disabled === undefined) {
+      const { keyframes, options } = motion.scale(1.1).type('bounce').animate();
+      animation(scope.current, keyframes, options);
+    }
   }
 
   function animateOut(): void {
-    const { keyframes, options } = motion.scale(1).type('bounce').animate();
-    animation(scope.current, keyframes, options);
+    if (disabled === false || disabled === undefined) {
+      const { keyframes, options } = motion.scale(1).type('bounce').animate();
+      animation(scope.current, keyframes, options);
+    }
   }
 
   return (
     <TooltipWrapper help={help} tooltip={tooltip}>
       <button
-        className={cn('flex h-6 w-6 items-center justify-center bg-transparent', className)}
+        className={cn(
+          'flex h-6 w-6 items-center justify-center bg-transparent disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:text-foreground',
+          className,
+        )}
+        disabled={disabled}
         onClick={callback}
         onMouseOver={animateOver}
         onMouseOut={animateOut}
