572e175135d61bf6f82b4432fad9c991f8dec4fe
alanloffler <alanmatiasloffler@gmail.com>
2024-12-11 20:08:50 -0300

    Add: input search. Wip

--- src/pages/appointments/Appointments.tsx
+++ src/pages/appointments/Appointments.tsx
@@ -1,15 +1,17 @@
 // Icons: https://lucide.dev/icons/
-import { Database, List, PlusCircle } from 'lucide-react';
+import { List, PlusCircle, Search, X } from 'lucide-react';
 // External components: https://ui.shadcn.com/docs/components
 import { Button } from '@core/components/ui/button';
 import { Card, CardContent } from '@core/components/ui/card';
+import { Input } from '@core/components/ui/input';
 // Components
 import { AppoFlowCard } from '@appointments/components/AppoFlowCard';
 import { ApposDataTable } from '@appointments/components/AppoDataTable';
+import { DBCountAppos } from '@appointments/components/common/DBCountAppos';
 import { PageHeader } from '@core/components/common/PageHeader';
 // External imports
+import { ChangeEvent, useEffect, useState } from 'react';
 import { spring, useAnimate } from 'motion/react';
-import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 // Imports
@@ -18,7 +20,6 @@ import { EAppointmentSearch, type IAppointmentSearch } from '@appointments/inter
 import { HEADER_CONFIG } from '@config/layout/header.config';
 import { useHeaderMenuStore } from '@layout/stores/header-menu.service';
 import { useHelpStore } from '@settings/stores/help.store';
-import { DBCountAppos } from './components/common/DBCountAppos';
 // React component
 export default function Appointments() {
   const [createScope, createAnimation] = useAnimate();
@@ -34,6 +35,10 @@ export default function Appointments() {
   useEffect(() => {
     setItemSelected(HEADER_CONFIG.headerMenu[1].id);
   }, [setItemSelected]);
+
+  function handleSearch(e: ChangeEvent<HTMLInputElement>): void {
+    console.log('search', e);
+  }
 
   return (
     <main className='flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8 lg:gap-8 lg:p-8'>
@@ -63,7 +68,30 @@ export default function Appointments() {
             <h1 className='text-center text-lg font-semibold'>{t('cardTitle.appointmentsList')}</h1>
           </header>
           <CardContent className='space-y-2 pt-0'>
-            <DBCountAppos className='!text-xsm' />
+            <section className='flex items-center justify-between'>
+              <div className='relative w-full items-center md:w-[200px]'>
+                <Search size={16} strokeWidth={2} className='absolute left-2 top-1/2 -translate-y-1/2 text-muted-foreground' />
+                <Input
+                  onClick={() =>
+                    search.value !== '' && search.type === EAppointmentSearch.NAME && setSearch({ value: '', type: EAppointmentSearch.DNI })
+                  }
+                  onChange={(e) => handleSearch(e)}
+                  value={search.type === EAppointmentSearch.DNI ? search.value : ''}
+                  type='number'
+                  placeholder={t('search.user')}
+                  className='h-7 bg-input pl-8 text-xsm'
+                />
+                {search.type === EAppointmentSearch.DNI && search.value && (
+                  <button
+                    onClick={() => setSearch({ value: '', type: EAppointmentSearch.DNI })}
+                    className='absolute right-3 top-3 text-muted-foreground hover:text-black'
+                  >
+                    <X size={16} strokeWidth={2} />
+                  </button>
+                )}
+              </div>
+              <DBCountAppos className='justify-end !text-xsm' />
+            </section>
             <ApposDataTable search={search} reload={reload} setReload={setReload} setErrorMessage={setErrorMessage} help={help} />
           </CardContent>
         </Card>
