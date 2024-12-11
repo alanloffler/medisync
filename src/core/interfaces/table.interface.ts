eaf5c7603e6f29f32c55b8f1f138dedf44684747
alanloffler <alanmatiasloffler@gmail.com>
2024-12-11 09:53:42 -0300

    Add: Appointments data table interface

--- src/core/interfaces/table.interface.ts
+++ src/core/interfaces/table.interface.ts
@@ -1,3 +1,4 @@
+import type { IAppointmentSearch } from '@appointments/interfaces/appointment-search.interface';
 import type { IProfessionalSearch } from '@professionals/interfaces/professional-search.interface';
 import type { IUserSearch } from '@users/interfaces/user-search.interface';
 import type { PaginationState, SortingState } from '@tanstack/react-table';
@@ -7,6 +8,10 @@ export interface IDataTable {
   reload: number;
   setErrorMessage: React.Dispatch<React.SetStateAction<string>>;
   setReload: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export interface IDataTableAppointments extends IDataTable {
+  search: IAppointmentSearch;
 }
 
 export interface IDataTableProfessionals extends IDataTable {
