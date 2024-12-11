974953d5a63414bb1e190af783fdadb1f8280e7e
alanloffler <alanmatiasloffler@gmail.com>
2024-12-10 23:41:29 -0300

    Add: DateTime component show appointment date info

--- src/pages/appointments/components/AppoItemMini.tsx
+++ src/pages/appointments/components/AppoItemMini.tsx
@@ -1,5 +1,6 @@
+// Components
+import { DateTime } from '@core/components/common/DateTime';
 // External imports
-import { format } from '@formkit/tempo';
 import { spring, useAnimate } from 'motion/react';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
@@ -28,14 +29,13 @@ export function AppoItemMini({ data }: { data: IAppointment }) {
       }}
     >
       <section className='flex w-[135px] items-center'>
-        <div ref={firstScope} className='flex items-center rounded-l-sm rounded-r-sm bg-slate-100 text-xsm'>
-          <p className='rounded-l-sm bg-slate-200 px-2 py-1 text-slate-600'>{format(data.day, 'short', i18n.resolvedLanguage).slice(0, -3)}</p>
-          <p className='rounded-sm px-2 py-1 text-slate-500'>{data.hour}</p>
+        <div ref={firstScope}>
+          <DateTime day={data.day} hour={data.hour} />
         </div>
       </section>
       <section className='flex flex-1 flex-row justify-between'>
-        <div className='flex w-1/2 justify-between space-x-2 text-slate-700'>
-          <div className='text-xsm font-bold'>{`${capitalize(data.user.firstName)} ${capitalize(data.user.lastName)}`}</div>
+        <div className='flex w-1/2 justify-around space-x-2 text-slate-700'>
+          <div className='flex text-xsm font-bold'>{`${capitalize(data.user.firstName)} ${capitalize(data.user.lastName)}`}</div>
           <div className='text-xs text-slate-500'>{`${t('label.identityCard')} ${i18n.format(data.user.dni, 'number', i18n.resolvedLanguage)}`}</div>
         </div>
         <div className='flex w-1/2 justify-end text-xs text-slate-700'>
