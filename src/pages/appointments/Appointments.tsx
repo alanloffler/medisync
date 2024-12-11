e35c18b82abccfdd0b066d292ede47dfd3c8b7c6
alanloffler <alanmatiasloffler@gmail.com>
2024-12-10 21:04:35 -0300

    Wip: statistic transition card

--- src/pages/appointments/Appointments.tsx
+++ src/pages/appointments/Appointments.tsx
@@ -1,8 +1,11 @@
 // Icons: https://lucide.dev/icons/
 import { ChartLine, Database, List, PlusCircle } from 'lucide-react';
-// External components: https://ui.shadcn.com/docs/components
+// External components:
+// https://ui.shadcn.com/docs/components
 import { Button } from '@core/components/ui/button';
 import { Card, CardContent } from '@core/components/ui/card';
+// https://number-flow.barvian.me/
+import NumberFlow, { NumberFlowGroup } from '@number-flow/react';
 // Components
 import { AppoItemMini } from '@appointments/components/AppoItemMini';
 import { InfoCard } from '@core/components/common/InfoCard';
@@ -10,6 +13,7 @@ import { LoadingDB } from '@core/components/common/LoadingDB';
 import { PageHeader } from '@core/components/common/PageHeader';
 import { TQPagination } from '@core/components/common/TQPagination';
 // External imports
+import { clsx } from 'clsx';
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import { spring, useAnimate } from 'motion/react';
 import { useEffect, useState } from 'react';
@@ -22,7 +26,6 @@ import { AppointmentApiService } from '@appointments/services/appointment.servic
 import { HEADER_CONFIG } from '@config/layout/header.config';
 import { queryClient } from '@lib/react-query';
 import { useHeaderMenuStore } from '@layout/stores/header-menu.service';
-import NumberFlow, { NumberFlowGroup } from '@number-flow/react';
 // React component
 export default function Appointments() {
   const _limit: number =
@@ -99,15 +102,20 @@ export default function Appointments() {
               <span>{t('button.generateAppointment')}</span>
             </Button>
           </div>
-          <Card className='space-y-4 p-4 pb-8 bg-amber-200 text-amber-600'>
+          <Card className='space-y-4 bg-amber-200 p-4 pb-8 text-amber-600'>
             <div className='flex items-center justify-between'>
               <h2 className='text-xsm font-semibold uppercase'>{t('pageTitle.appointments')}</h2>
               <ChartLine size={20} strokeWidth={2} className='stroke-amber-600' />
             </div>
             <NumberFlowGroup>
               <div className='flex flex-col items-center justify-center space-y-2 font-semibold'>
-                <div className='flex items-center space-x-2'>
-                  <NumberFlow format={{ style: 'decimal' }} value={flowValue.value} suffix='' className={`text-3xl ${flowValue.value < 0 ? '!text-red-600' : 'text-amber-600'}`} />
+                <div className={'flex items-center space-x-2'}>
+                  <NumberFlow
+                    className={clsx('text-3xl', flowValue.value < 0 ? 'fill-rose-500 text-rose-500' : 'fill-emerald-500 text-emerald-500')}
+                    format={{ style: 'decimal' }}
+                    value={flowValue.value}
+                    // prefix={flowValue.value > 0 ? '+' : ''}
+                  />
                   <span className='text-base font-light'>{flowValue.label}</span>
                 </div>
                 {/* <div className='flex items-center space-x-2'>
