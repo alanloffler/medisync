2ad2b47d4cf2424dbe223a238d0c43f5d0a05d0c
alanloffler <alanmatiasloffler@gmail.com>
2024-12-12 20:32:03 -0300

    Update: responsive styles. Hour hidden if media query is mobile

--- src/core/components/common/DateTime.tsx
+++ src/core/components/common/DateTime.tsx
@@ -17,11 +17,11 @@ export function DateTime({ className, day, hour }: IDateTime) {
 
   return (
     <section className={cn('flex justify-center text-xsm font-normal', className)}>
-      <div className='flex items-center space-x-1 rounded-l-sm bg-indigo-200 p-1 pr-2 text-indigo-600'>
+      <div className='flex items-center space-x-1 rounded-sm bg-indigo-200 p-1 pr-2 text-indigo-600 md:rounded-r-none'>
         <Calendar size={14} strokeWidth={2} />
         <span>{format(day, 'short', i18n.resolvedLanguage)}</span>
       </div>
-      <div className='flex items-center space-x-1 rounded-r-sm bg-indigo-100 p-1 pr-2 text-indigo-600'>
+      <div className='hidden items-center space-x-1 rounded-r-sm bg-indigo-100 p-1 pr-2 text-indigo-600 md:flex'>
         <Clock size={14} strokeWidth={2} />
         <span>{hour}</span>
       </div>
