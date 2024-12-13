675f4502560f84b848866ef7e1c2864e3f17c6c5
alanloffler <alanmatiasloffler@gmail.com>
2024-12-13 17:48:31 -0300

    Change: type of data. Update styles. Add error and loading handling

--- src/pages/appointments/components/AppoFlowCard.tsx
+++ src/pages/appointments/components/AppoFlowCard.tsx
@@ -5,68 +5,100 @@ import { ChartLine, TrendingDown, TrendingUp } from 'lucide-react';
 import { Card } from '@core/components/ui/card';
 // https://number-flow.barvian.me/
 import NumberFlow, { NumberFlowGroup } from '@number-flow/react';
+// Components
+import { LoadingText } from '@core/components/common/LoadingText';
 // External imports
 import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useTranslation } from 'react-i18next';
+// Imports
+import type { IResponse } from '@core/interfaces/response.interface';
+import { AppointmentApiService } from '@appointments/services/appointment.service';
 import { cn } from '@lib/utils';
-// import { useTranslation } from 'react-i18next';
+// Interfaces
+interface IStatistic {
+  count: IStatisticElement;
+  diff?: number;
+  last: IStatisticElement;
+}
+
+interface IStatisticElement {
+  label: string;
+  value: number;
+}
 // React component
 export function AppoFlowCard() {
-  const [flowValue, setFlowValue] = useState<{ value: number; label: string; diff: number; diffLabel: string }>({
-    value: 0,
-    label: '',
-    diff: 0,
-    diffLabel: '',
+  const [flowValue, setFlowValue] = useState<IStatistic | undefined>(undefined);
+  const { i18n, t } = useTranslation();
+
+  const {
+    data: flowContent,
+    error,
+    isError,
+    isLoading,
+  } = useQuery<IResponse<IStatistic[]>>({
+    queryKey: ['appointments', 'statistics'],
+    queryFn: async () => await AppointmentApiService.getStatistics(),
+    retry: 1,
+    refetchOnWindowFocus: 'always',
   });
-  // const { t } = useTranslation();
 
   useEffect(() => {
-    const flowContent = [
-      { value: 58, label: 'en total', diff: 0.07, diffLabel: 'que el mes pasado' },
-      { value: -12, label: 'esta semana', diff: -0.02, diffLabel: 'que la semana pasada' },
-      { value: 4, label: 'hoy', diff: 0.01, diffLabel: 'que ayer' },
-    ];
-
     let index: number = 0;
+    let interval: NodeJS.Timeout;
 
-    const interval = setInterval(() => {
-      setFlowValue(flowContent[index]);
-      index = (index + 1) % flowContent.length;
-    }, 5000);
+    if (!isError) {
+      interval = setInterval(() => {
+        setFlowValue(flowContent!.data[index]);
+        index = (index + 1) % flowContent!.data.length;
+      }, 3000);
+    }
 
     return () => clearInterval(interval);
-  }, []);
+  }, [flowContent, isError]);
 
   return (
-    <Card className='flex space-x-8 bg-amber-200 px-6 py-4 text-amber-600'>
+    <Card className='flex items-center space-x-8 bg-amber-200 px-6 py-4 text-amber-600'>
       <section className='my-auto'>
         <ChartLine size={20} strokeWidth={2} className='stroke-amber-600' />
       </section>
-      <NumberFlowGroup>
-        <div className='flex flex-col font-semibold'>
-          <div className='flex items-center space-x-2'>
-            <NumberFlow className='fill-amber-600 text-2xl text-amber-600' format={{ style: 'decimal' }} value={flowValue.value} />
-            <span className='text-base font-light'>{flowValue.label}</span>
-          </div>
-          <div className='flex items-center space-x-2'>
-            <NumberFlow
-              className={cn('!text-xsm', flowValue.diff < 0 ? 'fill-rose-500 text-rose-500' : 'fill-emerald-500 text-emerald-500')}
-              format={{ style: 'percent' }}
-              value={flowValue.diff}
-              prefix={flowValue.diff > 0 ? '+' : ''}
-            />
-            <div className='flex items-center space-x-1'>
-              <span>
-                {flowValue.diff > 0 ? (
-                  <TrendingUp size={12} strokeWidth={2} className='stroke-emerald-500' />
-                ) : (
-                  <TrendingDown size={12} strokeWidth={2} className='stroke-rose-500' />
-                )}
-              </span>
-              <span className='text-xs font-light'>{flowValue.diffLabel}</span>
+      {!isError && (isLoading || flowValue === undefined) ? (
+        <LoadingText text={t('loading.default')} suffix='...' className='!text-xsm text-amber-600' />
+      ) : (
+        flowValue?.count && (
+          <NumberFlowGroup>
+            <div className='flex flex-col font-semibold'>
+              {flowValue.count && (
+                <div className='flex items-center space-x-2'>
+                  <NumberFlow className='fill-amber-600 text-2xl text-amber-600' format={{ style: 'decimal' }} value={flowValue.count.value} />
+                  <span className='text-base font-light'>{t(`statistics.appointments.${flowValue.count.label}`)}</span>
+                </div>
+              )}
+              <div className='flex items-center space-x-2'>
+                <NumberFlow
+                  className={cn(
+                    '!text-xsm',
+                    flowValue.diff ? (flowValue.diff < 0 ? 'fill-rose-500 text-rose-500' : 'fill-emerald-500 text-emerald-500') : 'text-amber-600',
+                  )}
+                  value={flowValue.diff ?? flowValue.last.value}
+                  format={{ style: 'decimal', maximumFractionDigits: 1 }}
+                  locales={i18n.resolvedLanguage}
+                  prefix={flowValue.diff && flowValue.diff > 0 ? '+' : ''}
+                  suffix={flowValue.diff ? '%' : undefined}
+                />
+                <div className='flex items-center'>
+                  <span>
+                    {flowValue.diff && flowValue.diff > 0 && <TrendingUp size={12} strokeWidth={2} className='mr-1 stroke-emerald-500' />}
+                    {flowValue.diff && flowValue.diff < 0 && <TrendingDown size={12} strokeWidth={2} className='mr-1 stroke-rose-500' />}
+                  </span>
+                  {flowValue.last && <span className='text-xs font-light'>{t(`statistics.appointments.${flowValue.last.label}`)}</span>}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </NumberFlowGroup>
+          </NumberFlowGroup>
+        )
+      )}
+      {isError && <span className='!text-xsm'>{error.message}</span>}
     </Card>
   );
 }
