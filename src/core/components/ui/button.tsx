602d2d99de7ef0c7ec488604c979a247e87cbd77
alanloffler <alanmatiasloffler@gmail.com>
2024-12-13 20:43:43 -0300

    Add: variant link-table

--- src/core/components/ui/button.tsx
+++ src/core/components/ui/button.tsx
@@ -16,6 +16,7 @@ const buttonVariants = cva(
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        'link-table': 'p-0 font-normal underline-offset-4 hover:underline',
         tableHeader: 'rounded-full bg-slate-400 hover:bg-slate-500 text-slate-100',
         tableHeaderPrimary: 'rounded-full bg-primary text-primary-foreground hover:bg-primary/80',
         table: 'bg-slate-200 hover:bg-slate-200/80 shadow-sm text-primary-background text-sm',
