188a3ab1326d3cbf7547a3083051545654329e6b
alanloffler <alanmatiasloffler@gmail.com>
2024-12-13 19:30:10 -0300

    Create: url utils class

---
+++ src/core/services/utils/url.service.ts
@@ -0,0 +1,32 @@
+import type { IResponse } from '@core/interfaces/response.interface';
+import { APP_CONFIG } from '@config/app.config';
+import { EMethods } from '@core/enums/methods.enum';
+
+export class UtilsUrl {
+  public static create(path: string, params?: Record<string, string>): URL {
+    const url = new URL(path);
+    if (params) Object.entries(params).forEach(([key, value]) => url.searchParams.set(key, value));
+
+    return url;
+  }
+
+  public static async fetch(url: string | URL, method: EMethods, body?: any): Promise<IResponse> {
+    try {
+      const query: Response = await fetch(url, {
+        method: method,
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+        },
+        body: JSON.stringify(body),
+      });
+
+      const response: IResponse = await query.json();
+      if (!query.ok) throw new Error(response.message);
+
+      return response;
+    } catch (error) {
+      if (error instanceof TypeError) throw new Error(APP_CONFIG.error.server);
+      throw error;
+    }
+  }
+}
