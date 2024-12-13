1d4d4e8016eeba5aa19b04f2cd4fb085bde9829e
alanloffler <alanmatiasloffler@gmail.com>
2024-12-13 18:26:03 -0300

    Add: fetch method (TanStack Query response)

--- src/core/services/utils.service.ts
+++ src/core/services/utils.service.ts
@@ -1,8 +1,32 @@
+import { APP_CONFIG } from '@config/app.config';
+import { EMethods } from '@core/enums/methods.enum';
+import { IResponse } from '@core/interfaces/response.interface';
+
 export class UtilsService {
   public static createUrl(path: string, params?: Record<string, string>): URL {
     const url = new URL(path);
     if (params) Object.entries(params).forEach(([key, value]) => url.searchParams.set(key, value));
 
     return url;
+  }
+
+  public static async fetch(url: string | URL, method: EMethods, body?: any) {
+    try {
+      const query: Response = await fetch(url, {
+        method: method,
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+        },
+        body: JSON.stringify(body),
+      });
+
+      const response: IResponse = await query.json();
+      if (!query.ok) throw new Error(response.message);
+
+      return response;
+    } catch (error) {
+      if (error instanceof TypeError) throw new Error(APP_CONFIG.error.server);
+      throw error;
+    }
   }
 }
