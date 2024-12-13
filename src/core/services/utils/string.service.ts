f4b424bebbab43464b32c3bcddf40bac9fc22ef2
alanloffler <alanmatiasloffler@gmail.com>
2024-12-13 20:13:48 -0300

    Update: different uses cases of upperCase method. Add standardize param to lower case before apply

--- src/core/services/utils/string.service.ts
+++ src/core/services/utils/string.service.ts
@@ -1,12 +1,29 @@
+enum ECapitalize {
+  all = 'all',
+  each = 'each',
+  first = 'first',
+}
+
 export class UtilsString {
-  public static capitalize(str: string): string {
-    if (str) {
-      return str
-        .split(' ')
-        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-        .join(' ');
-    } else {
-      return '';
+  public static upperCase(content: string, type?: keyof typeof ECapitalize, standardize?: boolean): string {
+    let result: string = '';
+    if (!content) return result;
+    if (!type) type = 'first';
+    if (standardize) content = content.toLowerCase();
+
+    if (content) {
+      if (type === 'first') {
+        result = content.charAt(0).toUpperCase() + content.slice(1);
+      } else if (type === 'each') {
+        result = content
+          .split(' ')
+          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(' ');
+      } else if (type === 'all') {
+        result = content.toUpperCase();
+      }
     }
+
+    return result;
   }
 }
