877a93fe8dd69357b75b929f7a9634a514b64b15
alanloffler <alanmatiasloffler@gmail.com>
2024-12-14 17:45:31 -0300

    Update: code refactor. New status options object

--- src/pages/appointments/components/common/StatusSelect.tsx
+++ src/pages/appointments/components/common/StatusSelect.tsx
@@ -5,68 +5,80 @@ import { useEffect, useState } from 'react';
 // Imports
 import { cn } from '@lib/utils';
 import { AppoSchedule } from '@appointments/services/schedule.service';
-// Inteerface and type
+import { EStatus } from '@appointments/enums/status.enum';
+// Interfaces
 interface IStatusSelect {
   day: string;
   hour: string;
+  mode: 'update' | 'view';
   status: string;
 }
 
-type IStatusStyles = Record<string, { dark: string; light: string }>;
-// Constants
-const statusStyle: IStatusStyles = {
-  active: {
-    dark: 'bg-emerald-200',
-    light: 'bg-emerald-400',
-  },
-  inactive: {
-    dark: 'bg-rose-200',
-    light: 'bg-rose-400',
-  },
-  waiting: {
-    dark: 'bg-amber-200',
-    light: 'bg-amber-400',
-  },
-  not_status: {
-    dark: 'bg-slate-200',
-    light: 'bg-slate-400',
-  },
-};
+interface IStatusOption {
+  label: string;
+  style: { dark: string; light: string };
+  value: string;
+}
 // React component
-export function StatusSelect({ day, hour, status }: IStatusSelect) {
+export function StatusSelect({ day, hour, mode, status }: IStatusSelect) {
   const [itemSelected, setItemSelected] = useState<string>(status);
-  const [styles, setStyles] = useState<{ dark: string; light: string }>(statusStyle.not_status);
-
-  const statusOptions: { value: string; label: string }[] = [
-    { value: 'active', label: 'Asistió' },
-    { value: 'inactive', label: 'Faltó' },
-    { value: 'waiting', label: 'Esperando' },
-    { value: 'not_status', label: 'No definido' },
+  const statusOptions: IStatusOption[] = [
+    {
+      value: EStatus.ATTENDED,
+      label: 'Asistió',
+      style: {
+        dark: 'bg-emerald-200',
+        light: 'bg-emerald-400',
+      },
+    },
+    {
+      value: EStatus.NOT_ATTENDED,
+      label: 'No asistió',
+      style: {
+        dark: 'bg-rose-200',
+        light: 'bg-rose-400',
+      },
+    },
+    {
+      value: EStatus.NOT_STATUS,
+      label: 'Sin estado',
+      style: {
+        dark: 'bg-slate-200',
+        light: 'bg-slate-400',
+      },
+    },
+    {
+      value: EStatus.WAITING,
+      label: 'Aguardando',
+      style: {
+        dark: 'bg-amber-200',
+        light: 'bg-amber-400',
+      },
+    },
   ];
 
   useEffect(() => {
     const futureDate = AppoSchedule.isDatetimeInFuture(new Date(day), hour);
-
-    if (futureDate) {
-      setItemSelected('waiting');
-    } else {
-      setItemSelected(status);
-    }
+    futureDate ? setItemSelected(EStatus.WAITING) : setItemSelected(status);
   }, [day, hour, status]);
 
-  useEffect(() => {
-    const findStyle = Object.entries(statusStyle).find(([key, value]) => {
-      if (key === itemSelected) return value;
-    })![1];
-
-    setStyles(findStyle);
-  }, [itemSelected]);
+  function handleStatusChange(status: string) {
+    setItemSelected(status);
+    if (mode === 'update') {
+      console.log('can update');
+    }
+  }
 
   return (
-    <Select value={itemSelected} onValueChange={setItemSelected} disabled={itemSelected === 'waiting'}>
+    <Select value={itemSelected} onValueChange={(e) => handleStatusChange(e)} disabled={itemSelected === EStatus.WAITING || mode === 'view'}>
       <SelectTrigger className='h-5 w-5 justify-center bg-transparent p-0 [&_svg]:hidden'>
-        <div className={cn('flex h-4 w-4 items-center justify-center rounded-full bg-rose-200', styles.dark)}>
-          <span className={cn('h-2.5 w-2.5 rounded-full bg-rose-400', styles.light)}></span>
+        <div
+          className={cn(
+            'flex h-4 w-4 items-center justify-center rounded-full bg-rose-200',
+            statusOptions.find((item) => item.value === itemSelected)?.style.dark,
+          )}
+        >
+          <span className={cn('h-2.5 w-2.5 rounded-full bg-rose-400', statusOptions.find((item) => item.value === itemSelected)?.style.light)}></span>
         </div>
       </SelectTrigger>
       <SelectContent align='center'>
@@ -74,8 +86,8 @@ export function StatusSelect({ day, hour, status }: IStatusSelect) {
           {statusOptions.map((option) => (
             <SelectItem key={crypto.randomUUID()} value={option.value} className='[&_svg]:h-3 [&_svg]:w-3'>
               <div className='flex flex-row items-center space-x-2'>
-                <div className={cn('flex h-4 w-4 items-center justify-center rounded-full bg-rose-200', statusStyle[option.value].dark)}>
-                  <div className={cn('h-2.5 w-2.5 rounded-full bg-rose-400', statusStyle[option.value].light)}></div>
+                <div className={cn('flex h-4 w-4 items-center justify-center rounded-full', option.style.dark)}>
+                  <div className={cn('h-2.5 w-2.5 rounded-full', option.style.light)}></div>
                 </div>
                 <div className='text-xs'>{option.label}</div>
               </div>
