f48e7cc53caefee49617c362fedb31b633876c85
alanloffler <alanmatiasloffler@gmail.com>
2024-12-16 11:49:09 -0300

    Update: styles and structure

--- src/pages/appointments/components/AppoFlowCard.tsx
+++ src/pages/appointments/components/AppoFlowCard.tsx
@@ -26,6 +26,8 @@ interface IStatisticElement {
   label: string;
   value: number;
 }
+// Constants
+const intervalTime: number = 5000;
 // React component
 export function AppoFlowCard() {
   const [flowValue, setFlowValue] = useState<IStatistic | undefined>(undefined);
@@ -51,56 +53,61 @@ export function AppoFlowCard() {
       interval = setInterval(() => {
         setFlowValue(flowContent!.data[index]);
         index = (index + 1) % flowContent!.data.length;
-      }, 3000);
+      }, intervalTime);
     }
 
     return () => clearInterval(interval);
   }, [flowContent, isError]);
 
   return (
-    <Card className='flex items-center space-x-8 bg-amber-200 px-6 py-4 text-amber-600'>
-      <section className='my-auto'>
-        <ChartLine size={20} strokeWidth={2} className='stroke-amber-600' />
-      </section>
+    <Card className='flex flex-col bg-amber-100 text-amber-500'>
       {!isError && (isLoading || flowValue === undefined) ? (
-        <LoadingText text={t('loading.default')} suffix='...' className='!text-xsm text-amber-600' />
+        <LoadingText text={t('loading.default')} suffix='...' className='py-4 my-auto !text-xsm text-amber-500' />
       ) : (
-        flowValue?.count && (
-          <NumberFlowGroup>
-            <div className='flex flex-col font-semibold'>
-              {flowValue.count && (
-                <div className='flex items-center space-x-2'>
-                  <NumberFlow className='fill-amber-600 text-2xl text-amber-600' format={{ style: 'decimal' }} value={flowValue.count.value} />
-                  <span className='text-base font-light'>{t(`statistics.appointments.${flowValue.count.label}`)}</span>
-                </div>
-              )}
-              <div className='flex items-center space-x-2'>
-                <NumberFlow
-                  className={cn(
-                    '!text-xsm',
-                    flowValue.diff ? (flowValue.diff < 0 ? 'fill-rose-500 text-rose-500' : 'fill-emerald-500 text-emerald-500') : 'text-amber-600',
-                  )}
-                  value={flowValue.diff ?? flowValue.last.value}
-                  format={{ style: 'decimal', maximumFractionDigits: 1 }}
-                  locales={i18n.resolvedLanguage}
-                  prefix={flowValue.diff && flowValue.diff > 0 ? '+' : ''}
-                  suffix={flowValue.diff || flowValue.diff === 0 ? '%' : ''}
-                />
-                <div className='flex items-center'>
-                  {flowValue.diff ? (
-                    <>
-                      <span>{flowValue.diff > 0 && <TrendingUp size={12} strokeWidth={2} className='mr-1 stroke-emerald-500' />}</span>
-                      <span>{flowValue.diff < 0 && <TrendingDown size={12} strokeWidth={2} className='mr-1 stroke-rose-500' />}</span>
-                    </>
-                  ) : (
-                    <span>{flowValue.diff === 0 && <MoveRight size={12} strokeWidth={2} className='mr-1 stroke-amber-600' />}</span>
-                  )}
-                  {flowValue.last && <span className='text-xs font-light'>{t(`statistics.appointments.${flowValue.last.label}`)}</span>}
-                </div>
-              </div>
+        <>
+          <section className='flex w-full items-center space-x-2 bg-amber-200 rounded-md rounded-b-none p-2'>
+            <div>
+              <ChartLine size={16} strokeWidth={2} className='stroke-amber-500' />
             </div>
-          </NumberFlowGroup>
-        )
+            <span className='text-xsm font-semibold uppercase leading-none'>{t('statistics.appointments.title')}</span>
+          </section>
+          {flowValue?.count && (
+            <div className='flex flex-col items-center font-semibold p-4'>
+              <NumberFlowGroup>
+                {flowValue.count && (
+                  <div className='flex items-center space-x-2'>
+                    <NumberFlow className='fill-amber-500 text-3xl text-amber-500' format={{ style: 'decimal' }} value={flowValue.count.value} />
+                    <span className='text-base font-light'>{t(`statistics.appointments.${flowValue.count.label}`)}</span>
+                  </div>
+                )}
+                <div className='flex items-center space-x-2'>
+                  <NumberFlow
+                    className={cn(
+                      '!text-xsm',
+                      flowValue.diff ? (flowValue.diff < 0 ? 'fill-rose-500 text-rose-500' : 'fill-emerald-500 text-emerald-500') : 'text-amber-500',
+                    )}
+                    value={flowValue.diff ?? flowValue.last.value}
+                    format={{ style: 'decimal', maximumFractionDigits: 1 }}
+                    locales={i18n.resolvedLanguage}
+                    prefix={flowValue.diff && flowValue.diff > 0 ? '+' : ''}
+                    suffix={flowValue.diff || flowValue.diff === 0 ? '%' : ''}
+                  />
+                  <div className='flex items-center'>
+                    {flowValue.diff ? (
+                      <>
+                        <span>{flowValue.diff > 0 && <TrendingUp size={12} strokeWidth={2} className='mr-1 stroke-emerald-500' />}</span>
+                        <span>{flowValue.diff < 0 && <TrendingDown size={12} strokeWidth={2} className='mr-1 stroke-rose-500' />}</span>
+                      </>
+                    ) : (
+                      <span>{flowValue.diff === 0 && <MoveRight size={12} strokeWidth={2} className='mr-1 stroke-amber-500' />}</span>
+                    )}
+                    {flowValue.last && <span className='text-xs font-light'>{t(`statistics.appointments.${flowValue.last.label}`)}</span>}
+                  </div>
+                </div>
+              </NumberFlowGroup>
+            </div>
+          )}
+        </>
       )}
       {isError && <span className='!text-xsm'>{error.message}</span>}
     </Card>
