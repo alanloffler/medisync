2eff18026e23b22849351a9509332b7bb6a814cf
alanloffler <alanmatiasloffler@gmail.com>
2024-12-18 17:05:47 -0300

    Update: padding and font type

--- src/core/components/common/InfoCard.tsx
+++ src/core/components/common/InfoCard.tsx
@@ -8,7 +8,7 @@ export function InfoCard({ text, type, className }: IInfoCard) {
   const strokeColor: string = type === 'error' ? 'stroke-red-400' : type === 'success' ? 'stroke-green-400' : 'stroke-yellow-400';
 
   return (
-    <div className={cn('flex flex-row items-center justify-center space-x-2 p-2 text-sm font-medium text-slate-500', className)}>
+    <div className={cn('flex flex-row items-center justify-center space-x-2 text-sm font-normal text-slate-500', className)}>
       <div className='flex h-full flex-col justify-center'>
         {type === 'error' && <CircleX className={cn('h-5 w-5', strokeColor)} strokeWidth={2} />}
         {type === 'success' && <CircleCheck className={cn('h-5 w-5', strokeColor)} strokeWidth={2} />}
