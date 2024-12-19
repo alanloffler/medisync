87810852c2065f2b33f087344477117e28d88273
alanloffler <alanmatiasloffler@gmail.com>
2024-12-19 11:39:32 -0300

    Change: help store

--- src/core/components/common/RemoveDialog.tsx
+++ src/core/components/common/RemoveDialog.tsx
@@ -11,13 +11,13 @@ import { useMutation } from '@tanstack/react-query';
 // Imports
 import type { IResponse } from '@core/interfaces/response.interface';
 import { REMOVE_DIALOG_CONFIG } from '@config/common.config';
+import { useHelpStore } from '@settings/stores/help.store';
 // Interfaces
 interface IRemoveDialog {
   action: () => Promise<IResponse | Error>;
   callback?: any;
   dialogContent: ReactNode;
   dialogTexts: IDialogTexts;
-  help: boolean;
   tooltip: string;
   triggerButton: ReactNode;
 }
@@ -30,8 +30,9 @@ interface IDialogTexts {
   title?: string;
 }
 // React component
-export function RemoveDialog({ action, callback, dialogContent, dialogTexts, help, tooltip, triggerButton }: IRemoveDialog) {
+export function RemoveDialog({ action, callback, dialogContent, dialogTexts, tooltip, triggerButton }: IRemoveDialog) {
   const [openDialog, setOpenDialog] = useState(false);
+  const { help } = useHelpStore();
 
   const { error, isError, isPending, mutate, reset } = useMutation({
     mutationFn: action,
@@ -69,13 +70,13 @@ export function RemoveDialog({ action, callback, dialogContent, dialogTexts, hel
         <DialogContent>
           <DialogHeader>
             <DialogTitle className='text-xl'>{dialogTexts.title || REMOVE_DIALOG_CONFIG.default.title}</DialogTitle>
-            <DialogDescription className='text-xsm text-muted-foreground'>{dialogTexts.description || REMOVE_DIALOG_CONFIG.default.description}</DialogDescription>
+            <DialogDescription className='text-xsm text-muted-foreground'>
+              {dialogTexts.description || REMOVE_DIALOG_CONFIG.default.description}
+            </DialogDescription>
             <section className='flex flex-col py-4'>
-              <section className='text-sm space-y-2'>{dialogContent}</section>
+              <section className='space-y-2 text-sm'>{dialogContent}</section>
               {isError && <InfoCard text={error.message} type='error' />}
-              {isPending && (
-                <LoadingDB size='xs' variant='default' text={dialogTexts.deleting || REMOVE_DIALOG_CONFIG.default.deleting} />
-              )}
+              {isPending && <LoadingDB size='xs' variant='default' text={dialogTexts.deleting || REMOVE_DIALOG_CONFIG.default.deleting} />}
             </section>
             <footer className='flex justify-end space-x-4'>
               <Button onClick={() => setOpenDialog(false)} variant='ghost' size='sm'>
