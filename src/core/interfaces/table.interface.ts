a95005c559c1c2d7714ea82fdfb749f49bf4f3b3
alanloffler <alanmatiasloffler@gmail.com>
2024-12-19 22:30:20 -0300

    Delete: help property

--- src/core/interfaces/table.interface.ts
+++ src/core/interfaces/table.interface.ts
@@ -4,9 +4,10 @@ import type { IUserSearch } from '@users/interfaces/user-search.interface';
 import type { PaginationState, SortingState } from '@tanstack/react-table';
 
 export interface IDataTable {
-  reload: number;
-  setErrorMessage: React.Dispatch<React.SetStateAction<string>>;
-  setReload: React.Dispatch<React.SetStateAction<number>>;
+  help?: boolean;
+  reload?: number;
+  setErrorMessage?: React.Dispatch<React.SetStateAction<string>>;
+  setReload?: React.Dispatch<React.SetStateAction<number>>;
 }
 
 export interface IDataTableAppointments extends IDataTable {
