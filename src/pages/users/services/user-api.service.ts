ef7eeb3ceb723866fa0b96efba8ded74ff17aa00
alanloffler <alanmatiasloffler@gmail.com>
2024-12-21 16:21:18 -0300

    Change: unified method for user search (by name or identity number). Removed unused method

--- src/pages/users/services/user-api.service.ts
+++ src/pages/users/services/user-api.service.ts
@@ -3,7 +3,9 @@ import type { IUserForm } from '@users/interfaces/user.interface';
 import type { SortingState } from '@tanstack/react-table';
 import { APP_CONFIG } from '@config/app.config';
 import { EMethods } from '@core/enums/methods.enum';
+import { EUserSearch } from '@users/enums/user-search.enum';
 import { UserUtils } from '@users/services/user.utils';
+import { UtilsUrl } from '@core/services/utils/url.service';
 
 export class UserApiService {
   private static readonly API_URL: string = import.meta.env.VITE_API_URL;
@@ -27,38 +29,25 @@ export class UserApiService {
     }
   }
 
-  public static async findAll(search: string, sorting: SortingState, skip: number, limit: number) {
-    const url: string = `${this.API_URL}/users?search=${search}&skip=${skip}&limit=${limit}&sk=${sorting[0].id}&sv=${sorting[0].desc ? 'desc' : 'asc'}`;
+  // CHECKED: TRQ used on 
+  // - UsersCombo.tsx
+  // - UsersDataTable.tsx
+  // Find all users by identity number (many users with partial identity number search)
+  public static async searchUsersBy(search: string, sorting: SortingState, skip: number, limit: number, searchBy: string) {
+    let path: string = '';
+    if (searchBy === EUserSearch.NAME) path = `${this.API_URL}/users`;
+    if (searchBy === EUserSearch.IDENTITY) path = `${this.API_URL}/users/byIdentityNumber`;
+    if (!searchBy || searchBy === '') throw new Error('Dev Error: searchBy is required and must be a valid enum of EUserSearch');
 
-    try {
-      const query: Response = await fetch(url, {
-        headers: {
-          'content-type': 'application/json;charset=UTF-8',
-        },
-        method: 'GET',
-      });
+    const url: URL = new URL(path);
 
-      return await query.json();
-    } catch (error) {
-      return error;
-    }
-  }
-  // Find all users by DNI (many users with partial DNI search)
-  public static async findAllByDNI(search: string, sorting: SortingState, skip: number, limit: number) {
-    const url: string = `${this.API_URL}/users/byDNI?search=${search}&skip=${skip}&limit=${limit}&sk=${sorting[0].id}&sv=${sorting[0].desc ? 'desc' : 'asc'}`;
+    url.searchParams.append('search', search);
+    url.searchParams.append('skip', skip.toString());
+    url.searchParams.append('limit', limit.toString());
+    url.searchParams.append('sk', sorting[0].id);
+    url.searchParams.append('sv', sorting[0].desc ? 'desc' : 'asc');
 
-    try {
-      const query: Response = await fetch(url, {
-        headers: {
-          'content-type': 'application/json;charset=UTF-8',
-        },
-        method: 'GET',
-      });
-
-      return await query.json();
-    } catch (error) {
-      return error;
-    }
+    return await UtilsUrl.fetch(url, EMethods.GET);
   }
 
   public static async findOne(id: string) {
@@ -129,11 +118,11 @@ export class UserApiService {
     const prevMonth = this.countByMonth(month - 1, year);
 
     const response = await Promise.all([actualMonth, prevMonth]);
-    
+
     let percentage: number;
-    (response[0].data.total >= response[1].data.total) ? percentage = 100 : percentage = -100;
-    
-    return response[0].data.total / response[1].data.total * percentage;
+    response[0].data.total >= response[1].data.total ? (percentage = 100) : (percentage = -100);
+
+    return (response[0].data.total / response[1].data.total) * percentage;
   }
 
   private static async fetch(url: string, method: EMethods, body?: any) {
