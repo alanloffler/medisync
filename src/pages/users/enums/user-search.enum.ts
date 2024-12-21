4f9603b479d3002ee55f10bb03a360c45d18fb19
alanloffler <alanmatiasloffler@gmail.com>
2024-12-21 16:01:07 -0300

    Create: user search enums

---
+++ src/pages/users/enums/user-search.enum.ts
@@ -0,0 +1,10 @@
+export enum EUserSearch {
+  IDENTITY = 'dni',
+  NAME = 'name',
+}
+
+export enum ESortingKeys {
+  FIRST_NAME = 'firstName',
+  IDENTITY_NUMBER = 'dni',
+  LAST_NAME = 'lastName',
+}
