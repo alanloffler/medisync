821322f0c73df224405a379202d57d7b5fd4e8ce
alanloffler <alanmatiasloffler@gmail.com>
2024-12-23 09:55:31 -0300

    Update: useCallback on methods and useMemo on columns jsx element for better performance

--- src/pages/appointments/components/appos-record/ApposTable.tsx
+++ src/pages/appointments/components/appos-record/ApposTable.tsx
@@ -10,6 +10,7 @@ import { TableButton } from '@core/components/common/TableButton';
 import { Trans, useTranslation } from 'react-i18next';
 import { format } from '@formkit/tempo';
 import { type Cell, type ColumnDef, flexRender, getCoreRowModel, type Row, useReactTable } from '@tanstack/react-table';
+import { useCallback, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 // Imports
 import type { IAppointmentView } from '@appointments/interfaces/appointment.interface';
@@ -27,104 +28,110 @@ export function ApposTable({
   const navigate = useNavigate();
   const { i18n, t } = useTranslation();
 
-  const columns: ColumnDef<IAppointmentView>[] = [
-    {
-      accessorKey: 'day',
-      size: 80,
-      cell: ({ row }) => <DateTime day={row.original.day} hour={row.original.hour} />,
-      header: () => <div className='text-center'>{t(UV_CONFIG.table.header[0])}</div>,
+  const handleRowClick = useCallback(
+    (row: Row<IAppointmentView>, cell: Cell<IAppointmentView, unknown>): void => {
+      if (cell.column.getIndex() < row.getAllCells().length - 1) navigate(`/appointments/${row.original._id}`);
     },
-    {
-      accessorKey: 'lastName',
-      cell: ({ row }) => (
-        <div>
-          {UtilsString.upperCase(
-            `${row.original.professional?.title.abbreviation} ${row.original.professional?.firstName} ${row.original.professional?.lastName}`,
-            'each',
-          )}
-        </div>
-      ),
-      header: () => <div className='text-left'>{t(UV_CONFIG.table.header[1])}</div>,
-    },
-    {
-      accessorKey: 'actions',
-      size: 100,
-      cell: ({ row }) => (
-        <div className='flex items-center justify-center space-x-2'>
-          <TableButton callback={() => navigate(`/appointments/${row.original._id}`)} tooltip={t('tooltip.details')} className='hover:text-sky-500'>
-            <FileText size={16} strokeWidth={1.5} />
-          </TableButton>
-          <TableButton
-            callback={() => navigate(`/whatsapp/user/${row.original._id}`)}
-            tooltip={t('tooltip.sendMessage')}
-            className='hover:text-green-500'
-          >
-            <MessageCircle size={16} strokeWidth={1.5} />
-          </TableButton>
-          <RemoveDialog
-            action={() => AppointmentApiService.remove(row.original._id)}
-            callback={handleRefresh}
-            triggerButton={<Trash2 size={16} strokeWidth={1.5} />}
-            dialogContent={
-              <section className='flex flex-col space-y-3'>
-                <section className='flex flex-row gap-1'>
-                  <Trans
-                    i18nKey={'dialog.deleteAppointment.contentText'}
-                    values={{
-                      firstName: UtilsString.upperCase(row.original.user.firstName, 'each'),
-                      lastName: UtilsString.upperCase(row.original.user.lastName, 'each'),
-                    }}
-                    components={{
-                      span: <span className='font-semibold' />,
-                    }}
-                  />
+    [navigate],
+  );
+
+  const handleRefresh = useCallback((): void => {
+    setRefresh(crypto.randomUUID());
+  }, [setRefresh]);
+
+  const columns: ColumnDef<IAppointmentView>[] = useMemo(
+    () => [
+      {
+        accessorKey: 'day',
+        size: 80,
+        cell: ({ row }) => <DateTime day={row.original.day} hour={row.original.hour} />,
+        header: () => <div className='text-center'>{t(UV_CONFIG.table.header[0])}</div>,
+      },
+      {
+        accessorKey: 'lastName',
+        cell: ({ row }) => (
+          <div>
+            {UtilsString.upperCase(
+              `${row.original.professional?.title.abbreviation} ${row.original.professional?.firstName} ${row.original.professional?.lastName}`,
+              'each',
+            )}
+          </div>
+        ),
+        header: () => <div className='text-left'>{t(UV_CONFIG.table.header[1])}</div>,
+      },
+      {
+        accessorKey: 'actions',
+        size: 100,
+        cell: ({ row }) => (
+          <div className='flex items-center justify-center space-x-2'>
+            <TableButton callback={() => navigate(`/appointments/${row.original._id}`)} tooltip={t('tooltip.details')} className='hover:text-sky-500'>
+              <FileText size={16} strokeWidth={1.5} />
+            </TableButton>
+            <TableButton
+              callback={() => navigate(`/whatsapp/user/${row.original._id}`)}
+              tooltip={t('tooltip.sendMessage')}
+              className='hover:text-green-500'
+            >
+              <MessageCircle size={16} strokeWidth={1.5} />
+            </TableButton>
+            <RemoveDialog
+              action={() => AppointmentApiService.remove(row.original._id)}
+              callback={handleRefresh}
+              triggerButton={<Trash2 size={16} strokeWidth={1.5} />}
+              dialogContent={
+                <section className='flex flex-col space-y-3'>
+                  <section className='flex flex-row gap-1'>
+                    <Trans
+                      i18nKey={'dialog.deleteAppointment.contentText'}
+                      values={{
+                        firstName: UtilsString.upperCase(row.original.user.firstName, 'each'),
+                        lastName: UtilsString.upperCase(row.original.user.lastName, 'each'),
+                      }}
+                      components={{
+                        span: <span className='font-semibold' />,
+                      }}
+                    />
+                  </section>
+                  <section className='flex flex-row space-x-2 font-medium'>
+                    <div className='flex items-center space-x-2 rounded-sm bg-sky-100 px-2 py-1 pl-1 text-xsm text-sky-600'>
+                      <Calendar size={16} strokeWidth={2} />
+                      <span>{format(row.original.day, 'short', i18n.language)}</span>
+                    </div>
+                    <div className='flex items-center space-x-2 rounded-sm bg-emerald-100 px-2 py-1 pl-1 text-xsm text-emerald-600'>
+                      <Clock size={16} strokeWidth={2} />
+                      <span>{row.original.hour}</span>
+                    </div>
+                    <span className='font-medium'>
+                      {UtilsString.upperCase(
+                        `${row.original.professional.title.abbreviation} ${row.original.professional.firstName} ${row.original.professional.lastName}`,
+                        'each',
+                      )}
+                    </span>
+                  </section>
                 </section>
-                <section className='flex flex-row space-x-2 font-medium'>
-                  <div className='flex items-center space-x-2 rounded-sm bg-sky-100 px-2 py-1 pl-1 text-xsm text-sky-600'>
-                    <Calendar size={16} strokeWidth={2} />
-                    <span>{format(row.original.day, 'short', i18n.language)}</span>
-                  </div>
-                  <div className='flex items-center space-x-2 rounded-sm bg-emerald-100 px-2 py-1 pl-1 text-xsm text-emerald-600'>
-                    <Clock size={16} strokeWidth={2} />
-                    <span>{row.original.hour}</span>
-                  </div>
-                  <span className='font-medium'>
-                    {UtilsString.upperCase(
-                      `${row.original.professional.title.abbreviation} ${row.original.professional.firstName} ${row.original.professional.lastName}`,
-                      'each',
-                    )}
-                  </span>
-                </section>
-              </section>
-            }
-            dialogTexts={{
-              cancelButton: t('button.cancel'),
-              deleting: t('deleting.appointment'),
-              description: t('dialog.deleteAppointment.description'),
-              removeButton: t('button.deleteAppointment'),
-              title: t('dialog.deleteAppointment.title'),
-            }}
-            tooltip={t('tooltip.delete')}
-          />
-        </div>
-      ),
-      header: () => <div className='text-center'>{t(UV_CONFIG.table.header[2])}</div>,
-    },
-  ];
+              }
+              dialogTexts={{
+                cancelButton: t('button.cancel'),
+                deleting: t('deleting.appointment'),
+                description: t('dialog.deleteAppointment.description'),
+                removeButton: t('button.deleteAppointment'),
+                title: t('dialog.deleteAppointment.title'),
+              }}
+              tooltip={t('tooltip.delete')}
+            />
+          </div>
+        ),
+        header: () => <div className='text-center'>{t(UV_CONFIG.table.header[2])}</div>,
+      },
+    ],
+    [handleRefresh, i18n.language, navigate, t],
+  );
 
   const table = useReactTable<IAppointmentView>({
     data: appointments,
     columns,
     getCoreRowModel: getCoreRowModel(),
   });
-
-  function handleRowClick(row: Row<IAppointmentView>, cell: Cell<IAppointmentView, unknown>): void {
-    if (cell.column.getIndex() < row.getAllCells().length - 1) navigate(`/appointments/${row.original._id}`);
-  }
-
-  function handleRefresh(): void {
-    setRefresh(crypto.randomUUID());
-  }
 
   return (
     <Table>
