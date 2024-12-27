39cb16faf3b1cc26f79a9fae3dd654cbc0581c34
alanloffler <alanmatiasloffler@gmail.com>
2024-12-26 22:29:41 -0300

    Update: translated breadcrumb content. Removed unused content replaced by translation

--- src/config/whatsapp.config.ts
+++ src/config/whatsapp.config.ts
@@ -1,17 +1,6 @@
 export const WHATSAPP_CONFIG = {
   breadcrumb: [
-    { id: 1, name: 'Inicio', path: '/' },
-    { id: 2, name: 'WhatsApp', path: '/whatsapp' },
+    { id: 1, name: 'breadcrumb.home', path: '/' },
+    { id: 2, name: 'breadcrumb.phoneMessage', path: '/whatsapp' },
   ],
-  button: {
-    back: 'Volver',
-    cancel: 'Cancelar',
-    sendMessage: 'Enviar mensaje',
-  },
-  label: {
-    message: 'Mensaje',
-    phone: 'Teléfono',
-  },
-  subtitle: 'Vas a enviar un mensaje de texto de WhatsApp al teléfono celular de',
-  title: 'Enviar mensaje de WhatsApp',
 };
