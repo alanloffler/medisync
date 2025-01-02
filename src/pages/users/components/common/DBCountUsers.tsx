2b9dfbe2d1a06ac2737e44969ab64e760222088d
alanloffler <alanmatiasloffler@gmail.com>
2025-01-02 16:54:08 -0300

    Change: unified fetch method with useQuery. Data came processed from DB. Update styles

--- src/pages/users/components/common/DBCountUsers.tsx
+++ src/pages/users/components/common/DBCountUsers.tsx
@@ -1,5 +1,5 @@
 // Icons: https://lucide.dev/icons/
-import { Database, TrendingDown, TrendingUp } from 'lucide-react';
+import { Database, MoveRight, TrendingDown, TrendingUp } from 'lucide-react';
 // Components
 import { LoadingDB } from '@core/components/common/LoadingDB';
 // External imports
@@ -7,51 +7,37 @@ import i18n from '@core/i18n/i18n';
 import { useQuery } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
 // Imports
+import type { IResponse } from '@core/interfaces/response.interface';
 import { UserApiService } from '@users/services/user-api.service';
 // React component
 export function DBCountUsers() {
   const { t } = useTranslation();
 
-  const {
-    data: dbCount,
-    isLoading,
-    isError,
-  } = useQuery({
-    queryKey: ['users', 'db-count'],
-    queryFn: async () => await UserApiService.countAll(),
+  const { data, isError, isLoading } = useQuery<IResponse<{ percentage: number; today: number; total: number }>>({
+    queryKey: ['users', 'DBCountUsers'],
+    queryFn: async () => await UserApiService.newUsersToday(),
   });
-
-  const { data: diffPrevMonth } = useQuery({
-    queryKey: ['users', 'diffPrevMonth'],
-    queryFn: async () => await UserApiService.differenceBetweenMonths(new Date().getMonth() + 1, new Date().getFullYear()),
-  });
-
-  const total: number = dbCount?.data.total;
 
   if (isLoading) return <LoadingDB variant='default' empty />;
   if (isError) return <main className='py-2'></main>;
 
   return (
-    total && (
+    data?.data && (
       <main className='flex flex-row justify-end gap-2 py-3 text-xsm font-normal text-slate-400'>
         <section className='flex items-center space-x-1'>
-          <Database size={16} strokeWidth={2} className='text-blue-400' />
-          <span>{t('table.totalItems.users', { count: total })}</span>
+          <Database size={16} strokeWidth={2} className='text-orange-400' />
+          <span>{t('table.totalItems.users', { count: data.data.total })}</span>
         </section>
-        {diffPrevMonth !== undefined && (
-          <section className='flex items-center space-x-1'>
-            {diffPrevMonth >= 0 ? (
-              <TrendingUp size={16} strokeWidth={2} className='text-emerald-400' />
-            ) : (
-              <TrendingDown size={16} strokeWidth={2} className='text-rose-400' />
-            )}
-            <div>
-              <span>{diffPrevMonth >= 0 ? '+' : ''}</span>
-              <span>{`${i18n.format(diffPrevMonth, 'number', i18n.resolvedLanguage)}%`}</span>
-            </div>
-            <span>{t('table.difference.lastMonth')}</span>
-          </section>
-        )}
+        <section className='flex items-center space-x-1'>
+          {data.data.percentage > 0 && <TrendingUp size={16} strokeWidth={2} className='text-emerald-400' />}
+          {data.data.percentage < 0 && <TrendingDown size={16} strokeWidth={2} className='text-rose-400' />}
+          {data.data.percentage === 0 && <MoveRight size={16} strokeWidth={2} className='text-inherit' />}
+          <div>
+            <span>{data.data.percentage > 0 ? '+' : ''}</span>
+            <span>{`${i18n.format(Number(data.data.percentage), 'number', i18n.resolvedLanguage)}%`}</span>
+          </div>
+          <span>{t('table.difference.yesterday')}</span>
+        </section>
       </main>
     )
   );
