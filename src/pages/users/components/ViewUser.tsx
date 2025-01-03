ae920d1a870426e66835733b2173b74b8c99356a
alanloffler <alanmatiasloffler@gmail.com>
2025-01-03 19:34:59 -0300

    Add: remove dialog

--- src/pages/users/components/ViewUser.tsx
+++ src/pages/users/components/ViewUser.tsx
@@ -1,22 +1,25 @@
 // Icons: https://lucide.dev/icons/
-import { ArrowRight, CreditCard, Mail, MessageCircle, PencilLine, Send, Smartphone, Trash2 } from 'lucide-react';
+import { ArrowRight, CreditCard, Mail, MailX, MessageCircle, PencilLine, Smartphone, Trash2 } from 'lucide-react';
 // External components: https://ui.shadcn.com/docs/components
 import { Button } from '@core/components/ui/button';
 import { Card, CardContent } from '@core/components/ui/card';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@core/components/ui/dialog';
 // Components
 import { ApposRecord } from '@appointments/components/appos-record/ApposRecord';
 import { BackButton } from '@core/components/common/BackButton';
+import { InfoCard } from '@core/components/common/InfoCard';
 import { LoadingDB } from '@core/components/common/LoadingDB';
 import { PageHeader } from '@core/components/common/PageHeader';
 import { TableButton } from '@core/components/common/TableButton';
 // External imports
 import { format } from '@formkit/tempo';
 import { useAnimate } from 'motion/react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
 // Imports
+import type { IDialog } from '@core/interfaces/dialog.interface';
 import type { IResponse } from '@core/interfaces/response.interface';
 import type { IUser } from '@users/interfaces/user.interface';
 import { HEADER_CONFIG } from '@config/layout/header.config';
@@ -27,9 +30,10 @@ import { motion } from '@core/services/motion.service';
 import { useDelimiter } from '@core/hooks/useDelimiter';
 import { useHeaderMenuStore } from '@layout/stores/header-menu.service';
 import { useNotificationsStore } from '@core/stores/notifications.store';
-import { InfoCard } from '@core/components/common/InfoCard';
 // React component
 export default function ViewUser() {
+  const [dialogData, setDialogData] = useState<IDialog | undefined>(undefined);
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
   const [gotoScope, gotoAnimation] = useAnimate();
   const addNotification = useNotificationsStore((state) => state.addNotification);
   const delimiter = useDelimiter();
@@ -57,6 +61,20 @@ export default function ViewUser() {
     if (isError) addNotification({ type: 'error', message: error.message });
   }, [addNotification, error?.message, isError]);
 
+  function handleRemoveUserDialog(): void {
+    setOpenDialog(true);
+    setDialogData({
+      title: t('dialog.deleteUser.title'),
+      description: t('dialog.deleteUser.description'),
+      content: 'This is the content when removing an user',
+      callback: handleRemoveUser,
+    });
+  }
+
+  function handleRemoveUser(): void {
+    console.log(`Remove user ${user?.data.firstName}`, user?.data._id);
+  }
+
   function gotoAnimationOver(): void {
     const { keyframes, options } = motion.x(3).type('bounce').animate();
     gotoAnimation(gotoScope.current, keyframes, options);
@@ -68,109 +86,130 @@ export default function ViewUser() {
   }
 
   return (
-    <main className='flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8 lg:gap-8 lg:p-8'>
-      {/* Section: Page Header */}
-      <header className='flex items-center justify-between'>
-        <PageHeader title={t('pageTitle.viewUser')} breadcrumb={UV_CONFIG.breadcrumb} />
-        <BackButton label={t('button.back')} />
-      </header>
-      {/* Section: Page Content */}
-      <section className='grid w-full gap-4 md:grid-cols-5 md:gap-8 lg:grid-cols-5 lg:gap-8 xl:grid-cols-6 xl:gap-8'>
-        {isLoading && (
-          <LoadingDB
-            text={t('loading.userDetails')}
-            variant='card'
-            className='col-span-1 mx-auto h-fit w-full py-3 md:col-span-2 lg:col-span-2 xl:col-span-2'
-          />
-        )}
-        {isError && (
-          <Card className='col-span-1 mx-auto h-fit w-full py-3 md:col-span-2 lg:col-span-2 xl:col-span-2'>
-            <InfoCard type='error' text={error.message} />
-          </Card>
-        )}
-        {isSuccess && (
-          <Card className='col-span-1 mx-auto h-fit w-full md:col-span-2 lg:col-span-2 xl:col-span-2'>
-            <header className='relative flex items-center justify-center rounded-t-lg bg-slate-200 p-3 text-slate-700'>
-              <h1 className='text-center text-xl font-bold'>{UtilsString.upperCase(`${user.data.firstName} ${user.data.lastName}`, 'each')}</h1>
-            </header>
-            <CardContent className='mt-3 space-y-3 overflow-auto'>
-              <section className='flex items-center space-x-3'>
-                <div className='rounded-md bg-slate-100 p-1.5 text-slate-600'>
-                  <CreditCard size={17} strokeWidth={2} />
-                </div>
-                <span className='text-sm'>{i18n.format(user.data.dni, 'number', i18n.resolvedLanguage)}</span>
-              </section>
-              <section className='flex items-center space-x-3'>
-                <div className='rounded-md bg-slate-100 p-1.5 text-slate-600'>
-                  <Smartphone size={17} strokeWidth={2} />
-                </div>
-                <span className='text-sm'>{delimiter(user.data.phone, '-', 6)}</span>
-              </section>
-              {user.data.email && (
+    <>
+      <main className='flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8 lg:gap-8 lg:p-8'>
+        {/* Section: Page Header */}
+        <header className='flex items-center justify-between'>
+          <PageHeader title={t('pageTitle.viewUser')} breadcrumb={UV_CONFIG.breadcrumb} />
+          <BackButton label={t('button.back')} />
+        </header>
+        {/* Section: Page Content */}
+        <section className='grid w-full gap-4 md:grid-cols-5 md:gap-8 lg:grid-cols-5 lg:gap-8 xl:grid-cols-6 xl:gap-8'>
+          {isLoading && (
+            <LoadingDB
+              text={t('loading.userDetails')}
+              variant='card'
+              className='col-span-1 mx-auto h-fit w-full py-3 md:col-span-2 lg:col-span-2 xl:col-span-2'
+            />
+          )}
+          {isError && (
+            <Card className='col-span-1 mx-auto h-fit w-full py-3 md:col-span-2 lg:col-span-2 xl:col-span-2'>
+              <InfoCard type='error' text={error.message} />
+            </Card>
+          )}
+          {isSuccess && (
+            <Card className='col-span-1 mx-auto h-fit w-full md:col-span-2 lg:col-span-2 xl:col-span-2'>
+              <header className='relative flex items-center justify-center rounded-t-lg bg-slate-200 p-3 text-slate-700'>
+                <h1 className='text-center text-xl font-bold'>{UtilsString.upperCase(`${user.data.firstName} ${user.data.lastName}`, 'each')}</h1>
+              </header>
+              <CardContent className='mt-3 space-y-3 overflow-auto'>
                 <section className='flex items-center space-x-3'>
                   <div className='rounded-md bg-slate-100 p-1.5 text-slate-600'>
-                    <Mail size={17} strokeWidth={2} />
+                    <CreditCard size={17} strokeWidth={2} />
                   </div>
-                  <span className='text-sm'>{user.data.email}</span>
+                  <span className='text-sm'>{i18n.format(user.data.dni, 'number', i18n.resolvedLanguage)}</span>
                 </section>
-              )}
-              {user.data.createdAt && (
-                <section className='pt-2 text-sm'>
-                  {t('cardContent.userSince', {
-                    date: format(user.data.createdAt, 'long', localStorage.getItem('i18nextLng') ?? i18n.resolvedLanguage),
-                  })}
+                <section className='flex items-center space-x-3'>
+                  <div className='rounded-md bg-slate-100 p-1.5 text-slate-600'>
+                    <Smartphone size={17} strokeWidth={2} />
+                  </div>
+                  <span className='text-sm'>{delimiter(user.data.phone, '-', 6)}</span>
                 </section>
-              )}
-            </CardContent>
-            <section className='flex items-center justify-end space-x-2 border-t p-2'>
-              <TableButton
-                callback={() => navigate(`/email/${user.data._id}`)}
-                className='hover:text-sky-500'
-                disabled={!user?.data.email}
-                tooltip={t('tooltip.sendEmail')}
-              >
-                <Send size={18} strokeWidth={1.5} />
-              </TableButton>
-              <TableButton
-                callback={() => navigate(`/whatsapp/${user.data._id}`)}
-                className='hover:text-green-500'
-                tooltip={t('tooltip.sendMessage')}
-              >
-                <MessageCircle size={18} strokeWidth={1.5} />
-              </TableButton>
-              <TableButton
-                callback={() => navigate(`/users/update/${user.data._id}`)}
-                className='hover:text-fuchsia-500'
-                tooltip={t('tooltip.updateUser')}
-              >
-                <PencilLine size={18} strokeWidth={1.5} />
-              </TableButton>
-              {/* TODO: create dialog for user delete */}
-              <TableButton callback={() => console.log('Open dialog')} className='hover:text-rose-500' tooltip={t('tooltip.deleteUser')}>
-                <Trash2 size={18} strokeWidth={1.5} />
-              </TableButton>
+                {user.data.email && (
+                  <section className='flex items-center space-x-3'>
+                    <div className='rounded-md bg-slate-100 p-1.5 text-slate-600'>
+                      <Mail size={17} strokeWidth={2} />
+                    </div>
+                    <span className='text-sm'>{user.data.email}</span>
+                  </section>
+                )}
+                {user.data.createdAt && (
+                  <section className='pt-2 text-sm'>
+                    {t('cardContent.userSince', {
+                      date: format(user.data.createdAt, 'long', localStorage.getItem('i18nextLng') ?? i18n.resolvedLanguage),
+                    })}
+                  </section>
+                )}
+              </CardContent>
+              <section className='flex items-center justify-end space-x-2 border-t p-2'>
+                <TableButton
+                  callback={() => navigate(`/email/${user.data._id}`)}
+                  className='hover:text-sky-500'
+                  disabled={!user.data.email}
+                  tooltip={t('tooltip.sendEmail')}
+                >
+                  {!user.data.email ? <MailX size={18} strokeWidth={1.5} /> : <Mail size={18} strokeWidth={1.5} />}
+                </TableButton>
+                <TableButton
+                  callback={() => navigate(`/whatsapp/${user.data._id}`)}
+                  className='hover:text-green-500'
+                  tooltip={t('tooltip.sendMessage')}
+                >
+                  <MessageCircle size={18} strokeWidth={1.5} />
+                </TableButton>
+                <TableButton
+                  callback={() => navigate(`/users/update/${user.data._id}`)}
+                  className='hover:text-fuchsia-500'
+                  tooltip={t('tooltip.updateUser')}
+                >
+                  <PencilLine size={18} strokeWidth={1.5} />
+                </TableButton>
+                {/* TODO: create dialog for user delete */}
+                <TableButton callback={handleRemoveUserDialog} className='hover:text-rose-500' tooltip={t('tooltip.deleteUser')}>
+                  <Trash2 size={18} strokeWidth={1.5} />
+                </TableButton>
+              </section>
+            </Card>
+          )}
+          {isSuccess && (
+            <section className='col-span-1 overflow-y-auto md:col-span-3 lg:col-span-3 xl:col-span-4'>
+              <ApposRecord userId={user.data._id} />
             </section>
-          </Card>
+          )}
+        </section>
+        <footer className='mx-auto'>
+          <Button
+            variant='default'
+            size='default'
+            className='flex items-center gap-3'
+            onClick={() => navigate('/users')}
+            onMouseOver={gotoAnimationOver}
+            onMouseOut={gotoAnimationOut}
+          >
+            {t('button.goToUsers')}
+            <ArrowRight ref={gotoScope} size={16} strokeWidth={2} />
+          </Button>
+        </footer>
+      </main>
+      <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+        {dialogData && (
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className='text-xl'>{dialogData.title}</DialogTitle>
+              <DialogDescription>{dialogData.description}</DialogDescription>
+            </DialogHeader>
+            <section className='flex flex-col'>{dialogData.content}</section>
+            <footer className='flex flex-col md:justify-end md:flex-row gap-4'>
+              <Button variant='ghost' size='sm' onClick={() => setOpenDialog(false)} className='order-2 md:order-1'>
+                {t('button.cancel')}
+              </Button>
+              <Button variant='remove' size='sm' onClick={dialogData.callback} className='order-1 md:order-2'>
+                {true ? <LoadingDB className='p-0' text={t('loading.deleting')} variant='button' /> : t('button.deleteUser')}
+              </Button>
+            </footer>
+          </DialogContent>
         )}
-        {isSuccess && (
-          <section className='col-span-1 overflow-y-auto md:col-span-3 lg:col-span-3 xl:col-span-4'>
-            <ApposRecord userId={user.data._id} />
-          </section>
-        )}
-      </section>
-      <footer className='mx-auto'>
-        <Button
-          variant='default'
-          size='default'
-          className='flex items-center gap-3'
-          onClick={() => navigate('/users')}
-          onMouseOver={gotoAnimationOver}
-          onMouseOut={gotoAnimationOut}
-        >
-          {t('button.goToUsers')}
-          <ArrowRight ref={gotoScope} size={16} strokeWidth={2} />
-        </Button>
-      </footer>
-    </main>
+      </Dialog>
+    </>
   );
 }
