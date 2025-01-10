a8ebe418a4131cbf679a0057d764c6da44f43ca4
alanloffler <alanmatiasloffler@gmail.com>
2025-01-10 14:51:07 -0300

    Add: IPaginatedUsersVars interface

--- src/pages/users/interfaces/mutation-vars.interface.ts
+++ src/pages/users/interfaces/mutation-vars.interface.ts
@@ -1,7 +1,15 @@
-import { userSchema } from "@users/schemas/user.schema";
-import { z } from "zod";
+import type { ITableManager } from '@core/interfaces/table.interface';
+import type { IUserSearch } from '@users/interfaces/user-search.interface';
+import { userSchema } from '@users/schemas/user.schema';
+import { z } from 'zod';
 
 export interface IUpdateUserVars {
   id: string;
   data: z.infer<typeof userSchema>;
+}
+
+export interface IPaginatedUsersVars {
+  search: IUserSearch;
+  skipItems: number;
+  tableManager: ITableManager;
 }
