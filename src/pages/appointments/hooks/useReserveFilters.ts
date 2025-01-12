63d6e57b78fe18ab8a825c46cf5044490eae74f7
alanloffler <alanmatiasloffler@gmail.com>
2025-01-12 20:45:10 -0300

    Add: appos filters interface

--- src/pages/appointments/hooks/useReserveFilters.ts
+++ src/pages/appointments/hooks/useReserveFilters.ts
@@ -1,24 +1,30 @@
 // External imports
 import { useCallback } from 'react';
 import { useSearchParams } from 'react-router-dom';
+// Interface
+interface IApposFilters {
+  professional: string;
+}
 // React hook
 export function useReserveFilters() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const professional = searchParams.get('p') as IApposFilters['professional'];
+  // const date = searchParams.get('d') as IApposFilters['date'];
 
-  const professional: string = searchParams.get('p') as string;
-
-  const setFilters = useCallback((filters: any) => {
+  const setFilters = useCallback((filters: IApposFilters) => {
     setSearchParams((params) => {
       if (filters.professional !== undefined) params.set('p', filters.professional);
+      // if (filters.date !== undefined) params.set('d', filters.date);
 
       return params;
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const clearFilters = useCallback((filters: any) => {
+  const clearFilters = useCallback((filters: Partial<IApposFilters>) => {
     setSearchParams((params) => {
       if (filters.professional !== undefined) params.delete('p');
+      // if (filters.date !== undefined) params.delete('d');
 
       return params;
     });
@@ -26,6 +32,7 @@ export function useReserveFilters() {
   }, []);
 
   return {
+    // date,
     professional,
     setFilters,
     clearFilters,
