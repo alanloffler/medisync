6da8675d599fd5fbb580e5268205550475b060e6
alanloffler <alanmatiasloffler@gmail.com>
2025-01-14 19:52:12 -0300

    Delete: clear reserve filters, moved to root

--- src/pages/professionals/components/common/ProfessionalsCombobox.tsx
+++ src/pages/professionals/components/common/ProfessionalsCombobox.tsx
@@ -1,5 +1,5 @@
 // Icons: https://lucide.dev/icons
-import { Check, ChevronsUpDown, X } from 'lucide-react';
+import { Check, ChevronsUpDown } from 'lucide-react';
 // External components: https://ui.shadcn.com/docs/components
 import { Button } from '@core/components/ui/button';
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@core/components/ui/command';
@@ -7,11 +7,9 @@ import { Popover, PopoverContent, PopoverTrigger } from '@core/components/ui/pop
 // Components
 import { InfoCard } from '@core/components/common/InfoCard';
 import { LoadingText } from '@core/components/common/LoadingText';
-import { TooltipWrapper } from '@core/components/common/TooltipWrapper';
 // External imports
 import { SetStateAction, useCallback, useEffect, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
-import { useTranslation } from 'react-i18next';
 // Imports
 import type { IInfoCard } from '@core/components/common/interfaces/infocard.interface';
 import type { IProfessional } from '@professionals/interfaces/professional.interface';
@@ -20,7 +18,6 @@ import { ProfessionalApiService } from '@professionals/services/professional-api
 import { UtilsString } from '@core/services/utils/string.service';
 import { cn } from '@lib/utils';
 import { useNotificationsStore } from '@core/stores/notifications.store';
-import { useReserveFilters } from '@appointments/hooks/useReserveFilters';
 // Interface
 interface IProfessionalsCombobox {
   onSelectProfessional: (professional: IProfessional | undefined) => void;
@@ -33,14 +30,12 @@ interface IProfessionalsCombobox {
   className?: string;
 }
 // React component
-export function ProfessionalsCombobox({ onSelectProfessional, options, className }: IProfessionalsCombobox) {
+export function ProfessionalsCombobox({ className, onSelectProfessional, options }: IProfessionalsCombobox) {
   const [infoCard, setInfoCard] = useState<IInfoCard>({} as IInfoCard);
   const [openCombobox, setOpenCombobox] = useState<boolean>(false);
   const [value, setValue] = useState<string | undefined>(undefined);
   const addNotification = useNotificationsStore((state) => state.addNotification);
   const { loadingText, notFoundText, placeholder, searchText } = options;
-  const { dateParam, professionalParam, clearFilters, setFilters } = useReserveFilters();
-  const { t } = useTranslation();
 
   const {
     data: professionals,
@@ -63,84 +58,61 @@ export function ProfessionalsCombobox({ onSelectProfessional, options, className
       setValue(currentValue);
       setOpenCombobox(false);
       onSelectProfessional(professional);
-      setFilters({ professionalParam: professional._id });
     },
-    [onSelectProfessional, setFilters],
+    [onSelectProfessional],
   );
 
-  function handleClear(): void {
-    setValue('');
-    onSelectProfessional(undefined);
-    clearFilters({ professionalParam, dateParam });
-  }
-
-  useEffect(() => {
-    if (professionalParam) {
-      const finded = professionals?.data.find((prof) => prof._id === professionalParam);
-      if (finded) handleSelectProfessional(`${finded.title.abbreviation} ${finded.firstName} ${finded.lastName}`, finded);
-    }
-  }, [professionals?.data, professionalParam, handleSelectProfessional, value]);
-
   return (
-    <section className='flex items-center space-x-3'>
-      <Popover open={openCombobox} onOpenChange={setOpenCombobox}>
-        <PopoverTrigger asChild>
-          <Button
-            role='combobox'
-            aria-expanded={openCombobox}
-            className={cn('h-9 w-full justify-between !bg-slate-100/70 px-3 !text-sm font-normal text-foreground hover:!bg-slate-100', className)}
-          >
-            {isLoading ? <LoadingText text={loadingText} suffix='...' /> : value ? UtilsString.upperCase(value, 'each') : placeholder}
-            <ChevronsUpDown size={14} strokeWidth={2} className='ml-3 shrink-0 text-primary opacity-100' />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className='mt-2 w-full border p-0 shadow-sm' align='start'>
-          <Command>
-            {!isError && <CommandInput placeholder={searchText} className='h-8 text-xsm' />}
-            <CommandList>
-              {!isError && <CommandEmpty>{notFoundText}</CommandEmpty>}
-              <CommandGroup>
-                {isError ? (
-                  <div className='relative left-0 max-w-[300px]'>
-                    <InfoCard type={infoCard.type} text={infoCard.text} />
-                  </div>
-                ) : (
-                  <>
-                    {professionals?.data &&
-                      professionals?.data.map((professional) => (
-                        <CommandItem
-                          className='text-xsm'
-                          key={professional._id}
-                          value={`${professional.title.abbreviation} ${professional.firstName} ${professional.lastName}`}
-                          onSelect={(currentValue) => handleSelectProfessional(currentValue, professional)}
-                        >
-                          <Check
-                            className={cn(
-                              'mr-2',
-                              value === `${professional.title.abbreviation} ${professional.firstName} ${professional.lastName}`
-                                ? 'opacity-100'
-                                : 'opacity-0',
-                            )}
-                            size={16}
-                            strokeWidth={2}
-                          />
-                          {UtilsString.upperCase(`${professional.title.abbreviation} ${professional.firstName} ${professional.lastName}`, 'each')}
-                        </CommandItem>
-                      ))}
-                  </>
-                )}
-              </CommandGroup>
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </Popover>
-      {value && (
-        <TooltipWrapper tooltip={t('tooltip.delete')}>
-          <Button variant='clear' size='icon5' className='bg-rose-400 text-white hover:bg-rose-500' onClick={handleClear}>
-            <X size={14} strokeWidth={2} />
-          </Button>
-        </TooltipWrapper>
-      )}
-    </section>
+    <Popover open={openCombobox} onOpenChange={setOpenCombobox}>
+      <PopoverTrigger asChild>
+        <Button
+          role='combobox'
+          aria-expanded={openCombobox}
+          className={cn('h-9 w-full justify-between !bg-slate-100/70 px-3 !text-sm font-normal text-foreground hover:!bg-slate-100', className)}
+        >
+          {isLoading ? <LoadingText text={loadingText} suffix='...' /> : value ? UtilsString.upperCase(value, 'each') : placeholder}
+          <ChevronsUpDown size={14} strokeWidth={2} className='ml-3 shrink-0 text-primary opacity-100' />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className='mt-2 w-full border p-0 shadow-sm' align='start'>
+        <Command>
+          {!isError && <CommandInput placeholder={searchText} className='h-8 text-xsm' />}
+          <CommandList>
+            {!isError && <CommandEmpty>{notFoundText}</CommandEmpty>}
+            <CommandGroup>
+              {isError ? (
+                <div className='relative left-0 max-w-[300px]'>
+                  <InfoCard type={infoCard.type} text={infoCard.text} />
+                </div>
+              ) : (
+                <>
+                  {professionals?.data &&
+                    professionals?.data.map((professional) => (
+                      <CommandItem
+                        className='text-xsm'
+                        key={professional._id}
+                        value={`${professional.title.abbreviation} ${professional.firstName} ${professional.lastName}`}
+                        onSelect={(currentValue) => handleSelectProfessional(currentValue, professional)}
+                      >
+                        <Check
+                          className={cn(
+                            'mr-2',
+                            value === `${professional.title.abbreviation} ${professional.firstName} ${professional.lastName}`
+                              ? 'opacity-100'
+                              : 'opacity-0',
+                          )}
+                          size={16}
+                          strokeWidth={2}
+                        />
+                        {UtilsString.upperCase(`${professional.title.abbreviation} ${professional.firstName} ${professional.lastName}`, 'each')}
+                      </CommandItem>
+                    ))}
+                </>
+              )}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
   );
 }
