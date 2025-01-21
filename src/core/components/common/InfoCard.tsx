4d6131ed3ed818ffc5486ff4e138f3ed57f3788d
alanloffler <alanmatiasloffler@gmail.com>
2025-01-21 15:57:11 -0300

    Add: icon size property

--- src/core/components/common/InfoCard.tsx
+++ src/core/components/common/InfoCard.tsx
@@ -4,15 +4,16 @@ import { CircleAlert, CircleCheck, CircleX } from 'lucide-react';
 import type { IInfoCard } from '@core/components/common/interfaces/infocard.interface';
 import { cn } from '@lib/utils';
 // React component
-export function InfoCard({ text, type, className }: IInfoCard) {
+export function InfoCard({ iconSize, text, type, className }: IInfoCard) {
   const strokeColor: string = type === 'error' ? 'stroke-rose-400' : type === 'success' ? 'stroke-green-400' : 'stroke-yellow-400';
+  const defaultIconSize: number = 20;
 
   return (
     <div className={cn('flex flex-row items-center justify-center space-x-2 text-sm font-normal', className)}>
       <div className='flex h-full flex-col justify-center'>
-        {type === 'error' && <CircleX className={strokeColor} size={20} strokeWidth={2} />}
-        {type === 'success' && <CircleCheck className={strokeColor} size={20} strokeWidth={2} />}
-        {type === 'warning' && <CircleAlert className={strokeColor} size={20} strokeWidth={2} />}
+        {type === 'error' && <CircleX className={strokeColor} size={iconSize ?? defaultIconSize} strokeWidth={2} />}
+        {type === 'success' && <CircleCheck className={strokeColor} size={iconSize ?? defaultIconSize} strokeWidth={2} />}
+        {type === 'warning' && <CircleAlert className={strokeColor} size={iconSize ?? defaultIconSize} strokeWidth={2} />}
       </div>
       <div className='flex flex-col'>
         <span>{text}</span>
