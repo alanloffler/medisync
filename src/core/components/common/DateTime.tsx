3afbaeb4f8d7e51f7ad1ad08fe57867d26acd25e
alanloffler <alanmatiasloffler@gmail.com>
2025-01-22 16:40:11 -0300

    Change: all structure and style

--- src/core/components/common/DateTime.tsx
+++ src/core/components/common/DateTime.tsx
@@ -16,13 +16,13 @@ export function DateTime({ className, day, hour }: IDateTime) {
   const { i18n } = useTranslation();
 
   return (
-    <section className={cn('flex justify-center text-xsm font-normal', className)}>
-      <div className='flex items-center space-x-1 rounded-sm bg-indigo-200 p-1 pr-2 text-indigo-600 md:rounded-r-none'>
-        <Calendar size={14} strokeWidth={2} />
-        <span>{format(day, 'short', i18n.resolvedLanguage)}</span>
+    <section className={cn('flex flex-col gap-1 text-xs font-normal text-slate-500', className)}>
+      <div className='flex items-center space-x-2'>
+        <Calendar size={14} strokeWidth={2} className='text-sky-400' />
+        <span>{format(day, i18n.resolvedLanguage === 'en' ? 'short' : 'DD/MM/YY', i18n.resolvedLanguage)}</span>
       </div>
-      <div className='hidden items-center space-x-1 rounded-r-sm bg-indigo-100 p-1 pr-2 text-indigo-600 md:flex'>
-        <Clock size={14} strokeWidth={2} />
+      <div className='flex items-center space-x-2'>
+        <Clock size={14} strokeWidth={2} className='text-sky-400' />
         <span>{hour}</span>
       </div>
     </section>
