359472accfe17cc2c64034fedfa39ff9630afdd1
alanloffler <alanmatiasloffler@gmail.com>
2025-01-26 19:13:55 -0300

    Add: className property

--- src/core/components/common/header/CardHeaderPrimary.tsx
+++ src/core/components/common/header/CardHeaderPrimary.tsx
@@ -1,14 +1,17 @@
 // External imports
 import { ReactNode } from 'react';
+// Imports
+import { cn } from '@lib/utils';
 // Interface
 interface IProps {
-  children: ReactNode;
+  children?: ReactNode;
+  className?: string;
   title: string;
 }
 // React component
-export function CardHeaderPrimary({ title, children }: IProps) {
+export function CardHeaderPrimary({ className, children, title }: IProps) {
   return (
-    <main className='flex items-center space-x-4 rounded-t-lg bg-primary p-4 text-lg font-semibold text-white'>
+    <main className={cn('flex items-center space-x-4 rounded-t-lg bg-primary p-4 text-lg font-semibold text-white', className)}>
       {children}
       <span>{title}</span>
     </main>
