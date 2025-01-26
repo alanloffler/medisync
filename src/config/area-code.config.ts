322500fc428c7308b61608d086642efb6964e3e2
alanloffler <alanmatiasloffler@gmail.com>
2025-01-26 20:59:57 -0300

    Create: phone area code config file

---
+++ src/config/area-code.config.ts
@@ -0,0 +1,18 @@
+export const AREA_CODE = [
+  {
+    id: 1,
+    code: '549',
+    abbreviation: 'AR',
+    icon: 'argentina',
+    label: 'Argentina',
+    lang: 'es',
+  },
+  { 
+    id: 2, 
+    code: '1', 
+    abbreviation: 'US', 
+    icon: 'united-states', 
+    label: 'United States', 
+    lang: 'en', 
+  },
+];
