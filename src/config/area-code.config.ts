2fa11c1ceafac243d58374dbe41cc5b985155b03
alanloffler <alanmatiasloffler@gmail.com>
2025-01-28 12:10:36 -0300

    Add: Brazil and Paraguay area code items. Update Argentina code

--- src/config/area-code.config.ts
+++ src/config/area-code.config.ts
@@ -1,7 +1,7 @@
 export const AREA_CODE = [
   {
     id: 1,
-    code: '549',
+    code: '54',
     abbreviation: 'AR',
     icon: 'argentina',
     label: 'Argentina',
@@ -9,6 +9,22 @@ export const AREA_CODE = [
   },
   {
     id: 2,
+    code: '55',
+    abbreviation: 'BR',
+    icon: 'brazil',
+    label: 'Brasil',
+    lang: 'pt',
+  },
+  {
+    id: 3,
+    code: '595',
+    abbreviation: 'PY',
+    icon: 'paraguay',
+    label: 'Paraguay',
+    lang: 'es',
+  },
+  {
+    id: 4,
     code: '1',
     abbreviation: 'US',
     icon: 'united-states',
