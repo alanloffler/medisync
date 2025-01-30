d4131e2e9b4efb438cc9858df9b08d34add75636
alanloffler <alanmatiasloffler@gmail.com>
2025-01-29 22:34:49 -0300

    Change: forwardRef

--- src/pages/whatsapp/components/FakeTextarea.tsx
+++ src/pages/whatsapp/components/FakeTextarea.tsx
@@ -1,15 +1,16 @@
 // Imports
 import { cn } from '@lib/utils';
+import { forwardRef } from 'react';
 // Interface
 interface IProps {
   disabled?: boolean;
   text: string;
 }
 // React component
-export function FakeTextarea({ text, disabled }: IProps) {
+export const FakeTextarea = forwardRef<HTMLElement, IProps>(({ text, disabled }, ref) => {
   return (
-    <main className={cn('flex min-h-[80px] w-full rounded-md bg-slate-100/70 px-3 py-2 text-sm', disabled && 'cursor-not-allowed opacity-50')}>
+    <main ref={ref} className={cn('flex min-h-[80px] w-full rounded-md bg-slate-100/70 px-3 py-2 text-sm', disabled && 'cursor-not-allowed opacity-50')}>
       <div dangerouslySetInnerHTML={{ __html: text }} />
     </main>
   );
-}
+});
