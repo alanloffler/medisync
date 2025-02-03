56e9f32c61edde1b143ba2d7c2c3d999d7135c08
alanloffler <alanmatiasloffler@gmail.com>
2025-02-03 16:34:55 -0300

    Add: step property

--- src/core/components/common/form/FormHeader.tsx
+++ src/core/components/common/form/FormHeader.tsx
@@ -3,12 +3,14 @@ import { cn } from '@lib/utils';
 // Interface
 interface IProps {
   className?: string;
+  step?: number;
   title: string;
 }
 // React component
-export function FormHeader({ className, title }: IProps) {
+export function FormHeader({ className, step, title }: IProps) {
   return (
-    <header className={cn('rounded-sm bg-slate-100 px-3 py-2 text-sm font-medium text-slate-600', className)}>
+    <header className={cn('flex items-center space-x-3 rounded-sm bg-slate-100 px-3 py-2 text-sm font-medium text-slate-600', className)}>
+      {step && <div className='flex h-5 w-5 items-center justify-center rounded-full bg-slate-500 text-xs text-slate-50'>{step}</div>}
       <h5>{title}</h5>
     </header>
   );
