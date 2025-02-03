54452943552a5931ea311e773b561cd352c4a4bb
alanloffler <alanmatiasloffler@gmail.com>
2025-02-03 12:18:08 -0300

    Change: children and icon structure. Add icon property

--- src/core/components/common/header/CardHeaderPrimary.tsx
+++ src/core/components/common/header/CardHeaderPrimary.tsx
@@ -6,14 +6,18 @@ import { cn } from '@lib/utils';
 interface IProps {
   children?: ReactNode;
   className?: string;
+  icon?: ReactNode;
   title: string;
 }
 // React component
-export function CardHeaderPrimary({ className, children, title }: IProps) {
+export function CardHeaderPrimary({ className, children, icon, title }: IProps) {
   return (
-    <main className={cn('flex items-center space-x-4 rounded-t-lg bg-primary p-4 text-lg font-semibold text-white', className)}>
+    <main className={cn('flex items-center rounded-t-lg bg-primary p-4 text-lg font-semibold text-white', className)}>
+      <div className='flex items-center space-x-4'>
+        {icon}
+        <span>{title}</span>
+      </div>
       {children}
-      <span>{title}</span>
     </main>
   );
 }
