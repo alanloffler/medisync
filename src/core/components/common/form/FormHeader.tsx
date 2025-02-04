881ead05762f406964e7417adeb94d147653c7c5
alanloffler <alanmatiasloffler@gmail.com>
2025-02-04 16:04:22 -0300

    Update: increase background round corners

--- src/core/components/common/form/FormHeader.tsx
+++ src/core/components/common/form/FormHeader.tsx
@@ -9,7 +9,7 @@ interface IProps {
 // React component
 export function FormHeader({ className, step, title }: IProps) {
   return (
-    <header className={cn('flex items-center space-x-3 rounded-sm bg-slate-100 px-3 py-2 text-sm font-medium text-slate-600', className)}>
+    <header className={cn('flex items-center space-x-3 rounded-md bg-slate-100 px-3 py-2 text-sm font-medium text-slate-600', className)}>
       {step && <div className='flex h-5 w-5 items-center justify-center rounded-full bg-slate-500 text-xs text-slate-50'>{step}</div>}
       <h5>{title}</h5>
     </header>
