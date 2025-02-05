438ca903e0250299036e2945ba0f68291e5f59c6
alanloffler <alanmatiasloffler@gmail.com>
2025-02-05 20:26:22 -0300

    Add: throw error for testing purpose

--- src/pages/settings/services/schedule-settings.service.ts
+++ src/pages/settings/services/schedule-settings.service.ts
@@ -10,6 +10,7 @@ export class ScheduleService {
   // private static readonly API_URL: string = import.meta.env.VITE_REACT_BACKEND_API;
 
   public static async findAllSlotDurations(): Promise<number[]> {
+    // throw new Error('Can\'t fetch slot duration values');
     return [15, 30, 45, 60];
   }
 }
