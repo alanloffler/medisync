dd6a66676c6267c8e396fa89eb7afea50be3fa6c
alanloffler <alanmatiasloffler@gmail.com>
2025-02-05 18:36:11 -0300

    Update: font size on title

--- src/core/components/common/header/CardHeaderPrimary.tsx
+++ src/core/components/common/header/CardHeaderPrimary.tsx
@@ -12,7 +12,7 @@ interface IProps {
 // React component
 export function CardHeaderPrimary({ className, children, icon, title }: IProps) {
   return (
-    <main className={cn('flex items-center rounded-t-lg bg-primary p-4 text-lg font-semibold text-white', className)}>
+    <main className={cn('flex items-center rounded-t-lg bg-primary p-4 text-xl font-semibold text-white', className)}>
       <div className='flex items-center space-x-4'>
         {icon}
         <span>{title}</span>
