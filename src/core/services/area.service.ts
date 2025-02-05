b6ddff216ad22bd01987b3af6097f6db861f626d
alanloffler <alanmatiasloffler@gmail.com>
2025-02-05 20:41:55 -0300

    Change: fetch by custom class UtilsUrl

--- src/core/services/area.service.ts
+++ src/core/services/area.service.ts
@@ -1,34 +1,15 @@
-import i18next from 'i18next';
+import type { IArea } from '@core/interfaces/area.interface';
 import type { IResponse } from '@core/interfaces/response.interface';
 import { EMethods } from '@core/enums/methods.enum';
+import { UtilsUrl } from '@core/services/utils/url.service';
 
 export class AreaService {
   private static readonly API_URL: string = import.meta.env.VITE_API_URL;
 
-  public static async findAll() {
-    const url: string = `${this.API_URL}/areas`;
-    return await this.fetch(url, EMethods.GET);
-  }
+  public static async findAll(): Promise<IResponse<IArea[]>> {
+    const path: string = `${this.API_URL}/areas`;
+    const url: URL = new URL(path);
 
-  private static async fetch(url: string, method: EMethods, body?: any): Promise<IResponse<any>> {
-    try {
-      const query: Response = await fetch(url, {
-        method: method,
-        headers: {
-          'content-type': 'application/json;charset=UTF-8',
-        },
-        body: JSON.stringify(body),
-      });
-
-      const response: IResponse<any> = await query.json();
-      if (!query.ok) throw new Error(response.message);
-
-      return response;
-    } catch (error) {
-      if (error instanceof TypeError) {
-        throw new Error(i18next.t('error.internalServer'));
-      }
-      throw error;
-    }
+    return await UtilsUrl.fetch(url, EMethods.GET);
   }
 }
