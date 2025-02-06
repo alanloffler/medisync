a51bb48b3254ade67f0b6c6901d5f1a3f5e72a55
alanloffler <alanmatiasloffler@gmail.com>
2025-02-06 17:34:14 -0300

    Create: form error icon with popover

---
+++ src/core/components/common/form/FormError.tsx
@@ -0,0 +1,32 @@
+// External components: https://ui.shadcn.com/docs/components
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@core/components/ui/tooltip';
+// External imports
+import { useAnimate } from 'motion/react';
+import { useEffect } from 'react';
+// Interface
+interface IProps {
+  message: string;
+}
+// React component
+export function FormError({ message }: IProps) {
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    animate(scope.current, { opacity: 1 }, { duration: 0.35 });
+  }, [animate, scope]);
+
+  return (
+    <TooltipProvider delayDuration={0.3}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <div ref={scope} className='flex h-4 w-4 cursor-pointer items-center justify-center rounded-full bg-red-400 opacity-0'>
+            <span className='text-xs font-medium text-red-50'>!</span>
+          </div>
+        </TooltipTrigger>
+        <TooltipContent className='max-w-[150px] bg-red-50'>
+          <p className='select-none text-xs font-normal'>{message}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
