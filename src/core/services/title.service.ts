5182d65b3fe98aecf81f1d0bfa06a735789b94d7
alanloffler <alanmatiasloffler@gmail.com>
2025-02-05 21:31:02 -0300

    Change: titles fetch using TRQ useQuery

--- src/core/services/title.service.ts
+++ src/core/services/title.service.ts
@@ -1,19 +1,15 @@
+import type { IResponse } from '@core/interfaces/response.interface';
+import type { ITitle } from '@core/interfaces/title.interface';
+import { EMethods } from '@core/enums/methods.enum';
+import { UtilsUrl } from '@core/services/utils/url.service';
+
 export class TitleService {
   private static readonly API_URL: string = import.meta.env.VITE_API_URL;
 
-  public static async findAll() {
-    try {
-      const url: string = `${this.API_URL}/titles`;
-      const query: Response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'content-type': 'application/json;charset=UTF-8',
-        },
-      });
-      
-      return await query.json();
-    } catch (error) {
-      return error;
-    }
+  public static async findAll(): Promise<IResponse<ITitle[]>> {
+    const path: string = `${this.API_URL}/titles`;
+    const url: URL = new URL(path);
+
+    return await UtilsUrl.fetch(url, EMethods.GET);
   }
 }
