51f6a6cf2c145fb2fd7c3036ff770b40f07fb39c
alanloffler <alanmatiasloffler@gmail.com>
2025-02-06 23:32:54 -0300

    Change: area codes fetch with service, prev by config file. Error handling with FormError component

--- src/core/components/common/SelectPhoneArea.tsx
+++ src/core/components/common/SelectPhoneArea.tsx
@@ -1,82 +1,98 @@
 // External components: https://ui.shadcn.com/docs/components
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@core/components/ui/select';
+// Components
+import { FormError } from '@core/components/common/form/FormError';
 // External imports
 import { type ComponentPropsWithoutRef, type Dispatch, forwardRef, type SetStateAction, useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 // Imports
-import { AREA_CODE } from '@config/area-code.config';
+import type { IAreaCode } from '@core/interfaces/area-code.interface';
+import type { IResponse } from '@core/interfaces/response.interface';
+import { AreaCodeService } from '@core/services/area-code.service';
+import { useNotificationsStore } from '@core/stores/notifications.store';
 // Interface
 interface IProps extends ComponentPropsWithoutRef<'div'> {
   setArea?: Dispatch<SetStateAction<number | undefined>>;
   value: number;
 }
-
-interface IAreaCode {
-  id: number;
-  code: string;
-  abbreviation: string;
-  icon: string;
-  label: string;
-  lang: string;
-  default?: boolean;
-}
 // React component
 export const SelectPhoneArea = forwardRef<HTMLDivElement, IProps>(({ setArea, value, ...props }, ref) => {
   const [areaCode, setAreaCode] = useState<IAreaCode | undefined>();
+  const addNotification = useNotificationsStore((state) => state.addNotification);
+
+  const {
+    data: areaCodes,
+    error: areaCodesError,
+    isError: areaCodesIsError,
+  } = useQuery<IResponse<IAreaCode[]>, Error>({
+    queryKey: ['area-codes', 'find-all'],
+    queryFn: async () => await AreaCodeService.findAll(),
+  });
+
+  useEffect(() => {
+    if (areaCodesIsError) addNotification({ type: 'error', message: areaCodesError?.message });
+  }, [addNotification, areaCodesError?.message, areaCodesIsError]);
 
   useEffect(() => {
     if (isNaN(value)) {
-      // setAreaCode(AREA_CODE[0]);
       setAreaCode(undefined);
-      if (setArea) setArea(Number(AREA_CODE[0].code));
+      if (setArea) setArea(Number(areaCodes?.data[0].code));
     } else {
-      setAreaCode(AREA_CODE.find((area: IAreaCode) => area.code === String(value)));
+      setAreaCode(areaCodes?.data.find((area: IAreaCode) => area.code === String(value)));
     }
-  }, [setArea, value]);
+  }, [areaCodes?.data, setArea, value]);
 
   function onValueChange(e: string): void {
-    const areaCode: IAreaCode = AREA_CODE.find((area: IAreaCode) => area.code === e) ?? AREA_CODE[0];
-    setAreaCode(areaCode);
+    const areaCode: IAreaCode | undefined = areaCodes?.data.find((area: IAreaCode) => area.code === e) ?? areaCodes?.data[0];
+    if (areaCode) setAreaCode(areaCode);
     if (setArea) setArea(Number(e));
   }
 
   return (
     <div ref={ref} {...props}>
       <Select value={areaCode?.code} onValueChange={onValueChange}>
-        <SelectTrigger className='h-9 bg-slate-100/70 p-2 text-xs ring-offset-background hover:bg-slate-100 data-[state=open]:outline-none data-[state=open]:ring-1 data-[state=open]:ring-ring data-[state=open]:ring-offset-0'>
-          {areaCode ? (
-            <SelectValue>
-              <section className='mr-1 flex flex-row items-center gap-2'>
-                <img
-                  width={18}
-                  height={18}
-                  src={new URL(`../../../assets/icons/i18n/${areaCode?.icon}.svg`, import.meta.url).href}
-                  alt={areaCode?.label}
-                />
-                <div>{areaCode?.code}</div>
-              </section>
-            </SelectValue>
-          ) : (
-            <div className='min-w-[25px]'></div>
-          )}
-        </SelectTrigger>
+        <div className='flex items-center space-x-3'>
+          <SelectTrigger
+            className='h-9 bg-slate-100/70 p-2 text-xs ring-offset-background hover:bg-slate-100 data-[state=open]:outline-none data-[state=open]:ring-1 data-[state=open]:ring-ring data-[state=open]:ring-offset-0'
+            disabled={!areaCodes || areaCodesIsError}
+          >
+            {areaCode ? (
+              <SelectValue>
+                <section className='mr-2 flex flex-row items-center gap-2'>
+                  <img
+                    width={18}
+                    height={18}
+                    src={new URL(`../../../assets/icons/i18n/${areaCode?.icon}.svg`, import.meta.url).href}
+                    alt={areaCode?.label}
+                  />
+                  <div>{areaCode?.code}</div>
+                </section>
+              </SelectValue>
+            ) : (
+              <div className='min-w-[35px]'></div>
+            )}
+          </SelectTrigger>
+          {areaCodesIsError && <FormError message={areaCodesError?.message} />}
+        </div>
         <SelectContent onCloseAutoFocus={(e) => e.preventDefault()} className='min-w-0' align='end'>
           <SelectGroup>
-            {AREA_CODE.map(
-              (area) =>
-                area.code.length >= 1 && (
-                  <SelectItem key={crypto.randomUUID()} value={area.code}>
-                    <div className='flex flex-row items-center space-x-2'>
-                      <img
-                        width={18}
-                        height={18}
-                        src={new URL(`../../../assets/icons/i18n/${area.icon}.svg`, import.meta.url).href}
-                        alt={area.label}
-                      />
-                      <div className='text-xs'>{area.abbreviation}</div>
-                    </div>
-                  </SelectItem>
-                ),
-            )}
+            {areaCodes?.data &&
+              areaCodes?.data.map(
+                (area) =>
+                  area.code.length >= 1 && (
+                    <SelectItem key={crypto.randomUUID()} value={area.code}>
+                      <div className='flex flex-row items-center space-x-2'>
+                        <img
+                          width={18}
+                          height={18}
+                          src={new URL(`../../../assets/icons/i18n/${area.icon}.svg`, import.meta.url).href}
+                          alt={area.label}
+                        />
+                        <div className='text-xs'>{area.abbreviation}</div>
+                      </div>
+                    </SelectItem>
+                  ),
+              )}
           </SelectGroup>
         </SelectContent>
       </Select>
