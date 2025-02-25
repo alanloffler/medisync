8a45affe1421db4f47e0e7a22a5dfaced5ef13b7
alanloffler <alanmatiasloffler@gmail.com>
2025-02-25 08:53:10 -0300

    Update: icons size

--- src/pages/appointments/components/reserve/ProfessionalSelection.tsx
+++ src/pages/appointments/components/reserve/ProfessionalSelection.tsx
@@ -55,11 +55,11 @@ export function ProfessionalSelection({ clearFilters, professional, setSelected 
       {professional && (
         <section className='flex w-full flex-col space-y-1 text-sm font-normal text-slate-500 md:max-w-[300px]'>
           <div className='flex flex-row items-center space-x-2'>
-            <CalendarDays size={18} strokeWidth={2} className='min-w-[18px]' />
+            <CalendarDays size={17} strokeWidth={2} className='min-w-[18px]' />
             <span>{legibleWorkingDays}</span>
           </div>
           <div className='flex flex-row items-center space-x-2'>
-            <CalendarClock size={18} strokeWidth={2} />
+            <CalendarClock size={17} strokeWidth={2} />
             <span>
               {professional.configuration.unavailableTimeSlot?.timeSlotUnavailableInit &&
               professional.configuration.unavailableTimeSlot?.timeSlotUnavailableEnd
