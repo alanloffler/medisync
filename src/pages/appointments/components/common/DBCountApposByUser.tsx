664ce8a56d4bd8f4ca9d30bfc09e6b9da686fba5
alanloffler <alanmatiasloffler@gmail.com>
2025-03-01 20:06:41 -0300

    Wip: update content and value format

--- src/pages/appointments/components/common/DBCountApposByUser.tsx
+++ src/pages/appointments/components/common/DBCountApposByUser.tsx
@@ -1,23 +1,33 @@
 // Icons: https://lucide.dev/icons/
-import { Database } from 'lucide-react';
+import { ArrowDownRight, ArrowUpRight, Database } from 'lucide-react';
 // External imports
 import { useTranslation } from 'react-i18next';
 // Imports
+import type { IStats } from '@core/interfaces/response.interface';
 import { cn } from '@lib/utils';
 // Interface
 interface IProps {
   className?: string;
-  text: string;
-  value?: number;
+  stats?: IStats;
 }
 // React component
-export function DBCountApposByUser({ className, text, value }: IProps) {
+export function DBCountApposByUser({ className, stats }: IProps) {
   const { t } = useTranslation();
 
   return (
-    <section className={cn(className, 'flex items-center space-x-1 !text-xsm text-slate-400')}>
-      <Database size={16} strokeWidth={2} className='text-orange-400' />
-      <span>{t(text, { count: value })}</span>
-    </section>
+    <main className={cn('flex flex-row gap-3 !text-xsm text-slate-400', className)}>
+      <section className='flex items-center space-x-1'>
+        <Database size={16} strokeWidth={2} className='text-orange-400' />
+        <span>{t('table.totalItems.appointments', { count: stats?.total })}</span>
+      </section>
+      <section className={cn(className, 'flex items-center space-x-0.5')}>
+        <ArrowUpRight size={16} strokeWidth={2} className='text-emerald-400' />
+        <span>{t('table.attendance.attended', { count: stats?.attended })}</span>
+      </section>
+      <section className={cn(className, 'flex items-center space-x-0.5')}>
+        <ArrowDownRight size={16} strokeWidth={2} className='text-rose-400' />
+        <span>{t('table.attendance.notAttended', { count: stats?.notAttended })}</span>
+      </section>
+    </main>
   );
 }
