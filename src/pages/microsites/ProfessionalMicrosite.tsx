8e957615b596db1b62d73c028909570f410b9c37
alanloffler <alanmatiasloffler@gmail.com>
2025-03-06 17:19:05 -0300

    Update: error and loading handling on UI

--- src/pages/microsites/ProfessionalMicrosite.tsx
+++ src/pages/microsites/ProfessionalMicrosite.tsx
@@ -2,6 +2,8 @@
 import { Package2 } from 'lucide-react';
 // External components: https://ui.shadcn.com/docs/components
 import { Card } from '@core/components/ui/card';
+// Components
+import { InfoCard } from '@core/components/common/InfoCard';
 // External imports
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
@@ -11,36 +13,55 @@ import type { IProfessional } from '@professionals/interfaces/professional.inter
 import type { IResponse } from '@core/interfaces/response.interface';
 import { ProfessionalApiService } from '@professionals/services/professional-api.service';
 import { UtilsString } from '@core/services/utils/string.service';
+import { LoadingDB } from '@core/components/common/LoadingDB';
 // React component
 export default function ProfessionalMicrosite() {
   const { id } = useParams();
   const { t } = useTranslation();
 
-  const { data: professional } = useQuery<IResponse<IProfessional>, Error>({
+  const {
+    data: professional,
+    error: profError,
+    isError: profIsError,
+    isLoading: profIsLoading,
+    isSuccess: profIsSuccess,
+  } = useQuery<IResponse<IProfessional>, Error>({
     queryKey: ['professional', id],
     queryFn: async () => {
-      if (!id) throw new Error('No ID provided');
+      if (!id) throw new Error('Dev Error: No ID provided');
       return await ProfessionalApiService.findOne(id);
     },
   });
 
-  return (
-    <main className='flex h-screen flex-col bg-muted/70'>
-      <header className='sticky top-0 z-50 flex h-16 items-center justify-between gap-4 bg-background px-4 shadow-sm md:justify-normal md:gap-8 md:px-8'>
-        <div className='flex items-center gap-2 font-semibold md:text-base'>
-          <Package2 size={24} strokeWidth={2} />
-          <span>{t('appName')}</span>
-        </div>
-        <section className='text-base font-medium text-foreground'>Professional name</section>
-      </header>
-      <section className='flex flex-col gap-6 p-6 md:gap-8 md:p-8'>
-        <h2>
-          Bienvenido{' '}
-          {UtilsString.upperCase(`${professional?.data.title.abbreviation} ${professional?.data.firstName} ${professional?.data.lastName}`, 'each')}
-        </h2>
-        <Card className='p-4'>Statistics here</Card>
-        <section>Professional id {id}</section>
-      </section>
-    </main>
-  );
+  if (profIsLoading)
+    return (
+      <main className='flex h-screen flex-col bg-muted/70'>
+        <LoadingDB absolute size='default' text={t('loading.professional')} variant='card' />
+      </main>
+    );
+
+  if (profIsError)
+    return (
+      <main className='flex h-screen flex-col bg-muted/70'>
+        <InfoCard className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2' text={profError.message} variant='error' />
+      </main>
+    );
+
+  if (profIsSuccess)
+    return (
+      <main className='flex h-screen flex-col bg-muted/70'>
+        <header className='sticky top-0 z-50 flex h-16 items-center justify-between gap-4 bg-background px-4 shadow-sm md:justify-normal md:gap-8 md:px-8'>
+          <div className='flex items-center gap-2 font-semibold md:text-base'>
+            <Package2 size={24} strokeWidth={2} />
+            <span>{t('appName')}</span>
+          </div>
+          <h1 className='text-base font-medium text-foreground'>
+            {UtilsString.upperCase(`${professional?.data.title.abbreviation} ${professional?.data.firstName} ${professional?.data.lastName}`, 'each')}
+          </h1>
+        </header>
+        <section className='flex flex-col gap-6 p-6 md:gap-8 md:p-8'>
+          <Card className='p-4'>Statistics here</Card>
+        </section>
+      </main>
+    );
 }
