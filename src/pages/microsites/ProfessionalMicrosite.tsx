48a14d93efc56fc4c9bc4103957cfdae057a1a37
alanloffler <alanmatiasloffler@gmail.com>
2025-03-06 16:46:15 -0300

    Add: fetch professional info with useQuery. Welcome header

--- src/pages/microsites/ProfessionalMicrosite.tsx
+++ src/pages/microsites/ProfessionalMicrosite.tsx
@@ -1,13 +1,28 @@
 // Icons: https://lucide.dev/icons
 import { Package2 } from 'lucide-react';
+// External components: https://ui.shadcn.com/docs/components
+import { Card } from '@core/components/ui/card';
 // External imports
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
-import { Card } from '@core/components/ui/card';
+// Imports
+import type { IProfessional } from '@professionals/interfaces/professional.interface';
+import type { IResponse } from '@core/interfaces/response.interface';
+import { ProfessionalApiService } from '@professionals/services/professional-api.service';
+import { UtilsString } from '@core/services/utils/string.service';
 // React component
 export default function ProfessionalMicrosite() {
   const { id } = useParams();
   const { t } = useTranslation();
+
+  const { data: professional } = useQuery<IResponse<IProfessional>, Error>({
+    queryKey: ['professional', id],
+    queryFn: async () => {
+      if (!id) throw new Error('No ID provided');
+      return await ProfessionalApiService.findOne(id);
+    },
+  });
 
   return (
     <main className='flex h-screen flex-col bg-muted/70'>
@@ -18,7 +33,11 @@ export default function ProfessionalMicrosite() {
         </div>
         <section className='text-base font-medium text-foreground'>Professional name</section>
       </header>
-      <section className='flex flex-col p-6 gap-6 md:p-8 md:gap-8'>
+      <section className='flex flex-col gap-6 p-6 md:gap-8 md:p-8'>
+        <h2>
+          Bienvenido{' '}
+          {UtilsString.upperCase(`${professional?.data.title.abbreviation} ${professional?.data.firstName} ${professional?.data.lastName}`, 'each')}
+        </h2>
         <Card className='p-4'>Statistics here</Card>
         <section>Professional id {id}</section>
       </section>
