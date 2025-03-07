7a3ed7b1bd08ddca3cf14b17bd94397df8e9b58e
alanloffler <alanmatiasloffler@gmail.com>
2025-03-06 23:53:14 -0300

    Update: styles, calendar props. Add className prop. Set defaultDate to calendar

--- src/pages/microsites/common/DatePicker.tsx
+++ src/pages/microsites/common/DatePicker.tsx
@@ -6,25 +6,33 @@ import { Calendar } from '@core/components/ui/calendar';
 import { Popover, PopoverContent, PopoverTrigger } from '@core/components/ui/popover';
 // External imports
 import { format } from '@formkit/tempo';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 // Imports
 import { cn } from '@lib/utils';
 // Interface
 interface IProps {
+  className?: string;
+  defaultDate?: Date | undefined;
   onDateChange?: (date: Date | undefined) => void;
 }
 // React component
-export function DatePicker({ onDateChange }: IProps) {
-  const [date, setDate] = useState<Date | undefined>();
+export function DatePicker({ className, defaultDate, onDateChange }: IProps) {
+  const [date, setDate] = useState<Date | undefined>(defaultDate);
   const [open, setOpen] = useState<boolean>(false);
   const { t } = useTranslation();
 
   function handleDateSelect(selectedDate: Date | undefined): void {
-    setDate(selectedDate);
-    onDateChange?.(selectedDate);
-    setOpen(false);
+    if (selectedDate) {
+      setDate(selectedDate);
+      onDateChange?.(selectedDate);
+      setOpen(false);
+    }
   }
+
+  useEffect(() => {
+    setDate(defaultDate);
+  }, [defaultDate]);
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
@@ -34,6 +42,7 @@ export function DatePicker({ onDateChange }: IProps) {
           className={cn(
             'h-[37px] w-[140px] justify-start border-transparent !p-0 text-left !text-sm font-normal hover:bg-transparent data-[state=open]:border-slate-200',
             !date && 'text-muted-foreground',
+            className,
           )}
         >
           <div className='flex h-[35px] w-full items-center rounded-md bg-input px-3 py-2'>
@@ -43,7 +52,7 @@ export function DatePicker({ onDateChange }: IProps) {
         </Button>
       </PopoverTrigger>
       <PopoverContent className='w-auto p-0' align='start'>
-        <Calendar mode='single' selected={date} onSelect={handleDateSelect} initialFocus />
+        <Calendar disabled={{ dayOfWeek: [0] }} mode='single' showOutsideDays={false} selected={date} onSelect={handleDateSelect} initialFocus />
       </PopoverContent>
     </Popover>
   );
