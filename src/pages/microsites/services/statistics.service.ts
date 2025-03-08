eb522a7cf283326b23766dd7ab7af9fcf4484544
alanloffler <alanmatiasloffler@gmail.com>
2025-03-08 10:44:13 -0300

    Add IStats type to statistics service response

--- src/pages/microsites/services/statistics.service.ts
+++ src/pages/microsites/services/statistics.service.ts
@@ -1,11 +1,12 @@
 import type { IResponse } from '@core/interfaces/response.interface';
+import type { IStats } from '@microsites/interfaces/statistics.interface';
 import { EMethods } from '@core/enums/methods.enum';
 import { UtilsUrl } from '@core/services/utils/url.service';
 
 export class StatisticsService {
   private static readonly API_URL: string = import.meta.env.VITE_API_URL;
 
-  public static async countApposByProfessional(id: string): Promise<IResponse<number>> {
+  public static async countApposByProfessional(id: string): Promise<IResponse<IStats>> {
     const path: string = `${this.API_URL}/statistics/countApposByProfessional`;
     const url: URL = new URL(path);
 
