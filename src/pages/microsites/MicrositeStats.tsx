2efe44d4b11a9a03e4825b93b72eda76209fa747
alanloffler <alanmatiasloffler@gmail.com>
2025-03-08 23:25:23 -0300

    Add pie chart to microsite statistics view

--- src/pages/microsites/MicrositeStats.tsx
+++ src/pages/microsites/MicrositeStats.tsx
@@ -1,11 +1,15 @@
 // Icons: https://lucide.dev/icons
 import { ArrowDown, ArrowDownRight, ArrowRight, ArrowUp } from 'lucide-react';
+// External components
+import { ChartContainer } from '@core/components/ui/chart';
+import { Pie, PieChart } from 'recharts';
 // External imports
-import { useEffect } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 // Imports
 import type { IResponse } from '@core/interfaces/response.interface';
 import type { IStats } from '@microsites/interfaces/statistics.interface';
+import { EStatus } from '@appointments/enums/status.enum';
 import { StatisticsService } from '@microsites/services/statistics.service';
 // Interface
 interface IProps {
@@ -14,6 +18,8 @@ interface IProps {
 }
 // React component
 export function MicrositeStats({ professionalId, todayStats }: IProps) {
+  const [animationKey, setAnimationKey] = useState<string>('');
+
   const { data: historicalAppos, refetch: refetchHistoricalAppos } = useQuery<IResponse<IStats>, Error>({
     queryKey: ['microsite-stats', 'historical', professionalId],
     queryFn: async () => {
@@ -22,40 +28,70 @@ export function MicrositeStats({ professionalId, todayStats }: IProps) {
     },
   });
 
+  const chartData = useMemo(
+    () => [
+      { status: EStatus.ATTENDED, value: todayStats?.attended || 0, fill: 'oklch(0.765 0.177 163.223)' },
+      { status: EStatus.NOT_ATTENDED, value: todayStats?.notAttended || 0, fill: 'oklch(0.712 0.194 13.428)' },
+      { status: EStatus.NOT_STATUS, value: todayStats?.notStatus || 0, fill: 'oklch(0.704 0.04 256.788)' },
+      { status: EStatus.WAITING, value: todayStats?.waiting || 0, fill: 'oklch(0.828 0.189 84.429)' },
+    ],
+    [todayStats?.attended, todayStats?.notAttended, todayStats?.notStatus, todayStats?.waiting],
+  );
+
   useEffect(() => {
+    if (todayStats && chartData) setAnimationKey(JSON.stringify(chartData));
     refetchHistoricalAppos();
-  }, [refetchHistoricalAppos, todayStats]);
+  }, [chartData, refetchHistoricalAppos, todayStats]);
 
   return (
     <main className='flex flex-col gap-6 overflow-auto'>
       <section className='flex flex-col gap-2 text-sm'>
         <h1 className='text-xs font-semibold uppercase text-muted-foreground'>Hoy</h1>
-        <section className='flex flex-col'>
-          <div className='flex items-center gap-1'>
-            <span className='text-lg font-semibold'>{todayStats?.total}</span>
-            <span className='text-sm'>turnos en total</span>
-          </div>
-          <div className='flex items-center gap-3 text-xsm'>
-            <div className='flex items-center gap-0.5 text-sm'>
-              <span>{todayStats?.attended}</span>
-              <ArrowUp size={15} strokeWidth={2} className='text-emerald-400' />
+        <section className='flex flex-row items-center gap-3' key={animationKey}>
+          {todayStats && todayStats.total > 0 && (
+            <div>
+              <ChartContainer config={{}} className='aspect-square h-[50px] w-[50px]'>
+                <PieChart>
+                  <Pie
+                    isAnimationActive={true}
+                    animationDuration={1000}
+                    data={chartData}
+                    dataKey='value'
+                    nameKey='status'
+                    innerRadius={12.5}
+                    outerRadius={25}
+                  />
+                </PieChart>
+              </ChartContainer>
             </div>
-            <div className='flex items-center gap-0.5 text-sm'>
-              <span>{todayStats?.notAttended}</span>
-              <ArrowDown size={15} strokeWidth={2} className='text-rose-400' />
+          )}
+          <div className='flex flex-col'>
+            <div className='flex items-center gap-1'>
+              <span className='text-lg font-semibold'>{todayStats?.total}</span>
+              <span className='text-sm'>turnos en total</span>
             </div>
-            {todayStats && todayStats.waiting > 0 && (
+            <div className='flex items-center gap-3 text-xsm'>
               <div className='flex items-center gap-0.5 text-sm'>
-                <span>{todayStats?.waiting}</span>
-                <ArrowRight size={15} strokeWidth={2} className='text-amber-400' />
+                <span>{todayStats?.attended}</span>
+                <ArrowUp size={15} strokeWidth={2} className='text-emerald-400' />
               </div>
-            )}
-            {todayStats && todayStats.notStatus > 0 && (
               <div className='flex items-center gap-0.5 text-sm'>
-                <span>{todayStats?.notStatus}</span>
-                <ArrowDownRight size={15} strokeWidth={2} className='text-slate-400' />
+                <span>{todayStats?.notAttended}</span>
+                <ArrowDown size={15} strokeWidth={2} className='text-rose-400' />
               </div>
-            )}
+              {todayStats && todayStats.waiting > 0 && (
+                <div className='flex items-center gap-0.5 text-sm'>
+                  <span>{todayStats?.waiting}</span>
+                  <ArrowRight size={15} strokeWidth={2} className='text-amber-400' />
+                </div>
+              )}
+              {todayStats && todayStats.notStatus > 0 && (
+                <div className='flex items-center gap-0.5 text-sm'>
+                  <span>{todayStats?.notStatus}</span>
+                  <ArrowDownRight size={15} strokeWidth={2} className='text-slate-400' />
+                </div>
+              )}
+            </div>
           </div>
         </section>
       </section>
