6d73a28bd4055f0e514c3b9c16b48aa419117261
alanloffler <alanmatiasloffler@gmail.com>
2025-03-10 10:29:55 -0300

    Update progress calculation to use today's time slots
    Revises the progress calculation to be based on today's stats rather
    than historical data, restructures component layout for better UX, and changes progress section label.

--- src/pages/microsites/MicrositeStats.tsx
+++ src/pages/microsites/MicrositeStats.tsx
@@ -61,115 +61,85 @@ export function MicrositeStats({ apposIsLoading, professionalId, todayStats }: I
   }, [chartData, refetchHistoricalAppos, todayStats]);
 
   useEffect(() => {
-    if (!historicalAppos?.data.total) return;
-    const total: number = historicalAppos.data.total;
-    const notStatus: number = historicalAppos.data.notStatus;
-    const percentage: number = 100 - (notStatus / total) * 100;
-    setProgress(percentage);
-  }, [historicalAppos?.data.notStatus, historicalAppos?.data.total]);
+    const total: number | undefined = todayStats?.total;
+    if (!total) return;
+    if (todayStats?.timeSlots) {
+      console.log(total, todayStats?.timeSlots);
+      const percentage: number = (total / todayStats?.timeSlots) * 100;
+      setProgress(percentage);
+    } else {
+      setProgress(0);
+    }
+  }, [todayStats?.timeSlots, todayStats?.total]);
 
   return (
     <main className='flex flex-col gap-6 overflow-auto'>
-      {apposIsLoading && <SkeletonDonutStat />}
-      {!apposIsLoading && (
-        <section className='flex flex-col gap-2 text-sm'>
-          <h1 className='text-xs font-semibold uppercase text-muted-foreground'>Hoy</h1>
-          <section className='flex flex-row items-center gap-3'>
-            {todayStats && todayStats.total !== undefined && todayStats.total > 0 && (
-              <ChartContainer className='aspect-square h-[50px] w-[50px]' config={{}} key={animationKey}>
-                <PieChart>
-                  <Pie
-                    isAnimationActive={true}
-                    animationDuration={1000}
-                    data={chartData}
-                    dataKey='value'
-                    nameKey='status'
-                    innerRadius={12.5}
-                    outerRadius={25}
-                  />
-                </PieChart>
-              </ChartContainer>
-            )}
-            {todayStats && (
-              <motion.div
-                className='flex flex-col'
-                key={`${JSON.stringify(todayStats)}`}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 2, delay: todayStats?.total && todayStats.total > 0 ? 1 : 0 }}
-              >
-                <div className='flex items-center gap-1'>
-                  <span className='text-lg font-semibold'>{todayStats?.total}</span>
-                  <span className='text-sm'>turnos en total</span>
-                </div>
-                <div className='flex items-center gap-3 text-xsm'>
-                  <div className='flex items-center gap-0.5 text-sm'>
-                    <span>{todayStats?.attended}</span>
-                    <ArrowUp size={15} strokeWidth={2} className='text-emerald-400' />
-                  </div>
-                  <div className='flex items-center gap-0.5 text-sm'>
-                    <span>{todayStats?.notAttended}</span>
-                    <ArrowDown size={15} strokeWidth={2} className='text-rose-400' />
-                  </div>
-                  {todayStats && todayStats.waiting !== undefined && todayStats.waiting > 0 && (
-                    <div className='flex items-center gap-0.5 text-sm'>
-                      <span>{todayStats.waiting}</span>
-                      <ArrowRight size={15} strokeWidth={2} className='text-amber-400' />
-                    </div>
-                  )}
-                  {todayStats && todayStats.notStatus !== undefined && todayStats.notStatus > 0 && (
-                    <div className='flex items-center gap-0.5 text-sm'>
-                      <span>{todayStats.notStatus}</span>
-                      <ArrowDownRight size={15} strokeWidth={2} className='text-slate-400' />
-                    </div>
-                  )}
-                </div>
-              </motion.div>
-            )}
-          </section>
-        </section>
-      )}
-      {historicalIsLoading && (
+      {apposIsLoading && (
         <section className='flex flex-col gap-6'>
-          <SkeletonStat />
+          <SkeletonDonutStat />
           <SkeletonProgress />
         </section>
       )}
-      {historicalIsSuccess && (
+      {!apposIsLoading && (
         <>
           <section className='flex flex-col gap-2 text-sm'>
-            <h1 className='text-xs font-semibold uppercase text-muted-foreground'>Historial</h1>
-            <section className='flex flex-col'>
-              <div className='flex items-center gap-1'>
-                <span className='text-lg font-semibold'>{historicalAppos?.data.total}</span>
-                <span className='text-sm'>turnos en total</span>
-              </div>
-              <div className='flex items-center gap-3 text-xsm'>
-                <div className='flex items-center gap-0.5 text-sm'>
-                  <span>{historicalAppos?.data.attended}</span>
-                  <ArrowUp size={15} strokeWidth={2} className='text-emerald-400' />
-                </div>
-                <div className='flex items-center gap-0.5 text-sm'>
-                  <span>{historicalAppos?.data.notAttended}</span>
-                  <ArrowDown size={15} strokeWidth={2} className='text-rose-400' />
-                </div>
-                {historicalAppos && historicalAppos.data.waiting > 0 && (
-                  <div className='flex items-center gap-0.5 text-sm'>
-                    <span>{historicalAppos?.data.waiting}</span>
-                    <ArrowRight size={15} strokeWidth={2} className='text-amber-400' />
+            <h1 className='text-xs font-semibold uppercase text-muted-foreground'>Hoy</h1>
+            <section className='flex flex-row items-center gap-3'>
+              {todayStats && todayStats.total !== undefined && todayStats.total > 0 && (
+                <ChartContainer className='aspect-square h-[50px] w-[50px]' config={{}} key={animationKey}>
+                  <PieChart>
+                    <Pie
+                      isAnimationActive={true}
+                      animationDuration={1000}
+                      data={chartData}
+                      dataKey='value'
+                      nameKey='status'
+                      innerRadius={12.5}
+                      outerRadius={25}
+                    />
+                  </PieChart>
+                </ChartContainer>
+              )}
+              {todayStats && (
+                <motion.div
+                  className='flex flex-col'
+                  key={`${JSON.stringify(todayStats)}`}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 2, delay: todayStats?.total && todayStats.total > 0 ? 1 : 0 }}
+                >
+                  <div className='flex items-center gap-1'>
+                    <span className='text-lg font-semibold'>{todayStats?.total}</span>
+                    <span className='text-sm'>turnos en total</span>
                   </div>
-                )}
-                {historicalAppos && historicalAppos.data.notStatus > 0 && (
-                  <div className='flex items-center gap-0.5 text-sm'>
-                    <span>{historicalAppos?.data.notStatus}</span>
-                    <ArrowDownRight size={15} strokeWidth={2} className='text-slate-400' />
+                  <div className='flex items-center gap-3 text-xsm'>
+                    <div className='flex items-center gap-0.5 text-sm'>
+                      <span>{todayStats?.attended}</span>
+                      <ArrowUp size={15} strokeWidth={2} className='text-emerald-400' />
+                    </div>
+                    <div className='flex items-center gap-0.5 text-sm'>
+                      <span>{todayStats?.notAttended}</span>
+                      <ArrowDown size={15} strokeWidth={2} className='text-rose-400' />
+                    </div>
+                    {todayStats && todayStats.waiting !== undefined && todayStats.waiting > 0 && (
+                      <div className='flex items-center gap-0.5 text-sm'>
+                        <span>{todayStats.waiting}</span>
+                        <ArrowRight size={15} strokeWidth={2} className='text-amber-400' />
+                      </div>
+                    )}
+                    {todayStats && todayStats.notStatus !== undefined && todayStats.notStatus > 0 && (
+                      <div className='flex items-center gap-0.5 text-sm'>
+                        <span>{todayStats.notStatus}</span>
+                        <ArrowDownRight size={15} strokeWidth={2} className='text-slate-400' />
+                      </div>
+                    )}
                   </div>
-                )}
-              </div>
+                </motion.div>
+              )}
             </section>
           </section>
-          <section className='flex flex-col gap-2 text-sm'>
-            <h1 className='w-fit text-xs font-semibold uppercase text-muted-foreground'>Progreso de edición</h1>
+          <section className='flex flex-col gap-1 text-sm'>
+            <h1 className='w-fit text-xs font-medium text-muted-foreground'>Progreso de reservas</h1>
             <section className='flex w-full items-center gap-3 py-1 text-xsm lg:w-1/2'>
               <Progress value={progress} className='h-3 bg-slate-200 [&_div]:bg-sky-400' />
               <span className='font-medium text-sky-500'>
@@ -179,6 +149,40 @@ export function MicrositeStats({ apposIsLoading, professionalId, todayStats }: I
             </section>
           </section>
         </>
+      )}
+      {historicalIsLoading && <SkeletonStat />}
+      {historicalIsSuccess && (
+        <section className='flex flex-col gap-2 text-sm'>
+          <h1 className='text-xs font-semibold uppercase text-muted-foreground'>Historial</h1>
+          <section className='flex flex-col'>
+            <div className='flex items-center gap-1'>
+              <span className='text-lg font-semibold'>{historicalAppos?.data.total}</span>
+              <span className='text-sm'>turnos en total</span>
+            </div>
+            <div className='flex items-center gap-3 text-xsm'>
+              <div className='flex items-center gap-0.5 text-sm'>
+                <span>{historicalAppos?.data.attended}</span>
+                <ArrowUp size={15} strokeWidth={2} className='text-emerald-400' />
+              </div>
+              <div className='flex items-center gap-0.5 text-sm'>
+                <span>{historicalAppos?.data.notAttended}</span>
+                <ArrowDown size={15} strokeWidth={2} className='text-rose-400' />
+              </div>
+              {historicalAppos && historicalAppos.data.waiting > 0 && (
+                <div className='flex items-center gap-0.5 text-sm'>
+                  <span>{historicalAppos?.data.waiting}</span>
+                  <ArrowRight size={15} strokeWidth={2} className='text-amber-400' />
+                </div>
+              )}
+              {historicalAppos && historicalAppos.data.notStatus > 0 && (
+                <div className='flex items-center gap-0.5 text-sm'>
+                  <span>{historicalAppos?.data.notStatus}</span>
+                  <ArrowDownRight size={15} strokeWidth={2} className='text-slate-400' />
+                </div>
+              )}
+            </div>
+          </section>
+        </section>
       )}
     </main>
   );
