191ed5c6a97b4607a82a8d3f18f7f15b60102fc8
alanloffler <alanmatiasloffler@gmail.com>
2025-03-10 13:45:21 -0300

    Remove unused countTodayApposByProfessional method

--- src/pages/microsites/services/statistics.service.ts
+++ src/pages/microsites/services/statistics.service.ts
@@ -6,15 +6,6 @@ import { UtilsUrl } from '@core/services/utils/url.service';
 export class StatisticsService {
   private static readonly API_URL: string = import.meta.env.VITE_API_URL;
 
-  public static async countTodayApposByProfessional(id: string): Promise<IResponse<IStats>> {
-    const path: string = `${this.API_URL}/statistics/countTodayApposByProfessional`;
-    const url: URL = new URL(path);
-
-    url.searchParams.append('id', id);
-
-    return await UtilsUrl.fetch(url, EMethods.GET);
-  }
-
   public static async countApposByProfessional(id: string): Promise<IResponse<IStats>> {
     const path: string = `${this.API_URL}/statistics/countApposByProfessional`;
     const url: URL = new URL(path);
