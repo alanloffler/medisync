836a3d1ede845ae849b5d041ed272e535cffead3
alanloffler <alanmatiasloffler@gmail.com>
2025-03-13 22:08:57 -0300

    Add JWT token authentication to HTTP requests

--- src/core/services/utils/url.service.ts
+++ src/core/services/utils/url.service.ts
@@ -2,6 +2,9 @@ import type { IResponse } from '@core/interfaces/response.interface';
 import { EMethods } from '@core/enums/methods.enum';
 
 export class UtilsUrl {
+  // TODO: get token from http-only cookie
+  private static readonly token: string =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2QwZWQ3YTE5OTAyZDU1N2M0ZTM1NWYiLCJlbWFpbCI6ImFsYW5tYXRpYXNsb2ZmbGVyQGdtYWlsLmNvbSIsInJvbGUiOiJzdXBlciIsImlhdCI6MTc0MTkxMjI2NX0.-gIU2v6he5zedS_npdCPf-K-lSeSJZh3VO03JSlc_g8';
   public static create(path: string, params?: Record<string, string | undefined>): URL {
     const url = new URL(path);
     if (params) Object.entries(params).forEach(([key, value]) => value && url.searchParams.set(key, value));
@@ -15,6 +18,7 @@ export class UtilsUrl {
         method: method,
         headers: {
           'content-type': 'application/json;charset=UTF-8',
+          Authorization: `Bearer ${this.token}`,
         },
         body: JSON.stringify(body),
       });
