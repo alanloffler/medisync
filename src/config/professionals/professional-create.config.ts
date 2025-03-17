825cb1bc635fc2315f707cea23e657d5ef3652f0
alanloffler <alanmatiasloffler@gmail.com>
2025-03-17 20:44:24 -0300

    Update paths to use APP_CONFIG.appPrefix

--- src/config/professionals/professional-create.config.ts
+++ src/config/professionals/professional-create.config.ts
@@ -1,11 +1,13 @@
+import { APP_CONFIG } from '@config/app.config';
+
 export const PROFESSIONAL_CREATE_CONFIG = {
   breadcrumb: [
-    { id: 1, name: 'breadcrumb.home', path: '/' },
-    { id: 2, name: 'breadcrumb.professionals', path: '/professionals' },
-    { id: 3, name: 'breadcrumb.create', path: '/professionals/create' },
+    { id: 1, name: 'breadcrumb.home', path: APP_CONFIG.appPrefix },
+    { id: 2, name: 'breadcrumb.professionals', path: `${APP_CONFIG.appPrefix}/professionals` },
+    { id: 3, name: 'breadcrumb.create', path: `${APP_CONFIG.appPrefix}/professionals/create` },
   ],
   dropdownMenu: [
-    { id: 1, name: 'button.addArea', path: '/' },
-    { id: 2, name: 'button.addSpecialty', path: '/' },
+    { id: 1, name: 'button.addArea', path: APP_CONFIG.appPrefix },
+    { id: 2, name: 'button.addSpecialty', path: APP_CONFIG.appPrefix },
   ],
 };
