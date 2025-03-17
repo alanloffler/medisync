805064b13da2213978568482e782a9b6a7d0b44b
alanloffler <alanmatiasloffler@gmail.com>
2025-03-17 20:43:11 -0300

    Update paths to use APP_CONFIG.appPrefix in professionals config

--- src/config/professionals.config.ts
+++ src/config/professionals.config.ts
@@ -1,11 +1,13 @@
+import { APP_CONFIG } from '@config/app.config';
+
 export const PROF_UPDATE_CONFIG = {
   breadcrumb: [
-    { id: 1, name: 'breadcrumb.home', path: '/' },
-    { id: 2, name: 'breadcrumb.professionals', path: '/professionals' },
-    { id: 3, name: 'breadcrumb.professionalUpdate', path: '/professionals/update/:id' },
+    { id: 1, name: 'breadcrumb.home', path: APP_CONFIG.appPrefix },
+    { id: 2, name: 'breadcrumb.professionals', path: `${APP_CONFIG.appPrefix}/professionals` },
+    { id: 3, name: 'breadcrumb.professionalUpdate', path: `${APP_CONFIG.appPrefix}/professionals/update/:id` },
   ],
   dropdownMenu: [
-    { id: 1, name: 'button.addArea', path: '/' },
-    { id: 2, name: 'button.addSpecialty', path: '/' },
+    { id: 1, name: 'button.addArea', path: APP_CONFIG.appPrefix },
+    { id: 2, name: 'button.addSpecialty', path: APP_CONFIG.appPrefix },
   ],
 };
