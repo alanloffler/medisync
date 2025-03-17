4cd20894da42d162c6264fc54271ebdf8910314d
alanloffler <alanmatiasloffler@gmail.com>
2025-03-17 20:43:31 -0300

    Update professional breadcrumb paths to use appPrefix

--- src/config/professionals/professionals.config.ts
+++ src/config/professionals/professionals.config.ts
@@ -1,7 +1,9 @@
+import { APP_CONFIG } from '@config/app.config';
+
 export const PROFESSIONALS_CONFIG = {
   breadcrumb: [
-    { id: 1, name: 'breadcrumb.home', path: '/' },
-    { id: 2, name: 'breadcrumb.professionals', path: '/professionals' },
+    { id: 1, name: 'breadcrumb.home', path: APP_CONFIG.appPrefix },
+    { id: 2, name: 'breadcrumb.professionals', path: `${APP_CONFIG.appPrefix}/professionals` },
   ],
   table: {
     defaultPageSize: 10,
