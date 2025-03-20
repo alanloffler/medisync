e5d75d5710854ce1f546aeed1324dfb26591ae55
alanloffler <alanmatiasloffler@gmail.com>
2025-03-20 15:51:55 -0300

    Update paths with APP_CONFIG.appPrefix in breadcrumb links

--- src/config/users/user-view.config.ts
+++ src/config/users/user-view.config.ts
@@ -1,14 +1,16 @@
+import { APP_CONFIG } from '@config/app.config';
+
 export const USER_VIEW_CONFIG = {
   breadcrumb: [
-    { id: 1, name: 'breadcrumb.home', path: '/' },
-    { id: 2, name: 'breadcrumb.users', path: '/users' },
+    { id: 1, name: 'breadcrumb.home', path: APP_CONFIG.appPrefix },
+    { id: 2, name: 'breadcrumb.users', path: `${APP_CONFIG.appPrefix}/users` },
     { id: 3, name: 'breadcrumb.userDetails', path: '' },
   ],
   table: {
     appointments: {
       defaultItemsPerPage: 5,
       header: ['table.header.date', 'table.header.professional', 'table.header.actions'],
-      itemsPerPageOptions : [5, 10, 20, 50],
+      itemsPerPageOptions: [5, 10, 20, 50],
     },
   },
 };
