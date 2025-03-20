947dc86e8631544883dfaafa596c8837b1f1ad1f
alanloffler <alanmatiasloffler@gmail.com>
2025-03-20 15:51:47 -0300

    Use APP_CONFIG.appPrefix in user update breadcrumb paths

--- src/config/users/user-update.config.ts
+++ src/config/users/user-update.config.ts
@@ -1,7 +1,9 @@
+import { APP_CONFIG } from '@config/app.config';
+
 export const USER_UPDATE_CONFIG = {
   breadcrumb: [
-    { id: 1, name: 'breadcrumb.home', path: '/' },
-    { id: 2, name: 'breadcrumb.users', path: '/users' },
-    { id: 3, name: 'breadcrumb.userUpdate', path: '/users/update/:id' },
+    { id: 1, name: 'breadcrumb.home', path: APP_CONFIG.appPrefix },
+    { id: 2, name: 'breadcrumb.users', path: `${APP_CONFIG.appPrefix}/users` },
+    { id: 3, name: 'breadcrumb.userUpdate', path: `${APP_CONFIG.appPrefix}/users/update/:id` },
   ],
 };
