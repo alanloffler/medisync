d6fcab42b723445fef892d6eb88b46c89f12e7c3
alanloffler <alanmatiasloffler@gmail.com>
2025-03-20 15:40:52 -0300

    Add user data fetching to settings page (refresh token test)

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -3,6 +3,9 @@ import { Checkbox } from '@core/components/ui/checkbox';
 // Imports
 import { SETTINGS_CONFIG } from '@config/settings.config';
 import { useHelpStore } from '@settings/stores/help.store';
+import { useEffect, useState } from 'react';
+import { UserApiService } from '@users/services/user-api.service';
+import { IUser } from '@users/interfaces/user.interface';
 // React component
 export default function Settings() {
   const { help, setHelp } = useHelpStore();
@@ -10,11 +13,18 @@ export default function Settings() {
   function handleHelpChecked(checked: boolean): void {
     setHelp(checked);
   }
+  const [user, setUser] = useState<IUser | null>(null);
+  useEffect(() => {
+    UserApiService.findOne('677873665568db74bbce9144').then((user) => {
+      setUser(user.data);
+    });
+  }, []);
 
   return (
     <main className='flex w-full flex-col space-y-6 p-6'>
       <h1 className='text-2xl font-bold tracking-tight'>{SETTINGS_CONFIG.page.title}</h1>
       <section>
+        <section>{JSON.stringify(user)}</section>
         <section className='flex flex-col space-y-3'>
           <h2 className='text-xl font-semibold tracking-tight'>{SETTINGS_CONFIG.section.application.title}</h2>
           <div className='space-y-2 pl-3'>
