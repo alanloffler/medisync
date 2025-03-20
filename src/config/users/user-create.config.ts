f0d64ce934819e3c51280d5a265dd476b6ab3ec2
alanloffler <alanmatiasloffler@gmail.com>
2025-03-20 15:51:38 -0300

    Use APP_CONFIG.appPrefix for user create paths

--- src/config/users/user-create.config.ts
+++ src/config/users/user-create.config.ts
@@ -1,7 +1,9 @@
+import { APP_CONFIG } from '@config/app.config';
+
 export const USER_CREATE_CONFIG = {
   breadcrumb: [
-    { id: 1, name: 'breadcrumb.home', path: '/' },
-    { id: 2, name: 'breadcrumb.users', path: '/users' },
-    { id: 3, name: 'breadcrumb.create', path: '/create' },
+    { id: 1, name: 'breadcrumb.home', path: APP_CONFIG.appPrefix },
+    { id: 2, name: 'breadcrumb.users', path: `${APP_CONFIG.appPrefix}/users` },
+    { id: 3, name: 'breadcrumb.create', path: `${APP_CONFIG.appPrefix}/create` },
   ],
 };
