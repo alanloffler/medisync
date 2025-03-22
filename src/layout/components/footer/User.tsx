eb6d90ec3dde862a543182a2399eb489726439df
alanloffler <alanmatiasloffler@gmail.com>
2025-03-21 23:16:16 -0300

    Add user email and style logout button in user dropdown

--- src/layout/components/footer/User.tsx
+++ src/layout/components/footer/User.tsx
@@ -18,11 +18,13 @@ import { useTranslation } from 'react-i18next';
 // Imports
 import { HEADER_CONFIG } from '@config/layout/header.config';
 import { motion } from '@core/services/motion.service';
+import { useAuth } from '@core/auth/useAuth';
 // React component
 export function User() {
   const [scope, animation] = useAnimate();
   const navigate = useNavigate();
   const { t } = useTranslation();
+  const { user } = useAuth();
 
   function handleAnimationOver(): void {
     const { keyframes, options } = motion.scale(1.1).type('bounce').animate();
@@ -42,10 +44,19 @@ export function User() {
         </DropdownMenuTrigger>
       </TooltipWrapper>
       <DropdownMenuContent align='start' onCloseAutoFocus={(e) => e.preventDefault()}>
-        <DropdownMenuLabel className='text-xsm'>{t('user.title')}</DropdownMenuLabel>
+        <DropdownMenuLabel className='text-xsm'>
+          <div className='flex flex-col space-y-1'>
+            <div>{t('user.title')}</div>
+            {user && <div className='text-[10px] font-light'>{user.email}</div>}
+          </div>
+        </DropdownMenuLabel>
         <DropdownMenuSeparator />
         {HEADER_CONFIG.user.menuItems.map((item) => (
-          <DropdownMenuItem key={item.id} onClick={() => navigate(item.path)} className='text-xsm'>
+          <DropdownMenuItem
+            key={item.id}
+            onClick={() => navigate(item.path)}
+            className='text-xsm last:bg-primary/10 last:font-semibold last:text-primary last:hover:bg-primary last:hover:text-white'
+          >
             {t(item.key)}
           </DropdownMenuItem>
         ))}
