a60d6838e7cf384e5f0d97166759253f9a9a8d03
alanloffler <alanmatiasloffler@gmail.com>
2025-03-21 21:54:24 -0300

    Improve code organization and error message in useAuth hook

--- src/core/auth/useAuth.ts
+++ src/core/auth/useAuth.ts
@@ -1,9 +1,11 @@
+// External imports
 import { useContext } from 'react';
+// Imports
 import { AuthContext } from '@core/auth/AuthContext';
-
+// React hook
 export function useAuth() {
   const context = useContext(AuthContext);
-  if (context === undefined) throw new Error('useAuth must be used within an AuthProvider');
+  if (!context) throw new Error('useAuth hook must be used within an AuthProvider');
 
   return context;
 }
