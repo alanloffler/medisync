be65d848f938f3e29f6df31b7f90c2cf7a9f2cdb
alanloffler <alanmatiasloffler@gmail.com>
2025-03-21 21:45:37 -0300

    Add PrivateRoute component for authentication and role checking

---
+++ src/core/auth/PrivateRoute.tsx
@@ -0,0 +1,25 @@
+// Components
+import { Loading } from '@core/components/common/Loading';
+// External imports
+import type { ReactNode } from 'react';
+import { Navigate } from 'react-router-dom';
+// Imports
+import { useAuth } from '@core/auth/useAuth';
+// Interface
+interface IProps {
+  children: ReactNode;
+  roles: string[];
+}
+// React component
+export function PrivateRoute({ children, roles }: IProps) {
+  const { user, loading } = useAuth();
+
+  if (loading) return <Loading />;
+
+  if (!user) return <Navigate to='/login' />;
+
+  // TODO: unauthorized page
+  if (roles && !roles.includes(user.role)) return <Navigate to='/login' replace />;
+
+  return <>{children}</>;
+}
