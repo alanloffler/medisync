836e1a4f8e5be7f18f10e1ff377a5bb329313150
alanloffler <alanmatiasloffler@gmail.com>
2025-03-29 18:10:40 -0300

    Refactor countProfessionals to match other API methods

--- src/pages/dashboard/services/dashboard-api.service.ts
+++ src/pages/dashboard/services/dashboard-api.service.ts
@@ -8,7 +8,37 @@ import { IAppointment } from '@appointments/interfaces/appointment.interface';
 export class DashboardApiService {
   private static readonly API_URL: string = import.meta.env.VITE_API_URL;
 
-  // CHECKED: used on StatisticGroup.tsx
+  // * CHECKED: used on StatisticGroup.tsx
+  public static async countProfessionals(): Promise<IResponse<{ value1: string; value2: string } | undefined>> {
+    const path1: string = `${this.API_URL}/dashboard/countProfessionals`;
+    const path2: string = `${this.API_URL}/dashboard/countProfessionalsLastMonth`;
+    const url1: URL = UtilsUrl.create(path1);
+    const url2: URL = UtilsUrl.create(path2);
+
+    const value1: IResponse = await UtilsUrl.fetch(url1, EMethods.GET);
+    const value2: IResponse = await UtilsUrl.fetch(url2, EMethods.GET);
+
+    if (value1 instanceof Error || value2 instanceof Error)
+      return {
+        data: undefined,
+        message: APP_CONFIG.error.server,
+        statusCode: 500,
+      };
+    if (value1.statusCode > 399 || value2.statusCode > 399)
+      return {
+        data: undefined,
+        message: value1.message,
+        statusCode: value1.statusCode,
+      };
+
+    return {
+      data: { value1: value1.data, value2: value2.data },
+      message: 'Professionals count found',
+      statusCode: 200,
+    };
+  }
+
+  // * CHECKED: used on StatisticGroup.tsx
   public static async countAppointments(): Promise<IResponse<{ value1: string; value2: string } | undefined>> {
     const path1: string = `${this.API_URL}/dashboard/countAppointments`;
     const path2: string = `${this.API_URL}/dashboard/countAppointmentsLastMonth`;
@@ -69,21 +99,6 @@ export class DashboardApiService {
     if (value2.statusCode > 399) return { statusCode: value2.statusCode, message: value2.message, data: undefined };
 
     return { statusCode: 200, message: 'Users count found', data: { value1: value1.data, value2: value2.data } };
-  }
-
-  // CHECKED: used on StatisticGroup.tsx
-  public static async countProfessionals() {
-    const url1: string = `${this.API_URL}/dashboard/countProfessionals`;
-    const url2: string = `${this.API_URL}/dashboard/countProfessionalsLastMonth`;
-
-    const value1: IResponse = await this.fetch(url1, 'GET');
-    const value2: IResponse = await this.fetch(url2, 'GET');
-
-    if (value1 instanceof Error || value2 instanceof Error) return { statusCode: 500, message: APP_CONFIG.error.server, data: undefined };
-    if (value1.statusCode > 399) return { statusCode: value1.statusCode, message: value1.message, data: undefined };
-    if (value2.statusCode > 399) return { statusCode: value2.statusCode, message: value2.message, data: undefined };
-
-    return { statusCode: 200, message: 'Professionals count found', data: { value1: value1.data, value2: value2.data } };
   }
 
   // CHECKED: used on StatisticGroup.tsx
