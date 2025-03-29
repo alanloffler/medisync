7512da1b1755aafe710206c9e95334b728e875b3
alanloffler <alanmatiasloffler@gmail.com>
2025-03-29 18:05:22 -0300

    Refactor countAppointments method with type safety and URL utils

--- src/pages/dashboard/services/dashboard-api.service.ts
+++ src/pages/dashboard/services/dashboard-api.service.ts
@@ -9,17 +9,33 @@ export class DashboardApiService {
   private static readonly API_URL: string = import.meta.env.VITE_API_URL;
 
   // CHECKED: used on StatisticGroup.tsx
-  public static async countAppointments() {
-    const url1: string = `${this.API_URL}/dashboard/countAppointments`;
-    const url2: string = `${this.API_URL}/dashboard/countAppointmentsLastMonth`;
+  public static async countAppointments(): Promise<IResponse<{ value1: string; value2: string } | undefined>> {
+    const path1: string = `${this.API_URL}/dashboard/countAppointments`;
+    const path2: string = `${this.API_URL}/dashboard/countAppointmentsLastMonth`;
+    const url1: URL = UtilsUrl.create(path1);
+    const url2: URL = UtilsUrl.create(path2);
 
-    const value1: IResponse = await this.fetch(url1, 'GET');
-    const value2: IResponse = await this.fetch(url2, 'GET');
+    const value1: IResponse = await UtilsUrl.fetch(url1, EMethods.GET);
+    const value2: IResponse = await UtilsUrl.fetch(url2, EMethods.GET);
 
-    if (value1 instanceof Error || value2 instanceof Error) return { statusCode: 500, message: APP_CONFIG.error.server, data: undefined };
-    if (value1.statusCode > 399 || value2.statusCode > 399) return { statusCode: value1.statusCode, message: value1.message, data: undefined };
+    if (value1 instanceof Error || value2 instanceof Error)
+      return {
+        data: undefined,
+        message: APP_CONFIG.error.server,
+        statusCode: 500,
+      };
+    if (value1.statusCode > 399 || value2.statusCode > 399)
+      return {
+        data: undefined,
+        message: value1.message,
+        statusCode: value1.statusCode,
+      };
 
-    return { statusCode: 200, message: 'Appointments count found', data: { value1: value1.data, value2: value2.data } };
+    return {
+      data: { value1: value1.data, value2: value2.data },
+      message: 'Appointments count found',
+      statusCode: 200,
+    };
   }
 
   // * CHECKED: used on LatestAppos.tsx
