6535b7d0cec870e815edd4809109a472e091cc39
alanloffler <alanmatiasloffler@gmail.com>
2025-03-31 15:33:34 -0300

    Fix import path for payload interface in AuthContext

--- src/core/auth/AuthContext.ts
+++ src/core/auth/AuthContext.ts
@@ -1,6 +1,6 @@
 import { createContext } from 'react';
 import type { AxiosResponse } from 'axios';
-import type { IPayload } from './interfaces/payload.interface';
+import type { IPayload } from '@core/auth/interfaces/payload.interface';
 import type { IResponse } from '@core/interfaces/response.interface';
 // Interface
 interface IAuthContext {
