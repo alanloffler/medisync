933057500ccde5bc18b591ec7f20eb45266b09f1
alanloffler <alanmatiasloffler@gmail.com>
2025-04-02 20:11:46 -0300

    Add error handling and improve loading state in DBCountProfessionals

--- src/pages/professionals/components/common/DBCountProfessionals.tsx
+++ src/pages/professionals/components/common/DBCountProfessionals.tsx
@@ -7,15 +7,18 @@ import { useQuery } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
 // Imports
 import { ProfessionalApiService } from '@professionals/services/professional-api.service';
+import { IResponse } from '@core/interfaces/response.interface';
+import { AxiosError } from 'axios';
 // React component
 export function DBCountProfessionals() {
   const { t } = useTranslation();
 
   const {
     data: dbCount,
+    error,
     isLoading,
     isError,
-  } = useQuery({
+  } = useQuery<IResponse, AxiosError<any>>({
     queryKey: ['professionals', 'db-count'],
     queryFn: async () => await ProfessionalApiService.countAll(),
   });
@@ -24,12 +27,17 @@ export function DBCountProfessionals() {
   const available: number = dbCount?.data.available;
   const notAvailable: number = dbCount?.data.notAvailable;
 
-  if (isLoading) return <LoadingDB variant='default' empty className='py-2' />;
-  if (isError) return <main className='py-3'></main>;
+  if (isLoading)
+    return (
+      <main className='py-2.5'>
+        <LoadingDB variant='default' empty className='w-full justify-end p-0' />
+      </main>
+    );
+  if (isError) return <main className='py-3 text-right text-xsm text-rose-400'>{error.response?.data.message}</main>;
 
   return (
     <main className='flex flex-row justify-end gap-2 py-3 text-xsm font-normal text-slate-400'>
-      {total && (
+      {total && total > 0 && (
         <section className='flex items-center space-x-1'>
           <Database size={16} strokeWidth={2} className='text-orange-400' />
           <span>{t('table.totalItems.professionals', { count: total })}</span>
