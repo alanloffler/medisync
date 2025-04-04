80fdb86ec3236a8e8a164a2a5d011d3dcf483b37
alanloffler <alanmatiasloffler@gmail.com>
2025-04-03 22:10:48 -0300

    Refactor SpecializationService to use UtilsUrl helpers

--- src/core/services/specialization.service.ts
+++ src/core/services/specialization.service.ts
@@ -1,35 +1,15 @@
 import type { IResponse } from '@core/interfaces/response.interface';
-import { APP_CONFIG } from '@config/app.config';
+import type { ISpecialization } from '@core/interfaces/specialization.interface';
+import { EMethods } from '@core/enums/methods.enum';
+import { UtilsUrl } from '@core/services/utils/url.service';
 
 export class SpecializationService {
   private static readonly API_URL: string = import.meta.env.VITE_API_URL;
 
-  public static async findAll() {
-    const url: string = `${this.API_URL}/specializations`;
-    const method: string = 'GET';
+  public static async findAll(): Promise<IResponse<ISpecialization[]>> {
+    const path: string = `${this.API_URL}/specializations`;
+    const url: URL = UtilsUrl.create(path);
 
-    return await this.fetch(url, method);
-  }
-
-  private static async fetch(url: string, method: string) {
-    try {
-      const query: Response = await fetch(url, {
-        method: method,
-        headers: {
-          'content-type': 'application/json;charset=UTF-8',
-        },
-      });
-
-      const response: IResponse = await query.json();
-
-      if (!query.ok) throw new Error(response.message);
-
-      return response;
-    } catch (error) {
-      if (error instanceof TypeError) {
-        throw new Error(APP_CONFIG.error.server);
-      }
-      throw error;
-    }
+    return await UtilsUrl.fetch(url, EMethods.GET);
   }
 }
