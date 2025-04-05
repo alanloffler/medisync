17c4cd983a4969f6a162b53b02e96f6455a1df82
alanloffler <alanmatiasloffler@gmail.com>
2025-04-05 17:28:03 -0300

    Update user restore dialog with improved UI and remove fake data

--- src/pages/users/components/common/DialogRemovedUsers.tsx
+++ src/pages/users/components/common/DialogRemovedUsers.tsx
@@ -1,9 +1,11 @@
 // Icons: https://lucide.dev/icons/
-import { Trash2 } from 'lucide-react';
+import { Trash2, Undo } from 'lucide-react';
 // External components: https://ui.shadcn.com/docs/components
 import { Button } from '@core/components/ui/button';
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@core/components/ui/dialog';
 import { ScrollArea } from '@core/components/ui/scroll-area';
+// Components
+import { LoadingDB } from '@core/components/common/LoadingDB';
 // External imports
 import { useState, type Dispatch, type SetStateAction } from 'react';
 import { AxiosError } from 'axios';
@@ -13,6 +15,7 @@ import { useTranslation } from 'react-i18next';
 // Imports
 import type { IResponse } from '@core/interfaces/response.interface';
 import type { IUser } from '@users/interfaces/user.interface';
+import { AuthBadge } from '@core/auth/components/AuthBadge';
 import { UserApiService } from '@users/services/user-api.service';
 import { UtilsString } from '@core/services/utils/string.service';
 import { useNotificationsStore } from '@core/stores/notifications.store';
@@ -29,7 +32,7 @@ export function DialogRemovedUsers({ onRestoreSuccess, open, setOpen }: IProps) 
   const queryClient = useQueryClient();
   const { i18n, t } = useTranslation();
 
-  const { data: users } = useQuery<IResponse<IUser[]>, AxiosError>({
+  const { data: users, isLoading: isLoadingUsers } = useQuery<IResponse<IUser[]>, AxiosError>({
     queryKey: ['users', 'removed-users'],
     queryFn: () => UserApiService.findRemovedUsers(),
   });
@@ -54,57 +57,50 @@ export function DialogRemovedUsers({ onRestoreSuccess, open, setOpen }: IProps) 
     },
   });
 
-  const fakeData = [
-    { firstName: 'User 01' },
-    { firstName: 'User 02' },
-    { firstName: 'User 03' },
-    { firstName: 'User 04' },
-    { firstName: 'User 05' },
-    { firstName: 'User 06' },
-  ];
-
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>{t('dialog.restoreUser.title')}</DialogTitle>
-          <DialogDescription>{t('dialog.restoreUser.description')}</DialogDescription>
+          <DialogDescription className='flex items-center justify-between'>
+            <div>{t('dialog.restoreUser.description')}</div>
+            <AuthBadge />
+          </DialogDescription>
         </DialogHeader>
-        <section className='space-y-3 text-sm'>
+        <section className='flex flex-col items-center'>
+          {isLoadingUsers && <LoadingDB text={t('loading.users')} variant='default' />}
           {users?.data && (
-            <ScrollArea className='h-full max-h-[205px] w-full'>
-              <div className='flex flex-col space-y-1'>
-                {users.data.map((user, index) => (
-                  <div key={index} className='flex items-center justify-between bg-slate-50 px-3 py-2'>
-                    <div>{UtilsString.upperCase(`${user.lastName}, ${user.firstName}`, 'each')}</div>
-                    <div className='flex space-x-3'>
-                      <div className='flex items-center space-x-1'>
-                        <Trash2 size={12} strokeWidth={2} className='text-red-400' />
-                        <span className='text-xxs text-muted-foreground'>{format(user.updatedAt, 'medium', i18n.resolvedLanguage)}</span>
+            <div className='w-full space-y-3 text-sm'>
+              <ScrollArea className='h-full max-h-[205px] w-full'>
+                <div className='flex flex-col space-y-1'>
+                  {users.data.map((user, index) => (
+                    <div key={index} className='flex items-center justify-between bg-slate-50 px-3 py-2'>
+                      <div>{UtilsString.upperCase(`${user.lastName}, ${user.firstName}`, 'each')}</div>
+                      <div className='flex space-x-3'>
+                        <div className='flex items-center space-x-1'>
+                          <Trash2 size={13} strokeWidth={2} className='text-red-400' />
+                          <span className='text-xxs text-muted-foreground'>{format(user.updatedAt, 'medium', i18n.resolvedLanguage)}</span>
+                        </div>
+                        <Button
+                          className='gap-1 bg-amber-200 text-xs font-normal text-amber-600 hover:bg-amber-300/70 hover:text-amber-700/70'
+                          size='xs'
+                          onClick={() => {
+                            restore({ userId: user._id });
+                            setRestoredUser(user);
+                          }}
+                        >
+                          <Undo size={13} strokeWidth={2} />
+                          {t('button.restore')}
+                        </Button>
                       </div>
-                      <Button
-                        className='bg-amber-200 text-xs font-normal text-amber-600 hover:bg-amber-300/70 hover:text-amber-700/70'
-                        size='xs'
-                        onClick={() => {
-                          restore({ userId: user._id });
-                          setRestoredUser(user);
-                        }}
-                      >
-                        {t('button.restore')}
-                      </Button>
                     </div>
-                  </div>
-                ))}
-                {fakeData.map((user, index) => (
-                  <div key={index} className='flex justify-between bg-slate-50 p-2'>
-                    <div key={index + 'z'}>{user.firstName}</div>
-                  </div>
-                ))}
-              </div>
-            </ScrollArea>
+                  ))}
+                </div>
+              </ScrollArea>
+            </div>
           )}
         </section>
-        <DialogFooter className='pt-3'>
+        <DialogFooter>
           <Button size='sm' variant='default' onClick={() => setOpen(false)}>
             {t('button.close')}
           </Button>
