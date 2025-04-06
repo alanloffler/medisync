b3bdb6fa472fe6ed1f3efc80388a8e6998c46f18
alanloffler <alanmatiasloffler@gmail.com>
2025-04-05 22:01:19 -0300

    Change AuthBadge from section to span element

--- src/core/auth/components/AuthBadge.tsx
+++ src/core/auth/components/AuthBadge.tsx
@@ -7,9 +7,9 @@ export function AuthBadge() {
   const { user } = useAuth();
 
   return (
-    <section className='flex items-center !gap-2 rounded-md bg-indigo-100 p-1.5 pr-2 text-xxs font-normal uppercase text-indigo-500'>
+    <span className='flex items-center !gap-2 rounded-md bg-indigo-100 p-1.5 pr-2 text-xxs font-normal uppercase text-indigo-500'>
       <LockKeyholeOpen size={13} strokeWidth={2} />
       {user?.role}
-    </section>
+    </span>
   );
 }
