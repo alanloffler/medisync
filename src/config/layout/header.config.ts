59cc62dd32fac6974f5f19a815cdd7fc6bfaaead
alanloffler <alanmatiasloffler@gmail.com>
2025-04-06 18:50:35 -0300

    Add icons to header menu items

--- src/config/layout/header.config.ts
+++ src/config/layout/header.config.ts
@@ -3,10 +3,30 @@ import { APP_CONFIG } from '@config/app.config';
 export const HEADER_CONFIG = {
   appName: APP_CONFIG.appName,
   headerMenu: [
-    { id: 1, key: 'headerMenu.dashboard', path: `${APP_CONFIG.appPrefix}/dashboard` },
-    { id: 2, key: 'headerMenu.appointments', path: `${APP_CONFIG.appPrefix}/appointments` },
-    { id: 3, key: 'headerMenu.professionals', path: `${APP_CONFIG.appPrefix}/professionals` },
-    { id: 4, key: 'headerMenu.users', path: `${APP_CONFIG.appPrefix}/users` },
+    {
+      id: 1,
+      key: 'headerMenu.dashboard',
+      path: `${APP_CONFIG.appPrefix}/dashboard`,
+      icon: 'layout-dashboard',
+    },
+    {
+      id: 2,
+      key: 'headerMenu.appointments',
+      path: `${APP_CONFIG.appPrefix}/appointments`,
+      icon: 'calendar-days',
+    },
+    {
+      id: 3,
+      key: 'headerMenu.professionals',
+      path: `${APP_CONFIG.appPrefix}/professionals`,
+      icon: 'briefcase-medical',
+    },
+    {
+      id: 4,
+      key: 'headerMenu.users',
+      path: `${APP_CONFIG.appPrefix}/users`,
+      icon: 'contact-round',
+    },
   ],
   actionsButton: [
     { id: 1, key: 'headerMenu.links.appointment', menuId: 2, path: `${APP_CONFIG.appPrefix}/reserve`, default: true },
