915ecb218739dc7e7e970d38ce5a9f5194411ae6
alanloffler <alanmatiasloffler@gmail.com>
2025-04-06 23:38:35 -0300

    Update Auth interface to use IPayloadPlus instead of IPayload

--- src/core/auth/AuthContext.ts
+++ src/core/auth/AuthContext.ts
@@ -1,13 +1,13 @@
 import { createContext } from 'react';
 import type { AxiosResponse } from 'axios';
-import type { IPayload } from '@core/auth/interfaces/payload.interface';
+import type { IPayloadPlus } from '@core/auth/interfaces/payload.interface';
 import type { IResponse } from '@core/interfaces/response.interface';
 // Interface
 interface IAuthContext {
   loading: boolean;
-  login(email: string, password: string): Promise<AxiosResponse<IResponse<IPayload>> & { redirectPath?: string }>;
+  login(email: string, password: string): Promise<AxiosResponse<IResponse<IPayloadPlus>> & { redirectPath?: string }>;
   logout(): Promise<AxiosResponse<IResponse<null>> & { redirectPath?: string }>;
-  user: IPayload | null;
+  user: IPayloadPlus | null;
 }
 // Context
 export const AuthContext = createContext<IAuthContext | null>(null);
