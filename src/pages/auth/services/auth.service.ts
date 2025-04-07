fda7b185016fc98d6301b0e8c4050a4d5b51adeb
alanloffler <alanmatiasloffler@gmail.com>
2025-04-06 23:42:43 -0300

    Update interface imports and types from IPayload to IPayloadPlus

--- src/pages/auth/services/auth.service.ts
+++ src/pages/auth/services/auth.service.ts
@@ -1,11 +1,11 @@
 import type { AxiosResponse } from 'axios';
 import type { ILogin } from '@core/auth/interfaces/login.interface';
-import type { IPayload } from '@core/auth/interfaces/payload.interface';
+import type { IPayloadPlus } from '@core/auth/interfaces/payload.interface';
 import type { IResponse } from '@core/interfaces/response.interface';
 import { api } from '@auth/services/axios.service';
 
 export class AuthService {
-  public static async login({ email, password }: ILogin): Promise<AxiosResponse<IResponse<IPayload>>> {
+  public static async login({ email, password }: ILogin): Promise<AxiosResponse<IResponse<IPayloadPlus>>> {
     const response = await api({
       method: 'POST',
       url: '/auth/login',
@@ -26,7 +26,7 @@ export class AuthService {
     return response;
   }
 
-  public static async getAdmin(): Promise<AxiosResponse<IResponse<IPayload>>> {
+  public static async getAdmin(): Promise<AxiosResponse<IResponse<IPayloadPlus>>> {
     const response = await api({
       method: 'GET',
       url: '/auth/admin',
