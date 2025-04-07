e474db18580fcd1c4a29d202952920285179181a
alanloffler <alanmatiasloffler@gmail.com>
2025-04-07 19:20:59 -0300

    Remove TooltipWrapper component from User dropdown

--- src/layout/components/footer/User.tsx
+++ src/layout/components/footer/User.tsx
@@ -3,8 +3,6 @@ import { DynamicIcon } from 'lucide-react/dynamic';
 import { LogOut, ShieldUser } from 'lucide-react';
 // External components: https://ui.shadcn.com/docs/components
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@core/components/ui/dropdown-menu';
-// Components
-import { TooltipWrapper } from '@core/components/common/TooltipWrapper';
 // External imports
 import { motion } from 'motion/react';
 import { useNavigate } from 'react-router-dom';
@@ -29,32 +27,30 @@ export function User() {
 
   return (
     <DropdownMenu>
-      <TooltipWrapper tooltip={t('tooltip.account')}>
-        <DropdownMenuTrigger className={cn('flex h-8 w-full items-center', menuExpanded ? 'justify-start' : 'justify-center')}>
-          <div
-            className={cn(
-              'flex h-8 w-8 min-w-8 flex-shrink-0 items-center justify-center rounded-full text-white',
-              user?.role === ERole.Super ? 'bg-purple-500' : 'bg-sky-500',
-            )}
-          >
-            <span className='text-xs font-medium'>
-              <ShieldUser size={20} strokeWidth={1.5} />
-            </span>
-          </div>
-          {menuExpanded && <div className='w-3 flex-shrink-0' />}
-          {menuExpanded && (
-            <motion.div
-              className='flex flex-grow flex-col text-left'
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.1, delay: 0.2 }}
-            >
-              <div className='text-xs font-semibold'>{UtilsString.upperCase(`${user?.firstName} ${user?.lastName}`, 'each')}</div>
-              <div className='text-xxs'>{user?.role === ERole.Super ? t('auth.role.super') : t('auth.role.admin')}</div>
-            </motion.div>
+      <DropdownMenuTrigger className={cn('flex h-8 w-full items-center', menuExpanded ? 'justify-start' : 'justify-center')}>
+        <div
+          className={cn(
+            'flex h-8 w-8 min-w-8 flex-shrink-0 items-center justify-center rounded-full text-white',
+            user?.role === ERole.Super ? 'bg-purple-500' : 'bg-sky-500',
           )}
-        </DropdownMenuTrigger>
-      </TooltipWrapper>
+        >
+          <span className='text-xs font-medium'>
+            <ShieldUser size={20} strokeWidth={1.5} />
+          </span>
+        </div>
+        {menuExpanded && <div className='w-3 flex-shrink-0' />}
+        {menuExpanded && (
+          <motion.div
+            className='flex flex-grow flex-col text-left'
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.1, delay: 0.2 }}
+          >
+            <div className='text-xs font-semibold'>{UtilsString.upperCase(`${user?.firstName} ${user?.lastName}`, 'each')}</div>
+            <div className='text-xxs'>{user?.role === ERole.Super ? t('auth.role.super') : t('auth.role.admin')}</div>
+          </motion.div>
+        )}
+      </DropdownMenuTrigger>
       <DropdownMenuContent align='start' className='min-w-44 space-y-1' onCloseAutoFocus={(e) => e.preventDefault()}>
         {HEADER_CONFIG.user.menuItems.map((item) => (
           <DropdownMenuItem key={item.id} onClick={() => navigate(item.path)} className='flex space-x-3 text-xsm'>
